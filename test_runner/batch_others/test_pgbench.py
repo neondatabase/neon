neondatabase/neon

commit 532918e13da03fdd26d8a76dddbbd35a0b8d83a2

Fix branch creation at a point other than end-of-WAL
When creating a new branch, we copied all WAL from the source timeline
to the new one, and it was being picked up and digested into the
repository on first use of the timeline. Fix by copying the WAL only
up to the branch's starting point.

We should probably move the branch-creation code from the CLI to page
server itself - that's what I was starting to hack on when I noticed this
bug - but let's fix this first.

Add a regression test. To test multiple branches, enhance the python
test fixture to manage multiple running Postgres instances. Also, for
convenience, add a function to the postgres fixture to open a connection
to the server with psycopg2.

test_runner/fixtures/zenith_fixtures.py
@@ -1,4 +1,6 @@
+import getpass
 import os
+import psycopg2
 import pytest
 import shutil
 import subprocess
@@ -126,29 +128,28 @@ def pageserver(zenith_cli):
 class Postgres:
     """ An object representing a running postgres daemon. """
 
-    def __init__(self, zenith_cli, repo_dir):
+    def __init__(self, zenith_cli, repo_dir, instance_num):
         self.zenith_cli = zenith_cli
+        self.instance_num = instance_num
         self.running = False
         self.host = 'localhost'
-        self.port = 55432
+        self.port = 55431 + instance_num
         self.repo_dir = repo_dir
-        # path to conf is <repo_dir>/pgdatadirs/pg1/postgresql.conf
+        # path to conf is <repo_dir>/pgdatadirs/pg<instance_num>/postgresql.conf
 
-    def create_start(self, config_lines=None):
+    def create_start(self, branch="main", config_lines=None):
         """ create the pg data directory, and start the server """
-        self.zenith_cli.run(['pg', 'create'])
+        self.zenith_cli.run(['pg', 'create', branch])
         if config_lines is None:
             config_lines = []
         self.config(config_lines)
-        # FIXME: where did the name pg1 come from?
-        self.zenith_cli.run(['pg', 'start', 'pg1'])
+        self.zenith_cli.run(['pg', 'start', 'pg{}'.format(self.instance_num)])
         self.running = True
-
+        return
 
     #lines should be an array of valid postgresql.conf rows
     def config(self, lines):
-        #TODO use real node name, not just guessed pg1
-        filename = 'pgdatadirs/pg1/postgresql.conf'
+        filename = 'pgdatadirs/pg{}/postgresql.conf'.format(self.instance_num)
         config_name = os.path.join(self.repo_dir, filename)
         with open(config_name, 'a') as conf:
             for line in lines:
@@ -157,16 +158,43 @@ class Postgres:
 
     def stop(self):
         if self.running:
-            self.zenith_cli.run(['pg', 'stop', 'pg1'])
+            self.zenith_cli.run(['pg', 'stop', 'pg{}'.format(self.instance_num)])
 
+    # Open a psycopg2 connection to the server, ready for running queries.
+    def connect(self):
+        username = getpass.getuser()
+        conn_str = 'host={} port={} dbname=postgres user={}'.format(
+            self.host, self.port, username)
+
+        return psycopg2.connect(conn_str)
+
+class PostgresFactory:
+    """ An object representing multiple running postgres daemons. """
+    def __init__(self, zenith_cli, repo_dir):
+        self.zenith_cli = zenith_cli
+        self.host = 'localhost'
+        self.repo_dir = repo_dir
+        self.num_instances = 0
+        self.instances = []
+
+    def create_start(self, branch="main", config_lines=None):
+        pg = Postgres(self.zenith_cli, self.repo_dir, self.num_instances + 1)
+        self.num_instances += 1
+        self.instances.append(pg)
+        pg.create_start(branch, config_lines)
+        return pg
+
+    def stop_all(self):
+        for pg in self.instances:
+            pg.stop()
 
 @zenfixture
 def postgres(zenith_cli, repo_dir):
-    pg = Postgres(zenith_cli, repo_dir)
-    yield pg
+    pgfactory = PostgresFactory(zenith_cli, repo_dir)
+    yield pgfactory
     # After the yield comes any cleanup code we need.
     print('Starting postgres cleanup')
-    pg.stop()
+    pgfactory.stop_all()
 
 
 class PgBin:

test_runner/test_branch_behind.py
@@ -1,0 +1,68 @@
+import pytest
+import getpass
+import psycopg2
+
+pytest_plugins = ("fixtures.zenith_fixtures")
+
+#
+# Create a couple of branches off the main branch, at a historical point in time.
+#
+def test_branch_behind(zenith_cli, pageserver, postgres, pg_bin):
+    zenith_cli.run_init()
+    pageserver.start()
+    print('pageserver is running')
+
+    pgmain = postgres.create_start()
+    print('postgres is running on main branch')
+
+    main_pg_conn = pgmain.connect();
+    main_pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    main_cur = main_pg_conn.cursor()
+
+    # Create table, and insert the first 100 rows
+    main_cur.execute('CREATE TABLE foo (t text)');
+    main_cur.execute("INSERT INTO foo SELECT 'long string to consume some space' || g FROM generate_series(1, 100) g");
+    main_cur.execute('SELECT pg_current_wal_insert_lsn()');
+    lsn_a = main_cur.fetchone()[0]
+    print('LSN after 100 rows: ' + lsn_a)
+
+    # Insert some more rows. (This generates enough WAL to fill a few segments.)
+    main_cur.execute("INSERT INTO foo SELECT 'long string to consume some space' || g FROM generate_series(1, 100000) g");
+    main_cur.execute('SELECT pg_current_wal_insert_lsn()');
+    lsn_b = main_cur.fetchone()[0]
+    print('LSN after 100100 rows: ' + lsn_b)
+
+    # Branch at the point where only 100 rows were inserted
+    zenith_cli.run(["branch", "hundred", "main@"+lsn_a]);
+
+    # Insert many more rows. This generates enough WAL to fill a few segments.
+    main_cur.execute("INSERT INTO foo SELECT 'long string to consume some space' || g FROM generate_series(1, 100000) g");
+    main_cur.execute('SELECT pg_current_wal_insert_lsn()');
+
+    main_cur.execute('SELECT pg_current_wal_insert_lsn()');
+    lsn_c = main_cur.fetchone()[0]
+    print('LSN after 200100 rows: ' + lsn_c)
+
+    # Branch at the point where only 200 rows were inserted
+    zenith_cli.run(["branch", "more", "main@"+lsn_b]);
+
+    pg_hundred = postgres.create_start("hundred")
+    pg_more = postgres.create_start("more")
+
+    # On the 'hundred' branch, we should see only 100 rows
+    hundred_pg_conn = pg_hundred.connect()
+    hundred_pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    hundred_cur = hundred_pg_conn.cursor()
+    hundred_cur.execute('SELECT count(*) FROM foo');
+    assert(hundred_cur.fetchone()[0] == 100);
+
+    # On the 'more' branch, we should see 100200 rows
+    more_pg_conn = pg_more.connect()
+    more_pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    more_cur = more_pg_conn.cursor()
+    more_cur.execute('SELECT count(*) FROM foo');
+    assert(more_cur.fetchone()[0] == 100100);
+
+    # All the rows are visible on the main branch
+    main_cur.execute('SELECT count(*) FROM foo');
+    assert(main_cur.fetchone()[0] == 200100);

control_plane/src/local_env.rs
@@ -9,6 +9,8 @@ use bytes::Bytes;
 use rand::Rng;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
@@ -313,14 +315,60 @@ pub fn create_branch(
     let oldtimelinedir = repopath
         .join("timelines")
         .join(startpoint.timelineid.to_string());
-    let mut copy_opts = fs_extra::dir::CopyOptions::new();
-    copy_opts.content_only = true;
-    fs_extra::dir::copy(
-        oldtimelinedir.join("wal"),
-        newtimelinedir.join("wal"),
-        &copy_opts,
+    copy_wal(
+        &oldtimelinedir.join("wal"),
+        &newtimelinedir.join("wal"),
+        startpoint.lsn,
+        16 * 1024 * 1024 // FIXME: assume default WAL segment size
     )?;
 
+    Ok(())
+}
+
+///
+/// Copy all WAL segments from one directory to another, up to given LSN.
+///
+/// If the given LSN is in the middle of a segment, the last segment containing it
+/// is written out as .partial, and padded with zeros.
+///
+fn copy_wal(src_dir: &Path, dst_dir: &Path, upto: Lsn, wal_seg_size: u64) -> Result<()>{
+
+    let last_segno = upto.segment_number(wal_seg_size);
+    let last_segoff = upto.segment_offset(wal_seg_size);
+
+    for entry in fs::read_dir(src_dir).unwrap() {
+        if let Ok(entry) = entry {
+            let entry_name = entry.file_name();
+            let fname = entry_name.to_str().unwrap();
+
+            // Check if the filename looks like an xlog file, or a .partial file.
+            if !xlog_utils::IsXLogFileName(fname) && !xlog_utils::IsPartialXLogFileName(fname) {
+                continue
+            }
+            let (segno, _tli) = xlog_utils::XLogFromFileName(fname, wal_seg_size as usize);
+
+            let copylen;
+            let mut dst_fname = PathBuf::from(fname);
+            if segno > last_segno {
+                // future segment, skip
+                continue;
+            } else if segno < last_segno {
+                copylen = wal_seg_size;
+                dst_fname.set_extension("");
+            } else {
+                copylen = last_segoff;
+                dst_fname.set_extension("partial");
+            }
+
+            let src_file = File::open(entry.path())?;
+            let mut dst_file = File::create(dst_dir.join(&dst_fname))?;
+            std::io::copy(&mut src_file.take(copylen), &mut dst_file)?;
+
+            if copylen < wal_seg_size {
+                std::io::copy(&mut std::io::repeat(0).take(wal_seg_size - copylen), &mut dst_file)?;
+            }
+        }
+    }
     Ok(())
 }
 

commit ab2f0ad1a840cac4381ecd3235c2ac50d58edd3c

Fix and reorganize python tests.
- The 'pageserver' fixture now sets up the repository and starts up
  the Page Server automatically. In other words, the 'pageserver'
  fixture provides a Page Server that's up and running and ready to
  use in tests.

- The 'pageserver' fixture now also creates a branch called 'empty',
  right after initializing the repository. By convention, all the
  tests start by createing a new branch off 'empty' for the test. This
  allows running all the tests against the same Page Server
  concurrently.  (I haven't tested that though. pytest doensn't
  provide an option to run tests in parallel but there are extensions
  for that.)

- Remove the 'zen_simple' fixture. Now that 'pageserver' provides
  server that's up and running, it's pretty simple to use the
  'pageserver' and 'postgres' fixtures directly.

- Don't assume host name or ports in the tests. They now use the
  fields in the fixtures for that. That allows assigning the ports
  dynamically, making it possible to run multiple page servers in
  parallel, or running the tests in parallel with another page
  server. This commit still hard codes the Page Server's port in the
  fixture, though, so more work is needed to actually make it
  possible.

- I made some changes to the 'postgres' fixture in commit 532918e13d,
  which broke the other tests. Fix them.

- Divide the tests into two "batches" of roughly equal runtime, which
  can be run in parallel

- Merge the 'test_file' and 'test_filter' options in CircleCI config
  into one 'test_selection' option, for simplicity.

.circleci/config.yml
@@ -112,14 +112,16 @@ jobs:
     #description: "Run pytest"
     executor: python/default
     parameters:
-      # Use test_filter to name a test or a group of tests with the same prefix.
-      test_filter:
+      # pytest args to specify the tests to run.
+      #
+      # This can be a test file name, e.g. 'test_pgbench.py, or a subdirectory,
+      # or '-k foobar' to run tests containing string 'foobar'. See pytest man page
+      # section SPECIFYING TESTS / SELECTING TESTS for details.
+      #
+      # This parameter is required, to prevent the mistake of running all tests in one job.
+      test_selection:
         type: string
         default: ""
-      # Use test_file to name a python file containing tests to run.
-      # This parameter is required, to prevent the mistake of running all tests in one job.
-      test_file:
-        type: string
       # Arbitrary parameters to pytest. For example "-s" to prevent capturing stdout/stderr
       extra_params:
         type: string
@@ -144,21 +146,17 @@ jobs:
             - POSTGRES_DISTRIB_DIR: /tmp/zenith/pg_install
             - TEST_OUTPUT: /tmp/test_output
           command: |
-            TEST_FILE="<< parameters.test_file >>"
-            TEST_FILTER="<< parameters.test_filter >>"
+            TEST_SELECTION="<< parameters.test_selection >>"
             EXTRA_PARAMS="<< parameters.extra_params >>"
-            if [ -z "$TEST_FILE$TEST_FILTER" ]; then
-              echo "test_file or test_filter must be set"
+            if [ -z "$TEST_SELECTION" ]; then
+              echo "test_selection must be set"
               exit 1
-            fi
-            if [ -n "$TEST_FILTER" ]; then
-              TEST_FILTER="-k $TEST_FILTER"
             fi
             # Run the tests.
             #
             # The junit.xml file allows CircleCI to display more fine-grained test information
             # in its "Tests" tab in the results page.
-            pytest --junitxml=$TEST_OUTPUT/junit.xml --tb=short $TEST_FILE $TEST_FILTER $EXTRA_PARAMS
+            pytest --junitxml=$TEST_OUTPUT/junit.xml --tb=short $TEST_SELECTION $EXTRA_PARAMS
       - run:
           # CircleCI artifacts are preserved one file at a time, so skipping
           # this step isn't a good idea. If you want to extract the
@@ -189,14 +187,13 @@ workflows:
     jobs:
       - build-zenith
       - run-pytest:
-          name: pgbench test
-          test_file: test_pgbench.py
+          name: pg_regress tests
+          test_selection: batch_pg_regress
+          needs_postgres_source: true
           requires:
             - build-zenith
       - run-pytest:
-          name: pg_regress test
-          test_file: test_pg_regress.py
-          extra_params: -s
-          needs_postgres_source: true
+          name: other tests
+          test_selection: batch_others
           requires:
             - build-zenith

test_runner/conftest.py
@@ -1,0 +1,1 @@
+pytest_plugins = ("fixtures.zenith_fixtures")

test_runner/fixtures/zenith_fixtures.py
@@ -90,10 +90,6 @@ class ZenithCli:
         print('Running command "{}"'.format(' '.join(args)))
         subprocess.run(args, env=self.env, check=True)
 
-    def run_init(self):
-        """ Run the "zenith init <dir>" command. """
-        self.run(['init'])
-
 
 @zenfixture
 def zenith_cli(zenith_binpath, repo_dir, pg_distrib_dir):
@@ -107,23 +103,49 @@ class ZenithPageserver:
         self.zenith_cli = zenith_cli
         self.running = False
 
+    # Initialize the repository, i.e. run "zenith init"
+    def init(self):
+        self.zenith_cli.run(['init'])
+
+    # Start the page server
     def start(self):
         self.zenith_cli.run(['start'])
         self.running = True
 
+    # Stop the page server
     def stop(self):
         self.zenith_cli.run(['stop'])
         self.running = True
 
+    # The page server speaks the Postgres FE/BE protocol, so you can connect
+    # to it with any Postgres client, and run special commands. This function
+    # returns a libpq connection string for connecting to it.
+    def connstr(self):
+        username = getpass.getuser()
+        conn_str = 'host={} port={} dbname=postgres user={}'.format(
+            'localhost', 64000, username)
+        return conn_str
 
+# The 'pageserver' fixture provides a Page Server that's up and running.
+#
+# If TEST_SHARED_FIXTURES is set, the Page Server instance is shared by all
+# the tests. To avoid clashing with other tests, don't use the 'main' branch in
+# the tests directly. Instead, create a branch off the 'empty' branch and use
+# that.
+#
+# By convention, the test branches are named after the tests. For example,
+# test called 'test_foo' would create and use branches with the 'test_foo' prefix.
 @zenfixture
 def pageserver(zenith_cli):
     ps = ZenithPageserver(zenith_cli)
+    ps.init()
+    ps.start()
+    # For convenience in tests, create a branch from the freshly-initialized cluster.
+    zenith_cli.run(["branch", "empty", "main"]);
     yield ps
     # After the yield comes any cleanup code we need.
     print('Starting pageserver cleanup')
     ps.stop()
-
 
 class Postgres:
     """ An object representing a running postgres daemon. """
@@ -132,12 +154,13 @@ class Postgres:
         self.zenith_cli = zenith_cli
         self.instance_num = instance_num
         self.running = False
+        self.username = getpass.getuser()
         self.host = 'localhost'
         self.port = 55431 + instance_num
         self.repo_dir = repo_dir
         # path to conf is <repo_dir>/pgdatadirs/pg<instance_num>/postgresql.conf
 
-    def create_start(self, branch="main", config_lines=None):
+    def create_start(self, branch, config_lines=None):
         """ create the pg data directory, and start the server """
         self.zenith_cli.run(['pg', 'create', branch])
         if config_lines is None:
@@ -160,13 +183,11 @@ class Postgres:
         if self.running:
             self.zenith_cli.run(['pg', 'stop', 'pg{}'.format(self.instance_num)])
 
-    # Open a psycopg2 connection to the server, ready for running queries.
-    def connect(self):
-        username = getpass.getuser()
+    # Return a libpq connection string to connect to the Postgres instance
+    def connstr(self):
         conn_str = 'host={} port={} dbname=postgres user={}'.format(
-            self.host, self.port, username)
-
-        return psycopg2.connect(conn_str)
+            self.host, self.port, self.username)
+        return conn_str
 
 class PostgresFactory:
     """ An object representing multiple running postgres daemons. """
@@ -325,43 +346,3 @@ def pg_distrib_dir(base_dir):
     if not os.path.exists(os.path.join(pg_dir, 'bin/postgres')):
         raise Exception('postgres not found at "{}"'.format(pg_dir))
     return pg_dir
-
-
-class SimpleTest:
-    """ A fixture object that contains the things we need for a simple test.
-
-    This is an object with common fixture members:
-        zenith_cli
-        pageserver
-        postgres
-        pg_bin
-
-    Example:
-
-        @zenfixture
-        def my_test(zen_simple):
-            zen_simple.pg_bin.run(['pgbench', '-i'])
-
-    """
-
-    def __init__(self, zenith_cli, pageserver, postgres, pg_bin):
-        self.zenith_cli = zenith_cli
-        self.pageserver = pageserver
-        self.postgres = postgres
-        self.pg_bin = pg_bin
-
-    def start(self):
-        """ Start a pageserver and postgres. """
-        self.zenith_cli.run_init()
-        self.pageserver.start()
-        print('pageserver is running')
-
-        self.postgres.create_start()
-        print('postgres is running')
-
-
-@zenfixture
-def zen_simple(zenith_cli, pageserver, postgres, pg_bin):
-    simple = SimpleTest(zenith_cli, pageserver, postgres, pg_bin)
-    simple.start()
-    return simple

test_runner/batch_others/test_branch_behind.py
@@ -8,14 +8,13 @@ pytest_plugins = ("fixtures.zenith_fixtures")
 # Create a couple of branches off the main branch, at a historical point in time.
 #
 def test_branch_behind(zenith_cli, pageserver, postgres, pg_bin):
-    zenith_cli.run_init()
-    pageserver.start()
-    print('pageserver is running')
+    # Branch at the point where only 100 rows were inserted
+    zenith_cli.run(["branch", "test_branch_behind", "empty"]);
 
-    pgmain = postgres.create_start()
-    print('postgres is running on main branch')
+    pgmain = postgres.create_start('test_branch_behind')
+    print("postgres is running on 'test_branch_behind' branch")
 
-    main_pg_conn = pgmain.connect();
+    main_pg_conn = psycopg2.connect(pgmain.connstr());
     main_pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     main_cur = main_pg_conn.cursor()
 
@@ -33,7 +32,7 @@ def test_branch_behind(zenith_cli, pageserver, postgres, pg_bin):
     print('LSN after 100100 rows: ' + lsn_b)
 
     # Branch at the point where only 100 rows were inserted
-    zenith_cli.run(["branch", "hundred", "main@"+lsn_a]);
+    zenith_cli.run(["branch", "test_branch_behind_hundred", "test_branch_behind@"+lsn_a]);
 
     # Insert many more rows. This generates enough WAL to fill a few segments.
     main_cur.execute("INSERT INTO foo SELECT 'long string to consume some space' || g FROM generate_series(1, 100000) g");
@@ -44,20 +43,20 @@ def test_branch_behind(zenith_cli, pageserver, postgres, pg_bin):
     print('LSN after 200100 rows: ' + lsn_c)
 
     # Branch at the point where only 200 rows were inserted
-    zenith_cli.run(["branch", "more", "main@"+lsn_b]);
+    zenith_cli.run(["branch", "test_branch_behind_more", "test_branch_behind@"+lsn_b]);
 
-    pg_hundred = postgres.create_start("hundred")
-    pg_more = postgres.create_start("more")
+    pg_hundred = postgres.create_start("test_branch_behind_hundred")
+    pg_more = postgres.create_start("test_branch_behind_more")
 
     # On the 'hundred' branch, we should see only 100 rows
-    hundred_pg_conn = pg_hundred.connect()
+    hundred_pg_conn = psycopg2.connect(pg_hundred.connstr())
     hundred_pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     hundred_cur = hundred_pg_conn.cursor()
     hundred_cur.execute('SELECT count(*) FROM foo');
     assert(hundred_cur.fetchone()[0] == 100);
 
     # On the 'more' branch, we should see 100200 rows
-    more_pg_conn = pg_more.connect()
+    more_pg_conn = psycopg2.connect(pg_more.connstr())
     more_pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     more_cur = more_pg_conn.cursor()
     more_cur.execute('SELECT count(*) FROM foo');

test_runner/batch_others/test_config.py
@@ -6,21 +6,18 @@ import psycopg2
 pytest_plugins = ("fixtures.zenith_fixtures")
 
 
+#
+# Test starting Postgres with custom options
+#
 def test_config(zenith_cli, pageserver, postgres, pg_bin):
-    zenith_cli.run_init()
-    pageserver.start()
-    print('pageserver is running')
+    # Create a branch for us
+    zenith_cli.run(["branch", "test_config", "empty"]);
 
     # change config
-    postgres.create_start(['log_min_messages=debug1'])
+    pg = postgres.create_start('test_config', ['log_min_messages=debug1'])
+    print('postgres is running on test_config branch')
 
-    print('postgres is running')
-
-    username = getpass.getuser()
-    conn_str = 'host={} port={} dbname=postgres user={}'.format(
-        postgres.host, postgres.port, username)
-    print('conn_str is', conn_str)
-    pg_conn = psycopg2.connect(conn_str)
+    pg_conn = psycopg2.connect(pg.connstr())
     pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     cur = pg_conn.cursor()
 

test_runner/batch_others/test_pageserver_api.py
@@ -5,44 +5,45 @@ import json
 
 pytest_plugins = ("fixtures.zenith_fixtures")
 
-HOST = 'localhost'
-PAGESERVER_PORT = 64000
-
-def test_status(zen_simple):
-    username = getpass.getuser()
-    conn_str = 'host={} port={} dbname=postgres user={}'.format(
-        HOST, PAGESERVER_PORT, username)
-    pg_conn = psycopg2.connect(conn_str)
+def test_status(pageserver):
+    pg_conn = psycopg2.connect(pageserver.connstr())
     pg_conn.autocommit = True
     cur = pg_conn.cursor()
     cur.execute('status;')
     assert cur.fetchone() == ('hello world',)
     pg_conn.close()
 
-def test_pg_list(zen_simple):
-    username = getpass.getuser()
-    page_server_conn_str = 'host={} port={} dbname=postgres user={}'.format(
-        HOST, PAGESERVER_PORT, username)
-    page_server_conn = psycopg2.connect(page_server_conn_str)
+def test_pg_list(pageserver, zenith_cli):
+
+    # Create a branch for us
+    zenith_cli.run(["branch", "test_pg_list_main", "empty"]);
+
+    page_server_conn = psycopg2.connect(pageserver.connstr())
     page_server_conn.autocommit = True
     page_server_cur = page_server_conn.cursor()
 
     page_server_cur.execute('pg_list;')
     branches = json.loads(page_server_cur.fetchone()[0])
+    # Filter out branches created by other tests
+    branches = [x for x in branches if x['name'].startswith('test_pg_list')]
+
     assert len(branches) == 1
-    assert branches[0]['name'] == 'main'
+    assert branches[0]['name'] == 'test_pg_list_main'
     assert 'timeline_id' in branches[0]
     assert 'latest_valid_lsn' in branches[0]
 
-    zen_simple.zenith_cli.run(['branch', 'experimental', 'main'])
-    zen_simple.zenith_cli.run(['pg', 'create', 'experimental'])
+    # Create another branch, and start Postgres on it
+    zenith_cli.run(['branch', 'test_pg_list_experimental', 'test_pg_list_main'])
+    zenith_cli.run(['pg', 'create', 'test_pg_list_experimental'])
 
     page_server_cur.execute('pg_list;')
     new_branches = json.loads(page_server_cur.fetchone()[0])
+    # Filter out branches created by other tests
+    new_branches = [x for x in new_branches if x['name'].startswith('test_pg_list')]
     assert len(new_branches) == 2
     new_branches.sort(key=lambda k: k['name'])
 
-    assert new_branches[0]['name'] == 'experimental'
+    assert new_branches[0]['name'] == 'test_pg_list_experimental'
     assert new_branches[0]['timeline_id'] != branches[0]['timeline_id']
 
     # TODO: do the LSNs have to match here?

test_runner/batch_others/test_pgbench.py
@@ -1,0 +1,17 @@
+import pytest
+
+pytest_plugins = ("fixtures.zenith_fixtures")
+
+
+def test_pgbench(pageserver, postgres, pg_bin, zenith_cli):
+
+    # Create a branch for us
+    zenith_cli.run(["branch", "test_pgbench", "empty"]);
+
+    pg = postgres.create_start('test_pgbench')
+    print("postgres is running on 'test_pgbench' branch")
+
+    connstr = pg.connstr();
+
+    pg_bin.run_capture(['pgbench', '-i', connstr])
+    pg_bin.run_capture(['pgbench'] + '-c 10 -T 5 -P 1 -M prepared'.split() + [connstr])

test_runner/batch_pg_regress/test_pg_regress.py
@@ -11,14 +11,14 @@ HOST = 'localhost'
 PORT = 55432
 
 
-def test_pg_regress(zen_simple, test_output_dir, pg_distrib_dir, base_dir):
+def test_pg_regress(pageserver, postgres, pg_bin, zenith_cli, test_output_dir, pg_distrib_dir, base_dir, capsys):
+
+    # Create a branch for us
+    zenith_cli.run(["branch", "test_pg_regress", "empty"]);
 
     # Connect to postgres and create a database called "regression".
-    username = getpass.getuser()
-    conn_str = 'host={} port={} dbname=postgres user={}'.format(
-        HOST, PORT, username)
-    print('conn_str is', conn_str)
-    pg_conn = psycopg2.connect(conn_str)
+    pg = postgres.create_start('test_pg_regress')
+    pg_conn = psycopg2.connect(pg.connstr())
     pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     cur = pg_conn.cursor()
     cur.execute('CREATE DATABASE regression')
@@ -49,13 +49,13 @@ def test_pg_regress(zen_simple, test_output_dir, pg_distrib_dir, base_dir):
     ]
 
     env = {
-        'PGPORT': str(PORT),
-        'PGUSER': username,
-        'PGHOST': HOST,
+        'PGPORT': str(pg.port),
+        'PGUSER': pg.username,
+        'PGHOST': pg.host,
     }
 
     # Run the command.
     # We don't capture the output. It's not too chatty, and it always
     # logs the exact same data to `regression.out` anyway.
-
-    zen_simple.pg_bin.run(pg_regress_command, env=env, cwd=runpath)
+    with capsys.disabled():
+        pg_bin.run(pg_regress_command, env=env, cwd=runpath)

test_runner/test_broken.py
@@ -23,11 +23,10 @@ run_broken = pytest.mark.skipif(
 
 @run_broken
 def test_broken(zenith_cli, pageserver, postgres, pg_bin):
-    zenith_cli.run_init()
-    pageserver.start()
-    print('pageserver is running')
+    # Create a branch for us
+    zenith_cli.run(["branch", "test_broken", "empty"]);
 
-    postgres.create_start()
+    pg = postgres.create_start("test_broken")
     print('postgres is running')
 
     print('THIS NEXT COMMAND WILL FAIL:')

test_runner/test_pgbench.py
@@ -1,10 +1,0 @@
-import pytest
-
-pytest_plugins = ("fixtures.zenith_fixtures")
-
-
-def test_pgbench(zen_simple):
-    zen_simple.pg_bin.run_capture(
-        'pgbench -h localhost -p 55432 -i postgres'.split())
-    zen_simple.pg_bin.run_capture(
-        'pgbench -h localhost -p 55432 -c 10 -T 5 -P 1 -M prepared postgres'.split())