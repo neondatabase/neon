755d4f6b439911b8a87c9365c18a31fa1cc7a126
Stas Kelvich <stas.kelvich@gmail.com>
2021-05-19 15:57:25 +0300

    Accept semicolon right after branch_create command

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -730,7 +730,7 @@ impl Connection {
             // branch_create <branchname> <startpoint>
             // TODO lazy static
             // TOOD: escaping, to allow branch names with spaces
-            let re = Regex::new(r"^branch_create (\S+) (\S+)[\r\n\s]*;?$").unwrap();
+            let re = Regex::new(r"^branch_create (\S+) ([^\r\n\s;]+)[\r\n\s;]*;?$").unwrap();
             let caps = re.captures(&query_str).ok_or_else(err)?;
 
             let branchname: String = String::from(caps.get(1).ok_or_else(err)?.as_str());
