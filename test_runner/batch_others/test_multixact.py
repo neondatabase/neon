0ed9398676dd25e74547bc5a691583ae93568713
anastasia <lubennikovaav@gmail.com>
2021-05-26 12:38:04 +0300

    change test_multixact to take tuple locks deterministically

--- test_runner/batch_others/test_multixact.py
+++ test_runner/batch_others/test_multixact.py
@@ -5,12 +5,6 @@ import multiprocessing
 
 pytest_plugins = ("fixtures.zenith_fixtures")
 
-def runQuery(connstr):
-    con = psycopg2.connect(connstr) 
-    con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    cur = con.cursor()
-    cur.execute('select * from t1 for key share;') 
-
 
 def test_multixact(pageserver, postgres, pg_bin, zenith_cli, base_dir):
 
@@ -20,30 +14,38 @@ def test_multixact(pageserver, postgres, pg_bin, zenith_cli, base_dir):
 
     print("postgres is running on 'test_multixact' branch")
     pg_conn = psycopg2.connect(pg.connstr())
-    pg_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     cur = pg_conn.cursor()
 
     cur.execute('CREATE TABLE t1(i int primary key);'
-    'INSERT INTO t1 select * from generate_series(1,100);')
+    'INSERT INTO t1 select * from generate_series(1,10);')
+    pg_conn.commit()
+
+    cur.execute('SELECT next_multixact_id FROM pg_control_checkpoint();')
+    old_multixact_id = cur.fetchone()[0]
 
     # Lock entries in parallel connections to set multixact
-    nclients = 3
-    pool = multiprocessing.Pool(nclients)
-    args = [pg.connstr()] * nclients
-    pool.map(runQuery, args)
-    pool.close()
-    pool.join()
+    cur.execute('select * from t1 for key share') 
+
+    pg_conn2 = psycopg2.connect(pg.connstr())
+    cur2 = pg_conn2.cursor()
+    cur2.execute('select * from t1 for key share') 
+
+    pg_conn.commit()
+    pg_conn2.commit()
 
     # force wal flush
     cur.execute('checkpoint')
+    pg_conn.commit()
 
-    cur.execute('SELECT next_multixact_id, pg_current_wal_flush_lsn() FROM pg_control_checkpoint();')
+    cur.execute('SELECT next_multixact_id, next_multi_offset,'
+                'pg_current_wal_flush_lsn() FROM pg_control_checkpoint();')
     res = cur.fetchone()
     next_multixact_id = res[0]
-    lsn = res[1]
+    next_multi_offset = res[1]
+    lsn = res[2]
 
     # Ensure that we did lock some tuples
-    assert(int(next_multixact_id) > 1)
+    assert(int(next_multixact_id) > int(old_multixact_id))
 
     # Branch at this point
     zenith_cli.run(["branch", "test_multixact_new", "test_multixact@"+lsn]);
@@ -54,9 +56,11 @@ def test_multixact(pageserver, postgres, pg_bin, zenith_cli, base_dir):
     pg_new_conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     cur_new = pg_new_conn.cursor()
 
-    cur_new.execute('SELECT next_multixact_id FROM pg_control_checkpoint();')
-    next_multixact_id_new = cur_new.fetchone()[0]
+    cur_new.execute('SELECT next_multixact_id, next_multi_offset  FROM pg_control_checkpoint();')
+    res = cur_new.fetchone()
+    next_multixact_id_new = res[0]
+    next_multi_offset_new = res[1]
 
-    # Check that we restored pg_controlfile correctly
-    # TODO compare content of pg_multixact files?
+    # Check that we restored pg_controlfile field correctly
     assert(next_multixact_id_new == next_multixact_id)
+    assert(next_multi_offset_new == next_multi_offset)
