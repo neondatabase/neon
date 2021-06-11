b561e442b7436fa2220d171fa53953af45dcb087
Stas Kelvich <stas.kelvich@gmail.com>
2021-06-11 23:25:09 +0300

    remove --systemid from walkeeper

--- walkeeper/src/lib.rs
+++ walkeeper/src/lib.rs
@@ -11,12 +11,9 @@ pub mod send_wal;
 pub mod timeline;
 pub mod wal_service;
 
-use crate::pq_protocol::SystemId;
-
 #[derive(Debug, Clone)]
 pub struct WalAcceptorConf {
     pub data_dir: PathBuf,
-    pub systemid: SystemId,
     pub daemonize: bool,
     pub no_sync: bool,
     pub listen_addr: SocketAddr,
