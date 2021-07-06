034c5c7085abfe0626f7709d262138ac79d3acf6
Eric Seppanen <eric@zenith.tech>
2021-07-06 16:26:47 -0700

    suppress nullptr warnings on auto-generated bindgen unit tests
    Hopefully, this will be addressed upstream before too long; see
    rust-bindgen issue #1651.

--- postgres_ffi/src/lib.rs
+++ postgres_ffi/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// suppress warnings on rust 1.53 due to bindgen unit tests.
+// https://github.com/rust-lang/rust-bindgen/issues/1651
+#![allow(deref_nullptr)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub mod controlfile_utils;
