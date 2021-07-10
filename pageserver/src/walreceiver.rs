bfc27bee5e3e8c1df1d904dd80969917314e02e2
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2021-07-10 18:53:47 +0300

    Revert the fix to allegedly inaccurate comment.
    I misread the code. It does indeed only call checkpoint() every 10
    segments. Revert that change, but keep the rest of the comment fixes.

--- pageserver/src/walreceiver.rs
+++ pageserver/src/walreceiver.rs
@@ -198,7 +198,7 @@ fn walreceiver_main(
                 // flush ptr.
                 timeline.advance_last_valid_lsn(endlsn);
 
-                // Somewhat arbitrarily, if we have at least one complete wal segment (16 MB each),
+                // Somewhat arbitrarily, if we have at least 10 complete wal segments (16 MB each),
                 // "checkpoint" the repository to flush all the changes from WAL we've processed
                 // so far to disk. After this, we don't need the original WAL anymore, and it
                 // can be removed. This is probably too aggressive for production, but it's useful
