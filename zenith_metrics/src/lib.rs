212920e47ebc1aef86456738e312f3b046f9d2ba
Kirill Bulatov <mail4score@gmail.com>
2021-09-01 18:24:42 +0300

    Collect and expose I/O disk write metrics

--- zenith_metrics/src/lib.rs
+++ zenith_metrics/src/lib.rs
@@ -2,7 +2,7 @@
 //! make sure that we use the same dep version everywhere.
 //! Otherwise, we might not see all metrics registered via
 //! a default registry.
-pub use prometheus::gather;
+use lazy_static::lazy_static;
 pub use prometheus::{exponential_buckets, linear_buckets};
 pub use prometheus::{register_histogram, Histogram};
 pub use prometheus::{register_histogram_vec, HistogramVec};
@@ -13,4 +13,69 @@ pub use prometheus::{register_int_gauge_vec, IntGaugeVec};
 pub use prometheus::{Encoder, TextEncoder};
 
 mod wrappers;
+use libc::{c_long, getrusage, rusage, suseconds_t, time_t, timeval, RUSAGE_SELF};
 pub use wrappers::{CountedReader, CountedWriter};
+
+/// Gathers all Prometheus metrics and records the I/O stats just before that.
+///
+/// Metrics gathering is a relatively simple and standalone operation, so
+/// it might be fine to do it this way to keep things simple.
+pub fn gather() -> Vec<prometheus::proto::MetricFamily> {
+    update_io_metrics();
+    prometheus::gather()
+}
+
+lazy_static! {
+    static ref DISK_IO_BYTES: IntGaugeVec = register_int_gauge_vec!(
+        "pageserver_disk_io_bytes",
+        "Bytes written and read from disk, grouped by the operation (read|write)",
+        &["io_operation"]
+    )
+    .expect("Failed to register disk i/o bytes int gauge vec");
+}
+
+// Records I/O stats in a "cross-platform" way.
+// Compiles both on macOs and Linux, but current macOs implementation always returns 0 as values for I/O stats.
+// An alternative is to read procfs (`/proc/[pid]/io`) which does not work under macOs at all, hence abandoned.
+//
+// Uses https://www.freebsd.org/cgi/man.cgi?query=getrusage to retrieve the number of block operations
+// performed by the process.
+// We know the the size of the block, so we can determine the I/O bytes out of it.
+// The value might be not 100% exact, but should be fine for Prometheus metrics in this case.
+fn update_io_metrics() {
+    let mut usage = rusage {
+        ru_utime: timeval {
+            tv_sec: 0 as time_t,
+            tv_usec: 0 as suseconds_t,
+        },
+        ru_stime: timeval {
+            tv_sec: 0 as time_t,
+            tv_usec: 0 as suseconds_t,
+        },
+        ru_maxrss: 0 as c_long,
+        ru_ixrss: 0 as c_long,
+        ru_idrss: 0 as c_long,
+        ru_isrss: 0 as c_long,
+        ru_minflt: 0 as c_long,
+        ru_majflt: 0 as c_long,
+        ru_nswap: 0 as c_long,
+        ru_inblock: 0 as c_long,
+        ru_oublock: 0 as c_long,
+        ru_msgsnd: 0 as c_long,
+        ru_msgrcv: 0 as c_long,
+        ru_nsignals: 0 as c_long,
+        ru_nvcsw: 0 as c_long,
+        ru_nivcsw: 0 as c_long,
+    };
+    unsafe {
+        getrusage(RUSAGE_SELF, (&mut usage) as *mut rusage);
+    }
+
+    const BYTES_IN_BLOCK: i64 = 512;
+    DISK_IO_BYTES
+        .with_label_values(&["read"])
+        .set(usage.ru_inblock * BYTES_IN_BLOCK);
+    DISK_IO_BYTES
+        .with_label_values(&["write"])
+        .set(usage.ru_oublock * BYTES_IN_BLOCK);
+}
