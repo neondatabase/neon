181898a8a67bafe6578f75c10059d50542903fab
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2021-09-22 18:10:12 +0300

    Run sync test without zenith_cli

--- test_runner/batch_others/test_wal_acceptor.py
+++ test_runner/batch_others/test_wal_acceptor.py
@@ -1,11 +1,14 @@
 import pytest
 import random
 import time
+import os
+import subprocess
+import uuid
 
 from contextlib import closing
 from multiprocessing import Process, Value
-from fixtures.zenith_fixtures import WalAcceptorFactory, ZenithPageserver, PostgresFactory
-from fixtures.utils import lsn_to_hex
+from fixtures.zenith_fixtures import WalAcceptorFactory, ZenithPageserver, PostgresFactory, PgBin
+from fixtures.utils import lsn_to_hex, mkdir_if_needed
 
 pytest_plugins = ("fixtures.zenith_fixtures")
 
@@ -200,16 +203,59 @@ def test_race_conditions(zenith_cli, pageserver: ZenithPageserver, postgres: Pos
     stop_value.value = 1
     proc.join()
 
+class ProposerPostgres:
+    """ Directory with config, sufficient for running walproposer """
+    def __init__(self, pgdata_dir: str, pg_bin: PgBin, timeline_id: str, tenant_id: str):
+        self.pgdata_dir: str = pgdata_dir
+        self.pg_bin: PgBin = pg_bin
+        self.timeline_id: str = timeline_id
+        self.tenant_id: str = tenant_id
+
+    def pg_data_dir_path(self) -> str:
+        """ Path to data directory """
+        return self.pgdata_dir
+
+    def config_file_path(self) -> str:
+        """ Path to postgresql.conf """
+        return os.path.join(self.pgdata_dir, 'postgresql.conf')
+
+    def create_dir_config(self, wal_acceptors: str):
+        """ Create dir and config for running --sync-safekeepers """
+        mkdir_if_needed(self.pg_data_dir_path())
+        with open(self.config_file_path(), "w") as f:
+            f.write("zenith.zenith_timeline = '{}'\n".format(self.timeline_id))
+            f.write("zenith.zenith_tenant = '{}'\n".format(self.tenant_id))
+            f.write("synchronous_standby_names = '{}'\n".format("walproposer"))
+            f.write("wal_acceptors = '{}'\n".format(wal_acceptors))
+
+    def sync_safekeepers(self) -> subprocess.CompletedProcess:
+        """
+        Run 'postgres --sync-safekeepers'.
+        Returns execution result, which is commit_lsn after sync.
+        """
+
+        pg_path = os.path.join(self.pg_bin.pg_bin_path, 'postgres')
+        command = [pg_path, '--sync-safekeepers']
+        env = {
+            'PGDATA': self.pg_data_dir_path(),
+        }
+        
+        print('Running command "{}"'.format(' '.join(command)))
+        res = subprocess.run(command, env=env, check=True, text=True, stdout=subprocess.PIPE)
+
+        return res.stdout.strip('\n ')
+
 # insert wal in all safekeepers and run sync on proposer
-def test_sync_safekeepers(zenith_cli, postgres: PostgresFactory, wa_factory: WalAcceptorFactory):
+def test_sync_safekeepers(repo_dir: str, pg_bin: PgBin, wa_factory: WalAcceptorFactory):
     wa_factory.start_n_new(3)
 
-    zenith_cli.run(["branch", "test_sync_safekeepers", "empty"])
-    pg = postgres.create('test_sync_safekeepers',
-                               wal_acceptors=wa_factory.get_connstrs())
+    timeline_id = uuid.uuid4().hex
+    tenant_id = uuid.uuid4().hex
 
-    timeline_id = pg.timeline_from_config()
-    tenant_id = postgres.initial_tenant
+    # write config for proposer
+    pgdata_dir = os.path.join(repo_dir, 'proposer_pgdata')
+    pg = ProposerPostgres(pgdata_dir, pg_bin, timeline_id, tenant_id)
+    pg.create_dir_config(wa_factory.get_connstrs())
 
     # run sync to init safekeepers with ProposerGreeting
     initial_lsn = pg.sync_safekeepers()
