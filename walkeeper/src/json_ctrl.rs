5b867c13bfa5d68e544fc97ebc4cf6f216038e22
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2021-09-23 12:47:13 +0300

    Fix review comments

--- walkeeper/src/json_ctrl.rs
+++ walkeeper/src/json_ctrl.rs
@@ -53,11 +53,7 @@ pub fn handle_json_ctrl(
     pgb: &mut PostgresBackend,
     cmd: &Bytes,
 ) -> Result<()> {
-    let cmd = cmd.strip_prefix(b"JSON_CTRL");
-    if cmd.is_none() {
-        return Err(anyhow!("invalid prefix"));
-    }
-    let cmd = cmd.unwrap();
+    let cmd = cmd.strip_prefix(b"JSON_CTRL").ok_or_else(|| anyhow!("invalid prefix"))?;
     // trim zeroes in the end
     let cmd = cmd.strip_suffix(&[0u8]).unwrap_or(cmd);
 
