3a1fb42ba3788699e634ff8d9af16f58a858df4b
Kirill Bulatov <kirill@zenith.tech>
2021-10-20 19:20:56 +0300

    Simplify tracing declarations

--- zenith_utils/src/logging.rs
+++ zenith_utils/src/logging.rs
@@ -5,11 +5,6 @@ use std::{
 
 use anyhow::{Context, Result};
 
-use tracing::subscriber::set_global_default;
-use tracing_log::LogTracer;
-use tracing_subscriber::fmt;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-
 pub fn init(log_filename: impl AsRef<Path>, daemonize: bool) -> Result<File> {
     // Don't open the same file for output multiple times;
     // the different fds could overwrite each other's output.
@@ -23,34 +18,25 @@ pub fn init(log_filename: impl AsRef<Path>, daemonize: bool) -> Result<File> {
 
     // We fall back to printing all spans at info-level or above if
     // the RUST_LOG environment variable is not set.
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(default_filter_str));
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new(default_filter_str));
+
+    let base_logger = tracing_subscriber::fmt()
+        .with_env_filter(env_filter)
+        .with_target(false) // don't include event targets
+        .with_ansi(false); // don't use colors in log file;
 
     // we are cloning and returning log file in order to allow redirecting daemonized stdout and stderr to it
     // if we do not use daemonization (e.g. in docker) it is better to log to stdout directly
     // for example to be in line with docker log command which expects logs comimg from stdout
-    //
-    // TODO: perhaps use a more human-readable format when !daemonize
     if daemonize {
         let x = log_file.try_clone().unwrap();
-
-        let fmt_layer = fmt::layer()
-            .pretty()
-            .with_target(false) // don't include event targets
-            .with_ansi(false) // don't use colors in log file
-            .with_writer(move || x.try_clone().unwrap());
-        let subscriber = Registry::default().with(env_filter).with(fmt_layer);
-
-        set_global_default(subscriber).expect("Failed to set subscriber");
+        base_logger
+            .with_writer(move || x.try_clone().unwrap())
+            .init();
     } else {
-        let fmt_layer = fmt::layer().with_target(false); // don't include event targets
-        let subscriber = Registry::default().with(env_filter).with(fmt_layer);
-
-        set_global_default(subscriber).expect("Failed to set subscriber");
+        base_logger.init();
     }
-
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
 
     Ok(log_file)
 }
