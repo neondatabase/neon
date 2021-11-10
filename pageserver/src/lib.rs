d2e5e0e72878e74fccb4b86aed9c3e379031ecd9
Konstantin Knizhnik <knizhnik@garret.ru>
2021-11-11 00:16:37 +0300

    Fix compression

--- pageserver/src/lib.rs
+++ pageserver/src/lib.rs
@@ -34,10 +34,10 @@ pub mod defaults {
 
     // Minimal size of WAL records chain to trigger materialization of the page
     pub const DEFAULT_CHECKPOINT_DISTANCE: u64 = 0;
-    pub const DEFAULT_CHECKPOINT_PERIOD: Duration = Duration::from_secs(10);
+    pub const DEFAULT_CHECKPOINT_PERIOD: Duration = Duration::from_secs(1);
 
-    pub const DEFAULT_GC_HORIZON: u64 = 2000_000_000u64;
-    pub const DEFAULT_GC_PERIOD: Duration = Duration::from_secs(10000);
+    pub const DEFAULT_GC_HORIZON: u64 = 1024*1024;
+    pub const DEFAULT_GC_PERIOD: Duration = Duration::from_secs(1);
 
     pub const DEFAULT_SUPERUSER: &str = "zenith_admin";
     pub const DEFAULT_RELISH_STORAGE_MAX_CONCURRENT_SYNC_LIMITS: usize = 100;
