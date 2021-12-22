30c9af8ff5c20c498d50b95aa94311b665b11463
anastasia <lubennikovaav@gmail.com>
2021-12-22 20:06:23 +0300

    Extend replication protocol with ZentihFeedback message to pass current_timeline_size to compute node
    Put standby_status_update fields into ZenithFeedback and send them as one message.
    Pass values sizes together with keys in ZenithFeedback message.

--- walkeeper/src/safekeeper.rs
+++ walkeeper/src/safekeeper.rs
@@ -22,6 +22,7 @@ use zenith_metrics::{
 use zenith_utils::bin_ser::LeSer;
 use zenith_utils::lsn::Lsn;
 use zenith_utils::pq_proto::SystemId;
+use zenith_utils::pq_proto::ZenithFeedback;
 use zenith_utils::zid::{ZTenantId, ZTimelineId};
 
 pub const SK_MAGIC: u32 = 0xcafeceefu32;
@@ -278,9 +279,8 @@ pub struct AppendResponse {
     // We report back our awareness about which WAL is committed, as this is
     // a criterion for walproposer --sync mode exit
     pub commit_lsn: Lsn,
-    // Min disk consistent lsn of pageservers (portion of WAL applied and written to the disk by pageservers)
-    pub disk_consistent_lsn: Lsn,
     pub hs_feedback: HotStandbyFeedback,
+    pub zenith_feedback: ZenithFeedback,
 }
 
 impl AppendResponse {
@@ -289,8 +289,8 @@ impl AppendResponse {
             term,
             flush_lsn: Lsn(0),
             commit_lsn: Lsn(0),
-            disk_consistent_lsn: Lsn(0),
             hs_feedback: HotStandbyFeedback::empty(),
+            zenith_feedback: ZenithFeedback::empty(),
         }
     }
 }
@@ -395,10 +395,11 @@ impl AcceptorProposerMessage {
                 buf.put_u64_le(msg.term);
                 buf.put_u64_le(msg.flush_lsn.into());
                 buf.put_u64_le(msg.commit_lsn.into());
-                buf.put_u64_le(msg.disk_consistent_lsn.into());
                 buf.put_i64_le(msg.hs_feedback.ts);
                 buf.put_u64_le(msg.hs_feedback.xmin);
                 buf.put_u64_le(msg.hs_feedback.catalog_xmin);
+
+                msg.zenith_feedback.serialize(buf)?
             }
         }
 
@@ -614,9 +615,9 @@ where
             term: self.s.acceptor_state.term,
             flush_lsn: self.flush_lsn,
             commit_lsn: self.s.commit_lsn,
-            disk_consistent_lsn: Lsn(0),
             // will be filled by the upper code to avoid bothering safekeeper
             hs_feedback: HotStandbyFeedback::empty(),
+            zenith_feedback: ZenithFeedback::empty(),
         }
     }
 
