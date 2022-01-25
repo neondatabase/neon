be02f32640baf74ae13824a6b2b26274dbf01b06
Heikki Linnakangas <heikki@zenith.tech>
2022-01-25 14:24:51 +0200

    Tidy up the test case.

--- test_runner/batch_others/test_next_xid.py
+++ test_runner/batch_others/test_next_xid.py
@@ -12,30 +12,27 @@ pytest_plugins = ("fixtures.zenith_fixtures")
 # running.
 def test_next_xid(zenith_env_builder: ZenithEnvBuilder):
     # One safekeeper is enough for this test.
-    zenith_env_builder.num_safekeepers = 3
+    zenith_env_builder.num_safekeepers = 1
     env = zenith_env_builder.init()
 
     pg = env.postgres.create_start('main')
 
     conn = pg.connect()
     cur = conn.cursor()
-
     cur.execute('CREATE TABLE t(x integer)')
 
     iterations = 32
     for i in range(1, iterations + 1):
         print(f'iteration {i} / {iterations}')
 
-        # Stop and restart pageserver. This is a more or less graceful shutdown, although
-        # the page server doesn't currently have a shutdown routine so there's no difference
-        # between stopping and crashing.
+        # Kill and restart the pageserver.
         pg.stop()
         env.pageserver.stop(immediate=True)
         env.pageserver.start()
         pg.start()
 
-        retry_sleep = 2
-        max_retries = 2000
+        retry_sleep = 0.5
+        max_retries = 200
         retries = 0
         while True:
             try:
@@ -45,17 +42,14 @@ def test_next_xid(zenith_env_builder: ZenithEnvBuilder):
                 conn.close()
 
             except Exception as error:
-                # It's normal that it takes some time for the containers to restart,
-                # and the components to notice broken connections and such. So if a
-                # connection fails, retry it. It should eventually recover.
+                # It's normal that it takes some time for the pageserver to
+                # restart, and for the connection to fail until it does. It
+                # should eventually recover, so retry until it succeeds.
                 print(f'failed: {error}')
                 if retries < max_retries:
                     retries += 1
                     print(f'retry {retries} / {max_retries}')
                     time.sleep(retry_sleep)
-
-                    # Establish a new connection for the next retry. It's normal for the
-                    # connection to be broken if the proxy or the compute node was restarted.
                     continue
                 else:
                     raise
