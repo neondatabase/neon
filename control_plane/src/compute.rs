4df5490af8d952a88e9efde4f87816331954520a
Konstantin Knizhnik <knizhnik@garret.ru>
2022-01-28 16:22:46 +0300

    Update comment for wal_log_hints

--- control_plane/src/compute.rs
+++ control_plane/src/compute.rs
@@ -284,9 +284,8 @@ impl PostgresNode {
     fn setup_pg_conf(&self, auth_type: AuthType) -> Result<()> {
         let mut conf = PostgresConf::new();
         conf.append("max_wal_senders", "10");
-        // wal_log_hints is mandatory when running against pageserver (see gh issue#192)
-        // TODO: is it possible to check wal_log_hints at pageserver side via XLOG_PARAMETER_CHANGE?
-        conf.append("wal_log_hints", "off"); // Wal logging hint bits may cause significant (>5 times) write amplification
+        // Wal logging hint bits may cause significant (>5 times) write amplification
+        conf.append("wal_log_hints", "off");
         conf.append("max_replication_slots", "10");
         conf.append("hot_standby", "on");
         conf.append("shared_buffers", "1MB");
