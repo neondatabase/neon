8edec0b2ea9ac56f4784d358fc0c6b1981201572
Konstantin Knizhnik <knizhnik@garret.ru>
2022-03-15 18:06:36 +0300

    Choose unused port in wal restore test

--- test_runner/batch_others/test_wal_restore.py
+++ test_runner/batch_others/test_wal_restore.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 
 from fixtures.utils import mkdir_if_needed
-from fixtures.zenith_fixtures import (ZenithEnvBuilder, base_dir, vanilla_pg, pg_distrib_dir)
+from fixtures.zenith_fixtures import (ZenithEnvBuilder, VanillaPostgres, PortDistributor, PgBin, base_dir, vanilla_pg, pg_distrib_dir)
 from fixtures.log_helper import log
 
 
-def test_wal_restore(zenith_env_builder: ZenithEnvBuilder, test_output_dir, vanilla_pg):
+def test_wal_restore(zenith_env_builder: ZenithEnvBuilder, test_output_dir, port_distributor: PortDistributor):
     zenith_env_builder.num_safekeepers = 1
     env = zenith_env_builder.init_start()
     env.zenith_cli.create_branch("test_wal_restore")
@@ -14,13 +14,17 @@ def test_wal_restore(zenith_env_builder: ZenithEnvBuilder, test_output_dir, vani
     pg.safe_psql("create table t as select generate_series(1,1000000)")
     tenant_id = pg.safe_psql("show zenith.zenith_tenant")[0][0]
     env.zenith_cli.pageserver_stop()
+    port = port_distributor.get_port()
+    data_dir = os.path.join(test_output_dir, 'pgsql.restored')
+    pg = VanillaPostgres(data_dir, PgBin(test_output_dir), port)
     subprocess.call([
         'bash',
         os.path.join(base_dir, 'vendor/postgres/contrib/zenith/utils/restore_from_wal.sh'),
         os.path.join(pg_distrib_dir, 'bin'),
         os.path.join(test_output_dir, 'repo/safekeepers/sk1/{}/*'.format(tenant_id)),
-        test_output_dir
+        data_dir,
+        str(port)
     ])
-    vanilla_pg.start()
-    assert vanilla_pg.safe_psql('select count(*) from t') == [(1000000, )]
-    vanilla_pg.stop()
+    pg.start()
+    assert pg.safe_psql('select count(*) from t') == [(1000000, )]
+    pg.stop()
