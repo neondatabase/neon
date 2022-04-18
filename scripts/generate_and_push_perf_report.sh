cdc51b3a836c4f135205d875aac659c57ffb482e
Dmitry Rodionov <dmitry@zenith.tech>
2022-04-18 12:11:01 +0300

    temporary disable bench uploads

--- scripts/generate_and_push_perf_report.sh
+++ scripts/generate_and_push_perf_report.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Uploading perf report to zenith pg"
 # ingest per test results data into zenith backed postgres running in staging to build grafana reports on that data
-DATABASE_URL="$PERF_TEST_RESULT_CONNSTR" poetry run python "$SCRIPT_DIR"/ingest_perf_test_result.py --ingest "$REPORT_FROM"
+# DATABASE_URL="$PERF_TEST_RESULT_CONNSTR" poetry run python "$SCRIPT_DIR"/ingest_perf_test_result.py --ingest "$REPORT_FROM"
 
 # Activate poetry's venv. Needed because git upload does not run in a project dir (it uses tmp to store the repository)
 # so the problem occurs because poetry cannot find pyproject.toml in temp dir created by git upload
