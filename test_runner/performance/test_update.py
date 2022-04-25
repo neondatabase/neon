22996504e3224c86ff491de5b9ee498073b551a4
Konstantin Knizhnik <knizhnik@garret.ru>
2022-04-25 22:06:52 +0300

    Fix pythin formating

--- test_runner/performance/test_update.py
+++ test_runner/performance/test_update.py
@@ -8,12 +8,15 @@ from fixtures.benchmark_fixture import MetricReport, ZenithBenchmarker
 from fixtures.compare_fixtures import PgCompare
 import pytest
 
+
 def test_update(zenith_with_baseline: PgCompare):
     env = zenith_with_baseline
 
     with closing(env.pg.connect()) as conn:
         with conn.cursor() as cur:
-            cur.execute("create table t (pk integer, val bigint default 0, t text default repeat(' ', 100))")
+            cur.execute(
+                "create table t (pk integer, val bigint default 0, t text default repeat(' ', 100))"
+            )
             cur.execute('insert into t (pk) values (generate_series(1,10000000))')
             with env.record_duration('update'):
                 cur.execute('update t set val=val+1')
