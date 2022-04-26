5c5c3c64f3153b4b67c0ed4f51d4ab14c8aa1da2
Anastasia Lubennikova <lubennikovaav@gmail.com>
2022-04-26 19:35:07 +0300

    Fix tenant config parsing. Add a test

--- test_runner/batch_others/test_tenant_conf.py
+++ test_runner/batch_others/test_tenant_conf.py
@@ -3,21 +3,22 @@ from contextlib import closing
 import pytest
 
 from fixtures.zenith_fixtures import ZenithEnvBuilder
+from fixtures.log_helper import log
 
 
 def test_tenant_config(zenith_env_builder: ZenithEnvBuilder):
+    # set some non-default global config
+    zenith_env_builder.pageserver_config_override = '''
+page_cache_size=444;
+wait_lsn_timeout='111 s';
+tenant_config={checkpoint_distance = 10000, compaction_target_size = 1048576}'''
+
     env = zenith_env_builder.init_start()
     """Test per tenant configuration"""
-    tenant = env.zenith_cli.create_tenant(
-        conf={
-            'checkpoint_distance': '10000',
-            'compaction_target_size': '1048576',
-            'compaction_period': '60sec',
-            'compaction_threshold': '20',
-            'gc_horizon': '1024',
-            'gc_period': '100sec',
-            'pitr_interval': '3600sec',
-        })
+    tenant = env.zenith_cli.create_tenant(conf={
+        'checkpoint_distance': '20000',
+        'gc_period': '30sec',
+    })
 
     env.zenith_cli.create_timeline(f'test_tenant_conf', tenant_id=tenant)
     pg = env.postgres.create_start(
@@ -26,24 +27,44 @@ def test_tenant_config(zenith_env_builder: ZenithEnvBuilder):
         tenant,
     )
 
+    # check the configuration of the default tenant
+    # it should match global configuration
+    with closing(env.pageserver.connect()) as psconn:
+        with psconn.cursor() as pscur:
+            pscur.execute(f"show {env.initial_tenant.hex}")
+            res = pscur.fetchone()
+            log.info(f"initial_tenant res: {res}")
+            assert res == (10000, 1048576, 1, 10, 67108864, 100, 2592000)
+
+    # check the configuration of the new tenant
     with closing(env.pageserver.connect()) as psconn:
         with psconn.cursor() as pscur:
             pscur.execute(f"show {tenant.hex}")
-            assert pscur.fetchone() == (10000, 1048576, 60, 20, 1024, 100, 3600)
+            res = pscur.fetchone()
+            log.info(f"res: {res}")
+            assert res == (20000, 1048576, 1, 10, 67108864, 30, 2592000)
 
     # update the config and ensure that it has changed
     env.zenith_cli.config_tenant(tenant_id=tenant,
                                  conf={
-                                     'checkpoint_distance': '100000',
-                                     'compaction_target_size': '1048576',
-                                     'compaction_period': '30sec',
-                                     'compaction_threshold': '15',
-                                     'gc_horizon': '256',
-                                     'gc_period': '10sec',
-                                     'pitr_interval': '360sec',
+                                     'checkpoint_distance': '15000',
+                                     'gc_period': '80sec',
                                  })
 
     with closing(env.pageserver.connect()) as psconn:
         with psconn.cursor() as pscur:
             pscur.execute(f"show {tenant.hex}")
-            assert pscur.fetchone() == (100000, 1048576, 30, 15, 256, 10, 360)
+            res = pscur.fetchone()
+            log.info(f"after config res: {res}")
+            assert res == (15000, 1048576, 1, 10, 67108864, 80, 2592000)
+
+    # restart the pageserver and ensure that the config is still correct
+    env.pageserver.stop()
+    env.pageserver.start()
+
+    with closing(env.pageserver.connect()) as psconn:
+        with psconn.cursor() as pscur:
+            pscur.execute(f"show {tenant.hex}")
+            res = pscur.fetchone()
+            log.info(f"after restart res: {res}")
+            assert res == (15000, 1048576, 1, 10, 67108864, 80, 2592000)
