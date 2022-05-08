f58f5d6489cfc4e5eb206b0b3af823f16a1f03c8
Konstantin Knizhnik <knizhnik@garret.ru>
2022-05-08 08:00:29 +0300

    Working version of test_many_branches test

--- test_runner/performance/test_many_branches.py
+++ test_runner/performance/test_many_branches.py
@@ -8,7 +8,10 @@ from fixtures.utils import print_gc_result
 from fixtures.zenith_fixtures import ZenithEnvBuilder
 
 
-def test_branching(zenith_env_builder: ZenithEnvBuilder, zenbenchmark):
+def test_many_branches(zenith_env_builder: ZenithEnvBuilder, zenbenchmark):
+
+    branches = 50
+    records = 10000
 
     # Use safekeeper in this test to avoid a subtle race condition.
     # Without safekeeper, walreceiver reconnection can stuck
@@ -20,24 +23,21 @@ def test_branching(zenith_env_builder: ZenithEnvBuilder, zenbenchmark):
 
     env.zenith_cli.create_branch('b0')
     pg = env.postgres.create_start('b0')
-    connstr = pg.connstr()
-    branches = 50
-    records = 10000
 
     conn = pg.connect()
     cur = conn.cursor()
-    cur.execute('create table t(x integer)');
+    cur.execute('create table t(x integer)')
 
     for i in range(branches):
         env.zenith_cli.create_branch('b{}'.format(i + 1), 'b{}'.format(i))
         pg = env.postgres.create_start('b{}'.format(i + 1))
         conn = pg.connect()
         cur = conn.cursor()
-        cur.execute(f'insert into t values (generate_series(1,{records}))');
-
-    conn = pg.connect()
-    cur = conn.cursor()
+        cur.execute(f'insert into t values (generate_series(1,{records}))')
+        cur.execute('SELECT count(*) from t')
+        rows = cur.fetchone()[0]
+        print('Iteration {i} rows {rows}')
     cur.execute('vacuum t')
     with zenbenchmark.record_duration('test_query'):
-        cur.execute('SELECT count(x) from t')
-        assert cur.fetchone()[0] == branches*records
+        cur.execute('SELECT count(*) from t')
+        assert cur.fetchone()[0] == branches * records
