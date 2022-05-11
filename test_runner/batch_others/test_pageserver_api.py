d8f136bfe1164587f754a8e076b4a85276f4aff5
Thang Pham <thang@neon.tech>
2022-05-11 13:47:49 -0400

    add tests for new `GET /wal_receiver` API

--- test_runner/batch_others/test_pageserver_api.py
+++ test_runner/batch_others/test_pageserver_api.py
@@ -1,6 +1,7 @@
+from typing import Tuple
 from uuid import uuid4, UUID
 import pytest
-from fixtures.zenith_fixtures import ZenithEnv, ZenithEnvBuilder, ZenithPageserverHttpClient
+from fixtures.zenith_fixtures import DEFAULT_BRANCH_NAME, ZenithEnv, ZenithEnvBuilder, ZenithPageserverHttpClient, ZenithPageserverApiException
 
 
 # test that we cannot override node id
@@ -11,6 +12,19 @@ def test_pageserver_init_node_id(zenith_env_builder: ZenithEnvBuilder):
             match="node id can only be set during pageserver init and cannot be overridden"):
         env.pageserver.start(overrides=['--pageserver-config-override=id=10'])
 
+def create_tenant_and_root_branch(env: ZenithEnv, client: ZenithPageserverHttpClient, branch_name: str = DEFAULT_BRANCH_NAME) -> Tuple[UUID, UUID]:
+    """A helper function for creating a tenant and a root branch for that tenant.
+
+    # Returns
+    Tuple[UUID, UUID]
+       the tuple of the new tenant's ID and the new branch's timeline ID
+    """
+    tenant_id = uuid4()
+    client.tenant_create(tenant_id)
+
+    timeline_id = env.zenith_cli.create_root_branch(branch_name, tenant_id=tenant_id)
+
+    return tenant_id, timeline_id
 
 def check_client(client: ZenithPageserverHttpClient, initial_tenant: UUID):
     client.check_status()
@@ -46,6 +60,35 @@ def check_client(client: ZenithPageserverHttpClient, initial_tenant: UUID):
         local_timeline_details = timeline_details.get('local')
         assert local_timeline_details is not None
         assert local_timeline_details['timeline_state'] == 'Loaded'
+
+
+def test_pageserver_http_get_wal_receiver_not_found(zenith_simple_env: ZenithEnv):
+    env = zenith_simple_env
+    client = env.pageserver.http_client()
+
+    tenant_id, timeline_id = create_tenant_and_root_branch(env, client, "test_get_wal_receiver_not_found")
+
+    # no PG compute node is running, so no WAL receiver is running
+    with pytest.raises(ZenithPageserverApiException) as e:
+        _ = client.wal_receiver_get(tenant_id, timeline_id)
+        assert "Not Found" in str(e.value)
+
+
+def test_pageserver_http_get_wal_receiver_success(zenith_simple_env: ZenithEnv):
+    env = zenith_simple_env
+    client = env.pageserver.http_client()
+
+    tenant_id, timeline_id = create_tenant_and_root_branch(env, client, "test_get_wal_receiver_success")
+    pg = env.postgres.create_start('test_get_wal_receiver_success', tenant_id=tenant_id)
+
+    res = client.wal_receiver_get(tenant_id, timeline_id)
+    assert list(res.keys()) == ['thread_id', 'wal_producer_connstr', 'last_received_msg_lsn', 'last_received_msg_ts']
+
+    # make a DB modification then expect getting a new WAL receiver's data
+    cur = pg.connect().cursor()
+    cur.execute('CREATE TABLE t(key int primary key, value text)')
+    res2 = client.wal_receiver_get(tenant_id, timeline_id)
+    assert res2['last_received_msg_lsn'] > res['last_received_msg_lsn']
 
 
 def test_pageserver_http_api_client(zenith_simple_env: ZenithEnv):
