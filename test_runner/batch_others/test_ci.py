d4585bcb671e1c6a8cfd08d0bdc884e05702e126
Bojan Serafimov <bojan@pop-os.localdomain>
2022-05-30 16:49:38 -0400

    Fix typo

--- test_runner/batch_others/test_ci.py
+++ test_runner/batch_others/test_ci.py
@@ -5,7 +5,7 @@ import time
 from fixtures.zenith_fixtures import ZenithEnvBuilder
 
 
-@pytest.mark.parametrize('iteration', [range(100)])
+@pytest.mark.parametrize('iteration', list(range(100)))
 def test_timeout(zenith_env_builder: ZenithEnvBuilder, test_output_dir: str, iteration: int):
     env = zenith_env_builder.init_start()
 
