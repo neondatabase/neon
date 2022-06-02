4a569882c539849fce97fb4556744b8f11e23803
Thang Pham <thang@neon.tech>
2022-06-02 17:38:18 -0400

    rename zenith to neon

--- test_runner/performance/test_compare_pg_stats.py
+++ test_runner/performance/test_compare_pg_stats.py
@@ -1,10 +1,9 @@
 import pytest
-from fixtures.compare_fixtures import PgCompare, VanillaCompare, ZenithCompare, zenith_compare
+from fixtures.compare_fixtures import VanillaCompare, NeonCompare
 
 from performance.test_perf_pgbench import (get_durations_matrix, get_scales_matrix)
 
 
 @pytest.mark.parametrize("scale", get_scales_matrix())
-@pytest.mark.parametrize("duration", get_durations_matrix())
-def test_pgbench(zenith_compare: ZenithCompare, vanilla_compare: VanillaCompare, scale: int, duration: int):
+def test_compare_pg_stats_with_pgbench(neon_compare: NeonCompare, vanilla_compare: VanillaCompare, scale: int):
     ...
