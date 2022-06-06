03dc15502beb9599813e082208ad9f67d884e246
Thang Pham <thang@neon.tech>
2022-06-06 15:44:56 -0400

    re-structure `test_measure_read_latency_heavy_write_workload`

--- test_runner/performance/test_wal_backpressure.py
+++ test_runner/performance/test_wal_backpressure.py
@@ -1,46 +1,67 @@
-from random import randint, random
+from random import randint
+import statistics
 import time
 import threading
 import timeit
 from batch_others.test_backpressure import pg_cur
+from fixtures.benchmark_fixture import MetricReport
+from fixtures.compare_fixtures import PgCompare
 from fixtures.neon_fixtures import NeonEnv, Postgres
 from fixtures.log_helper import log
 
 
-def measure_read_latency(pg: Postgres, read_query: str):
-    for _ in range(20):
-        time.sleep(1.0)
+def start_heavy_write_workload(pg: Postgres, scale: int = 1, n_iters: int = 10):
+    """Start an intensive write workload which first initializes a table `t` with `new_rows_each_update` rows.
+    At each subsequent step, we update a subset of rows in the table and insert new `new_rows_each_update` rows.
+    The variable `new_rows_each_update` is equal to `scale * 100_000`."""
+    new_rows_each_update = scale * 100_000
 
-        with pg_cur(pg) as cur:
-            t = timeit.default_timer()
-            cur.execute(read_query)
-            res = cur.fetchall()
-            duration = timeit.default_timer() - t
+    with pg_cur(pg) as cur:
+        cur.execute("CREATE TABLE t(key int primary key, tag int, cnt int)")
 
-            log.info(f"Executing read query '{read_query}', got {res}, took {duration}s")
+    with pg_cur(pg) as cur:
+        cur.execute(
+            f"INSERT INTO t SELECT s, s % 10, 0 FROM generate_series(1,{new_rows_each_update}) s")
+    n_rows = new_rows_each_update
 
-
-def test_measure_read_latency_with_intensive_write_workload(neon_simple_env: NeonEnv):
-    env = neon_simple_env
-
-    pg = env.postgres.create_start("main")
-
-    pg.safe_psql("CREATE TABLE t(key int primary key, tag int, cnt int)")
-
-    read_thread = threading.Thread(target=measure_read_latency, args=(pg, "SELECT count(*) from t"))
-    read_thread.start()
-
-    # Start an intensive write workload which first initializes the table t with 1000000 rows.
-    # At each subsequent step, we update a subset of rows and insert new 100000 rows.
-    pg.safe_psql("INSERT INTO t SELECT s, s % 10, 0 FROM generate_series(1,1000000) s")
-    n_rows = 1000000
-    while read_thread.is_alive():
+    for _ in range(n_iters):
         # the subset of rows updated in this step is determined by the `tag` column
         tag = randint(0, 10)
         with pg_cur(pg) as cur:
             cur.execute(f"UPDATE t SET cnt=cnt+1 WHERE tag={tag}")
             cur.execute(
-                f"INSERT INTO t SELECT s+{n_rows}, s % 10, 0 FROM generate_series(1,100000) s")
-        n_rows += 100000
+                f"INSERT INTO t SELECT s+{n_rows}, s % 10, 0 FROM generate_series(1,{new_rows_each_update}) s"
+            )
+        n_rows += new_rows_each_update
 
-        time.sleep(1.0)
+
+def test_measure_read_latency_heavy_write_workload(neon_with_baseline: PgCompare):
+    env = neon_with_baseline
+    pg = env.pg
+
+    with env.record_duration("run_duration"):
+        write_thread = threading.Thread(target=start_heavy_write_workload, args=(pg, ))
+        write_thread.start()
+
+        read_latencies = []
+        while write_thread.is_alive():
+            time.sleep(1.0)
+
+            with pg_cur(pg) as cur:
+                t = timeit.default_timer()
+                cur.execute("SELECT count(*) from t")
+                res = cur.fetchone()[0]
+                duration = timeit.default_timer() - t
+
+                log.info(f"Get the number of rows in the table `t`, got {res}, took {duration}s")
+
+                read_latencies.append(duration)
+
+    env.zenbenchmark.record("read_latency_avg",
+                            statistics.mean(read_latencies),
+                            's',
+                            MetricReport.LOWER_IS_BETTER)
+    env.zenbenchmark.record("read_latency_stdev",
+                            statistics.stdev(read_latencies),
+                            's',
+                            MetricReport.LOWER_IS_BETTER)
