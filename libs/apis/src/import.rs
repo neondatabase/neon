2ac9199dd87200b783613798d0630edfa4a2cab9
Bojan Serafimov <bojan.serafimov7@gmail.com>
2022-06-08 11:50:01 -0400

    Add todos

--- libs/apis/src/import.rs
+++ libs/apis/src/import.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use bytes::Bytes;
 use serde::{Serialize, Deserialize};
 
@@ -12,4 +14,12 @@ pub enum ImportFeMessage {
 pub enum ImportBeMessage {
     Done,
     Error(String),
+}
+
+// TODO add a thin binary wrapper for this function, aside from neon_local
+pub fn send_basebackup(basebackup_dir: PathBuf) -> anyhow::Result<()> {
+    // TODO change return type
+    // TODO implement as sender of ImportFeMessage and receiver of ImportBeMessage
+    //      on generic channel.
+    Ok(())
 }
