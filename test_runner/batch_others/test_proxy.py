1ca49111d55e905eb3c508d363c340dd10ae2324
Kliment Serafimov <kliment@mit.edu>
2022-06-15 09:52:07 +0200

    [Proxy test] Added project option argument to test_proxy_select_1.

--- test_runner/batch_others/test_proxy.py
+++ test_runner/batch_others/test_proxy.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_proxy_select_1(static_proxy):
-    static_proxy.safe_psql("select 1;")
+    static_proxy.safe_psql("select 1;", options="project=test-proxy-select-1")
 
 
 # Pass extra options to the server.
