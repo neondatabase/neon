94eb9cdd1ff983661d6a1624d26055aba1e2e375
Konstantin Knizhnik <knizhnik@garret.ru>
2022-06-28 08:09:32 +0300

    Fix pythin style

--- test_runner/performance/test_latency.py
+++ test_runner/performance/test_latency.py
@@ -15,10 +15,11 @@ from fixtures.neon_fixtures import Postgres
 from performance.test_perf_pgbench import get_scales_matrix
 from performance.test_backpressure import record_read_latency
 
+
 def start_write_workload(pg: Postgres, scale: int = 10):
     with pg_cur(pg) as cur:
-        cur.execute(
-            f"create table big as select generate_series(1,{scale*100_000})")
+        cur.execute(f"create table big as select generate_series(1,{scale*100_000})")
+
 
 @pytest.mark.parametrize("scale", get_scales_matrix(1))
 def test_measure_read_latency_heavy_write_workload(neon_with_baseline: PgCompare, scale: int):
@@ -28,7 +29,7 @@ def test_measure_read_latency_heavy_write_workload(neon_with_baseline: PgCompare
     with pg_cur(pg) as cur:
         cur.execute(f"create table small as select generate_series(1,{scale*100_000})")
 
-    write_thread = threading.Thread(target=start_write_workload, args=(pg, scale*100))
+    write_thread = threading.Thread(target=start_write_workload, args=(pg, scale * 100))
     write_thread.start()
 
     record_read_latency(env, write_thread, "SELECT count(*) from small")
