b0e7a2b0a3ab6d37473c10628ae3c38c6c9a0198
Bojan Serafimov <bojan.serafimov7@gmail.com>
2022-06-30 19:52:11 -0400

    Fix type hint

--- test_runner/fixtures/neon_fixtures.py
+++ test_runner/fixtures/neon_fixtures.py
@@ -2140,7 +2140,7 @@ def check_restored_datadir_content(test_output_dir: str, env: NeonEnv, pg: Postg
     assert (mismatch, error) == ([], [])
 
 
-def wait_until(number_of_iterations: int, interval: int, func):
+def wait_until(number_of_iterations: int, interval: float, func):
     """
     Wait until 'func' returns successfully, without exception. Returns the last return value
     from the the function.
