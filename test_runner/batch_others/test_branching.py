f2315e8c21a048049091e6244f40c50093a2e818
Thang Pham <thang@neon.tech>
2022-07-06 17:17:31 -0400

    update test

--- test_runner/batch_others/test_branching.py
+++ test_runner/batch_others/test_branching.py
@@ -1,50 +1,65 @@
-import subprocess
-from contextlib import closing
-
-import psycopg2.extras
+from typing import List
+import threading
 import pytest
+from fixtures.neon_fixtures import NeonEnv, PgBin, Postgres
+import time
+import random
 from fixtures.log_helper import log
-from fixtures.utils import print_gc_result
-from fixtures.zenith_fixtures import ZenithEnvBuilder
+from performance.test_perf_pgbench import get_scales_matrix
 
 
-def test_branching(zenith_env_builder: ZenithEnvBuilder, pg_bin):
+@pytest.mark.parametrize("n_branches", [10])
+@pytest.mark.parametrize("scale", get_scales_matrix(1))
+def test_cascade_branching_with_pgbench(neon_simple_env: NeonEnv,
+                                        pg_bin: PgBin,
+                                        n_branches: int,
+                                        scale: int):
+    env = neon_simple_env
 
-    # Use safekeeper in this test to avoid a subtle race condition.
-    # Without safekeeper, walreceiver reconnection can stuck
-    # because of IO deadlock.
-    #
-    # See https://github.com/zenithdb/zenith/issues/1068
-    zenith_env_builder.num_safekeepers = 1
-    env = zenith_env_builder.init()
+    tenant, _ = env.neon_cli.create_tenant(
+         conf={
+             'gc_period': '5 s',
+             'gc_horizon': f'{1024 ** 2}',
+             'checkpoint_distance': f'{1024 ** 2}',
+             'compaction_target_size': f'{1024 ** 2}',
+             'compaction_period': '5 s',
+             'compaction_threshold': '2',
+             # set PITR interval to be small, so we can do GC
+             'pitr_interval': '5 s'
+         })
 
-    env.pageserver.start(overrides=[
-        '--pageserver-config-override="gc_period"="10 s"',
-        '--pageserver-config-override="gc_horizon"=1024',
-        '--pageserver-config-override="checkpoint_distance"=4194304',
-        '--pageserver-config-override="compaction_period"="10 m"',
-        '--pageserver-config-override="compaction_threshold"=2',
-        '--pageserver-config-override="compaction_target_size"=4194304'
-    ])
-    env.safekeepers[0].start()
-
-    env.zenith_cli.create_branch('b0')
-    pg = env.postgres.create_start('b0')
-    connstr = pg.connstr()
-    branches = 10
-
-    pg_bin.run_capture(['pgbench', '-i', '-s', '100', connstr])
-    pg_bin.run_capture(['pgbench'] + '-c 10 -T 10 -N -M prepared'.split() + [connstr])
-    pg_bin.run_capture(['pgbench'] + '-c 10 -T 10 -S -M prepared'.split() + [connstr])
-
-    for i in range(branches):
-        env.zenith_cli.create_branch('b{}'.format(i+1), 'b{}'.format(i))
-        pg = env.postgres.create_start('b{}'.format(i+1))
+    def run_pgbench(pg: Postgres):
         connstr = pg.connstr()
+
+        log.info(f"Start a pgbench workload on pg {connstr}")
+
+        pg_bin.run_capture(['pgbench', '-i', f'-s{scale}', connstr])
         pg_bin.run_capture(['pgbench'] + '-c 10 -T 10 -N -M prepared'.split() + [connstr])
         pg_bin.run_capture(['pgbench'] + '-c 10 -T 10 -S -M prepared'.split() + [connstr])
 
-    conn = pg.connect()
-    cur = conn.cursor()
-    cur.execute('SELECT count(aid) from pgbench_accounts')
-    assert cur.fetchone()[0] == 10000000
+    env.neon_cli.create_branch('b0', tenant_id=tenant)
+    pgs: List[Postgres] = []
+    pgs.append(env.postgres.create_start('b0', tenant_id=tenant))
+
+    threads: List[threading.Thread] = []
+    threads.append(threading.Thread(target=run_pgbench, args=(pgs[0], )))
+    threads[-1].start()
+
+    for i in range(n_branches):
+        # random a delay between [0, 5]
+        delay = random.random() * 5
+        time.sleep(delay)
+        log.info(f"Sleep {delay}s")
+
+        env.neon_cli.create_branch('b{}'.format(i + 1), 'b{}'.format(i), tenant_id=tenant)
+        pgs.append(env.postgres.create_start('b{}'.format(i + 1), tenant_id=tenant))
+
+        threads.append(threading.Thread(target=run_pgbench, args=(pgs[-1], )))
+        threads[-1].start()
+
+    for thread in threads:
+        thread.join()
+
+    for pg in pgs:
+        res = pg.safe_psql('SELECT count(*) from pgbench_accounts')
+        assert res[0] == (100000 * scale, )
