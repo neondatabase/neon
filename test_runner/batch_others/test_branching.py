bc1c16f9eca433eae3be9d4df5fc7a97fe694a01
Thang Pham <thang@neon.tech>
2022-07-11 15:23:42 -0400

    update branching test: add flat branching test

--- test_runner/batch_others/test_branching.py
+++ test_runner/batch_others/test_branching.py
@@ -10,10 +10,12 @@ from performance.test_perf_pgbench import get_scales_matrix
 
 @pytest.mark.parametrize("n_branches", [10])
 @pytest.mark.parametrize("scale", get_scales_matrix(1))
-def test_cascade_branching_with_pgbench(neon_simple_env: NeonEnv,
-                                        pg_bin: PgBin,
-                                        n_branches: int,
-                                        scale: int):
+@pytest.mark.parametrize("ty", ["cascade", "flat"])
+def test_branching_with_pgbench(neon_simple_env: NeonEnv,
+                                pg_bin: PgBin,
+                                n_branches: int,
+                                scale: int,
+                                ty: str):
     env = neon_simple_env
 
     tenant, _ = env.neon_cli.create_tenant(
@@ -23,7 +25,6 @@ def test_cascade_branching_with_pgbench(neon_simple_env: NeonEnv,
              'checkpoint_distance': f'{1024 ** 2}',
              'compaction_target_size': f'{1024 ** 2}',
              'compaction_period': '5 s',
-             'compaction_threshold': '2',
              # set PITR interval to be small, so we can do GC
              'pitr_interval': '5 s'
          })
@@ -42,7 +43,7 @@ def test_cascade_branching_with_pgbench(neon_simple_env: NeonEnv,
     pgs.append(env.postgres.create_start('b0', tenant_id=tenant))
 
     threads: List[threading.Thread] = []
-    threads.append(threading.Thread(target=run_pgbench, args=(pgs[0], )))
+    threads.append(threading.Thread(target=run_pgbench, args=(pgs[0], ), daemon=True))
     threads[-1].start()
 
     for i in range(n_branches):
@@ -51,10 +52,14 @@ def test_cascade_branching_with_pgbench(neon_simple_env: NeonEnv,
         time.sleep(delay)
         log.info(f"Sleep {delay}s")
 
-        env.neon_cli.create_branch('b{}'.format(i + 1), 'b{}'.format(i), tenant_id=tenant)
+        if ty == "cascade":
+            env.neon_cli.create_branch('b{}'.format(i + 1), 'b{}'.format(i), tenant_id=tenant)
+        else:
+            env.neon_cli.create_branch('b{}'.format(i + 1), 'b0', tenant_id=tenant)
+
         pgs.append(env.postgres.create_start('b{}'.format(i + 1), tenant_id=tenant))
 
-        threads.append(threading.Thread(target=run_pgbench, args=(pgs[-1], )))
+        threads.append(threading.Thread(target=run_pgbench, args=(pgs[-1], ), daemon=True))
         threads[-1].start()
 
     for thread in threads:
