6a891c5921e98b70937131709ec378f5949fcd2e
Thang Pham <thang@neon.tech>
2022-07-14 14:30:32 -0400

    add test branch with many children

--- test_runner/performance/test_branching.py
+++ test_runner/performance/test_branching.py
@@ -7,11 +7,16 @@ import pytest
 from typing import List
 from fixtures.benchmark_fixture import MetricReport
 from fixtures.compare_fixtures import NeonCompare
-from fixtures.neon_fixtures import Postgres
 from fixtures.log_helper import log
+from performance.test_perf_pgbench import run_pgbench
 
 
 @pytest.mark.parametrize("n_branches", [20])
+# Test measures the latency of branch creation during a heavy [1] workload.
+#
+# [1]: to simulate a heavy workload, the test tweaks the GC and compaction settings
+# to increase the task's frequency. The test runs `pgbench` in each new branch.
+# Each branch is created from a randomly picked source branch.
 def test_branch_creation(neon_compare: NeonCompare, n_branches: int):
     env = neon_compare.env
     pg_bin = neon_compare.pg_bin
@@ -77,3 +82,23 @@ def test_branch_creation(neon_compare: NeonCompare, n_branches: int):
                                      statistics.stdev(branch_creation_durations),
                                      's',
                                      MetricReport.LOWER_IS_BETTER)
+
+
+@pytest.mark.parametrize("n_branches", [1024])
+def test_branch_with_many_children(neon_compare: NeonCompare, n_branches: int):
+    env = neon_compare.env
+
+    env.neon_cli.create_branch('b0')
+    env.neon_cli.create_branch('other')
+
+    pg0 = env.postgres.create_start('b0')
+    pg_other = env.postgres.create_start('other')
+
+    neon_compare.pg_bin.run_capture(['pgbench', '-i', '-s10', pg0.connstr()])
+    neon_compare.pg_bin.run_capture(['pgbench', '-i', '-s10', pg_other.connstr()])
+
+    for i in range(n_branches):
+        env.neon_cli.create_branch('b{}'.format(i + 1), 'b0')
+
+    run_pgbench(neon_compare, "many_children_branch", ['pgbench', '-c10', '-T10', pg0.connstr()])
+    run_pgbench(neon_compare, "no_children_branch", ['pgbench', '-c10', '-T10', pg_other.connstr()])
