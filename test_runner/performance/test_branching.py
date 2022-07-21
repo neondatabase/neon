f7d7aa04938b2f8e43ca662cf174ac9df6ca00e2
Thang Pham <thang@neon.tech>
2022-07-21 15:44:47 -0400

    add performance tests comparing child and root branches performance

---
+++ test_runner/performance/test_branching.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+import timeit
+from typing import List
+from fixtures.benchmark_fixture import PgBenchRunResult
+from fixtures.compare_fixtures import NeonCompare
+from performance.test_perf_pgbench import utc_now_timestamp
+
+
+def test_compare_child_and_root_pgbench_perf(neon_compare: NeonCompare):
+    env = neon_compare.env
+    pg_bin = neon_compare.pg_bin
+
+    def run_pgbench_on_branch(branch: str, cmd: List[str]):
+        run_start_timestamp = utc_now_timestamp()
+        t0 = timeit.default_timer()
+        out = pg_bin.run_capture(cmd, )
+        run_duration = timeit.default_timer() - t0
+        run_end_timestamp = utc_now_timestamp()
+
+        stdout = Path(f"{out}.stdout").read_text()
+
+        res = PgBenchRunResult.parse_from_stdout(
+            stdout=stdout,
+            run_duration=run_duration,
+            run_start_timestamp=run_start_timestamp,
+            run_end_timestamp=run_end_timestamp,
+        )
+        neon_compare.zenbenchmark.record_pg_bench_result(branch, res)
+
+    env.neon_cli.create_branch('root')
+    pg_root = env.postgres.create_start('root')
+    pg_bin.run_capture(['pgbench', '-i', pg_root.connstr(), '-s10'])
+
+    env.neon_cli.create_branch('child', 'root')
+    pg_child = env.postgres.create_start('child')
+
+    run_pgbench_on_branch('root', ['pgbench', '-c10', '-T10', pg_root.connstr()])
+    run_pgbench_on_branch('child', ['pgbench', '-c10', '-T10', pg_child.connstr()])
+
+
+def test_compare_child_and_root_write_perf(neon_compare: NeonCompare):
+    env = neon_compare.env
+    env.neon_cli.create_branch('root')
+    pg_root = env.postgres.create_start('root')
+
+    pg_root.safe_psql(
+        "CREATE TABLE foo(key serial primary key, t text default 'foooooooooooooooooooooooooooooooooooooooooooooooooooo')",
+    )
+
+    env.neon_cli.create_branch('child', 'root')
+    pg_child = env.postgres.create_start('child')
+
+    with neon_compare.record_duration("root_run_duration"):
+        pg_root.safe_psql("INSERT INTO foo SELECT FROM generate_series(1,1000000)")
+    with neon_compare.record_duration("child_run_duration"):
+        pg_child.safe_psql("INSERT INTO foo SELECT FROM generate_series(1,1000000)")
+
+
+def test_compare_child_and_root_read_perf(neon_compare: NeonCompare):
+    env = neon_compare.env
+    env.neon_cli.create_branch('root')
+    pg_root = env.postgres.create_start('root')
+
+    pg_root.safe_psql_many([
+        "CREATE TABLE foo(key serial primary key, t text default 'foooooooooooooooooooooooooooooooooooooooooooooooooooo')",
+        "INSERT INTO foo SELECT FROM generate_series(1,1000000)"
+    ])
+
+    env.neon_cli.create_branch('child', 'root')
+    pg_child = env.postgres.create_start('child')
+
+    with neon_compare.record_duration("root_run_duration"):
+        pg_root.safe_psql("SELECT count(*) from foo")
+    with neon_compare.record_duration("child_run_duration"):
+        pg_child.safe_psql("SELECT count(*) from foo")
