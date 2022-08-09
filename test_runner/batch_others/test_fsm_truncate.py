4227cfc96eda8ddde3c4b4df51efac6f0f1fc8c0
Konstantin Knizhnik <knizhnik@garret.ru>
2022-08-09 22:45:33 +0300

    Safe truncate (#2218)
    * Move relation sie cache to layered timeline
    
    * Fix obtaining current LSN for relation size cache
    
    * Resolve merge conflicts
    
    * Resolve merge conflicts
    
    * Reestore 'lsn' field in DatadirModification
    
    * adjust DatadirModification lsn in ingest_record
    
    * Fix formatting
    
    * Pass lsn to get_relsize
    
    * Fix merge conflict
    
    * Update pageserver/src/pgdatadir_mapping.rs
    
    Co-authored-by: Heikki Linnakangas <heikki@zenith.tech>
    
    * Update pageserver/src/pgdatadir_mapping.rs
    
    Co-authored-by: Heikki Linnakangas <heikki@zenith.tech>
    
    * Check if relation exists before trying to truncat it
    
    refer #1932
    
    * Add test reporducing FSM truncate problem
    
    Co-authored-by: Heikki Linnakangas <heikki@zenith.tech>

---
+++ test_runner/batch_others/test_fsm_truncate.py
@@ -0,0 +1,11 @@
+from fixtures.log_helper import log
+from fixtures.neon_fixtures import NeonEnv, NeonEnvBuilder, NeonPageserverHttpClient
+import pytest
+
+
+def test_fsm_truncate(neon_env_builder: NeonEnvBuilder):
+    env = neon_env_builder.init_start()
+    env.neon_cli.create_branch("test_fsm_truncate")
+    pg = env.postgres.create_start('test_fsm_truncate')
+    pg.safe_psql(
+        'CREATE TABLE t1(key int); CREATE TABLE t2(key int); TRUNCATE TABLE t1; TRUNCATE TABLE t2;')
