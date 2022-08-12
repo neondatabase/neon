414279726db3acf9c352b29815ebb77ceb118249
Bojan Serafimov <bojan.serafimov7@gmail.com>
2022-08-12 09:01:17 -0400

    Reproduce pageserver.pid lock on restart issue

--- test_runner/batch_others/test_pageserver_restart.py
+++ test_runner/batch_others/test_pageserver_restart.py
@@ -2,6 +2,19 @@ from fixtures.neon_fixtures import NeonEnvBuilder
 from fixtures.log_helper import log
 
 
+# Test that the pageserver fixture is implemented correctly, allowing quick restarts.
+# This is a regression test, see https://github.com/neondatabase/neon/issues/2247
+def test_fixture_restart(neon_env_builder: NeonEnvBuilder):
+    env = neon_env_builder.init_start()
+
+    env.neon_cli.create_branch('test_fixture_restart')
+    pg = env.postgres.create_start('test_fixture_restart')
+
+    for i in range(3):
+        env.pageserver.stop()
+        env.pageserver.start()
+
+
 # Test restarting page server, while safekeeper and compute node keep
 # running.
 def test_pageserver_restart(neon_env_builder: NeonEnvBuilder):
