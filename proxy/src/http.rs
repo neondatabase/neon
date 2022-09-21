e9a103c09f4e24a70697a3187419b4a51b024209
Dmitry Ivanov <ivadmi5@gmail.com>
2022-09-21 21:42:47 +0300

    [proxy] Pass extra parameters to the console (#2467)
    With this change we now pass additional params
    to the console's auth methods.

--- proxy/src/http.rs
+++ proxy/src/http.rs
@@ -1,27 +1,81 @@
-use anyhow::anyhow;
-use hyper::{Body, Request, Response, StatusCode};
-use std::net::TcpListener;
-use utils::http::{endpoint, error::ApiError, json::json_response, RouterBuilder, RouterService};
+pub mod server;
 
-async fn status_handler(_: Request<Body>) -> Result<Response<Body>, ApiError> {
-    json_response(StatusCode::OK, "")
+use crate::url::ApiUrl;
+
+/// Thin convenience wrapper for an API provided by an http endpoint.
+#[derive(Debug, Clone)]
+pub struct Endpoint {
+    /// API's base URL.
+    endpoint: ApiUrl,
+    /// Connection manager with built-in pooling.
+    client: reqwest::Client,
 }
 
-fn make_router() -> RouterBuilder<hyper::Body, ApiError> {
-    let router = endpoint::make_router();
-    router.get("/v1/status", status_handler)
-}
-
-pub async fn thread_main(http_listener: TcpListener) -> anyhow::Result<()> {
-    scopeguard::defer! {
-        println!("http has shut down");
+impl Endpoint {
+    /// Construct a new HTTP endpoint wrapper.
+    pub fn new(endpoint: ApiUrl, client: reqwest::Client) -> Self {
+        Self { endpoint, client }
     }
 
-    let service = || RouterService::new(make_router().build()?);
+    pub fn url(&self) -> &ApiUrl {
+        &self.endpoint
+    }
 
-    hyper::Server::from_tcp(http_listener)?
-        .serve(service().map_err(|e| anyhow!(e))?)
-        .await?;
+    /// Return a [builder](reqwest::RequestBuilder) for a `GET` request,
+    /// appending a single `path` segment to the base endpoint URL.
+    pub fn get(&self, path: &str) -> reqwest::RequestBuilder {
+        let mut url = self.endpoint.clone();
+        url.path_segments_mut().push(path);
+        self.client.get(url.into_inner())
+    }
 
-    Ok(())
+    /// Execute a [request](reqwest::Request).
+    pub async fn execute(
+        &self,
+        request: reqwest::Request,
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        self.client.execute(request).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn optional_query_params() -> anyhow::Result<()> {
+        let url = "http://example.com".parse()?;
+        let endpoint = Endpoint::new(url, reqwest::Client::new());
+
+        // Validate that this pattern makes sense.
+        let req = endpoint
+            .get("frobnicate")
+            .query(&[
+                ("foo", Some("10")), // should be just `foo=10`
+                ("bar", None),       // shouldn't be passed at all
+            ])
+            .build()?;
+
+        assert_eq!(req.url().as_str(), "http://example.com/frobnicate?foo=10");
+
+        Ok(())
+    }
+
+    #[test]
+    fn uuid_params() -> anyhow::Result<()> {
+        let url = "http://example.com".parse()?;
+        let endpoint = Endpoint::new(url, reqwest::Client::new());
+
+        let req = endpoint
+            .get("frobnicate")
+            .query(&[("session_id", uuid::Uuid::nil())])
+            .build()?;
+
+        assert_eq!(
+            req.url().as_str(),
+            "http://example.com/frobnicate?session_id=00000000-0000-0000-0000-000000000000"
+        );
+
+        Ok(())
+    }
 }
