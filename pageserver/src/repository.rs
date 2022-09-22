f3073a4db93e2d4e39e2bbef03ed6b742ef3afa0
Konstantin Knizhnik <knizhnik@garret.ru>
2022-09-22 08:35:06 +0300

    R-Tree layer map (#2317)
    Replace the layer array and linear search with R-tree
    
    So far, the in-memory layer map that holds information about layer
    files that exist, has used a simple Vec, in no particular order, to
    hold information about all the layers. That obviously doesn't scale
    very well; with thousands of layer files the linear search was
    consuming a lot of CPU. Replace it with a two-dimensional R-tree, with
    Key and LSN ranges as the dimensions.
    
    For the R-tree, use the 'rstar' crate. To be able to use that, we
    convert the Keys and LSNs into 256-bit integers. 64 bits would be
    enough to represent LSNs, and 128 bits would be enough to represent
    Keys. However, we use 256 bits, because rstar internally performs
    multiplication to calculate the area of rectangles, and the result of
    multiplying two 128 bit integers doesn't necessarily fit in 128 bits,
    causing integer overflow and, if overflow-checks are enabled,
    panic. To avoid that, we use 256 bit integers.
    
    Add a performance test that creates a lot of layer files, to
    demonstrate the benefit.

--- pageserver/src/repository.rs
+++ pageserver/src/repository.rs
@@ -24,6 +24,19 @@ pub struct Key {
 pub const KEY_SIZE: usize = 18;
 
 impl Key {
+    /// 'field2' is used to store tablespaceid for relations and small enum numbers for other relish.
+    /// As long as Neon does not support tablespace (because of lack of access to local file system),
+    /// we can assume that only some predefined namespace OIDs are used which can fit in u16
+    pub fn to_i128(&self) -> i128 {
+        assert!(self.field2 < 0xFFFF || self.field2 == 0xFFFFFFFF || self.field2 == 0x22222222);
+        (((self.field1 & 0xf) as i128) << 120)
+            | (((self.field2 & 0xFFFF) as i128) << 104)
+            | ((self.field3 as i128) << 72)
+            | ((self.field4 as i128) << 40)
+            | ((self.field5 as i128) << 32)
+            | self.field6 as i128
+    }
+
     pub fn next(&self) -> Key {
         self.add(1)
     }
