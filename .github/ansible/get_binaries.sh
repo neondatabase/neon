093264a69523c5f8f007b35cf26be4e0b11c1de9
Anastasia Lubennikova <anastasia@neon.tech>
2022-09-23 19:59:27 +0300

    Fix deploy bin and lib paths for postgres

--- .github/ansible/get_binaries.sh
+++ .github/ansible/get_binaries.sh
@@ -24,8 +24,8 @@ tar -xzf postgres_install.tar.gz -C neon_install
 docker cp ${ID}:/usr/local/bin/pageserver neon_install/bin/
 docker cp ${ID}:/usr/local/bin/safekeeper neon_install/bin/
 docker cp ${ID}:/usr/local/bin/proxy neon_install/bin/
-docker cp ${ID}:/usr/local/v14/bin/postgres neon_install/bin/v14
-docker cp ${ID}:/usr/local/v15/bin/postgres neon_install/bin/v15
+docker cp ${ID}:/usr/local/v14/bin/ neon_install/v14/bin/
+docker cp ${ID}:/usr/local/v15/bin/ neon_install/v15/bin/
 docker rm -vf ${ID}
 
 # store version to file (for ansible playbooks) and create binaries tarball
