975ab769fd2feb1eb427950078eb70c9055b315e
Sergey Melnikov <sergey@neon.tech>
2022-10-15 01:04:12 +0300

    Migrate safekeeper initialisation to management api v2

--- .github/ansible/scripts/init_safekeeper.sh
+++ .github/ansible/scripts/init_safekeeper.sh
@@ -25,8 +25,7 @@ EOF
 if ! curl -sf -X PATCH -d '{}' {{ console_mgmt_base_url }}/api/v1/safekeepers/${INSTANCE_ID} -o /dev/null; then
 
     # not registered, so register it now
-    ID=$(curl -sf -X POST {{ console_mgmt_base_url }}/api/v1/safekeepers -d@/tmp/payload | jq -r '.ID')
-
+    ID=$(curl -sf -X POST -H "Authorization: Bearer {{ CONSOLE_API_TOKEN }}" {{ console_mgmt_base_url }}/management/api/v2/safekeepers -d@/tmp/payload | jq -r '.id')
     # init safekeeper
     sudo -u safekeeper /usr/local/bin/safekeeper --id ${ID} --init -D /storage/safekeeper/data
 fi
