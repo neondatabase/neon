f03b7c345821083d1e6c91397b7eca06fb54a63b
Kirill Bulatov <kirill@neon.tech>
2022-10-15 01:55:31 +0300

    Bump regular dependencies (#2618)
    * etcd-client is not updated, since we plan to replace it with another client and the new version errors with some missing prost library error
    * clap had released another major update that requires changing every CLI declaration again, deserves a separate PR

--- libs/safekeeper_api/src/models.rs
+++ libs/safekeeper_api/src/models.rs
@@ -1,20 +1,23 @@
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
+
 use utils::{
     id::{NodeId, TenantId, TimelineId},
     lsn::Lsn,
 };
 
+#[serde_as]
 #[derive(Serialize, Deserialize)]
 pub struct TimelineCreateRequest {
-    #[serde(with = "serde_with::rust::display_fromstr")]
+    #[serde_as(as = "DisplayFromStr")]
     pub tenant_id: TenantId,
-    #[serde(with = "serde_with::rust::display_fromstr")]
+    #[serde_as(as = "DisplayFromStr")]
     pub timeline_id: TimelineId,
     pub peer_ids: Option<Vec<NodeId>>,
     pub pg_version: u32,
     pub system_id: Option<u64>,
     pub wal_seg_size: Option<u32>,
-    #[serde(with = "serde_with::rust::display_fromstr")]
+    #[serde_as(as = "DisplayFromStr")]
     pub commit_lsn: Lsn,
     // If not passed, it is assigned to the beginning of commit_lsn segment.
     pub local_start_lsn: Option<Lsn>,
