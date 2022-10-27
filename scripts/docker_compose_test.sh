245f216c5695c5699586b6d6a69f8be4e845ae0d
Kirill Bulatov <kirill@neon.tech>
2022-10-27 11:11:15 +0300

    Compose fixes

--- scripts/docker_compose_test.sh
+++ scripts/docker_compose_test.sh
@@ -6,16 +6,16 @@ set -eux -o pipefail
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 COMPOSE_FILE=$SCRIPT_DIR/../docker-compose/docker-compose.yml
 
-COMPUTE_CONTAINER_NAME=dockercompose_compute_1
+COMPUTE_CONTAINER_NAME=docker-compose-compute-1
 SQL="CREATE TABLE t(key int primary key, value text); insert into t values(1,1); select * from t;"
 PSQL_OPTION="-h localhost -U cloud_admin -p 55433 -c '$SQL' postgres"
 
 cleanup() {
 	echo "show container information"
 	docker ps
-	docker-compose -f $COMPOSE_FILE logs
+	docker compose -f $COMPOSE_FILE logs
 	echo "stop containers..."
-	docker-compose -f $COMPOSE_FILE down
+	docker compose -f $COMPOSE_FILE down
 }
 
 echo "clean up containers if exists"
@@ -23,7 +23,7 @@ cleanup
 
 for pg_version in 14 15; do
 	echo "start containers (pg_version=$pg_version)."
-	PG_VERSION=$pg_version TAG=latest docker-compose -f $COMPOSE_FILE up --build -d
+	PG_VERSION=$pg_version TAG=latest docker compose -f $COMPOSE_FILE up --build -d
 
 	echo "wait until the compute is ready. timeout after 60s. "
 	cnt=0
@@ -38,12 +38,12 @@ for pg_version in 14 15; do
 
 		# check if the compute is ready
 		set +o pipefail
-		result=`docker-compose -f $COMPOSE_FILE logs "compute_is_ready" | grep "accepting connections" | wc -l`
+		result=`docker compose -f $COMPOSE_FILE logs "compute_is_ready" | grep "accepting connections" | wc -l`
 		set -o pipefail
 		if [ $result -eq 1 ]; then
 			echo "OK. The compute is ready to connect."
 			echo "execute simple queries."
-			docker exec -it $COMPUTE_CONTAINER_NAME /bin/bash -c "psql $PSQL_OPTION"
+			docker exec --rm $COMPUTE_CONTAINER_NAME /bin/bash -c "psql $PSQL_OPTION"
 			cleanup
 			break
 		fi
