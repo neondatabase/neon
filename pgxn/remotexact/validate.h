d74cc7f5ec63959cf0f4aa157a7da6fe887c678e
Cuong Nguyen <ctring23@gmail.com>
2022-11-02 00:54:40 -0400

    Function to validate table scan

---
+++ pgxn/remotexact/validate.h
@@ -0,0 +1,18 @@
+/*-------------------------------------------------------------------------
+ *
+ * validate.h
+ *
+ * contrib/remotexact/validate.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef VALIDATE_H
+#define VALIDATE_H
+
+#include "postgres.h"
+
+#include "utils/snapshot.h"
+
+void validate_table_scan(Oid relid, XidCSN read_csn);
+
+#endif							/* VALIDATE_H */
