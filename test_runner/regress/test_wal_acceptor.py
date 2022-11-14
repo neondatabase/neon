d0105cea1f0f774c0521578948ff6ec4d40c2774
Heikki Linnakangas <heikki@neon.tech>
2022-11-14 16:03:41 +0200

    Avoid errors when removing a timeline that's still active

--- test_runner/regress/test_wal_acceptor.py
+++ test_runner/regress/test_wal_acceptor.py
@@ -538,6 +538,7 @@ def test_s3_wal_replay(neon_env_builder: NeonEnvBuilder, remote_storage_kind: Re
     )
 
     pg.stop_and_destroy()
+    ps_cli.timeline_delete(tenant_id, timeline_id)
 
     # Also delete and manually create timeline on safekeepers -- this tests
     # scenario of manual recovery on different set of safekeepers.
@@ -562,7 +563,6 @@ def test_s3_wal_replay(neon_env_builder: NeonEnvBuilder, remote_storage_kind: Re
         shutil.copy(f_partial_saved, f_partial_path)
 
     # recreate timeline on pageserver from scratch
-    ps_cli.timeline_delete(tenant_id, timeline_id)
     ps_cli.timeline_create(tenant_id, timeline_id)
 
     wait_lsn_timeout = 60 * 3
