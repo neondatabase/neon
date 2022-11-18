65a18a0e2ab31309c7296723003cca8997e7593a
Konstantin Knizhnik <knizhnik@garret.ru>
2022-11-18 11:53:03 +0200

    Use wait_for_last_flush_lsn instead of sleep in test_bulk_update.py

--- test_runner/performance/test_bulk_update.py
+++ test_runner/performance/test_bulk_update.py
@@ -1,7 +1,9 @@
 import time
 
-from fixtures.neon_fixtures import NeonEnvBuilder
-
+from fixtures.neon_fixtures import (
+    NeonEnvBuilder,
+    wait_for_last_flush_lsn,
+)
 
 #
 # Benchmark estimating effect of prefetch on bulk update operations
@@ -11,7 +13,8 @@ def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark):
     env = neon_env_builder.init_start()
     n_records = 10000000
 
-    env.neon_cli.create_branch("test_bulk_update")
+    timeline_id = env.neon_cli.create_branch("test_bulk_update")
+    tenant_id = env.initial_tenant
     pg = env.postgres.create_start("test_bulk_update")
     cur = pg.connect().cursor()
     cur.execute("set statement_timeout=0")
@@ -22,13 +25,13 @@ def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark):
         cur.execute(f"insert into t values (generate_series(1,{n_records}))")
 
     cur.execute("vacuum t")
-    time.sleep(10)  # wait until pageserver catch-up
+    wait_for_last_flush_lsn(env, pg, tenant_id, timeline_id)
 
     with zenbenchmark.record_duration("update-no-prefetch"):
         cur.execute("update t set x=x+1")
 
     cur.execute("vacuum t")
-    time.sleep(10)  # wait until pageserver catch-up
+    wait_for_last_flush_lsn(env, pg, tenant_id, timeline_id)
 
     with zenbenchmark.record_duration("delete-no-prefetch"):
         cur.execute("delete from t")
@@ -43,13 +46,13 @@ def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark):
         cur.execute(f"insert into t2 values (generate_series(1,{n_records}))")
 
     cur.execute("vacuum t2")
-    time.sleep(10)  # wait until pageserver catch-up
+    wait_for_last_flush_lsn(env, pg, tenant_id, timeline_id)
 
     with zenbenchmark.record_duration("update-with-prefetch"):
         cur.execute("update t2 set x=x+1")
 
     cur.execute("vacuum t2")
-    time.sleep(10)  # wait until pageserver catch-up
+    wait_for_last_flush_lsn(env, pg, tenant_id, timeline_id)
 
     with zenbenchmark.record_duration("delete-with-prefetch"):
         cur.execute("delete from t2")
