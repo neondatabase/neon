704419f348fa2b261450bc94884b0677e0850911
Konstantin Knizhnik <knizhnik@garret.ru>
2022-11-18 15:27:08 +0200

    Run test_bulkupdate test with different fill factors

--- test_runner/performance/test_bulk_update.py
+++ test_runner/performance/test_bulk_update.py
@@ -1,4 +1,4 @@
-import time
+import pytest
 
 from fixtures.neon_fixtures import (
     NeonEnvBuilder,
@@ -8,10 +8,12 @@ from fixtures.neon_fixtures import (
 #
 # Benchmark estimating effect of prefetch on bulk update operations
 #
-def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark):
+@pytest.mark.timeout(10000)
+@pytest.mark.parametrize("fillfactor", [10,50,100])
+def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark, fillfactor):
 
     env = neon_env_builder.init_start()
-    n_records = 10000000
+    n_records = 1000000
 
     timeline_id = env.neon_cli.create_branch("test_bulk_update")
     tenant_id = env.initial_tenant
@@ -19,7 +21,7 @@ def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark):
     cur = pg.connect().cursor()
     cur.execute("set statement_timeout=0")
 
-    cur.execute("create table t(x integer)")
+    cur.execute(f"create table t(x integer) WITH (fillfactor={fillfactor})")
 
     with zenbenchmark.record_duration("insert-1"):
         cur.execute(f"insert into t values (generate_series(1,{n_records}))")
@@ -38,9 +40,9 @@ def test_bulk_update(neon_env_builder: NeonEnvBuilder, zenbenchmark):
 
     cur.execute("drop table t")
     cur.execute("set enable_seqscan_prefetch=on")
-    cur.execute("set seqscan_prefetch_buffers=10")
+    cur.execute("set seqscan_prefetch_buffers=100")
 
-    cur.execute("create table t2(x integer)")
+    cur.execute(f"create table t2(x integer) WITH (fillfactor={fillfactor})")
 
     with zenbenchmark.record_duration("insert-2"):
         cur.execute(f"insert into t2 values (generate_series(1,{n_records}))")
