f476e56315ab373e08c1131160ab0c376c1b764f
Bojan Serafimov <bojan.serafimov7@gmail.com>
2022-12-01 21:58:34 -0500

    Actually implement

--- pageserver/src/tenant/bst_layer_map.rs
+++ pageserver/src/tenant/bst_layer_map.rs
@@ -3,9 +3,22 @@ use std::collections::BTreeMap;
 use rpds::RedBlackTreeMap;
 
 
+/// Layer map implemented using persistent binary search tree.
+/// This implementation is only good enough to run benchmarks,
+/// so it's missing unnecessary details. Values are String for now.
 pub struct BSTLM {
-    head: RedBlackTreeMap<u32, String>,
-    historic: BTreeMap<u32, RedBlackTreeMap<u32, String>>,
+    /// Mapping key to the latest layer (if any) until the next key
+    head: RedBlackTreeMap<u32, Option<String>>,
+
+    /// All previous states of `self.head`
+    historic: BTreeMap<u32, RedBlackTreeMap<u32, Option<String>>>,
+}
+
+impl std::fmt::Debug for BSTLM {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let head_vec: Vec<_> = self.head.iter().collect();
+        write!(f, "BSTLM: head: {:?}", head_vec)
+    }
 }
 
 /// Layer map (good enough for benchmarks) implemented using persistent segment tree
@@ -18,30 +31,65 @@ impl BSTLM {
     }
 
     pub fn insert(self: &mut Self, key_begin: u32, key_end: u32, lsn: u32, value: String) {
-        self.head.insert_mut(key_begin, value.clone());
-        self.head.insert_mut(key_end, value.clone()); // TODO wrong value
-        // TODO also remove what's in between
-        self.historic.insert(lsn, self.head.clone());  // O(1) clone
+        // TODO check for off-by-one errors
+
+        // NOTE The order of the following lines is important!!
+
+        // Preserve information after right endpoint
+        let value_at_end = match self.head.range(0..key_end).last() {
+            Some((_, Some(v))) => Some(v.clone()),
+            Some((_, None)) => None,
+            None => None,
+        };
+        self.head.insert_mut(key_end, value_at_end);
+
+        // Insert the left endpoint
+        self.head.insert_mut(key_begin, Some(value.clone()));
+
+        // Cover the inside of the interval
+        let to_remove: Vec<_> = self.head.range((key_begin + 1)..key_end)
+            .map(|(k, _)| k.clone())
+            .collect();
+        for key in to_remove {
+            self.head.remove_mut(&key);
+        }
+
+        // Remember history. Clone is O(1)
+        self.historic.insert(lsn, self.head.clone());
     }
 
     pub fn query(self: &Self, key: u32, lsn: u32) -> Option<&String> {
+        // TODO check for off-by-one errors
+
         let version = self.historic.range(0..=lsn).rev().next()?.1;
-        Some(version.range(key..).next()?.1)
+        version.range(0..=key).rev().next()?.1.as_ref()
     }
+
+    // TODO add API for delta layers with lsn range
 }
 
-
-fn test_bstlm() {
-    let mut bstlm = BSTLM::new();
-    bstlm.insert(0, 5, 100, "layer 1".to_string());
-    bstlm.insert(3, 9, 110, "layer 2".to_string());
-
-    dbg!(bstlm.query(1, 105));
-    dbg!(bstlm.query(4, 105));
-    dbg!(bstlm.query(4, 115));
-}
 
 #[test]
-fn test_bstlm_() {
-    test_bstlm()
+fn test_bstlm() {
+    let mut bstlm = BSTLM::new();
+    bstlm.insert(0, 5, 100, "Layer 1".to_string());
+    dbg!(&bstlm);
+    bstlm.insert(3, 9, 110, "Layer 2".to_string());
+    dbg!(&bstlm);
+    bstlm.insert(5, 6, 120, "Layer 3".to_string());
+    dbg!(&bstlm);
+
+    // After Layer 1 insertion
+    assert_eq!(bstlm.query(1, 105), Some(&"Layer 1".to_string()));
+    assert_eq!(bstlm.query(4, 105), Some(&"Layer 1".to_string()));
+
+    // After Layer 2 insertion
+    assert_eq!(bstlm.query(4, 115), Some(&"Layer 2".to_string()));
+    assert_eq!(bstlm.query(8, 115), Some(&"Layer 2".to_string()));
+    assert_eq!(bstlm.query(11, 115), None);
+
+    // After Layer 3 insertion
+    assert_eq!(bstlm.query(4, 125), Some(&"Layer 2".to_string()));
+    assert_eq!(bstlm.query(5, 125), Some(&"Layer 3".to_string()));
+    assert_eq!(bstlm.query(7, 125), Some(&"Layer 2".to_string()));
 }
