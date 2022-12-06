2b6e8df006bc9dc4402a399635e4bb2f64ccbff1
Kirill Bulatov <kirill@neon.tech>
2022-12-06 20:51:46 +0200

    Make the request tracing test more useful

--- test_runner/regress/test_read_trace.py
+++ test_runner/regress/test_read_trace.py
@@ -1,10 +1,14 @@
 from contextlib import closing
 
-from fixtures.neon_fixtures import NeonEnvBuilder
+from fixtures.neon_fixtures import NeonEnvBuilder, wait_for_last_record_lsn
+from fixtures.types import Lsn, TenantId, TimelineId
+from fixtures.utils import query_scalar
 
 
 # This test demonstrates how to collect a read trace. It's useful until
 # it gets replaced by a test that actually does stuff with the trace.
+#
+# Additionally, tests that pageserver is able to create tenants with custom configs.
 def test_read_request_tracing(neon_env_builder: NeonEnvBuilder):
     neon_env_builder.num_safekeepers = 1
     env = neon_env_builder.init_start()
@@ -23,6 +27,12 @@ def test_read_request_tracing(neon_env_builder: NeonEnvBuilder):
             cur.execute("create table t (i integer);")
             cur.execute(f"insert into t values (generate_series(1,{10000}));")
             cur.execute("select count(*) from t;")
+            tenant_id = TenantId(pg.safe_psql("show neon.tenant_id")[0][0])
+            timeline_id = TimelineId(pg.safe_psql("show neon.timeline_id")[0][0])
+            current_lsn = Lsn(query_scalar(cur, "SELECT pg_current_wal_flush_lsn()"))
+    # wait until pageserver receives that data
+    pageserver_http = env.pageserver.http_client()
+    wait_for_last_record_lsn(pageserver_http, tenant_id, timeline_id, current_lsn)
 
     # Stop pg so we drop the connection and flush the traces
     pg.stop()
