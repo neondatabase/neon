f51499695e3f31fba5077fb84dd1b9a0b87154ed
Konstantin Knizhnik <knizhnik@garret.ru>
2022-12-07 21:59:35 +0200

    Pass base image content and LSN as pair to request_redo

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -2642,33 +2642,22 @@ impl Timeline {
                     data.records.len()
                 );
             } else {
-                let mut base_img_lsn = Lsn(0);
-                let base_img = if let Some((img_lsn, img)) = data.img {
-                    base_img_lsn = img_lsn;
+                if data.img.is_some() {
                     trace!(
                         "found {} WAL records and a base image for {} at {}, performing WAL redo",
                         data.records.len(),
                         key,
                         request_lsn
                     );
-                    Some(img)
                 } else {
                     trace!("found {} WAL records that will init the page for {} at {}, performing WAL redo", data.records.len(), key, request_lsn);
-                    None
                 };
 
                 let last_rec_lsn = data.records.last().unwrap().0;
 
                 let img = self
                     .walredo_mgr
-                    .request_redo(
-                        key,
-                        request_lsn,
-                        base_img,
-                        base_img_lsn,
-                        data.records,
-                        self.pg_version,
-                    )
+                    .request_redo(key, request_lsn, data.img, data.records, self.pg_version)
                     .context("Failed to reconstruct a page image:")?;
 
                 if img.len() == page_cache::PAGE_SZ {
