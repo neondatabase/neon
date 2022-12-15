e14a39ce9fec76c6474f94f1cdb1c9a30e918080
Joonas Koivunen <joonas@neon.tech>
2022-12-15 17:31:35 +0200

    pthread_cond support

--- libs/shmempipe/src/shared.rs
+++ libs/shmempipe/src/shared.rs
@@ -219,12 +219,206 @@ mod pthread {
         }
     }
 
+    /// Following std implementation, but in-place.
+    struct Condvar {
+        inner: libc::pthread_cond_t,
+        /// Used to check against "using different mutexes is UB".
+        mutex: std::sync::atomic::AtomicPtr<libc::pthread_mutex_t>,
+        _marker: std::marker::PhantomPinned,
+    }
+
+    impl Condvar {
+        pub(super) fn initialize_at(
+            place: &mut MaybeUninit<Self>,
+        ) -> std::io::Result<Pin<&mut Self>> {
+            let mut attr = MaybeUninit::<libc::pthread_condattr_t>::uninit();
+            unsafe { libc::pthread_condattr_init(attr.as_mut_ptr()) };
+            let mut attr = CondAttr(&mut attr);
+
+            // there are others in rust std
+            #[cfg(not(target_os = "macos"))]
+            attr.set_monotonic_clock();
+
+            // maybe this does not have to go through maybeuninit, since the aggregate is already
+            // maybeuninit?
+            let target = unsafe { std::ptr::addr_of_mut!((*place.as_mut_ptr()).inner) };
+
+            let res = unsafe { libc::pthread_cond_init(target, attr.0.as_mut_ptr()) };
+            assert_eq!(res, 0);
+
+            drop(attr);
+
+            let mutex = unsafe { std::ptr::addr_of_mut!((*place.as_mut_ptr()).mutex) };
+            unsafe { mutex.write(std::sync::atomic::AtomicPtr::default()) };
+
+            // Safety: all fields are initialized
+            unsafe { Ok(Pin::new_unchecked(place.assume_init_mut())) }
+        }
+
+        fn verify(self: Pin<&Self>, mutex: *mut libc::pthread_mutex_t) {
+            use std::sync::atomic::Ordering::Relaxed;
+            // Relaxed is enough for this check which can be raced by other threads, no additional
+            // effects are depended on
+            match self
+                .mutex
+                .compare_exchange(std::ptr::null_mut(), mutex, Relaxed, Relaxed)
+            {
+                Ok(_) => {}
+                Err(same) if same == mutex => {}
+                Err(_other) => panic!("misusing same condvar with different mutexes"),
+            }
+        }
+
+        fn inner(self: Pin<&Self>) -> *mut libc::pthread_cond_t {
+            // Safety: libc fns expect *mut
+            &self.inner as *const _ as *mut _
+        }
+
+        pub(super) fn notify_one(self: Pin<&Self>) {
+            let inner = self.inner();
+            let res = unsafe { libc::pthread_cond_signal(inner) };
+            cvt_nz(res).expect("notify_one failed");
+        }
+
+        pub(super) fn notify_all(self: Pin<&Self>) {
+            let inner = self.inner();
+            let res = unsafe { libc::pthread_cond_broadcast(inner) };
+            cvt_nz(res).expect("notify_all failed");
+        }
+
+        pub(super) fn wait(self: Pin<&Self>, mutex: Pin<&Mutex>) {
+            let mutex = mutex.inner();
+            self.verify(mutex);
+            let inner = self.inner();
+            let res = unsafe { libc::pthread_cond_wait(inner, mutex) };
+            cvt_nz(res).expect("wait failed");
+        }
+
+        #[cfg(not(target_os = "macos"))]
+        pub(super) fn wait_timeout(
+            self: Pin<&Self>,
+            dur: std::time::Duration,
+            mutex: Pin<&Mutex>,
+        ) -> bool {
+            let mutex = mutex.inner();
+            self.verify(mutex);
+
+            #[cfg(target_pointer_width = "32")]
+            todo!("check all the abi complications with x64_32 or other 32-bit");
+
+            // FIXME: this is not y2038 safe! rust std uses clock_gettime64 through a weak symbol
+            // which requires some ABI tricks, but libc does not expose it.
+            let mut now = libc::timespec {
+                tv_sec: 0,
+                // range 0..999_999_999
+                tv_nsec: 0,
+            };
+
+            // it is a shame one cannot reuse std's implementations for these.
+            // std uses clock_gettime64, but nix doesn't expose it either as of writing this
+            let res = unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut now as *mut _) };
+            assert_eq!(res, 0);
+
+            let until = libc::timespec::from(
+                Timespec::from(now)
+                    .checked_add_duration(dur)
+                    .unwrap_or(Timespec::MAX),
+            );
+
+            let inner = self.inner();
+            let res = unsafe { libc::pthread_cond_timedwait(inner, mutex, &until as *const _) };
+
+            if res != 0 && res != libc::ETIMEDOUT {
+                cvt_nz(res).expect("wait_timeout failed");
+            }
+
+            res == 0
+        }
+    }
+
+    // This and all of it's functionality is after rust std: library/std/src/sys/unix/time.rs
+    #[derive(Clone, Copy)]
+    struct Timespec {
+        tv_sec: i64,
+        tv_nsec: u32,
+    }
+
+    impl From<libc::timespec> for Timespec {
+        fn from(ts: libc::timespec) -> Self {
+            let ns = if ts.tv_nsec < 1_000_000_000 {
+                ts.tv_nsec as u32
+            } else {
+                panic!("libc::timespec.tv_nsec out of range: {}", ts.tv_nsec);
+            };
+            Timespec {
+                tv_sec: ts.tv_sec,
+                tv_nsec: ns,
+            }
+        }
+    }
+
+    impl From<Timespec> for libc::timespec {
+        fn from(ts: Timespec) -> Self {
+            libc::timespec {
+                tv_sec: ts.tv_sec,
+                tv_nsec: ts.tv_nsec.try_into().unwrap(),
+            }
+        }
+    }
+
+    impl Timespec {
+        const MAX: Timespec = Timespec {
+            tv_sec: libc::time_t::MAX,
+            tv_nsec: 999_999_999,
+        };
+
+        fn checked_add_duration(&self, dur: std::time::Duration) -> Option<Self> {
+            let mut secs = dur
+                .as_secs()
+                .try_into()
+                .ok()
+                .and_then(|secs: i64| secs.checked_add(self.tv_sec))?;
+
+            let mut nsecs = self.tv_nsec + dur.subsec_nanos();
+
+            if nsecs >= 1_000_000_000 {
+                nsecs -= 1_000_000_000;
+                secs = secs.checked_add(1)?;
+            }
+
+            Some(Timespec {
+                tv_sec: secs,
+                tv_nsec: nsecs,
+            })
+        }
+    }
+
+    struct CondAttr<'a>(&'a mut MaybeUninit<libc::pthread_condattr_t>);
+
+    impl CondAttr<'_> {
+        /// Rust std sets this on linux, perhaps others. Does not set it on mac.
+        #[cfg(not(target_os = "macos"))]
+        fn set_monotonic_clock(&mut self) {
+            let res = unsafe {
+                libc::pthread_condattr_setclock(self.0.as_mut_ptr(), libc::CLOCK_MONOTONIC)
+            };
+            assert_eq!(res, 0);
+        }
+    }
+
+    impl Drop for CondAttr<'_> {
+        fn drop(&mut self) {
+            let res = unsafe { libc::pthread_condattr_destroy(self.0.as_mut_ptr()) };
+            assert_eq!(res, 0);
+        }
+    }
+
     #[test]
     fn smoke_local() {
         let mut m = MaybeUninit::uninit();
         let m = Mutex::initialize_at(&mut m).unwrap();
         let m = &*m;
-        let m = unsafe { std::pin::Pin::new_unchecked(m) };
+        let m = unsafe { Pin::new_unchecked(m) };
         assert!(matches!(m.lock(), Locked::Ok));
         m.unlock();
         assert!(matches!(m.try_lock(), Some(Locked::Ok)));
@@ -232,42 +426,89 @@ mod pthread {
 
         // now we cannot send the same mutex to another thread, nor does our rust version have
         // scoped threads...
+
+        let mut cv = MaybeUninit::uninit();
+        let cv = Condvar::initialize_at(&mut cv).unwrap();
+        let cv = &*cv;
+        let cv = unsafe { Pin::new_unchecked(cv) };
+
+        m.lock();
+        let started_at = std::time::Instant::now();
+        let woken_up = cv.wait_timeout(std::time::Duration::from_millis(1), m);
+        let elapsed = started_at.elapsed();
+        m.unlock();
+
+        // 50% is close enough
+        assert!((elapsed.as_secs_f64() - 0.001).abs() < 0.0005);
+        assert!(!woken_up);
     }
 
     #[test]
     fn smoke_threaded() {
+        use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
         use std::sync::Arc;
 
         // we cannot do this through Box because Arc allocates and moves the contents
-        let mut m = Arc::new(MaybeUninit::uninit());
+        let mut pair: Arc<MaybeUninit<(Mutex, Condvar)>> = Arc::new(MaybeUninit::uninit());
+        // condition value updated only when lock is held
+        let fake_condition = Arc::new(AtomicUsize::new(0));
 
         {
-            let only = Arc::get_mut(&mut m).unwrap();
-            Mutex::initialize_at(only).unwrap();
+            let only = Arc::get_mut(&mut pair).unwrap();
+
+            let mutex = unsafe {
+                std::ptr::addr_of_mut!((*only.as_mut_ptr()).0)
+                    .cast::<MaybeUninit<Mutex>>()
+                    .as_mut()
+                    .unwrap()
+            };
+            Mutex::initialize_at(mutex).unwrap();
+
+            let cv = unsafe {
+                std::ptr::addr_of_mut!((*only.as_mut_ptr()).1)
+                    .cast::<MaybeUninit<Condvar>>()
+                    .as_mut()
+                    .unwrap()
+            };
+            Condvar::initialize_at(cv).unwrap();
         }
 
         // sadly also the Arc::assume_init is unstable
 
         let spawned = {
-            let m = m.clone();
+            let pair = pair.clone();
+            let fake_condition = fake_condition.clone();
             move || {
-                let inner = &*m;
+                let pair = &*pair;
                 // Safety: it was initialized
-                let m = unsafe { inner.assume_init_ref() };
+                let pair = unsafe { pair.assume_init_ref() };
                 // Safety: Arc is StableDeref, nor can mutable refs be created right now at least
-                let m = unsafe { std::pin::Pin::new_unchecked(m) };
+                let m = unsafe { std::pin::Pin::new_unchecked(&pair.0) };
+                let cv = unsafe { std::pin::Pin::new_unchecked(&pair.1) };
+
                 assert!(matches!(m.lock(), Locked::Ok));
                 // using print is actually unsafe without a dropguard
                 println!("hello, from other side");
+
+                cv.notify_all();
+
+                fake_condition.store(1, SeqCst);
+
+                while fake_condition.load(SeqCst) < 1 {
+                    println!("checking condition");
+                    cv.wait(m);
+                }
+
                 m.unlock();
             }
         };
 
-        let inner = &*m;
+        let pair = &*pair;
         // Safety: see above
-        let m = unsafe { inner.assume_init_ref() };
+        let pair = unsafe { pair.assume_init_ref() };
         // Safety: see above
-        let m = unsafe { std::pin::Pin::new_unchecked(m) };
+        let m = unsafe { std::pin::Pin::new_unchecked(&pair.0) };
+        let cv = unsafe { std::pin::Pin::new_unchecked(&pair.1) };
 
         assert!(matches!(m.lock(), Locked::Ok));
 
@@ -277,10 +518,17 @@ mod pthread {
 
         std::thread::sleep(std::time::Duration::from_millis(100));
 
-        m.unlock();
-        jh.join().unwrap();
+        while fake_condition.load(SeqCst) == 0 {
+            cv.wait(m);
+        }
 
-        println!("joined");
+        fake_condition.store(2, SeqCst);
+
+        cv.notify_all();
+
+        m.unlock();
+
+        jh.join().unwrap();
     }
 }
 
@@ -292,7 +540,7 @@ mod parking_lot_shim {
     use super::Locked;
 
     use parking_lot::lock_api::RawMutex as _;
-    use parking_lot::RawMutex;
+    use parking_lot::{Condvar as PLCondvar, RawMutex};
 
     pub struct Mutex(RawMutex, std::marker::PhantomPinned);
 
@@ -328,6 +576,36 @@ mod parking_lot_shim {
     impl Drop for Mutex {
         fn drop(&mut self) {
             // dunno how to make this not optimized out
+        }
+    }
+
+    struct Condvar(PLCondvar, std::marker::PhantomPinned);
+
+    impl Condvar {
+        pub(super) fn initialize_at(
+            place: &mut MaybeUninit<Condvar>,
+        ) -> std::io::Result<Pin<&mut Condvar>> {
+            let condvar = unsafe { std::ptr::addr_of_mut!((*place.as_mut_ptr()).0) };
+
+            unsafe { condvar.write(PLCondvar::new()) };
+
+            unsafe { Ok(Pin::new_unchecked(condvar.assume_init_mut())) }
+        }
+
+        pub(super) fn notify_one(self: Pin<&Self>) {
+            self.0.notify_one();
+        }
+
+        pub(super) fn notify_all(self: Pin<&Self>) {
+            self.0.notify_all();
+        }
+
+        pub(super) fn wait(self: Pin<&Self>, mutex: Pin<&Mutex>) {
+            todo!("this cannot be implemented by shimming, need full reimpl of condvar because MutexGuard is expected")
+        }
+
+        pub(super) fn wait_timeout(self: Pin<&Self>, mutex: Pin<&Mutex>) -> bool {
+            todo!("this cannot be implemented by shimming, need full reimpl of condvar because MutexGuard is expected")
         }
     }
 }
