308d0e1f4406f8ece625ce910547de45f3f3e7b9
Cuong Nguyen <ctring23@gmail.com>
2023-01-17 05:11:16 -0500

    Disable track_count to avoid auto ANALYZE

--- docker-compose/compute_wrapper/shell/compute-multi-region.sh
+++ docker-compose/compute_wrapper/shell/compute-multi-region.sh
@@ -13,17 +13,25 @@ done
 echo "Page server is ready."
 
 tenant_id=$(curl -s http://${PAGESERVER_HOST}:9898/v1/tenant/ | jq -r '.[0].id')
+if [ $tenant_id = "null" ]; then
+     echo "ERROR: Tenant id is null"
+     exit 1
+fi
 
-echo "Tenant id: ${tenant_id}"
 timelines=$(curl -s http://${PAGESERVER_HOST}:9898/v1/tenant/${tenant_id}/timeline/)
-
 region_id_to_timeline_id=$(echo ${timelines} | jq '[.[] | {key: .region_id, value: .timeline_id}] | from_entries')
 
 timeline_id=$(echo ${region_id_to_timeline_id} | jq -r ".[\"${REGION}\"]")
+if [ $timeline_id = "null" ]; then
+     echo "ERROR: Timeline id is null"
+     exit 1
+fi
 
-echo "Selected timeline ${timeline_id} for region ${REGION}"
-echo "Overwrite tenant id and timeline id in spec file"
+echo "Region id: ${REGION}"
+echo "Tenant id: ${tenant_id}"
+echo "Timeline id: ${timeline_id}"
 
+echo "Overwrite variables in spec file"
 sed "s/TENANT_ID/${tenant_id}/" ${SPEC_FILE_ORG} > ${SPEC_FILE}
 sed -i "s/TIMELINE_ID/${timeline_id}/" ${SPEC_FILE}
 sed -i "s/SAFEKEEPERS_ADDR/${SAFEKEEPERS_ADDR}/" ${SPEC_FILE}
