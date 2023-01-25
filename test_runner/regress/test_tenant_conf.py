ccd8dbbee527379285a37a6903c8637c6d872ee1
Joonas Koivunen <joonas@neon.tech>
2023-01-25 17:40:16 +0200

    test: reproduce config issue for attached tenants

--- test_runner/regress/test_tenant_conf.py
+++ test_runner/regress/test_tenant_conf.py
@@ -1,8 +1,16 @@
+import shutil
 from contextlib import closing
+from pathlib import Path
 
 import psycopg2.extras
 from fixtures.log_helper import log
-from fixtures.neon_fixtures import NeonEnvBuilder
+from fixtures.neon_fixtures import (
+    LocalFsStorage,
+    NeonEnvBuilder,
+    RemoteStorageKind,
+    assert_tenant_status,
+)
+from fixtures.utils import wait_until
 
 
 def test_tenant_config(neon_env_builder: NeonEnvBuilder):
@@ -158,3 +166,53 @@ tenant_config={checkpoint_distance = 10000, compaction_target_size = 1048576}"""
                     "pitr_interval": 60,
                 }.items()
             )
+
+
+def test_creating_tenant_conf_after_attach(neon_env_builder: NeonEnvBuilder):
+    neon_env_builder.enable_remote_storage(
+        remote_storage_kind=RemoteStorageKind.LOCAL_FS,
+        test_name="test_creating_tenant_conf_after_attach",
+    )
+
+    env = neon_env_builder.init_start()
+    assert isinstance(env.remote_storage, LocalFsStorage)
+
+    # tenant is created with defaults, as in without config file
+    (tenant_id, timeline_id) = env.neon_cli.create_tenant()
+    config_path = env.repo_dir / "tenants" / str(tenant_id) / "config"
+    assert config_path.exists(), "config file is always initially created"
+
+    env.pageserver.stop()
+
+    remote_dir = (
+        env.remote_storage.root / "tenants" / str(tenant_id) / "timelines" / str(timeline_id)
+    )
+    found_any = False
+    for path in Path.iterdir(remote_dir):
+        if not path.is_file():
+            continue
+        found_any = True
+        break
+
+    assert found_any, "should had have files under timeline directory"
+
+    # now prepare for attaching it once more
+    shutil.rmtree(Path(env.repo_dir) / "tenants" / str(tenant_id))
+
+    env.pageserver.start()
+    http_client = env.pageserver.http_client()
+    http_client.tenant_attach(tenant_id)
+    wait_until(
+        number_of_iterations=5,
+        interval=1,
+        func=lambda: assert_tenant_status(http_client, tenant_id, "Active"),
+    )
+
+    env.neon_cli.config_tenant(tenant_id, {"gc_horizon": "1000000"})
+    contents_first = config_path.read_text("utf-8")
+    env.neon_cli.config_tenant(tenant_id, {"gc_horizon": "0"})
+    contents_later = config_path.read_text("utf-8")
+
+    # dont test applying the setting here, we have that another test case to show it
+    # we just care about being able to create the file
+    assert len(contents_first) > len(contents_later)
