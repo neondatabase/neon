c0648ea123199103f57fca169e1414da150d7a96
Konstantin Knizhnik <knizhnik@garret.ru>
2023-01-26 16:17:06 +0200

    Change formula for excess layers calculation

--- pageserver/src/bin/layer_map_analyzer.rs
+++ pageserver/src/bin/layer_map_analyzer.rs
@@ -171,22 +171,24 @@ fn main() -> Result<()> {
             for i in 0..layers.len() {
                 if !layers[i].is_delta {
                     let mut n_deltas_since_last_image = 0usize;
+					let mut n_skipped = 0usize;
+					let img_key_range = &layers[i].key_range;
                     for j in (0..i).rev() {
-                        if range_overlaps(&layers[i].key_range, &layers[j].key_range) {
+                        if range_overlaps(img_key_range, &layers[j].key_range) {
                             if layers[j].is_delta {
-                                if layers[j].skips(&layers[i].key_range) {
-                                    n_holes += 1;
-                                } else {
-                                    n_deltas_since_last_image += 1;
+                                n_deltas_since_last_image += 1;
+                                if layers[j].skips(img_key_range) {
+                                    n_skipped += 1;
                                 }
                             } else {
                                 break;
                             }
                         }
                     }
-                    if n_deltas_since_last_image < 3 {
+                    if n_deltas_since_last_image >= 3 && n_deltas_since_last_image - n_skipped < 3 {
                         n_excess_layers += 1;
                     }
+					n_holes += n_skipped;
                 }
             }
             println!(
