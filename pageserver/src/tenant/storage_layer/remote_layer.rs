38754b2cc0bb6482ed7c526413e6fbddec905be1
Konstantin Knizhnik <knizhnik@garret.ru>
2023-01-29 09:24:15 +0200

    Revert "Make clippy happy"
    This reverts commit 390d144d7cc9d6df4c1de3f486071bc024aa459c.

--- pageserver/src/tenant/storage_layer/remote_layer.rs
+++ pageserver/src/tenant/storage_layer/remote_layer.rs
@@ -106,25 +106,28 @@ impl Layer for RemoteLayer {
     }
 
     fn get_holes(&self) -> Result<Option<Vec<Hole>>> {
-        Ok(self.layer_metadata.holes())
+        Ok(self.layer_metadata.holes().clone())
     }
 
     fn overlaps(&self, key_range: &Range<Key>) -> anyhow::Result<bool> {
         if !range_overlaps(&self.key_range, key_range) {
             Ok(false)
-        } else if let Some(holes) = &self.layer_metadata.holes() {
-            let start = match holes.binary_search_by_key(&key_range.start, |hole| hole.0.start) {
-                Ok(index) => index,
-                Err(index) => {
-                    if index == 0 {
-                        return Ok(true);
-                    }
-                    index - 1
-                }
-            };
-            Ok(holes[start].0.end < key_range.end)
         } else {
-            Ok(true)
+            if let Some(holes) = &self.layer_metadata.holes() {
+                let start = match holes.binary_search_by_key(&key_range.start, |hole| hole.0.start)
+                {
+                    Ok(index) => index,
+                    Err(index) => {
+                        if index == 0 {
+                            return Ok(true);
+                        }
+                        index - 1
+                    }
+                };
+                Ok(holes[start].0.end < key_range.end)
+            } else {
+                Ok(true)
+            }
         }
     }
 
