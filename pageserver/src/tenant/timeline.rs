3e6288d7d82b8176223393f01673ad8ef859af9e
Konstantin Knizhnik <knizhnik@garret.ru>
2023-03-09 15:01:24 +0200

    Update pageserver/src/tenant/timeline.rs
    Co-authored-by: Joonas Koivunen <joonas@neon.tech>

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -3048,7 +3048,10 @@ impl Timeline {
                 image_layers.push(image_layer);
             }
         }
-        // All wanted layers are taken in account by time_for_new_image_layer
+        // All wanted layers are taken in account by time_for_new_image_layer.
+        // The watend_image_layers could get updated out of turn and we could
+        // clear something which hasn't been looked at all. This is fine, because
+        // next gc round any wanted would get added back in.
         *self.wanted_image_layers.lock().unwrap() = None;
 
         // Sync the new layer to disk before adding it to the layer map, to make sure
