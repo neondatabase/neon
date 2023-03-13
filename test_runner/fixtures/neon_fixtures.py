2c48626c5c2289048aefa1a7ed161a1c49cdca90
Joonas Koivunen <joonas@neon.tech>
2023-03-13 11:39:45 +0200

    test: stricly find WARN, ERROR lines
    this prevents flakyness where WARN appears in some other part of the
    line, for example in random filename.

--- test_runner/fixtures/neon_fixtures.py
+++ test_runner/fixtures/neon_fixtures.py
@@ -2155,7 +2155,7 @@ class NeonPageserver(PgProtocol):
 
     def assert_no_errors(self):
         logfile = open(os.path.join(self.env.repo_dir, "pageserver.log"), "r")
-        error_or_warn = re.compile("ERROR|WARN")
+        error_or_warn = re.compile(r"^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z)\s+(ERROR|WARN)")
         errors = []
         while True:
             line = logfile.readline()
