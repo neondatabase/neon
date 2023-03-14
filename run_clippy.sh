c23c8946a3a867229735ae7732417fa2b0cb98f9
Joonas Koivunen <joonas@neon.tech>
2023-03-14 15:29:02 +0200

    chore: clippies introduced with rust 1.68 (#3781)
    - handle automatically fixable future clippies
    - tune run-clippy.sh to remove macos specifics which we no longer have
    
    Co-authored-by: Alexander Bayandin <alexander@neon.tech>

--- run_clippy.sh
+++ run_clippy.sh
@@ -8,21 +8,13 @@
 # warnings and errors right in the editor.
 # In vscode, this setting is Rust-analyzer>Check On Save:Command
 
-
-# Not every feature is supported in macOS builds. Avoid running regular linting
-# script that checks every feature.
-#
 # manual-range-contains wants
 #   !(4..=MAX_STARTUP_PACKET_LENGTH).contains(&len)
 # instead of
 #   len < 4 || len > MAX_STARTUP_PACKET_LENGTH
 # , let's disagree.
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # no extra features to test currently, add more here when needed
-    cargo clippy --locked --all --all-targets --features testing -- -A unknown_lints  -A clippy::manual-range-contains -D warnings
-else
-    # * `-A unknown_lints` – do not warn about unknown lint suppressions
-    #                        that people with newer toolchains might use
-    # * `-D warnings`      - fail on any warnings (`cargo` returns non-zero exit status)
-    cargo clippy --locked --all --all-targets --all-features -- -A unknown_lints -A clippy::manual-range-contains -D warnings
-fi
+
+# * `-A unknown_lints` – do not warn about unknown lint suppressions
+#                        that people with newer toolchains might use
+# * `-D warnings`      - fail on any warnings (`cargo` returns non-zero exit status)
+cargo clippy --locked --all --all-targets --all-features -- -A unknown_lints -A clippy::manual-range-contains -D warnings
