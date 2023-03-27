ddc963e8836d2e6d0242c698c9d53e8cd9d4a990
Joonas Koivunen <joonas@neon.tech>
2023-03-27 15:40:23 +0200

    hack: avoid redownloads post-restart by synthetic_size
    with #3867 we started to retain a set of layers which will be needed
    right after the next restart. with this hack, we will also retain the
    layers which are needed most likely for the next synthetic size
    calculation.
    
    the actual size calculation is not of importance, but we need to
    calculate all of the sizes, so we only call tenant::size::gather_inputs.
    
    Co-authored-by: Christian Schwarz <christian@neon.tech>

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -142,6 +142,9 @@ pub struct Tenant {
     /// Cached logical sizes updated updated on each [`Tenant::gather_size_inputs`].
     cached_logical_sizes: tokio::sync::Mutex<HashMap<(TimelineId, Lsn), u64>>,
     cached_synthetic_tenant_size: Arc<AtomicU64>,
+    /// Used by eviction tasks of this tenants timelines to synchronize calls to
+    /// `tenant::size::gather_inputs`.
+    eviction_task_synthetic_size: tokio::sync::Mutex<Option<Instant>>,
 }
 
 /// A timeline with some of its files on disk, being initialized.
@@ -1781,6 +1784,7 @@ impl Tenant {
             state,
             cached_logical_sizes: tokio::sync::Mutex::new(HashMap::new()),
             cached_synthetic_tenant_size: Arc::new(AtomicU64::new(0)),
+            eviction_task_synthetic_size: tokio::sync::Mutex::new(None),
         }
     }
 
