fe0b6162992b32f874fe9c21d48b0580013a556f
Joonas Koivunen <joonas@neon.tech>
2023-04-27 20:55:35 +0300

    feat(page_service): read timeouts (#4093)
    Introduce read timeouts to our `page_service` connections. Without read
    timeouts, we essentially leak connections.
    
    This is a port of #3995. Split the refactorings to the other PR: #4097.
    
    Fixes #4028.

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -250,6 +250,15 @@ async fn page_service_conn_main(
 
     let peer_addr = socket.peer_addr().context("get peer address")?;
 
+    // setup read timeout of 10 minutes. the timeout is rather arbitrary for requirements:
+    // - long enough for most valid compute connections
+    // - less than infinite to stop us from "leaking" connections to long-gone computes
+    //
+    // no write timeout is used, because the kernel is assumed to error writes after some time.
+    let mut socket = tokio_io_timeout::TimeoutReader::new(socket);
+    socket.set_timeout(Some(std::time::Duration::from_secs(60 * 10)));
+    let socket = std::pin::pin!(socket);
+
     // XXX: pgbackend.run() should take the connection_ctx,
     // and create a child per-query context when it invokes process_query.
     // But it's in a shared crate, so, we store connection_ctx inside PageServerHandler
