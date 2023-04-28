775a5de3a113e444bb5d450ba11528e0800b6cbc
Stas Kelvich <stas.kelvich@gmail.com>
2023-04-28 13:51:11 +0300

    fix python styling

--- test_runner/regress/test_metric_collection.py
+++ test_runner/regress/test_metric_collection.py
@@ -201,8 +201,10 @@ def proxy_metrics_handler(request: Request) -> Response:
 
 @pytest.fixture(scope="session")
 def proxy_with_metric_collector(
-    port_distributor: PortDistributor, neon_binpath: Path, httpserver_listen_address,
-    test_output_dir: Path
+    port_distributor: PortDistributor,
+    neon_binpath: Path,
+    httpserver_listen_address,
+    test_output_dir: Path,
 ) -> Iterator[NeonProxy]:
     """Neon proxy that routes through link auth and has metric collection enabled."""
 
