6e202befd7f12fdec35c042a67f741074fb99dbb
Alexander Bayandin <alexander@neon.tech>
2023-05-06 19:55:51 +0100

    Allure Report: merge release and debug reports

--- scripts/pr-comment-test-report.js
+++ scripts/pr-comment-test-report.js
@@ -1,6 +1,5 @@
 //
 // The script parses Allure reports and posts a comment with a summary of the test results to the PR.
-// It accepts an array of items and creates a comment with a summary for each one (for "release" and "debug", together or separately if any of them failed to be generated).
 //
 // The comment is updated on each run with the latest results.
 //
@@ -13,11 +12,14 @@
 //         github,
 //         context,
 //         fetch,
-//         reports: [{...}, ...], // each report is expected to have "buildType", "reportUrl", and "jsonUrl" properties
+//         report: {
+//           reportUrl: "...",
+//           reportJsonUrl: "...",
+//         },
 //       })
 //
 
-module.exports = async ({ github, context, fetch, reports }) => {
+module.exports = async ({ github, context, fetch, report }) => {
     // Marker to find the comment in the subsequent runs
     const startMarker = `<!--AUTOMATIC COMMENT START #${context.payload.number}-->`
     // GitHub bot id taken from (https://api.github.com/users/github-actions[bot])
@@ -25,7 +27,7 @@ module.exports = async ({ github, context, fetch, reports }) => {
     // The latest commit in the PR URL
     const commitUrl = `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/pull/${context.payload.number}/commits/${context.payload.pull_request.head.sha}`
     // Commend body itself
-    let commentBody = `${startMarker}\n### Test results for ${commitUrl}:\n___\n`
+    let commentBody = `${startMarker}\n`
 
     // Common parameters for GitHub API requests
     const ownerRepoParams = {
@@ -33,74 +35,71 @@ module.exports = async ({ github, context, fetch, reports }) => {
         repo: context.repo.repo,
     }
 
-    for (const report of reports) {
-        const {buildType, reportUrl, jsonUrl} = report
+    const {reportUrl, reportJsonUrl} = report
 
-        if (!reportUrl || !jsonUrl) {
-            commentBody += `#### ${buildType} build: no tests were run or test report is not available\n`
-            continue
-        }
+    if (!reportUrl || !reportJsonUrl) {
+        commentBody += `#### No tests were run or test report is not available\n`
+        return
+    }
 
-        const suites = await (await fetch(jsonUrl)).json()
+    const suites = await (await fetch(reportJsonUrl)).json()
 
-        // Allure distinguishes "failed" (with an assertion error) and "broken" (with any other error) tests.
-        // For this report it's ok to treat them in the same way (as failed).
-        failedTests = []
-        passedTests = []
-        skippedTests = []
+    // Allure distinguishes "failed" (with an assertion error) and "broken" (with any other error) tests.
+    // For this report it's ok to treat them in the same way (as failed).
+    failedTests = []
+    passedTests = []
+    skippedTests = []
 
-        retriedTests = []
-        retriedStatusChangedTests = []
+    retriedTests = []
+    retriedStatusChangedTests = []
 
-        for (const parentSuite of suites.children) {
-            for (const suite of parentSuite.children) {
-                for (const test of suite.children) {
-                    pytestName = `${parentSuite.name.replace(".", "/")}/${suite.name}.py::${test.name}`
-                    test.pytestName = pytestName
+    for (const parentSuite of suites.children) {
+        for (const suite of parentSuite.children) {
+            for (const test of suite.children) {
+                pytestName = `${parentSuite.name.replace(".", "/")}/${suite.name}.py::${test.name}`
+                test.pytestName = pytestName
 
-                    if (test.status === "passed") {
-                        passedTests.push(test);
-                    } else if (test.status === "failed" || test.status === "broken") {
-                        failedTests.push(test);
-                    } else if (test.status === "skipped") {
-                        skippedTests.push(test);
-                    }
-
-                    if (test.retriesCount > 0) {
-                        retriedTests.push(test);
-
-                        if (test.retriesStatusChange) {
-                            retriedStatusChangedTests.push(test);
-                        }
-                    }
+                if (test.status === "passed") {
+                    passedTests.push(test);
+                } else if (test.status === "failed" || test.status === "broken") {
+                    failedTests.push(test);
+                } else if (test.status === "skipped") {
+                    skippedTests.push(test);
                 }
-            }
-        }
 
-        const totalTestsCount = failedTests.length + passedTests.length + skippedTests.length
-        commentBody += `#### ${buildType} build: ${totalTestsCount} tests run: ${passedTests.length} passed, ${failedTests.length} failed, ${skippedTests.length} skipped ([full report](${reportUrl}))\n`
-        if (failedTests.length > 0) {
-            commentBody += `Failed tests:\n`
-            for (const test of failedTests) {
-                const allureLink = `${reportUrl}#suites/${test.parentUid}/${test.uid}`
-
-                commentBody += `- [\`${test.pytestName}\`](${allureLink})`
                 if (test.retriesCount > 0) {
-                    commentBody += ` (ran [${test.retriesCount + 1} times](${allureLink}/retries))`
+                    retriedTests.push(test);
+
+                    if (test.retriesStatusChange) {
+                        retriedStatusChangedTests.push(test);
+                    }
                 }
-                commentBody += "\n"
+            }
+        }
+    }
+
+    const totalTestsCount = failedTests.length + passedTests.length + skippedTests.length
+    commentBody += `### ${totalTestsCount} tests run: ${passedTests.length} passed, ${failedTests.length} failed, ${skippedTests.length} skipped ([full report](${reportUrl}) for ${commitUrl})\n___\n`
+    if (failedTests.length > 0) {
+        commentBody += `Failed tests:\n`
+        for (const test of failedTests) {
+            const allureLink = `${reportUrl}#suites/${test.parentUid}/${test.uid}`
+
+            commentBody += `- [\`${test.pytestName}\`](${allureLink})`
+            if (test.retriesCount > 0) {
+                commentBody += ` (ran [${test.retriesCount + 1} times](${allureLink}/retries))`
             }
             commentBody += "\n"
         }
-        if (retriedStatusChangedTests > 0) {
-            commentBody += `Flaky tests:\n`
-            for (const test of retriedStatusChangedTests) {
-                const status = test.status === "passed" ? ":white_check_mark:" : ":x:"
-                commentBody += `- ${status} [\`${test.pytestName}\`](${reportUrl}#suites/${test.parentUid}/${test.uid}/retries)\n`
-            }
-            commentBody += "\n"
+        commentBody += "\n"
+    }
+    if (retriedStatusChangedTests > 0) {
+        commentBody += `Flaky tests:\n`
+        for (const test of retriedStatusChangedTests) {
+            const status = test.status === "passed" ? ":white_check_mark:" : ":x:"
+            commentBody += `- ${status} [\`${test.pytestName}\`](${reportUrl}#suites/${test.parentUid}/${test.uid}/retries)\n`
         }
-        commentBody += "___\n"
+        commentBody += "\n"
     }
 
     const { data: comments } = await github.rest.issues.listComments({
