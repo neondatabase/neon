612dabbb283afda60e3f7795a0ccc7f678e1ded2
Alex Chi <iskyzh@gmail.com>
2023-05-15 11:16:42 -0400

    try with deprecated API
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- libs/remote_storage/src/s3_bucket.rs
+++ libs/remote_storage/src/s3_bucket.rs
@@ -139,8 +139,16 @@ impl S3Bucket {
             .credentials_cache(CredentialsCache::lazy())
             .credentials_provider(credentials_provider);
 
+        #[allow(deprecated)]
         if let Some(custom_endpoint) = aws_config.endpoint.clone() {
-            config_builder = config_builder.endpoint_url(custom_endpoint);
+            use aws_config::endpoint::Endpoint;
+            let endpoint = Endpoint::immutable_uri(
+                custom_endpoint
+                    .parse()
+                    .expect("Failed to parse S3 custom endpoint"),
+            )
+            .expect("failed to create endpoint");
+            config_builder.set_endpoint_resolver(Some(Arc::new(endpoint)));
         }
         let client = Client::from_conf(config_builder.build());
 
