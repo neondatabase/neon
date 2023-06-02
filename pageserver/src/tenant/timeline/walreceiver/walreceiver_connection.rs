ba1592aa21cad1884c5759d72e8a9f9eab76c1b0
Joonas Koivunen <joonas@neon.tech>
2023-06-02 13:59:44 +0300

    chore: remove dead code

--- pageserver/src/tenant/timeline/walreceiver/walreceiver_connection.rs
+++ pageserver/src/tenant/timeline/walreceiver/walreceiver_connection.rs
@@ -264,8 +264,6 @@ pub(super) async fn handle_walreceiver_connection(
                     let mut decoded = DecodedWALRecord::default();
                     let mut modification = timeline.begin_modification(endlsn);
                     while let Some((lsn, recdata)) = waldecoder.poll_decode()? {
-                        // let _enter = info_span!("processing record", lsn = %lsn).entered();
-
                         // It is important to deal with the aligned records as lsn in getPage@LSN is
                         // aligned and can be several bytes bigger. Without this alignment we are
                         // at risk of hitting a deadlock.
