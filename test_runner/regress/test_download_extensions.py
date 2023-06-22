913eb7501d91eacd154614b40f027661d533672b
Alek Westover <alek.westover@gmail.com>
2023-06-22 16:12:09 -0400

    tests that we can download multiple control files

--- test_runner/regress/test_download_extensions.py
+++ test_runner/regress/test_download_extensions.py
@@ -12,7 +12,6 @@ from fixtures.neon_fixtures import (
 """
 TODO:
 - add more tests
-  -- multiple extension.control files to download
   -- does CREATE EXTENSION actually call extension_server?
 
 - Handle not just Shared Extensions but also other types
@@ -40,28 +39,33 @@ def test_file_download(neon_env_builder: NeonEnvBuilder):
     assert env.ext_remote_storage is not None
     assert env.remote_storage_client is not None
 
-    TEST_EXT_PATH = "v14/share/postgresql/extension/test_ext.control"
+    NUM_EXT = 5
+    TEST_EXT_PATHS = [f"v14/share/postgresql/extension/test_ext{i}.control" for i in range(NUM_EXT)]
     BUCKET_PREFIX = "5314225671"  # this is the build number
 
     # 4. Upload test_ext.control file to the bucket
     # In the non-mock version this is done by CI/CD
 
-    test_ext_file = BytesIO(
-        b"""# mock extension
-comment = 'This is a mock extension'
-default_version = '1.0'
-module_pathname = '$libdir/test_ext'
-relocatable = true
-    """
-    )
-    env.remote_storage_client.upload_fileobj(
-        test_ext_file,
-        env.ext_remote_storage.bucket_name,
-        os.path.join(BUCKET_PREFIX, TEST_EXT_PATH),
-    )
+    for i in range(NUM_EXT):
+        test_ext_file = BytesIO(
+            bytes(
+                f"# mock extension\
+                comment = 'This is a mock extension'\
+                default_version = '1.0'\
+                module_pathname = '$libdir/test_ext{i}'\
+                relocatable = true\
+                ",
+                "utf-8",
+            )
+        )
+        env.remote_storage_client.upload_fileobj(
+            test_ext_file,
+            env.ext_remote_storage.bucket_name,
+            os.path.join(BUCKET_PREFIX, TEST_EXT_PATHS[i]),
+        )
 
     # 5. Download file from the bucket to correct local location
-    # Later this will be replaced by our rust code
+    # our rust code should be equivalent to the following:
     # resp = env.remote_storage_client.get_object(
     #     Bucket=env.ext_remote_storage.bucket_name, Key=os.path.join(BUCKET_PREFIX, TEST_EXT_PATH)
     # )
@@ -99,4 +103,5 @@ relocatable = true
             cur.execute("SELECT * FROM pg_available_extensions")
             all_extensions = [x[0] for x in cur.fetchall()]
             log.info(all_extensions)
-            assert "test_ext" in all_extensions
+            for i in range(NUM_EXT):
+                assert f"test_ext{i}" in all_extensions
