2f618f46be510178632bb44afa5f0c1dfc96b7a4
Anastasia Lubennikova <anastasia@neon.tech>
2023-06-22 17:06:16 +0300

    Use BUILD_TAG in compute_ctl binary. (#4541)
    Pass BUILD_TAG to compute_ctl binary. 
    We need it to access versioned extension storage.

--- compute_tools/src/bin/compute_ctl.rs
+++ compute_tools/src/bin/compute_ctl.rs
@@ -54,8 +54,14 @@ use compute_tools::monitor::launch_monitor;
 use compute_tools::params::*;
 use compute_tools::spec::*;
 
+const BUILD_TAG_DEFAULT: &str = "local";
+
 fn main() -> Result<()> {
     init_tracing_and_logging(DEFAULT_LOG_LEVEL)?;
+
+    let build_tag = option_env!("BUILD_TAG").unwrap_or(BUILD_TAG_DEFAULT);
+
+    info!("build_tag: {build_tag}");
 
     let matches = cli().get_matches();
 
