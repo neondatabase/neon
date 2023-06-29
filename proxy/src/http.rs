b1b3fd32e390af1e2ff713bcc4a522eb06280b69
Shany Pozin <shany@neon.tech>
2023-06-29 14:28:10 +0300

    move to total_retry_duration

--- proxy/src/http.rs
+++ proxy/src/http.rs
@@ -24,19 +24,20 @@ pub fn new_client() -> ClientWithMiddleware {
         .build()
 }
 
-pub fn new_client_with_timeout_and_retries(
-    default_timout: Duration,
-    total_retries: u32,
-) -> ClientWithMiddleware {
+pub fn new_client_with_timeout(default_timout: Duration) -> ClientWithMiddleware {
     let timeout_client = reqwest::ClientBuilder::new()
         .timeout(default_timout)
         .build()
         .expect("Failed to create http client with timeout");
 
-    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(total_retries);
+    let retry_policy =
+        ExponentialBackoff::builder().build_with_total_retry_duration(default_timout);
 
     reqwest_middleware::ClientBuilder::new(timeout_client)
         .with(reqwest_tracing::TracingMiddleware::default())
+        // As per docs, "This middleware always errors when given requests with streaming bodies".
+        // That's all right because we only use this client to send `serde_json::RawValue`, which
+        // is not a stream.
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
         .build()
 }
