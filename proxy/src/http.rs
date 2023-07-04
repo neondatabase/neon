ba4d4d31ed76a58907141e3e6a574c38e6b7504e
Stas Kelvich <stas.kelvich@gmail.com>
2023-07-04 13:27:08 +0300

    Minimalistic pool for http endpoint compute connections (under opt-in flag)
    Cache up to 20 connections per endpoint. Once all pooled connections
    are used current implementation can open an extra connection, so the
    maximum number of simultaneous connections is not enforced.
    
    There are more things to do here, especially with background clean-up
    of closed connections, and checks for transaction state. But current
    implementation allows to check for smaller coonection latencies that
    this cache should bring.

--- proxy/src/http.rs
+++ proxy/src/http.rs
@@ -2,6 +2,7 @@
 //! Other modules should use stuff from this module instead of
 //! directly relying on deps like `reqwest` (think loose coupling).
 
+pub mod conn_pool;
 pub mod server;
 pub mod sql_over_http;
 pub mod websocket;
