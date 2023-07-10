7bfde488b6b6350d1d7760399ea19c01d3329313
Alek Westover <alek.westover@gmail.com>
2023-07-10 13:59:00 -0400

    fixed python script

--- scripts/combine_control_files.py
+++ scripts/combine_control_files.py
@@ -5,14 +5,14 @@ index = {}
 os.chdir("control_files")
 for prefix in os.listdir("."):
     for file in os.listdir(prefix):
-        with open(fname, "r") as f:
-            ext_name = fname.replace(".control", "")
+        with open(os.path.join(prefix, file), "r") as f:
+            ext_name = file.replace(".control", "")
             control = f.read()
             index[ext_name] = {
                 "path": f"{prefix}/{ext_name}.tar.gz",
                 "control": control
             }
 
-with open("control_index.json", "w") as f:
+with open("../control_index.json", "w") as f:
     json.dump(index, f)
 
