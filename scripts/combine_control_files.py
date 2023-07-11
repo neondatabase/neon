f37980a026b0a803dec96a42ce5314b3ab3e610e
Alek Westover <alek.westover@gmail.com>
2023-07-11 13:52:19 -0400

    fix directory structure

--- scripts/combine_control_files.py
+++ scripts/combine_control_files.py
@@ -1,14 +1,16 @@
 import json
 import os
 
-index = {}
+ext_index = {}
 os.chdir("control_files")
 for prefix in os.listdir("."):
+    ext_index[prefix] = {}
     for file in os.listdir(prefix):
         with open(os.path.join(prefix, file), "r") as f:
             ext_name = file.replace(".control", "")
             control = f.read()
-            index[ext_name] = {"path": f"{prefix}/{ext_name}.tar.gz", "control": control}
+            ext_index[prefix][ext_name] = {"path": f"extensions/{prefix}/{ext_name}.tar.gz", "control": control}
 
-with open("../control_index.json", "w") as f:
-    json.dump(index, f)
+with open("../ext_index.json", "w") as f:
+    json.dump(ext_index, f)
+
