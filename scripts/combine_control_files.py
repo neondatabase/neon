b7a8eba4de0e814ea0a2b37d12facd76476d8986
Alek Westover <alek.westover@gmail.com>
2023-07-18 15:14:29 -0400

    revert to python script

---
+++ scripts/combine_control_files.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+# enable custom extensions for specific tenants
+enabled_extensions = {"123454321": ["anon"], "public": ["embedding"]}
+
+control_data = {}
+os.chdir("control_files")
+for control_file in os.listdir("."):
+    ext_name = control_file.replace(".control", "")
+    with open(control_file, "r") as f:
+        control_data[ext_name] = f.read()
+
+all_data = {"enabled_extensions": enabled_extensions, "control_data": control_data}
+
+with open("ext_index.json", "w") as f:
+    json.dump(all_data, f)
