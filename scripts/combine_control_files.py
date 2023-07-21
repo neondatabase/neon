a96963575b4b8413457659afa055d3e5cb59ed12
Alek Westover <alek.westover@gmail.com>
2023-07-21 11:33:04 -0400

    add python script to generate ext_index.json, call it from Dockerfile

---
+++ scripts/combine_control_files.py
@@ -0,0 +1,38 @@
+import json
+import os
+import sys
+from os.path import isdir, join
+
+# import shutil
+
+pg_version = sys.argv[1]
+#  build_tag = sys.argv[2]
+assert pg_version in ("v14", "v15")
+
+build_numbers = {}
+with open("build_numbers.txt", "r") as f:
+    for line in f.readlines():
+        ext_name, build_num = line.split(" ")
+        build_numbers[ext_name] = build_num
+
+ext_index = {}
+os.chdir("extensions")
+for extension in os.listdir("."):
+    if isdir(extension):
+        control_data = {}
+        for control_file in os.listdir(extension):
+            if ".control" not in control_file:
+                continue
+            with open(join(extension, control_file), "r") as f:
+                control_data[control_file] = f.read()
+        ext_index[extension] = {
+            "control_data": control_data,
+            "archive_path": f"{build_numbers[extension]}/{pg_version}/{extension}.tar.zst",
+        }
+
+        # TODO: uncomment this to enable de-duplication:
+        #  if build_numbers[extension] != build_tag:
+        #      shutil.rmtree(extension)
+
+with open("ext_index.json", "w") as f:
+    json.dump(ext_index, f)
