4235c3bf8444b56102428bb5d36fac01cf9b3107
Alek Westover <alek.westover@gmail.com>
2023-07-25 09:19:15 -0400

    python style: use better libraries

--- scripts/combine_control_files.py
+++ scripts/combine_control_files.py
@@ -1,15 +1,17 @@
 #! /usr/bin/env python3
 # Script to generate ext_index.json metadata file
 # that stores content of the control files and location of extension archives
-# for all extensions in /extensions subdir.
+# for all extensions in extensions subdir.
 import json
-import os
-import sys
-from os.path import isdir, join
+from pathlib import Path
+import argparse
 
 if __name__ == "__main__":
-    pg_version = sys.argv[1]
-    assert pg_version in ("v14", "v15")
+    parser = argparse.ArgumentParser(description="generate ext_index.json")
+    parser.add_argument("pg_version", type=str, choices=["v14", "v15"], help="pg_version")
+    parser.add_argument("BUILD_TAG", type=str, help="BUILD_TAG for this compute image")
+    args = parser.parse_args()
+    pg_version = args.pg_version
 
     build_numbers = {}
     with open("build_numbers.txt", "r") as f:
@@ -18,20 +20,19 @@ if __name__ == "__main__":
             build_numbers[ext_name] = build_num.strip()
 
     ext_index = {}
-    os.chdir("extensions")
-    for extension in os.listdir("."):
-        if isdir(extension):
+    EXT_PATH = Path("extensions")
+    for extension in EXT_PATH.iterdir():
+        if extension.is_dir():
             control_data = {}
-            for control_file in os.listdir(extension):
-                if ".control" not in control_file:
+            for control_file in extension.iterdir():
+                if control_file.suffix != ".control":
                     continue
-                with open(join(extension, control_file), "r") as f:
-                    control_data[control_file] = f.read()
-            ext_index[extension] = {
+                with open(control_file, "r") as f:
+                    control_data[control_file.name] = f.read()
+            ext_index[extension.name] = {
                 "control_data": control_data,
-                "archive_path": f"{build_numbers[extension]}/{pg_version}/extensions/{extension}.tar.zst",
+                "archive_path": f"{build_numbers[extension.name]}/{pg_version}/extensions/{extension.name}.tar.zst",
             }
 
-    os.chdir("..")
     with open("ext_index.json", "w") as f:
         json.dump(ext_index, f)
