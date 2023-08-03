37cc3bf192839c7d8aba9b7b76c2dbaccdefbcf6
John Spray <john@neon.tech>
2023-08-03 13:24:53 +0100

    pageserver: enhance `timed` helper to warn earlier
    When something is running for a long time, it might
    deadlock: in this case we are interested in seeing
    a warning logged as soon as the wrapped function
    exceeds its deadline -- we might never get far enough
    to see the final "took more than expected" with the
    total elapsed time.

--- pageserver/src/lib.rs
+++ pageserver/src/lib.rs
@@ -24,6 +24,7 @@ pub mod walredo;
 use std::path::Path;
 
 use crate::task_mgr::TaskKind;
+use futures::{stream::FuturesUnordered, FutureExt, StreamExt};
 use tracing::info;
 
 /// Current storage format version
@@ -199,17 +200,49 @@ pub struct InitializationOrder {
 }
 
 /// Time the future with a warning when it exceeds a threshold.
-async fn timed<Fut: std::future::Future>(
+async fn timed<Fut: std::future::Future + Send>(
     fut: Fut,
     name: &str,
     warn_at: std::time::Duration,
 ) -> <Fut as std::future::Future>::Output {
     let started = std::time::Instant::now();
 
-    let ret = fut.await;
+    // Compose an unordered collection of the future we are waiting for, and
+    // a future that will become ready when we reach the warning time threshold
+    let mut futs = FuturesUnordered::new();
+    futs.push(async move { Ok(fut.await) }.boxed());
+    futs.push(
+        async move {
+            tokio::time::sleep(warn_at).await;
+            Err(())
+        }
+        .boxed(),
+    );
+
+    let mut ret = None;
+    while let Some(r) = futs.next().await {
+        match r {
+            Ok(inner_ret) => {
+                // The inner future completed
+                ret = Some(inner_ret);
+                break;
+            }
+            Err(_) => {
+                // The warn_at time was reached before the inner future completed
+                tracing::warn!(
+                    task = name,
+                    elapsed_so_far = started.elapsed().as_millis(),
+                    "still running, taking longer than expected...",
+                );
+            }
+        }
+    }
+
+    // We should only have broken out of the loop over futures when we hit
+    // the one that populates ret.
+    assert!(ret.is_some());
 
     let elapsed = started.elapsed();
-
     if elapsed >= warn_at {
         // this has a global allowed_error
         tracing::warn!(
@@ -221,7 +254,7 @@ async fn timed<Fut: std::future::Future>(
         tracing::info!(task = name, elapsed_ms = elapsed.as_millis(), "completed");
     }
 
-    ret
+    ret.unwrap()
 }
 
 #[cfg(test)]
@@ -252,5 +285,39 @@ mod backoff_defaults_tests {
             DEFAULT_MAX_BACKOFF_SECONDS,
             "Given big enough of retries, backoff should reach its allowed max value"
         );
+    }
+}
+
+#[cfg(test)]
+mod timed_tests {
+    use super::timed;
+    use std::time::Duration;
+    use tokio;
+
+    #[tokio::test]
+    async fn test_timed() {
+        // A future that completes on time should have its result returned
+        let r1 = timed(
+            async move {
+                tokio::time::sleep(Duration::from_millis(10)).await;
+                123
+            },
+            "test 1",
+            Duration::from_millis(50),
+        )
+        .await;
+        assert_eq!(r1, 123);
+
+        // A future that completes too slowly should also have its result returned
+        let r1 = timed(
+            async move {
+                tokio::time::sleep(Duration::from_millis(50)).await;
+                456
+            },
+            "test 1",
+            Duration::from_millis(10),
+        )
+        .await;
+        assert_eq!(r1, 456);
     }
 }
