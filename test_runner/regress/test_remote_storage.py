a70af8037f728af4a97a1f89f978a5389cdbc1ba
John Spray <john@neon.tech>
2023-08-15 17:26:58 +0100

    Add reference to issue for deletions getting delayed by compaction

--- test_runner/regress/test_remote_storage.py
+++ test_runner/regress/test_remote_storage.py
@@ -608,6 +608,7 @@ def test_timeline_deletion_with_files_stuck_in_upload_queue(
     )
 
     # Generous timeout, because currently deletions can get blocked waiting for compaction
+    # This can be reduced when https://github.com/neondatabase/neon/issues/4998 is fixed.
     timeline_delete_wait_completed(client, tenant_id, timeline_id, iterations=30, interval=1)
 
     assert not timeline_path.exists()
