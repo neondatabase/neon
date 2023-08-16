9304f42ea5f7af7c353d06c9b8e45c93733130ff
Joonas Koivunen <joonas@neon.tech>
2023-08-16 15:40:31 +0300

    tenant: remove unused imports

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -133,9 +133,7 @@ pub(crate) mod timeline;
 pub mod size;
 
 pub(crate) use timeline::span::debug_assert_current_span_has_tenant_and_timeline_id;
-pub(crate) use timeline::{
-    LocalLayerInfoForDiskUsageEviction, LogicalSizeCalculationCause, PageReconstructError, Timeline,
-};
+pub(crate) use timeline::{LogicalSizeCalculationCause, PageReconstructError, Timeline};
 
 // re-export for use in remote_timeline_client.rs
 pub use crate::tenant::metadata::save_metadata;
