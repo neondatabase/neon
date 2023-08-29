d67d4b3eeee8edb749bad0ae11ee7ed2c1ce41ca
Joonas Koivunen <joonas@neon.tech>
2023-08-29 11:11:27 +0300

    doc: add validation

--- pageserver/src/tenant/storage_layer/layer.rs
+++ pageserver/src/tenant/storage_layer/layer.rs
@@ -773,7 +773,7 @@ impl LayerInner {
     }
 
     fn is_file_present_and_good_size(&self, m: &std::fs::Metadata) -> Result<(), NeedsDownload> {
-        // in future, this should include sha2-256 of the file.
+        // in future, this should include sha2-256 validation of the file.
         if !m.is_file() {
             Err(NeedsDownload::NotFile)
         } else if m.len() != self.desc.file_size {
