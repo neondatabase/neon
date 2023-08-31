300a5aa05e66899354b8d1e1a3ddce2c8d065603
John Spray <john@neon.tech>
2023-08-31 10:40:46 +0100

    pageserver: fix test v4_indexpart_is_parsed (#5157)
    ## Problem
    
    Two recent PRs raced:
    - https://github.com/neondatabase/neon/pull/5153
    - https://github.com/neondatabase/neon/pull/5140
    
    ## Summary of changes
    
    Add missing `generation` argument to IndexLayerMetadata construction

--- pageserver/src/tenant/remote_timeline_client/index.rs
+++ pageserver/src/tenant/remote_timeline_client/index.rs
@@ -335,11 +335,13 @@ mod tests {
             layer_metadata: HashMap::from([
                 ("000000000000000000000000000000000000-FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF__0000000001696070-00000000016960E9".parse().unwrap(), IndexLayerMetadata {
                     file_size: 25600000,
+                    generation: Generation::none()
                 }),
                 ("000000000000000000000000000000000000-FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF__00000000016B59D8-00000000016B5A51".parse().unwrap(), IndexLayerMetadata {
                     // serde_json should always parse this but this might be a double with jq for
                     // example.
                     file_size: 9007199254741001,
+                    generation: Generation::none()
                 })
             ]),
             disk_consistent_lsn: "0/16960E8".parse::<Lsn>().unwrap(),
