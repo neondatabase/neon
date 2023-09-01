715077ab5be2800630e71f01af6f50815221fb53
John Spray <john@neon.tech>
2023-09-01 08:36:38 +0100

    tests: broaden a log allow regex in `test_ignored_tenant_stays_broken_without_metadata` (#5168)
    ## Problem
    
    - https://github.com/neondatabase/neon/issues/5167
    
    ## Summary of changes
    
    Accept "will not become active" log line with _either_ Broken or
    Stopping state, because we may hit it while in the process of doing the
    `/ignore` (earlier in the test than the test expects to see the same
    line with Broken)

--- test_runner/regress/test_tenant_detach.py
+++ test_runner/regress/test_tenant_detach.py
@@ -688,7 +688,7 @@ def test_ignored_tenant_stays_broken_without_metadata(
     # temporarily detached produces these errors in the pageserver log.
     env.pageserver.allowed_errors.append(f".*Tenant {tenant_id} not found.*")
     env.pageserver.allowed_errors.append(
-        f".*Tenant {tenant_id} will not become active\\. Current state: Broken.*"
+        f".*Tenant {tenant_id} will not become active\\. Current state: (Broken|Stopping).*"
     )
 
     # ignore the tenant and remove its metadata
