45582a0ec471bf053ce9b7c08e25ef418c18b73b
Joonas Koivunen <joonas@neon.tech>
2023-09-06 17:10:11 +0300

    DownloadError: align Display with anyhow impl

--- libs/remote_storage/src/lib.rs
+++ libs/remote_storage/src/lib.rs
@@ -208,7 +208,14 @@ impl std::fmt::Display for DownloadError {
                 write!(f, "Failed to download a remote file due to user input: {e}")
             }
             DownloadError::NotFound => write!(f, "No file found for the remote object id given"),
-            DownloadError::Other(e) => write!(f, "Failed to download a remote file: {e:?}"),
+            DownloadError::Other(e) => {
+                write!(f, "Failed to download a remote file: ")?;
+                if f.alternate() {
+                    write!(f, "{e:#}")
+                } else {
+                    write!(f, "{e}")
+                }
+            }
         }
     }
 }
