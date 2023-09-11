9fde7aef3509dae8a46392b0a50b03d3ea023b0b
Joonas Koivunen <joonas.koivunen@gmail.com>
2023-09-11 13:49:34 +0300

    chore: clippy dislikes extra closure

--- pageserver/src/metrics.rs
+++ pageserver/src/metrics.rs
@@ -607,7 +607,7 @@ impl StorageIoTime {
     }
 }
 
-pub(crate) static STORAGE_IO_TIME_METRIC: Lazy<StorageIoTime> = Lazy::new(|| StorageIoTime::new());
+pub(crate) static STORAGE_IO_TIME_METRIC: Lazy<StorageIoTime> = Lazy::new(StorageIoTime::new);
 
 const STORAGE_IO_SIZE_OPERATIONS: &[&str] = &["read", "write"];
 
