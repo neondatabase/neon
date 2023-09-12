b6ef3b771938c0fbacaf7deb1814d23b9294df82
Matthias van de Meent <matthias@neon.tech>
2023-09-12 09:43:28 +0200

    Fix some WAL record constants in pg_constants.rs, and rebase PostgreSQL to the 16.0 stamp commit.

--- libs/postgres_ffi/src/pg_constants.rs
+++ libs/postgres_ffi/src/pg_constants.rs
@@ -166,11 +166,13 @@ pub const RM_LOGICALMSG_ID: u8 = 21;
 // from neon_rmgr.h
 pub const RM_NEON_ID: u8 = 134;
 
+pub const XLOG_NEON_HEAP_INIT_PAGE: u8 = 0x80;
+
 pub const XLOG_NEON_HEAP_INSERT: u8 = 0x00;
 pub const XLOG_NEON_HEAP_DELETE: u8 = 0x10;
 pub const XLOG_NEON_HEAP_UPDATE: u8 = 0x20;
-pub const XLOG_NEON_HEAP_HOT_UPDATE: u8 = 0x40;
-pub const XLOG_NEON_HEAP_INIT_PAGE: u8 = 0x80;
+pub const XLOG_NEON_HEAP_HOT_UPDATE: u8 = 0x30;
+pub const XLOG_NEON_HEAP_LOCK: u8 = 0x40;
 pub const XLOG_NEON_HEAP_MULTI_INSERT: u8 = 0x50;
 
 pub const XLOG_NEON_HEAP_VISIBLE: u8 = 0x40;
