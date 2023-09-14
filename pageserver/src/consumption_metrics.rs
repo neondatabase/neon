e8def1ee9d463595f4810c2f9ef827dfd91c1dc5
Joonas Koivunen <joonas@neon.tech>
2023-09-14 18:38:59 +0300

    test: introduce tests for new metrics
    fix one bug.

--- pageserver/src/consumption_metrics.rs
+++ pageserver/src/consumption_metrics.rs
@@ -785,10 +785,9 @@ impl TenantSnapshot {
                 }
             }
 
-            // FIXME: the local synthetic_size is not read, but this will be fixed with the tests
-            if self.synthetic_size != 0 {
+            if synthetic_size != 0 {
                 // only send non-zeroes because otherwise these show up as errors in logs
-                Some(factory.at(now, self.synthetic_size))
+                Some(factory.at(now, synthetic_size))
             } else {
                 None
             }
@@ -978,9 +977,7 @@ mod tests {
         lsn::Lsn,
     };
 
-    use crate::consumption_metrics::MetricsKey;
-
-    use super::TimelineSnapshot;
+    use super::*;
     use chrono::{DateTime, Utc};
 
     #[test]
@@ -1165,6 +1162,164 @@ mod tests {
             let actual = serde_json::to_string(&e).unwrap();
             assert_eq!(expected, actual, "example from line {line}");
         }
+    }
+
+    #[test]
+    fn post_restart_written_sizes_with_rolled_back_last_record_lsn() {
+        // it can happen that we lose the inmemorylayer but have previously sent metrics and we
+        // should never go backwards
+
+        let tenant_id = TenantId::generate();
+        let timeline_id = TimelineId::generate();
+
+        let [later, now, at_restart] = time_backwards();
+
+        // FIXME: tests would be so much easier if we did not need to juggle back and forth
+        // SystemTime and DateTime::<Utc> ... Could do the conversion only at upload time?
+        let now = DateTime::<Utc>::from(now);
+        let later = DateTime::<Utc>::from(later);
+        let before_restart = at_restart - std::time::Duration::from_secs(5 * 60);
+        let way_before = before_restart - std::time::Duration::from_secs(10 * 60);
+        let before_restart = DateTime::<Utc>::from(before_restart);
+        let way_before = DateTime::<Utc>::from(way_before);
+
+        let snap = TimelineSnapshot {
+            loaded_at: (Lsn(50), at_restart),
+            last_record_lsn: Lsn(50),
+            current_exact_logical_size: None,
+        };
+
+        let mut cache = HashMap::from([
+            MetricsKey::written_size(tenant_id, timeline_id).at(before_restart, 100),
+            MetricsKey::written_size_delta(tenant_id, timeline_id).from_previous_up_to(
+                way_before,
+                before_restart,
+                // not taken into account, but the timestamps are important
+                999_999_999,
+            ),
+        ]);
+
+        let mut metrics = Vec::new();
+        snap.to_metrics(tenant_id, timeline_id, now, &mut metrics, &cache);
+
+        assert_eq!(
+            metrics,
+            &[
+                MetricsKey::written_size_delta(tenant_id, timeline_id).from_previous_up_to(
+                    before_restart,
+                    now,
+                    0
+                ),
+                MetricsKey::written_size(tenant_id, timeline_id).at(now, 100),
+            ]
+        );
+
+        // now if we cache these metrics, and re-run while "still in recovery"
+        cache.extend(metrics.drain(..));
+
+        // "still in recovery", because our snapshot did not change
+        snap.to_metrics(tenant_id, timeline_id, later, &mut metrics, &cache);
+
+        assert_eq!(
+            metrics,
+            &[
+                MetricsKey::written_size_delta(tenant_id, timeline_id)
+                    .from_previous_up_to(now, later, 0),
+                MetricsKey::written_size(tenant_id, timeline_id).at(later, 100),
+            ]
+        );
+    }
+
+    #[test]
+    fn post_restart_current_exact_logical_size_uses_cached() {
+        let tenant_id = TenantId::generate();
+        let timeline_id = TimelineId::generate();
+
+        let [now, at_restart] = time_backwards();
+
+        let now = DateTime::<Utc>::from(now);
+        let before_restart = at_restart - std::time::Duration::from_secs(5 * 60);
+        let before_restart = DateTime::<Utc>::from(before_restart);
+
+        let snap = TimelineSnapshot {
+            loaded_at: (Lsn(50), at_restart),
+            last_record_lsn: Lsn(50),
+            current_exact_logical_size: None,
+        };
+
+        let cache = HashMap::from([
+            MetricsKey::timeline_logical_size(tenant_id, timeline_id).at(before_restart, 100)
+        ]);
+
+        let mut metrics = Vec::new();
+        snap.to_metrics(tenant_id, timeline_id, now, &mut metrics, &cache);
+
+        metrics.retain(|(key, _)| key.metric == Name::LogicalSize);
+
+        assert_eq!(
+            metrics,
+            &[MetricsKey::timeline_logical_size(tenant_id, timeline_id).at(now, 100)]
+        );
+    }
+
+    #[test]
+    fn post_restart_synthetic_size_uses_cached_if_available() {
+        let tenant_id = TenantId::generate();
+
+        let ts = TenantSnapshot {
+            resident_size: 1000,
+            remote_size: 1000,
+            // not yet calculated
+            synthetic_size: 0,
+        };
+
+        let now = SystemTime::now();
+        let before_restart = DateTime::<Utc>::from(now - std::time::Duration::from_secs(5 * 60));
+        let now = DateTime::<Utc>::from(now);
+
+        let cached =
+            HashMap::from([MetricsKey::synthetic_size(tenant_id).at(before_restart, 1000)]);
+
+        let mut metrics = Vec::new();
+        ts.to_metrics(tenant_id, now, &cached, &mut metrics);
+
+        assert_eq!(
+            metrics,
+            &[
+                MetricsKey::remote_storage_size(tenant_id).at(now, 1000),
+                MetricsKey::resident_size(tenant_id).at(now, 1000),
+                MetricsKey::synthetic_size(tenant_id).at(now, 1000),
+            ]
+        );
+    }
+
+    #[test]
+    fn post_restart_synthetic_size_is_not_sent_when_not_cached() {
+        let tenant_id = TenantId::generate();
+
+        let ts = TenantSnapshot {
+            resident_size: 1000,
+            remote_size: 1000,
+            // not yet calculated
+            synthetic_size: 0,
+        };
+
+        let now = SystemTime::now();
+        let now = DateTime::<Utc>::from(now);
+
+        let cached = HashMap::new();
+
+        let mut metrics = Vec::new();
+        ts.to_metrics(tenant_id, now, &cached, &mut metrics);
+
+        assert_eq!(
+            metrics,
+            &[
+                MetricsKey::remote_storage_size(tenant_id).at(now, 1000),
+                MetricsKey::resident_size(tenant_id).at(now, 1000),
+                // no synthetic size here
+            ]
+        );
     }
 
     fn time_backwards<const N: usize>() -> [std::time::SystemTime; N] {
