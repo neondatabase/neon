ed3c1e0d89ed610be916d2c7c69599e30987f425
Heikki Linnakangas <heikki@neon.tech>
2023-09-15 00:59:38 +0300

    Expand the test case to cover HOT UPDATE too.

--- test_runner/regress/test_vm_bits.py
+++ test_runner/regress/test_vm_bits.py
@@ -24,17 +24,22 @@ def test_vm_bit_clear(neon_simple_env: NeonEnv):
     cur.execute("INSERT INTO vmtest_delete VALUES (1)")
     cur.execute("VACUUM FREEZE vmtest_delete")
 
-    cur.execute("CREATE TABLE vmtest_update (id integer PRIMARY KEY)")
-    cur.execute("INSERT INTO vmtest_update SELECT g FROM generate_series(1, 1000) g")
-    cur.execute("VACUUM FREEZE vmtest_update")
+    cur.execute("CREATE TABLE vmtest_hot_update (id integer PRIMARY KEY, filler text)")
+    cur.execute("INSERT INTO vmtest_hot_update VALUES (1, 'x')")
+    cur.execute("VACUUM FREEZE vmtest_hot_update")
 
-    cur.execute("CREATE TABLE vmtest_update2 (id integer PRIMARY KEY, filler text)")
-    cur.execute("INSERT INTO vmtest_update2 SELECT g, '' FROM generate_series(1, 1000) g")
-    cur.execute("VACUUM FREEZE vmtest_update2")
+    cur.execute("CREATE TABLE vmtest_cold_update (id integer PRIMARY KEY)")
+    cur.execute("INSERT INTO vmtest_cold_update SELECT g FROM generate_series(1, 1000) g")
+    cur.execute("VACUUM FREEZE vmtest_cold_update")
+
+    cur.execute("CREATE TABLE vmtest_cold_update2 (id integer PRIMARY KEY, filler text) WITH (fillfactor=100)")
+    cur.execute("INSERT INTO vmtest_cold_update2 SELECT g, '' FROM generate_series(1, 1000) g")
+    cur.execute("VACUUM FREEZE vmtest_cold_update2")
 
     # DELETE and UPDATE the rows.
     cur.execute("DELETE FROM vmtest_delete WHERE id = 1")
-    cur.execute("UPDATE vmtest_update SET id = 5000 WHERE id = 1")
+    cur.execute("UPDATE vmtest_hot_update SET filler='x' WHERE id = 1")
+    cur.execute("UPDATE vmtest_cold_update SET id = 5000 WHERE id = 1")
 
     # Clear the VM bit on the last page with an INSERT. Then clear the VM bit on
     # the page where row 1 is (block 0), by doing an UPDATE. The UPDATE is a
@@ -43,9 +48,9 @@ def test_vm_bit_clear(neon_simple_env: NeonEnv):
     # on the page containing the old tuple. We had a bug where we got the old
     # and new pages mixed up, and that only shows up when one of the bits is
     # cleared, but not the other one.
-    cur.execute("INSERT INTO vmtest_update2 VALUES (9999, 'x')");
+    cur.execute("INSERT INTO vmtest_cold_update2 VALUES (9999, 'x')");
     # Clears the VM bit on the old page
-    cur.execute("UPDATE vmtest_update2 SET id = 5000, filler=repeat('x', 200) WHERE id = 1")
+    cur.execute("UPDATE vmtest_cold_update2 SET id = 5000, filler=repeat('x', 200) WHERE id = 1")
 
     # Branch at this point, to test that later
     fork_at_current_lsn(env, endpoint, "test_vm_bit_clear_new", "test_vm_bit_clear")
@@ -67,9 +72,11 @@ def test_vm_bit_clear(neon_simple_env: NeonEnv):
 
     cur.execute("SELECT id FROM vmtest_delete WHERE id = 1")
     assert cur.fetchall() == []
-    cur.execute("SELECT id FROM vmtest_update WHERE id = 1")
+    cur.execute("SELECT id FROM vmtest_hot_update WHERE id = 1")
     assert cur.fetchall() == []
-    cur.execute("SELECT id FROM vmtest_update2 WHERE id = 1")
+    cur.execute("SELECT id FROM vmtest_cold_update WHERE id = 1")
+    assert cur.fetchall() == []
+    cur.execute("SELECT id FROM vmtest_cold_update2 WHERE id = 1")
     assert cur.fetchall() == []
 
     cur.close()
@@ -96,7 +103,9 @@ def test_vm_bit_clear(neon_simple_env: NeonEnv):
 
     cur_new.execute("SELECT id FROM vmtest_delete WHERE id = 1")
     assert cur_new.fetchall() == []
-    cur_new.execute("SELECT id FROM vmtest_update WHERE id = 1")
+    cur_new.execute("SELECT id FROM vmtest_hot_update WHERE id = 1")
     assert cur_new.fetchall() == []
-    cur_new.execute("SELECT id FROM vmtest_update2 WHERE id = 1")
+    cur_new.execute("SELECT id FROM vmtest_cold_update WHERE id = 1")
+    assert cur_new.fetchall() == []
+    cur_new.execute("SELECT id FROM vmtest_cold_update2 WHERE id = 1")
     assert cur_new.fetchall() == []
