1099c9bbfa77a8ba3fe3c90908fc2a04e2bc20f6
Konstantin Knizhnik <knizhnik@neon.tech>
2023-10-08 15:07:11 +0300

    Fix style

--- pgxn/neon/pagestore_smgr.c
+++ pgxn/neon/pagestore_smgr.c
@@ -858,7 +858,8 @@ prefetch_register_buffer(BufferTag tag, bool *force_latest, XLogRecPtr *force_ls
 	if (flush_every_n_requests > 0 &&
 		MyPState->ring_unused - MyPState->ring_flush >= flush_every_n_requests)
 	{
-		if (!page_server->flush()) {
+		if (!page_server->flush())
+		{
 			/* Prefetch set is reset in case of error, so we should try to register our request once again */
 			goto Retry;
 		}
