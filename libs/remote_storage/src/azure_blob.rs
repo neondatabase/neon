5f0c2c35e1d18be5c0731e478a4480910a793376
Arpad Müller <arpad@neon.tech>
2023-10-11 16:06:32 +0200

    Implement prefix listing

--- libs/remote_storage/src/azure_blob.rs
+++ libs/remote_storage/src/azure_blob.rs
@@ -1,7 +1,11 @@
 //! Azure Blob Storage wrapper
 
+use std::borrow::Cow;
+
 use anyhow::Result;
 use azure_storage_blobs::prelude::ContainerClient;
+use futures_util::StreamExt;
+use http_types::StatusCode;
 use tokio::io::AsyncRead;
 
 use crate::{Download, DownloadError, RemotePath, RemoteStorage, StorageMetadata};
@@ -26,7 +30,39 @@ impl RemoteStorage for AzureBlobStorage {
         &self,
         prefix: Option<&RemotePath>,
     ) -> Result<Vec<RemotePath>, DownloadError> {
-        todo!()
+        let prefix = prefix.map(|p| Cow::from(p.to_string()));
+        let mut builder = self.client.list_blobs();
+        if let Some(prefix) = prefix {
+            builder = builder.prefix(prefix);
+        }
+
+        let mut response = builder.into_stream();
+        let mut res = Vec::new();
+        while let Some(l) = response.next().await {
+            let entry = match l {
+                Ok(l) => l,
+                Err(e) => {
+                    return Err(if let Some(htttp_err) = e.as_http_error() {
+                        match htttp_err.status() {
+                            StatusCode::NotFound => DownloadError::NotFound,
+                            StatusCode::BadRequest => {
+                                DownloadError::BadInput(anyhow::Error::new(e))
+                            }
+                            _ => DownloadError::Other(anyhow::Error::new(e)),
+                        }
+                    } else {
+                        DownloadError::Other(e.into())
+                    });
+                }
+            };
+            let paths_res = entry
+                .blobs
+                .blobs()
+                .map(|bl| RemotePath::from_string(&bl.name));
+            itertools::process_results(paths_res, |paths| res.extend(paths))
+                .map_err(DownloadError::Other)?;
+        }
+        Ok(res)
     }
 
     async fn upload(
