3ade289b389089fcbcdd8b3480c6c90af93d858c
Arseny Sher <sher-ars@yandex.ru>
2023-10-17 17:12:32 +0300

    Add test for heap rewrite and fixes for it.

--- test_runner/regress/test_logical_replication.py
+++ test_runner/regress/test_logical_replication.py
@@ -13,20 +13,28 @@ def test_logical_replication(neon_simple_env: NeonEnv, vanilla_pg):
 
     tenant_id = env.initial_tenant
     timeline_id = env.neon_cli.create_branch("test_logical_replication", "empty")
-    endpoint = env.endpoints.create_start("test_logical_replication")
+    endpoint = env.endpoints.create_start(
+        "test_logical_replication", config_lines=["log_statement=all"]
+    )
 
     log.info("postgres is running on 'test_logical_replication' branch")
     pg_conn = endpoint.connect()
     cur = pg_conn.cursor()
 
     cur.execute("create table t(pk integer primary key, payload integer)")
-    cur.execute("create publication pub1 for table t")
+    cur.execute(
+        "CREATE TABLE replication_example(id SERIAL PRIMARY KEY, somedata int, text varchar(120));"
+    )
+    cur.execute("create publication pub1 for table t, replication_example")
 
     # now start subscriber
     vanilla_pg.start()
     vanilla_pg.safe_psql("create table t(pk integer primary key, payload integer)")
+    vanilla_pg.safe_psql(
+        "CREATE TABLE replication_example(id SERIAL PRIMARY KEY, somedata int, text varchar(120), testcolumn1 int, testcolumn2 int, testcolumn3 int);"
+    )
     connstr = endpoint.connstr().replace("'", "''")
-    print(f"connstr='{connstr}'")
+    log.info(f"ep connstr is {endpoint.connstr()}, subscriber connstr {vanilla_pg.connstr()}")
     vanilla_pg.safe_psql(f"create subscription sub1 connection '{connstr}' publication pub1")
 
     # Wait logical replication channel to be established
@@ -58,8 +66,76 @@ def test_logical_replication(neon_simple_env: NeonEnv, vanilla_pg):
     # Check that subscribers receives all data
     assert vanilla_pg.safe_psql("select count(*) from t")[0][0] == 2000
 
-    sp = endpoint.safe_psql("table pg_replication_slots;")
-    log.info(f"slots are {sp}, connstr {endpoint.connstr()}")
+    # Test that save/restore of RewriteMappingFile works. Partial copy of
+    # rewrite.sql test.
+    log.info("checking rewriteheap")
+    vanilla_pg.stop()
+    cmds = """
+INSERT INTO replication_example(somedata) VALUES (1);
+
+BEGIN;
+INSERT INTO replication_example(somedata) VALUES (2);
+ALTER TABLE replication_example ADD COLUMN testcolumn1 int;
+INSERT INTO replication_example(somedata, testcolumn1) VALUES (3,  1);
+COMMIT;
+
+BEGIN;
+INSERT INTO replication_example(somedata) VALUES (3);
+ALTER TABLE replication_example ADD COLUMN testcolumn2 int;
+INSERT INTO replication_example(somedata, testcolumn1, testcolumn2) VALUES (4,  2, 1);
+COMMIT;
+
+VACUUM FULL pg_am;
+VACUUM FULL pg_amop;
+VACUUM FULL pg_proc;
+VACUUM FULL pg_opclass;
+VACUUM FULL pg_type;
+VACUUM FULL pg_index;
+VACUUM FULL pg_database;
+
+
+-- repeated rewrites that fail
+BEGIN;
+CLUSTER pg_class USING pg_class_oid_index;
+CLUSTER pg_class USING pg_class_oid_index;
+ROLLBACK;
+
+-- repeated rewrites that succeed
+BEGIN;
+CLUSTER pg_class USING pg_class_oid_index;
+CLUSTER pg_class USING pg_class_oid_index;
+CLUSTER pg_class USING pg_class_oid_index;
+COMMIT;
+
+-- repeated rewrites in different transactions
+VACUUM FULL pg_class;
+VACUUM FULL pg_class;
+
+-- reindexing of important relations / indexes
+REINDEX TABLE pg_class;
+REINDEX INDEX pg_class_oid_index;
+REINDEX INDEX pg_class_tblspc_relfilenode_index;
+
+INSERT INTO replication_example(somedata, testcolumn1) VALUES (5, 3);
+
+BEGIN;
+INSERT INTO replication_example(somedata, testcolumn1) VALUES (6, 4);
+ALTER TABLE replication_example ADD COLUMN testcolumn3 int;
+INSERT INTO replication_example(somedata, testcolumn1, testcolumn3) VALUES (7, 5, 1);
+COMMIT;
+"""
+    endpoint.safe_psql_many([q for q in cmds.splitlines() if q != "" and not q.startswith("-")])
+
+    # refetch rewrite files from pageserver
+    endpoint.stop()
+    endpoint.start()
+
+    vanilla_pg.start()
+    logical_replication_sync(vanilla_pg, endpoint)
+    eq_q = "select testcolumn1, testcolumn2, testcolumn3 from replication_example order by 1, 2, 3"
+    assert vanilla_pg.safe_psql(eq_q) == endpoint.safe_psql(eq_q)
+    log.info("rewriteheap synced")
+
     # test that removal of repl slots works across restart
     vanilla_pg.stop()
     time.sleep(1)  # wait for conn termination; active slots can't be dropped
@@ -69,6 +145,5 @@ def test_logical_replication(neon_simple_env: NeonEnv, vanilla_pg):
     wait_for_last_flush_lsn(env, endpoint, tenant_id, timeline_id)
     endpoint.stop()
     endpoint.start()
-    # time.sleep(43432423)
     # it must be gone (but walproposer slot still exists, hence 1)
     assert endpoint.safe_psql("select count(*) from pg_replication_slots")[0][0] == 1
