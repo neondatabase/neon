0d80d6ce1873b067d608acc8942ba9ef909f8c08
Konstantin Knizhnik <knizhnik@garret.ru>
2023-10-25 20:35:23 +0300

    Ignore missed AUX_FILES_KEY when generating image layer (#5660)
    ## Problem
    
    Logical replication requires new AUX_FILES_KEY which is definitely
    absent in existed database.
    We do not have function to check if key exists in our KV storage.
    So I have to handle the error in `list_aux_files` method.
    But this key is also included in key space range and accessed y
    `create_image_layer` method.
    
    ## Summary of changes
    
    Check if AUX_FILES_KEY  exists before including it in keyspace.
    
    ---------
    
    Co-authored-by: Konstantin Knizhnik <knizhnik@neon.tech>
    Co-authored-by: Shany Pozin <shany@neon.tech>
    Co-authored-by: Arpad Müller <arpad-m@users.noreply.github.com>

--- pageserver/src/pgdatadir_mapping.rs
+++ pageserver/src/pgdatadir_mapping.rs
@@ -675,8 +675,9 @@ impl Timeline {
 
         result.add_key(CONTROLFILE_KEY);
         result.add_key(CHECKPOINT_KEY);
-        result.add_key(AUX_FILES_KEY);
-
+        if self.get(AUX_FILES_KEY, lsn, ctx).await.is_ok() {
+            result.add_key(AUX_FILES_KEY);
+        }
         Ok(result.to_keyspace())
     }
 
