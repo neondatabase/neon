ccb9fe9b3387a5d0cdf191abc127a3551a0e84bd
Christian Schwarz <me@cschwarz.com>
2023-10-26 16:30:11 +0000

    find a way to duplicate a tenant in local_fs
    Use the script like so, against the tenant to duplicate:
    
        poetry run python3 ./test_runner/duplicate_tenant.py 7ea51af32d42bfe7fb93bf5f28114d09  200 8
    
    backup of pageserver.toml
    
        d =1
        pg_distrib_dir ='/home/admin/neon-main/pg_install'
        http_auth_type ='Trust'
        pg_auth_type ='Trust'
        listen_http_addr ='127.0.0.1:9898'
        listen_pg_addr ='127.0.0.1:64000'
        broker_endpoint ='http://127.0.0.1:50051/'
        #control_plane_api ='http://127.0.0.1:1234/'
    
        # Initial configuration file created by 'pageserver --init'
        #listen_pg_addr = '127.0.0.1:64000'
        #listen_http_addr = '127.0.0.1:9898'
    
        #wait_lsn_timeout = '60 s'
        #wal_redo_timeout = '60 s'
    
        #max_file_descriptors = 10000
        #page_cache_size = 160000
    
        # initial superuser role name to use when creating a new tenant
        #initial_superuser_name = 'cloud_admin'
    
        #broker_endpoint = 'http://127.0.0.1:50051'
    
        #log_format = 'plain'
    
        #concurrent_tenant_size_logical_size_queries = '1'
    
        #metric_collection_interval = '10 min'
        #cached_metric_collection_interval = '0s'
        #synthetic_size_calculation_interval = '10 min'
    
        #disk_usage_based_eviction = { max_usage_pct = .., min_avail_bytes = .., period = "10s"}
    
        #background_task_maximum_delay = '10s'
    
        [tenant_config]
        #checkpoint_distance = 268435456 # in bytes
        #checkpoint_timeout = 10 m
        #compaction_target_size = 134217728 # in bytes
        #compaction_period = '20 s'
        #compaction_threshold = 10
    
        #gc_period = '1 hr'
        #gc_horizon = 67108864
        #image_creation_threshold = 3
        #pitr_interval = '7 days'
    
        #min_resident_size_override = .. # in bytes
        #evictions_low_residence_duration_metric_threshold = '24 hour'
        #gc_feedback = false
    
        # make it determinsitic
        gc_period = '0s'
        checkpoint_timeout = '3650 day'
        compaction_period = '20 s'
        compaction_threshold = 10
        compaction_target_size = 134217728
        checkpoint_distance = 268435456
        image_creation_threshold = 3
    
        [remote_storage]
        local_path = '/home/admin/neon-main/bench_repo_dir/repo/remote_storage_local_fs'
    
    remove http handler
    
    switch to generalized rewrite_summary  & impl page_ctl subcommand to use it
    
    WIP: change duplicate_tenant.py script to use the pagectl command
    
    The script works but at restart, we detach the created tenants because
    they're not known to the attachment service:
    
      Detaching tenant, control plane omitted it in re-attach response tenant_id=1e399d390e3aee6b11c701cbc716bb6c
    
    => figure out how to further integrate this

--- pageserver/src/http/routes.rs
+++ pageserver/src/http/routes.rs
@@ -764,11 +764,12 @@ async fn tenant_list_handler(
             ApiError::ResourceUnavailable("Tenant map is initializing or shutting down".into())
         })?
         .iter()
-        .map(|(id, state)| TenantInfo {
+        .map(|(id, state, gen)| TenantInfo {
             id: *id,
             state: state.clone(),
             current_physical_size: None,
             attachment_status: state.attachment_status(),
+            generation: (*gen).into(),
         })
         .collect::<Vec<TenantInfo>>();
 
@@ -797,6 +798,7 @@ async fn tenant_status(
             state: state.clone(),
             current_physical_size: Some(current_physical_size),
             attachment_status: state.attachment_status(),
+            generation: tenant.generation().into(),
         })
     }
     .instrument(info_span!("tenant_status_handler", %tenant_id))
