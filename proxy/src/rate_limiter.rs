6144a07b3da4e2e17b50764d6fd36078e1edc25e
Anna Khanova <annakhanova@neon.tech>
2023-11-10 19:07:59 +0100

    Fmt

--- proxy/src/rate_limiter.rs
+++ proxy/src/rate_limiter.rs
@@ -2,5 +2,5 @@ mod aimd;
 mod limit_algorithm;
 mod limiter;
 pub use aimd::Aimd;
-pub use limit_algorithm::{Fixed, RateLimitAlgorithm, RateLimiterConfig, AimdConfig};
+pub use limit_algorithm::{AimdConfig, Fixed, RateLimitAlgorithm, RateLimiterConfig};
 pub use limiter::Limiter;
