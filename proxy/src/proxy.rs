1f5dc5da5851bc813ed94473f99fd32d3eedd05a
Conrad Ludgate <conradludgate@gmail.com>
2023-11-23 18:08:43 +0000

    proxy: fix memory leak again

--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -294,9 +294,11 @@ pub async fn task_main(
                     }),
                 );
             }
-            Some(Err(e)) = connections.join_next(), if !connections.is_empty() => {
-                if !e.is_panic() && !e.is_cancelled() {
-                    warn!("unexpected error from joined connection task: {e:?}");
+            Some(res) = connections.join_next() => {
+                if let Err(e) = res {
+                    if !e.is_panic() && !e.is_cancelled() {
+                        warn!("unexpected error from joined connection task: {e:?}");
+                    }
                 }
             }
             _ = cancellation_token.cancelled() => {
