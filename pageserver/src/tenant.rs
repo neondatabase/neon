37895c21fa8e663b91ba594170654f168fa081c9
Shany Pozin <shany@neon.tech>
2023-11-23 14:13:57 +0200

    monitor both shutdown_was_requested and canell token

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -2616,7 +2616,7 @@ impl Tenant {
         // See comments in [`Tenant::branch_timeline`] for more information
         // about why branch creation task can run concurrently with timeline's GC iteration.
         for timeline in gc_timelines {
-            if task_mgr::is_shutdown_requested() {
+            if task_mgr::is_shutdown_requested() || cancel.is_cancelled() {
                 // We were requested to shut down. Stop and return with the progress we
                 // made.
                 break;
