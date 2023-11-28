585234c7031730924ba46d6fe45c86dda7433500
Arpad Müller <arpad@neon.tech>
2023-11-29 00:27:46 +0100

    Fix test

--- pageserver/src/walingest.rs
+++ pageserver/src/walingest.rs
@@ -2118,7 +2118,7 @@ mod tests {
             .load()
             .await;
         let tline = tenant
-            .bootstrap_timeline(TIMELINE_ID, pg_version, &ctx)
+            .bootstrap_timeline(TIMELINE_ID, pg_version, None, &ctx)
             .await
             .unwrap();
 
