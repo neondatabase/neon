a33f66eeaf47067c775b0449f461cc5cb34f5971
Arpad Müller <arpad@neon.tech>
2023-11-30 19:47:21 +0100

    Don't require local directory to be empty for timeline-delete-after-index-delete failpoint
    This is because now, we do have remains left in S3, so it is an
    "existent" timeline, so clean_up_timelines() does not clean it up.
    
    In other words, we now get a "Failed to load index_part from remote storage, failed creation?"
    warning instead.

--- test_runner/regress/test_timeline_delete.py
+++ test_runner/regress/test_timeline_delete.py
@@ -311,8 +311,9 @@ def test_delete_timeline_exercise_crash_safety_failpoints(
         )
 
     timeline_dir = env.pageserver.timeline_dir(env.initial_tenant, timeline_id)
-    # Check local is empty
-    assert (not timeline_dir.exists()) or len(os.listdir(timeline_dir)) == 0
+    if failpoint != "timeline-delete-after-index-delete":
+        # Check local is empty
+        assert (not timeline_dir.exists()) or len(os.listdir(timeline_dir)) == 0
     # Check no delete mark present
     assert not (timeline_dir.parent / f"{timeline_id}.___deleted").exists()
 
