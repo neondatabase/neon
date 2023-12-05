d6b7bc2abcbcd1e90a16012c4a510e524c4232d8
Christian Schwarz <me@cschwarz.com>
2023-12-05 18:36:55 +0000

    implement a basebackup benchmark

--- pageserver/pagebench/src/main.rs
+++ pageserver/pagebench/src/main.rs
@@ -2,18 +2,21 @@ use clap::Parser;
 
 pub(crate) mod util;
 
+mod basebackup;
 mod getpage_latest_lsn;
 
 /// Component-level performance test for pageserver.
 #[derive(clap::Parser)]
 enum Args {
     GetPageLatestLsn(getpage_latest_lsn::Args),
+    Basebackup(basebackup::Args),
 }
 
 fn main() {
     let args = Args::parse();
     match args {
         Args::GetPageLatestLsn(args) => getpage_latest_lsn::main(args),
+        Args::Basebackup(args) => basebackup::main(args),
     }
     .unwrap()
 }
