a95ee30e0f2337f09e584c8d723a2c603c3c32bd
Joonas Koivunen <joonas@neon.tech>
2023-12-07 13:52:29 +0000

    chore: prettify copypasta

--- libs/remote_storage/tests/test_real_azure.rs
+++ libs/remote_storage/tests/test_real_azure.rs
@@ -7,7 +7,9 @@ use std::sync::Arc;
 use std::time::UNIX_EPOCH;
 
 use anyhow::Context;
+use bytes::Bytes;
 use camino::Utf8Path;
+use futures::stream::Stream;
 use once_cell::sync::OnceCell;
 use remote_storage::{
     AzureConfig, Download, GenericRemoteStorage, RemotePath, RemoteStorageConfig, RemoteStorageKind,
@@ -616,14 +618,14 @@ async fn upload_simple_azure_data(
 fn upload_stream(
     content: std::borrow::Cow<'static, [u8]>,
 ) -> (
-    impl futures::stream::Stream<Item = std::io::Result<bytes::Bytes>> + Send + Sync + 'static,
+    impl Stream<Item = std::io::Result<Bytes>> + Send + Sync + 'static,
     usize,
 ) {
     use std::borrow::Cow;
 
     let content = match content {
-        Cow::Borrowed(x) => bytes::Bytes::from_static(x),
-        Cow::Owned(vec) => bytes::Bytes::from(vec),
+        Cow::Borrowed(x) => Bytes::from_static(x),
+        Cow::Owned(vec) => Bytes::from(vec),
     };
     wrap_stream(content)
 }
@@ -631,7 +633,7 @@ fn upload_stream(
 fn wrap_stream(
     content: bytes::Bytes,
 ) -> (
-    impl futures::stream::Stream<Item = std::io::Result<bytes::Bytes>> + Send + Sync + 'static,
+    impl Stream<Item = std::io::Result<Bytes>> + Send + Sync + 'static,
     usize,
 ) {
     let len = content.len();
--- libs/remote_storage/tests/test_real_s3.rs
+++ libs/remote_storage/tests/test_real_s3.rs
@@ -7,7 +7,9 @@ use std::sync::Arc;
 use std::time::UNIX_EPOCH;
 
 use anyhow::Context;
+use bytes::Bytes;
 use camino::Utf8Path;
+use futures::stream::Stream;
 use once_cell::sync::OnceCell;
 use remote_storage::{
     GenericRemoteStorage, RemotePath, RemoteStorageConfig, RemoteStorageKind, S3Config,
@@ -541,14 +543,14 @@ async fn upload_simple_s3_data(
 fn upload_stream(
     content: std::borrow::Cow<'static, [u8]>,
 ) -> (
-    impl futures::stream::Stream<Item = std::io::Result<bytes::Bytes>> + Send + Sync + 'static,
+    impl Stream<Item = std::io::Result<Bytes>> + Send + Sync + 'static,
     usize,
 ) {
     use std::borrow::Cow;
 
     let content = match content {
-        Cow::Borrowed(x) => bytes::Bytes::from_static(x),
-        Cow::Owned(vec) => bytes::Bytes::from(vec),
+        Cow::Borrowed(x) => Bytes::from_static(x),
+        Cow::Owned(vec) => Bytes::from(vec),
     };
     wrap_stream(content)
 }
@@ -556,7 +558,7 @@ fn upload_stream(
 fn wrap_stream(
     content: bytes::Bytes,
 ) -> (
-    impl futures::stream::Stream<Item = std::io::Result<bytes::Bytes>> + Send + Sync + 'static,
+    impl Stream<Item = std::io::Result<Bytes>> + Send + Sync + 'static,
     usize,
 ) {
     let len = content.len();
