8d604a1390d3522b6d66e3d5f49342bc2c10f381
Christian Schwarz <me@cschwarz.com>
2023-12-11 17:28:45 +0000

    drag page_cache into repro

--- repro-problem/src/main.rs
+++ repro-problem/src/main.rs
@@ -1,11 +1,26 @@
+use page_cache::FileId;
 use virtual_file::VirtualFile;
 
+mod page_cache;
 mod virtual_file;
 
 #[tokio::main]
 async fn main() {
+    page_cache::init(10);
 
-    let file = VirtualFile::open(camino::Utf8Path::new("foo")).await.unwrap();
-    file.read_exact_at().await;
+    let cache = page_cache::get();
 
+    let res = cache
+        .read_immutable_buf(page_cache::next_file_id(), 0)
+        .await
+        .unwrap();
+    match res {
+        page_cache::ReadBufResult::Found(found) => todo!(),
+        page_cache::ReadBufResult::NotFound(write_guard) => {
+            let file = VirtualFile::open(camino::Utf8Path::new("foo"))
+                .await
+                .unwrap();
+            file.read_exact_at(write_guard, 0).await;
+        }
+    }
 }
