e41e6e7a15ed51a3520c740785760222ec5a965b
Christian Schwarz <christian@neon.tech>
2023-12-13 13:53:11 +0000

    pagebench: add a 'getpage@lsn' benchmark

--- pageserver/pagebench/src/main.rs
+++ pageserver/pagebench/src/main.rs
@@ -3,17 +3,20 @@ use clap::Parser;
 pub(crate) mod util;
 
 mod basebackup;
+mod getpage_latest_lsn;
 
 /// Component-level performance test for pageserver.
 #[derive(clap::Parser)]
 enum Args {
     Basebackup(basebackup::Args),
+    GetPageLatestLsn(getpage_latest_lsn::Args),
 }
 
 fn main() {
     let args = Args::parse();
     match args {
         Args::Basebackup(args) => basebackup::main(args),
+        Args::GetPageLatestLsn(args) => getpage_latest_lsn::main(args),
     }
     .unwrap()
 }
