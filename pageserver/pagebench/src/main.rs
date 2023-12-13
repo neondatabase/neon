5ce5874ddf1dc4f86ea4eb60edf9e49e2324a3ae
Christian Schwarz <christian@neon.tech>
2023-12-13 14:04:22 +0000

    pagebench: WIP: command to trigger initial logical size calculation

--- pageserver/pagebench/src/main.rs
+++ pageserver/pagebench/src/main.rs
@@ -4,12 +4,14 @@ pub(crate) mod util;
 
 mod basebackup;
 mod getpage_latest_lsn;
+mod trigger_initial_size_calculation;
 
 /// Component-level performance test for pageserver.
 #[derive(clap::Parser)]
 enum Args {
     Basebackup(basebackup::Args),
     GetPageLatestLsn(getpage_latest_lsn::Args),
+    TriggerInitialSizeCalculation(trigger_initial_size_calculation::Args),
 }
 
 fn main() {
@@ -17,6 +19,7 @@ fn main() {
     match args {
         Args::Basebackup(args) => basebackup::main(args),
         Args::GetPageLatestLsn(args) => getpage_latest_lsn::main(args),
+        Args::TriggerInitialSizeCalculation(args) => trigger_initial_size_calculation::main(args),
     }
     .unwrap()
 }
