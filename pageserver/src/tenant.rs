215cdd18c4032adf915332252fcd4a685b9ca406
Arpad Müller <arpad-m@users.noreply.github.com>
2023-12-15 13:11:25 +0100

    Make initdb upload retries cancellable and seek to beginning (#6147)
    * initdb uploads had no cancellation token, which means that when we
    were stuck in upload retries, we wouldn't be able to delete the
    timeline. in general, the combination of retrying forever and not having
    cancellation tokens is quite dangerous.
    * initdb uploads wouldn't rewind the file. this wasn't discovered in the
    purposefully unreliable test-s3 in pytest because those fail on the
    first byte always, not somewhere during the connection. we'd be getting
    errors from the AWS sdk that the file was at an unexpected end.
    
    slack thread: https://neondb.slack.com/archives/C033RQ5SPDH/p1702632247784079

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -3106,8 +3106,9 @@ impl Tenant {
                     3,
                     u32::MAX,
                     "persist_initdb_tar_zst",
-                    // TODO: use a cancellation token (https://github.com/neondatabase/neon/issues/5066)
-                    backoff::Cancel::new(CancellationToken::new(), || unreachable!()),
+                    backoff::Cancel::new(self.cancel.clone(), || {
+                        anyhow::anyhow!("initdb upload cancelled")
+                    }),
                 )
                 .await?;
 
