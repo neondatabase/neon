9b468b2f81220e3c4ca6b10b5830407d3eaa1e61
Christian Schwarz <christian@neon.tech>
2023-12-18 11:18:25 +0000

    preserve the information about which layer it was in case of errors

--- pageserver/src/tenant/storage_layer/layer.rs
+++ pageserver/src/tenant/storage_layer/layer.rs
@@ -261,6 +261,7 @@ impl Layer {
             .get_value_reconstruct_data(key, lsn_range, reconstruct_data, &self.0, ctx)
             .instrument(tracing::debug_span!("get_value_reconstruct_data", layer=%self))
             .await
+            .with_context(|| format!("get_value_reconstruct_data for layer {self}"))
     }
 
     /// Download the layer if evicted.
