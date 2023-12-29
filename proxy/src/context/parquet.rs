f161017da9534b2822c146e97571b565fb27f675
Conrad Ludgate <conradludgate@gmail.com>
2023-12-29 16:02:46 +0000

    add file tests

--- proxy/src/context/parquet.rs
+++ proxy/src/context/parquet.rs
@@ -5,7 +5,10 @@ use bytes::BytesMut;
 use futures::{Future, Stream, StreamExt};
 use parquet::{
     basic::ZstdLevel,
-    file::{properties::WriterProperties, writer::SerializedFileWriter},
+    file::{
+        properties::{WriterProperties, WriterPropertiesPtr},
+        writer::SerializedFileWriter,
+    },
     record::RecordWriter,
 };
 use remote_storage::{GenericRemoteStorage, RemotePath, RemoteStorageConfig, RemoteStorageKind};
@@ -88,30 +91,47 @@ pub async fn worker(cancellation_token: CancellationToken) -> anyhow::Result<()>
     });
     let rx = rx.map(RequestData::from);
 
-    worker_inner(storage, rx).await
+    worker_inner(storage, rx, ParquetConfig::default()).await
+}
+
+struct ParquetConfig {
+    propeties: WriterPropertiesPtr,
+    rows_per_group: usize,
+    file_size: i64,
+}
+
+impl Default for ParquetConfig {
+    fn default() -> Self {
+        Self {
+            propeties: Arc::new(
+                WriterProperties::builder()
+                    .set_compression(parquet::basic::Compression::ZSTD(ZstdLevel::default()))
+                    .build(),
+            ),
+            // 8192 takes about 40 seconds in us-east-2 to fill up
+            rows_per_group: 8192,
+            // 100 MiB
+            file_size: 100 * 1024 * 1024,
+        }
+    }
 }
 
 async fn worker_inner(
     storage: GenericRemoteStorage,
     rx: impl Stream<Item = RequestData>,
+    config: ParquetConfig,
 ) -> anyhow::Result<()> {
     let mut rx = std::pin::pin!(rx);
-    let mut rows = Vec::with_capacity(1024);
+
+    let mut rows = Vec::with_capacity(config.rows_per_group);
 
     let schema = rows.as_slice().schema()?;
-    let properties = Arc::new(
-        WriterProperties::builder()
-            .set_compression(parquet::basic::Compression::ZSTD(ZstdLevel::default()))
-            .build(),
-    );
-
-    let mut w =
-        SerializedFileWriter::new(BytesWriter::default(), schema.clone(), properties.clone())
-            .unwrap();
+    let file = BytesWriter::default();
+    let mut w = SerializedFileWriter::new(file, schema.clone(), config.propeties.clone())?;
 
     while let Some(row) = rx.next().await {
         rows.push(row);
-        if rows.len() == 1024 {
+        if rows.len() == config.rows_per_group {
             (rows, w) = flush_rows(rows, w).await?;
 
             let len = w
@@ -119,9 +139,9 @@ async fn worker_inner(
                 .iter()
                 .map(|rg| rg.compressed_size())
                 .sum::<i64>();
-            if len > 100 * 1024 * 1024 {
+            if len > config.file_size {
                 let file = upload_parquet(w, len, &storage).await?;
-                w = SerializedFileWriter::new(file, schema.clone(), properties.clone())?;
+                w = SerializedFileWriter::new(file, schema.clone(), config.propeties.clone())?;
             }
         }
     }
@@ -219,5 +239,175 @@ impl std::io::Write for BytesWriter {
 
     fn flush(&mut self) -> std::io::Result<()> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{net::Ipv4Addr, sync::Arc};
+
+    use camino::Utf8Path;
+    use itertools::Itertools;
+    use parquet::{
+        basic::ZstdLevel,
+        file::{
+            properties::WriterProperties, reader::FileReader,
+            serialized_reader::SerializedFileReader,
+        },
+    };
+    use rand::{rngs::StdRng, Rng, SeedableRng};
+    use remote_storage::{GenericRemoteStorage, RemoteStorageConfig, RemoteStorageKind};
+
+    use super::{worker_inner, ParquetConfig, RequestData};
+
+    fn generate_request_data(rng: &mut impl Rng) -> RequestData {
+        RequestData {
+            session_id: uuid::Builder::from_random_bytes(rng.gen()).into_uuid(),
+            peer_addr: Ipv4Addr::from(rng.gen::<[u8; 4]>()).to_string(),
+            timestamp: chrono::NaiveDateTime::from_timestamp_millis(
+                rng.gen_range(1703862754..1803862754),
+            )
+            .unwrap(),
+            application_name: Some("test".to_owned()),
+            username: Some(hex::encode(rng.gen::<[u8; 4]>())),
+            endpoint_id: Some(hex::encode(rng.gen::<[u8; 4]>())),
+            project: Some(hex::encode(rng.gen::<[u8; 4]>())),
+            branch: Some(hex::encode(rng.gen::<[u8; 4]>())),
+            protocol: "random",
+            region: "tests",
+        }
+    }
+
+    async fn run_test(tmpdir: &Utf8Path, config: ParquetConfig) -> Vec<(u64, usize, i64)> {
+        let remote_storage_config = RemoteStorageConfig {
+            storage: RemoteStorageKind::LocalFs(tmpdir.to_path_buf()),
+        };
+        let storage = GenericRemoteStorage::from_config(&remote_storage_config).unwrap();
+
+        // stable rng
+        let mut rng = StdRng::from_seed([0x39; 32]);
+        let rx = std::iter::repeat_with(|| generate_request_data(&mut rng))
+            .take(100_000)
+            .collect_vec();
+
+        worker_inner(storage.clone(), futures::stream::iter(rx), config)
+            .await
+            .unwrap();
+
+        let mut files = storage.list_files(None).await.unwrap();
+        files.sort();
+
+        files
+            .into_iter()
+            .map(|path| std::fs::File::open(tmpdir.join(path.get_path()).as_std_path()).unwrap())
+            .map(|file| {
+                (
+                    file.metadata().unwrap(),
+                    SerializedFileReader::new(file).unwrap().metadata().clone(),
+                )
+            })
+            .map(|(file_meta, parquet_meta)| {
+                (
+                    file_meta.len(),
+                    parquet_meta.num_row_groups(),
+                    parquet_meta.file_metadata().num_rows(),
+                )
+            })
+            .collect()
+    }
+
+    #[tokio::test]
+    async fn verify_parquet_no_compression() {
+        let tmpdir = camino_tempfile::tempdir().unwrap();
+
+        let config = ParquetConfig {
+            propeties: Arc::new(WriterProperties::new()),
+            rows_per_group: 2_000,
+            file_size: 1_000_000,
+        };
+
+        let file_stats = run_test(tmpdir.path(), config).await;
+
+        assert_eq!(
+            file_stats,
+            [
+                (1186400, 6, 12000),
+                (1186279, 6, 12000),
+                (1186301, 6, 12000),
+                (1186520, 6, 12000),
+                (1186486, 6, 12000),
+                (1186560, 6, 12000),
+                (1186537, 6, 12000),
+                (1186366, 6, 12000),
+                (395418, 2, 4000),
+            ],
+        );
+
+        tmpdir.close().unwrap();
+    }
+
+    #[tokio::test]
+    async fn verify_parquet_min_compression() {
+        let tmpdir = camino_tempfile::tempdir().unwrap();
+
+        let config = ParquetConfig {
+            propeties: Arc::new(
+                WriterProperties::builder()
+                    .set_compression(parquet::basic::Compression::ZSTD(ZstdLevel::default()))
+                    .build(),
+            ),
+            rows_per_group: 2_000,
+            file_size: 1_000_000,
+        };
+
+        let file_stats = run_test(tmpdir.path(), config).await;
+
+        // with compression, there are fewer files with more rows per file
+        assert_eq!(
+            file_stats,
+            [
+                (1024754, 9, 18000),
+                (1019868, 9, 18000),
+                (1023771, 9, 18000),
+                (1018223, 9, 18000),
+                (1021727, 9, 18000),
+                (568302, 5, 10000),
+            ],
+        );
+
+        tmpdir.close().unwrap();
+    }
+
+    #[tokio::test]
+    async fn verify_parquet_strong_compression() {
+        let tmpdir = camino_tempfile::tempdir().unwrap();
+
+        let config = ParquetConfig {
+            propeties: Arc::new(
+                WriterProperties::builder()
+                    .set_compression(parquet::basic::Compression::ZSTD(
+                        ZstdLevel::try_new(10).unwrap(),
+                    ))
+                    .build(),
+            ),
+            rows_per_group: 2_000,
+            file_size: 1_000_000,
+        };
+
+        let file_stats = run_test(tmpdir.path(), config).await;
+
+        // with strong compression, there are even fewer files with more rows per file
+        assert_eq!(
+            file_stats,
+            [
+                (1114282, 10, 20000),
+                (1114441, 10, 20000),
+                (1114420, 10, 20000),
+                (1114353, 10, 20000),
+                (1114591, 10, 20000),
+            ],
+        );
+
+        tmpdir.close().unwrap();
     }
 }
