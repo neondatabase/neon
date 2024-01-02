02c00b14183f37971f6b9d43290f07ce09dc4f4f
John Spray <john@neon.tech>
2024-01-02 14:23:13 +0000

    compute_api: add shard_stripe_size to ComputeSpec

--- libs/compute_api/src/spec.rs
+++ libs/compute_api/src/spec.rs
@@ -75,6 +75,9 @@ pub struct ComputeSpec {
     pub remote_extensions: Option<RemoteExtSpec>,
 
     pub pgbouncer_settings: Option<HashMap<String, String>>,
+
+    // Stripe size for pageserver sharding, in pages
+    pub shard_stripe_size: Option<usize>,
 }
 
 /// Feature flag to signal `compute_ctl` to enable certain experimental functionality.
