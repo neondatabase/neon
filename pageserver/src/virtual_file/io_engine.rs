9c9744ba4b7be4c2613d7dcd4a8e7e735558777d
Christian Schwarz <christian@neon.tech>
2024-01-12 10:32:33 +0000

    refactor(VirtualFile): configurable IO engine

---
+++ pageserver/src/virtual_file/io_engine.rs
@@ -0,0 +1,95 @@
+//! [`super::VirtualFile`] supports different IO engines.
+//!
+//! The [`IoEngineKind`] enum identifies them.
+//!
+//! The choice of IO engine is global.
+//! Initialize using [`init`].
+//!
+//! Then use [`get`] and  [`OpenOptions`].
+
+#[derive(
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    Hash,
+    strum_macros::EnumString,
+    strum_macros::Display,
+    serde_with::DeserializeFromStr,
+    serde_with::SerializeDisplay,
+    Debug,
+)]
+#[strum(serialize_all = "kebab-case")]
+pub enum IoEngineKind {
+    StdFs,
+}
+
+static IO_ENGINE: once_cell::sync::OnceCell<IoEngineKind> = once_cell::sync::OnceCell::new();
+
+#[cfg(not(test))]
+pub(super) fn init(engine: IoEngineKind) {
+    if IO_ENGINE.set(engine).is_err() {
+        panic!("called twice");
+    }
+}
+
+pub(super) fn get() -> &'static IoEngineKind {
+    #[cfg(test)]
+    {
+        let env_var_name = "NEON_PAGESERVER_UNIT_TEST_VIRTUAL_FILE_IOENGINE";
+        IO_ENGINE.get_or_init(|| match std::env::var(env_var_name) {
+            Ok(v) => match v.parse::<IoEngineKind>() {
+                Ok(engine_kind) => engine_kind,
+                Err(e) => {
+                    panic!("invalid VirtualFile io engine for env var {env_var_name}: {e:#}: {v:?}")
+                }
+            },
+            Err(std::env::VarError::NotPresent) => {
+                crate::config::defaults::DEFAULT_VIRTUAL_FILE_IO_ENGINE
+                    .parse()
+                    .unwrap()
+            }
+            Err(std::env::VarError::NotUnicode(_)) => {
+                panic!("env var {env_var_name} is not unicode");
+            }
+        })
+    }
+    #[cfg(not(test))]
+    IO_ENGINE.get().unwrap()
+}
+
+use std::os::unix::prelude::FileExt;
+
+use super::FileGuard;
+
+impl IoEngineKind {
+    pub(super) async fn read_at<B>(
+        &self,
+        file_guard: FileGuard,
+        offset: u64,
+        mut buf: B,
+    ) -> ((FileGuard, B), std::io::Result<usize>)
+    where
+        B: tokio_epoll_uring::BoundedBufMut + Send,
+    {
+        match self {
+            IoEngineKind::StdFs => {
+                // SAFETY: `dst` only lives at most as long as this match arm, during which buf remains valid memory.
+                let dst = unsafe {
+                    std::slice::from_raw_parts_mut(buf.stable_mut_ptr(), buf.bytes_total())
+                };
+                let res = file_guard.with_std_file(|std_file| std_file.read_at(dst, offset));
+                if let Ok(nbytes) = &res {
+                    assert!(*nbytes <= buf.bytes_total());
+                    // SAFETY: see above assertion
+                    unsafe {
+                        buf.set_init(*nbytes);
+                    }
+                }
+                #[allow(dropping_references)]
+                drop(dst);
+                ((file_guard, buf), res)
+            }
+        }
+    }
+}
--- pageserver/src/virtual_file/open_options.rs
+++ pageserver/src/virtual_file/open_options.rs
@@ -2,12 +2,18 @@
 
 use std::{os::fd::OwnedFd, path::Path};
 
+use super::IoEngineKind;
+
 #[derive(Debug, Clone)]
-pub struct OpenOptions(std::fs::OpenOptions);
+pub enum OpenOptions {
+    StdFs(std::fs::OpenOptions),
+}
 
 impl Default for OpenOptions {
     fn default() -> Self {
-        Self(std::fs::OpenOptions::new())
+        match super::io_engine::get() {
+            IoEngineKind::StdFs => Self::StdFs(std::fs::OpenOptions::new()),
+        }
     }
 }
 
@@ -17,43 +23,73 @@ impl OpenOptions {
     }
 
     pub fn read(&mut self, read: bool) -> &mut OpenOptions {
-        let _ = self.0.read(read);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.read(read);
+            }
+        }
         self
     }
 
     pub fn write(&mut self, write: bool) -> &mut OpenOptions {
-        let _ = self.0.write(write);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.write(write);
+            }
+        }
         self
     }
 
     pub fn create(&mut self, create: bool) -> &mut OpenOptions {
-        let _ = self.0.create(create);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.create(create);
+            }
+        }
         self
     }
 
     pub fn create_new(&mut self, create_new: bool) -> &mut OpenOptions {
-        let _ = self.0.create_new(create_new);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.create_new(create_new);
+            }
+        }
         self
     }
 
     pub fn truncate(&mut self, truncate: bool) -> &mut OpenOptions {
-        let _ = self.0.truncate(truncate);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.truncate(truncate);
+            }
+        }
         self
     }
 
     pub(in crate::virtual_file) async fn open(&self, path: &Path) -> std::io::Result<OwnedFd> {
-        self.0.open(path).map(|file| file.into())
+        match self {
+            OpenOptions::StdFs(x) => x.open(path).map(|file| file.into()),
+        }
     }
 }
 
 impl std::os::unix::prelude::OpenOptionsExt for OpenOptions {
     fn mode(&mut self, mode: u32) -> &mut OpenOptions {
-        let _ = self.0.mode(mode);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.mode(mode);
+            }
+        }
         self
     }
 
     fn custom_flags(&mut self, flags: i32) -> &mut OpenOptions {
-        let _ = self.0.custom_flags(flags);
+        match self {
+            OpenOptions::StdFs(x) => {
+                let _ = x.custom_flags(flags);
+            }
+        }
         self
     }
 }
