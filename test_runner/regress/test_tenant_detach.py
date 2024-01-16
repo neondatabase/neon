0337d6a852fa14bc1f33404254b8e42f7e7160cb
Abhijeet Patil <abhijeet@neon.tech>
2024-01-16 15:35:56 +0000

    added page server warning message exclusion

--- test_runner/regress/test_tenant_detach.py
+++ test_runner/regress/test_tenant_detach.py
@@ -748,9 +748,10 @@ def test_metrics_while_ignoring_broken_tenant_and_reloading(
     env = neon_env_builder.init_start()
 
     client = env.pageserver.http_client()
-    env.pageserver.allowed_errors.append(
-        r".* Changing Active tenant to Broken state, reason: broken from test"
-    )
+    env.pageserver.allowed_errors.append([
+        r".* Changing Active tenant to Broken state, reason: broken from test",
+        ".*Dropped remote consistent LSN updates for tenant *."
+    ])
 
     def only_int(samples: List[Sample]) -> Optional[int]:
         if len(samples) == 1:
