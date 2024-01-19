fdc92817f21e41dca1ee8eaec60a207b3bcc7837
Heikki Linnakangas <heikki@neon.tech>
2024-01-19 16:10:09 +0200

    Fix comment on the test

--- test_runner/regress/test_pageserver_reconnect.py
+++ test_runner/regress/test_pageserver_reconnect.py
@@ -1,6 +1,3 @@
-# This test spawns pgbench in a thread in the background and concurrently restarts pageserver,
-# checking how client is able to transparently restore connection to pageserver
-#
 import threading
 import time
 from contextlib import closing
@@ -9,8 +6,11 @@ from fixtures.log_helper import log
 from fixtures.neon_fixtures import NeonEnv, PgBin
 
 
-# Test restarting page server, while safekeeper and compute node keep
-# running.
+# Test updating neon.pageserver_connstring setting on the fly.
+#
+# This merely changes some whitespace in the connection string, so
+# this doesn't prove that the new string actually takes effect. But at
+# least the code gets exercised.
 def test_pageserver_reconnect(neon_simple_env: NeonEnv, pg_bin: PgBin):
     env = neon_simple_env
     env.neon_cli.create_branch("test_pageserver_restarts")
