9ef31bc950cfd9a18e79a5c56e111fae04d84fdc
Abhijeet Patil <abhijeet@neon.tech>
2024-01-19 14:26:58 +0000

    reverting debug changes

--- libs/walproposer/build.rs
+++ libs/walproposer/build.rs
@@ -41,7 +41,6 @@ fn main() -> anyhow::Result<()> {
     println!("cargo:rustc-link-lib=static=walproposer");
     println!("cargo:rustc-link-search={walproposer_lib_search_str}");
 
-    println!("{:?}", pg_install_abs.into_os_string().into_string());
     let pg_config_bin = pg_install_abs.join("v16").join("bin").join("pg_config");
     let inc_server_path: String = if pg_config_bin.exists() {
         let output = Command::new(pg_config_bin)
