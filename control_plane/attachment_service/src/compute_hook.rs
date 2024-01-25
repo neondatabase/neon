a60c18696ab87f6700d4fe5e7d0cef4bbffa9aed
John Spray <john@neon.tech>
2024-01-25 12:04:11 +0000

    control_plane/attachment_service: implement HTTP compute hook

--- control_plane/attachment_service/src/compute_hook.rs
+++ control_plane/attachment_service/src/compute_hook.rs
@@ -2,10 +2,15 @@ use std::collections::HashMap;
 
 use control_plane::endpoint::ComputeControlPlane;
 use control_plane::local_env::LocalEnv;
+use hyper::{Method, StatusCode};
 use pageserver_api::shard::{ShardCount, ShardIndex, ShardNumber, TenantShardId};
 use postgres_connection::parse_host_port;
 use serde::{Deserialize, Serialize};
-use utils::id::{NodeId, TenantId};
+use tokio_util::sync::CancellationToken;
+use utils::{
+    backoff,
+    id::{NodeId, TenantId},
+};
 
 use crate::service::Config;
 
@@ -13,31 +18,54 @@ pub(super) struct ComputeHookTenant {
     shards: Vec<(ShardIndex, NodeId)>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ComputeHookNotifyRequestShard {
     node_id: NodeId,
     shard_number: ShardNumber,
 }
 
 /// Request body that we send to the control plane to notify it of where a tenant is attached
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ComputeHookNotifyRequest {
     tenant_id: TenantId,
     shards: Vec<ComputeHookNotifyRequestShard>,
 }
 
+/// Error type for attempts to call into the control plane compute notification hook
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum NotifyError {
+    // Request was not send successfully, e.g. transport error
+    #[error("Sending request: {0}")]
+    Request(#[from] reqwest::Error),
+    // Request could not be serviced right now due to ongoing Operation in control plane, but should be possible soon.
+    #[error("Control plane tenant busy")]
+    Busy,
+    // Explicit 429 response asking us to retry less frequently
+    #[error("Control plane overloaded")]
+    SlowDown,
+    // A 503 response indicates the control plane can't handle the request right now
+    #[error("Control plane unavailable (status {0})")]
+    Unavailable(StatusCode),
+    // API returned unexpected non-success status.  We will retry, but log a warning.
+    #[error("Control plane returned unexpected status {0}")]
+    Unexpected(StatusCode),
+    // We shutdown while sending
+    #[error("Shutting down")]
+    ShuttingDown,
+    // A response indicates we will never succeed, such as 400 or 404
+    #[error("Non-retryable error {0}")]
+    Fatal(StatusCode),
+}
+
 impl ComputeHookTenant {
-    async fn maybe_reconfigure(
-        &mut self,
-        tenant_id: TenantId,
-    ) -> anyhow::Result<Option<ComputeHookNotifyRequest>> {
+    async fn maybe_reconfigure(&mut self, tenant_id: TenantId) -> Option<ComputeHookNotifyRequest> {
         // Find the highest shard count and drop any shards that aren't
         // for that shard count.
         let shard_count = self.shards.iter().map(|(k, _v)| k.shard_count).max();
         let Some(shard_count) = shard_count else {
             // No shards, nothing to do.
             tracing::info!("ComputeHookTenant::maybe_reconfigure: no shards");
-            return Ok(None);
+            return None;
         };
 
         self.shards.retain(|(k, _v)| k.shard_count == shard_count);
@@ -46,7 +74,7 @@ impl ComputeHookTenant {
 
         if self.shards.len() == shard_count.0 as usize || shard_count == ShardCount(0) {
             // We have pageservers for all the shards: emit a configuration update
-            return Ok(Some(ComputeHookNotifyRequest {
+            return Some(ComputeHookNotifyRequest {
                 tenant_id,
                 shards: self
                     .shards
@@ -56,7 +84,7 @@ impl ComputeHookTenant {
                         node_id: *node_id,
                     })
                     .collect(),
-            }));
+            });
         } else {
             tracing::info!(
                 "ComputeHookTenant::maybe_reconfigure: not enough shards ({}/{})",
@@ -65,7 +93,7 @@ impl ComputeHookTenant {
             );
         }
 
-        Ok(None)
+        None
     }
 }
 
@@ -75,13 +103,20 @@ impl ComputeHookTenant {
 pub(super) struct ComputeHook {
     config: Config,
     state: tokio::sync::Mutex<HashMap<TenantId, ComputeHookTenant>>,
+    authorization_header: Option<String>,
 }
 
 impl ComputeHook {
     pub(super) fn new(config: Config) -> Self {
+        let authorization_header = config
+            .control_plane_jwt_token
+            .clone()
+            .map(|jwt| format!("Bearer {}", jwt));
+
         Self {
             state: Default::default(),
             config,
+            authorization_header,
         }
     }
 
@@ -123,6 +158,81 @@ impl ComputeHook {
         Ok(())
     }
 
+    async fn do_notify_iteration(
+        &self,
+        client: &reqwest::Client,
+        url: &String,
+        reconfigure_request: &ComputeHookNotifyRequest,
+    ) -> Result<(), NotifyError> {
+        let req = client.request(Method::POST, url);
+        let req = if let Some(value) = &self.authorization_header {
+            req.header(reqwest::header::AUTHORIZATION, value)
+        } else {
+            req
+        };
+
+        tracing::debug!(
+            "Sending notify request to {} ({:?})",
+            url,
+            reconfigure_request
+        );
+        let send_result = req.json(&reconfigure_request).send().await;
+        let response = match send_result {
+            Ok(r) => r,
+            Err(e) => return Err(e.into()),
+        };
+
+        // Treat all 2xx responses as success
+        if response.status() >= StatusCode::OK && response.status() < StatusCode::MULTIPLE_CHOICES {
+            if response.status() != StatusCode::OK {
+                // Non-200 2xx response: it doesn't make sense to retry, but this is unexpected, so
+                // log a warning.
+                tracing::warn!(
+                    "Unexpected 2xx response code {} from control plane",
+                    response.status()
+                );
+            }
+
+            return Ok(());
+        }
+
+        // Error response codes:
+        //  - 423: an operation in progress prevents reconfiguring compute
+        //  - 429: please delay, under heavy load
+        //  - 5xx: control plane is unavailable, perhaps undergoing restart.
+        //  - 200: compute has been updated (or wasn't running)
+        //  - other 2xx: unexpected
+        match response.status() {
+            StatusCode::TOO_MANY_REQUESTS => Err(NotifyError::SlowDown),
+            StatusCode::LOCKED => Err(NotifyError::Busy),
+            StatusCode::SERVICE_UNAVAILABLE
+            | StatusCode::GATEWAY_TIMEOUT
+            | StatusCode::BAD_GATEWAY => Err(NotifyError::Unavailable(response.status())),
+            StatusCode::BAD_REQUEST | StatusCode::UNAUTHORIZED | StatusCode::FORBIDDEN => {
+                Err(NotifyError::Fatal(response.status()))
+            }
+            _ => Err(NotifyError::Unexpected(response.status())),
+        }
+    }
+
+    async fn do_notify(
+        &self,
+        url: &String,
+        reconfigure_request: ComputeHookNotifyRequest,
+    ) -> Result<(), NotifyError> {
+        let client = reqwest::Client::new();
+        let cancel = CancellationToken::new();
+        backoff::retry(
+            || self.do_notify_iteration(&client, url, &reconfigure_request),
+            |e| matches!(e, NotifyError::Fatal(_)),
+            3,
+            10,
+            "Send compute notification",
+            backoff::Cancel::new(cancel, || NotifyError::ShuttingDown),
+        )
+        .await
+    }
+
     /// Call this to notify the compute (postgres) tier of new pageservers to use
     /// for a tenant.  notify() is called by each shard individually, and this function
     /// will decide whether an update to the tenant is sent.  An update is sent on the
@@ -134,7 +244,7 @@ impl ComputeHook {
         &self,
         tenant_shard_id: TenantShardId,
         node_id: NodeId,
-    ) -> anyhow::Result<()> {
+    ) -> Result<(), NotifyError> {
         let mut locked = self.state.lock().await;
         let entry = locked
             .entry(tenant_shard_id.tenant_id)
@@ -156,7 +266,7 @@ impl ComputeHook {
             entry.shards.push((shard_index, node_id));
         }
 
-        let reconfigure_request = entry.maybe_reconfigure(tenant_shard_id.tenant_id).await?;
+        let reconfigure_request = entry.maybe_reconfigure(tenant_shard_id.tenant_id).await;
         let Some(reconfigure_request) = reconfigure_request else {
             // The tenant doesn't yet have pageservers for all its shards: we won't notify anything
             // until it does.
@@ -164,10 +274,16 @@ impl ComputeHook {
             return Ok(());
         };
 
-        if let Some(_notify_url) = &self.config.compute_hook_url {
-            todo!();
+        if let Some(notify_url) = &self.config.compute_hook_url {
+            self.do_notify(notify_url, reconfigure_request).await
         } else {
-            self.do_notify_local(reconfigure_request).await
+            self.do_notify_local(reconfigure_request)
+                .await
+                .map_err(|e| {
+                    // This path is for testing only, so munge the error into our prod-style error type.
+                    tracing::error!("Local notification hook failed: {e}");
+                    NotifyError::Fatal(StatusCode::INTERNAL_SERVER_ERROR)
+                })
         }
     }
 }
