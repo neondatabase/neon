35ef556e348d920d3182a808f2a6c81027fde184
Sasha Krassovsky <sasha@neon.tech>
2024-01-30 07:15:04 +0000

    Fix server version computation

--- compute_tools/src/spec.rs
+++ compute_tools/src/spec.rs
@@ -761,7 +761,7 @@ END $$;
         r#"
 DO $$
 BEGIN
-    IF (SELECT setting::numeric >= 16 FROM pg_settings WHERE name = 'server_version_num') THEN
+    IF (SELECT setting::numeric >= 160000 FROM pg_settings WHERE name = 'server_version_num') THEN
         EXECUTE 'GRANT pg_create_subscription TO neon_superuser';
     END IF;
 END
