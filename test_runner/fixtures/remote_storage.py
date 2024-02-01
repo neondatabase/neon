77f9b3bf937cc92ce89b456b18223a02f30fbe97
Arpad Müller <arpad-m@users.noreply.github.com>
2024-02-01 15:04:38 +0100

    Update test_runner/fixtures/remote_storage.py
    Co-authored-by: Alexander Bayandin <alexander@neon.tech>

--- test_runner/fixtures/remote_storage.py
+++ test_runner/fixtures/remote_storage.py
@@ -175,16 +175,14 @@ class S3Storage:
             return {
                 "AWS_PROFILE": self.aws_profile,
             }
-        else:
-            if self.access_key is not None and self.secret_key is not None:
-                return {
-                    "AWS_ACCESS_KEY_ID": self.access_key,
-                    "AWS_SECRET_ACCESS_KEY": self.secret_key,
-                }
-            else:
-                raise RuntimeError(
-                    "Either AWS_PROFILE or (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY) have to be set for S3Storage"
-                )
+        if self.access_key is not None and self.secret_key is not None:
+            return {
+                "AWS_ACCESS_KEY_ID": self.access_key,
+                "AWS_SECRET_ACCESS_KEY": self.secret_key,
+            }
+        raise RuntimeError(
+            "Either AWS_PROFILE or (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY) have to be set for S3Storage"
+        )
 
     def to_string(self) -> str:
         return json.dumps(
