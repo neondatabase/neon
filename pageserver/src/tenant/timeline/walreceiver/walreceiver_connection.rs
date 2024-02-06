c6ebfebcb6b9706ff37ec21fa70b3991eed976aa
Konstantin Knizhnik <knizhnik@neon.tech>
2024-02-06 15:06:48 +0200

    Add comment

--- pageserver/src/tenant/timeline/walreceiver/walreceiver_connection.rs
+++ pageserver/src/tenant/timeline/walreceiver/walreceiver_connection.rs
@@ -344,6 +344,13 @@ pub(super) async fn handle_walreceiver_connection(
                             uncommitted_records = 0;
                             filtered_records = 0;
 
+                            //
+                            // We should check checkpoint distance after appending each ingest_batch_size bytes because otherwise
+                            // layer size can become much larger than `checkpoint_distance`.
+                            // It can append because wal-sender is sending WAL using 125kb chucks and some WAL records can cause writing large
+                            // amount of data to key-value storage. So performing this check only after processing
+                            // all WAL records in the chunk, can cause huge L0 layer files.
+                            //
                             timeline
                                 .check_checkpoint_distance()
                                 .await
