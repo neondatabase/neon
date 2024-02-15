5a523516c4ff6f7bbcab1c13cf831330d3a72db2
Abhijeet Patil <abhijeet@neon.tech>
2024-02-15 13:24:09 +0000

    removed old code form shell file

--- extensions_regression_tests/docker_compose_test.sh
+++ extensions_regression_tests/docker_compose_test.sh
@@ -9,18 +9,6 @@
 
 set -eux -o pipefail
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-COMPOSE_FILE=$SCRIPT_DIR/docker-compose.yml
-
-COMPUTE_CONTAINER_NAME=docker-compose-compute-1
-SQL="CREATE TABLE t(key int primary key, value text); insert into t values(1,1); select * from t;"
-PSQL_OPTION="-h localhost -U postgres -p 55433 -c '$SQL' postgres"
-docker exec $COMPUTE_CONTAINER_NAME /bin/bash -c "psql $PSQL_OPTION"
-
-
-set -eux -o pipefail
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 COMPOSE_FILE=extensions_regression_tests/docker-compose.yml
 
 COMPUTE_CONTAINER_NAME=docker-compose-compute-1
