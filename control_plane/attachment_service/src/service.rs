bbb57e5dd927f605c5cc5a51466c5ee0748aa1be
John Spray <john@neon.tech>
2024-02-20 09:25:42 +0000

    fixup suggestion

--- control_plane/attachment_service/src/service.rs
+++ control_plane/attachment_service/src/service.rs
@@ -1488,7 +1488,7 @@ impl Service {
             .await?;
 
         // If any shards >0 haven't finished deletion yet, don't start deletion on shard zero
-        if statuses.iter().any(|s| s != StatusCode::NotFound) {
+        if statuses.iter().any(|s| s != &StatusCode::NOT_FOUND) {
             return Ok(StatusCode::ACCEPTED);
         }
 
