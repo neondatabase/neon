8dc57c5c5b49200eaecbb659f06bc2f1574a07b0
Joonas Koivunen <joonas@neon.tech>
2024-02-28 02:04:55 +0000

    add eager metric initialization

--- pageserver/src/metrics.rs
+++ pageserver/src/metrics.rs
@@ -2566,6 +2566,7 @@ pub fn preinitialize_metrics() {
     Lazy::force(&TENANT_MANAGER);
 
     Lazy::force(&crate::tenant::storage_layer::layer::LAYER_IMPL_METRICS);
+    Lazy::force(&disk_usage_based_eviction::METRICS);
 
     // countervecs
     [&BACKGROUND_LOOP_PERIOD_OVERRUN_COUNT]
