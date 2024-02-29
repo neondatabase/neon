46b5b4c1ad5713bf6107790f672c316ce1d03395
Joonas Koivunen <joonas@neon.tech>
2024-02-29 09:23:29 +0000

    bench: run branch_creation_many at 500, seeded

--- test_runner/performance/test_branch_creation.py
+++ test_runner/performance/test_branch_creation.py
@@ -89,10 +89,13 @@ def test_branch_creation_heavy_write(neon_compare: NeonCompare, n_branches: int)
     _record_branch_creation_durations(neon_compare, branch_creation_durations)
 
 
-@pytest.mark.parametrize("n_branches", [1024])
+@pytest.mark.parametrize("n_branches", [500, 1024])
 # Test measures the latency of branch creation when creating a lot of branches.
 def test_branch_creation_many(neon_compare: NeonCompare, n_branches: int):
     env = neon_compare.env
+
+    # seed the prng so we will measure the same structure every time
+    random.seed("2024-02-29")
 
     env.neon_cli.create_branch("b0")
 
