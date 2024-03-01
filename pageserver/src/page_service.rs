f8bdce101542ace882cf891f001f53c702a9685b
John Spray <john@neon.tech>
2024-03-01 13:26:45 +0000

    pageserver: fix duplicate shard_id in span (#6981)
    ## Problem
    
    shard_id in span is repeated:
    - https://github.com/neondatabase/neon/issues/6723
    
    Closes: #6723
    
    ## Summary of changes
    
    - Only add shard_id to the span when fetching a cached timeline, as it
    is already added when loading an uncached timeline.

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -1115,7 +1115,10 @@ impl PageServerHandler {
         ctx: &RequestContext,
     ) -> Result<PagestreamBeMessage, PageStreamError> {
         let timeline = match self.get_cached_timeline_for_page(req) {
-            Ok(tl) => tl,
+            Ok(tl) => {
+                set_tracing_field_shard_id(tl);
+                tl
+            }
             Err(key) => {
                 match self
                     .load_timeline_for_page(tenant_id, timeline_id, key)
@@ -1139,9 +1142,6 @@ impl PageServerHandler {
                 }
             }
         };
-
-        // load_timeline_for_page sets shard_id, but get_cached_timeline_for_page doesn't
-        set_tracing_field_shard_id(timeline);
 
         let _timer = timeline
             .query_metrics
