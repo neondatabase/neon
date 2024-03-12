80e01da25ab0af9d4f1dab49f19c768f5e7746c3
Christian Schwarz <christian@neon.tech>
2024-03-12 09:56:40 +0000

    extract the copy loop into its own function

--- pageserver/src/tenant/remote_timeline_client/download/layer_file/tokio_epoll_uring.rs
+++ pageserver/src/tenant/remote_timeline_client/download/layer_file/tokio_epoll_uring.rs
@@ -67,82 +67,10 @@ pub(crate) async fn download_layer_file<'a>(
 
             let mut download = storage.download(&remote_path, cancel).await?;
 
-            // This async block is the tokio-epoll-uring version of tokio::io::copy_buf
-            // TODO: abstract away & unit test.
-            let bytes_amount = async {
-                // TODO: use vectored write (writev) once supported by tokio-epoll-uring.
-                // There's chunks_vectored()
-                let mut buf = BytesMut::with_capacity(BUFFER_SIZE);
-                let mut bytes_amount: u64 = 0;
-                while let Some(chunk) = download.download_stream.next().await {
-                    let mut chunk = match chunk {
-                        Ok(chunk) => chunk,
-                        Err(e) => return Err(e),
-                    };
-
-                    if buf.len() + chunk.len() > BUFFER_SIZE {
-                        {
-                            // flush buf
-                            let res;
-                            let buf_pre_write = buf.len();
-                            (buf, res) = destination_file.write_all(buf).await;
-                            let nwritten = res?;
-                            assert_eq!(nwritten, buf_pre_write);
-                            buf.clear();
-                            bytes_amount += u64::try_from(nwritten).unwrap();
-                        }
-                    }
-
-                    // avoid memcpy for the middle of the chunk
-                    if chunk.len() >= BUFFER_SIZE {
-                        // do a big write
-                        let res;
-                        let buf_pre_write = chunk.len();
-                        (chunk, res) = destination_file.write_all(chunk).await;
-                        let nwritten = res?;
-                        assert_eq!(nwritten, buf_pre_write);
-                        bytes_amount += u64::try_from(nwritten).unwrap();
-                        drop(chunk);
-                        continue;
-                    }
-
-                    // in-memory copy the < BUFFER_SIZED tail of the chunk
-                    assert!(chunk.len() < BUFFER_SIZE);
-                    let mut chunk = &chunk[..];
-                    while !chunk.is_empty() {
-                        let need = BUFFER_SIZE - buf.len();
-                        let have = chunk.len();
-                        let n = std::cmp::min(need, have);
-                        buf.extend_from_slice(&chunk[..n]);
-                        chunk = &chunk[n..];
-                        if buf.len() >= BUFFER_SIZE {
-                            assert_eq!(buf.len(), BUFFER_SIZE);
-                            {
-                                // flush buf
-                                let res;
-                                let buf_pre_write = buf.len();
-                                (buf, res) = destination_file.write_all(buf).await;
-                                let nwritten = res?;
-                                assert_eq!(nwritten, buf_pre_write);
-                                buf.clear();
-                                bytes_amount += u64::try_from(nwritten).unwrap();
-                            }
-                        }
-                    }
-                    assert!(chunk.is_empty(), "by now we should have drained the chunk");
-                }
-                {
-                    // flush buf
-                    let res;
-                    let buf_pre_write = buf.len();
-                    (buf, res) = destination_file.write_all(buf).await;
-                    let nwritten = res?;
-                    assert_eq!(nwritten, buf_pre_write);
-                    buf.clear();
-                    bytes_amount += u64::try_from(nwritten).unwrap();
-                }
-                Ok(bytes_amount)
-            }
+            let bytes_amount = copy_download_stream_to_virtual_file(
+                &mut download.download_stream,
+                &mut destination_file,
+            )
             .await;
 
             match bytes_amount {
@@ -204,5 +132,85 @@ pub(crate) async fn download_layer_file<'a>(
 
     tracing::debug!("download complete: {local_path}");
 
+    Ok(bytes_amount)
+}
+
+/// Equivalent to `tokio::io::copy_buf` but with an
+/// owned-buffers-style Write API, as required by VirtualFile / tokio-epoll-uring.
+async fn copy_download_stream_to_virtual_file(
+    src: &mut remote_storage::DownloadStream,
+    dst: &mut VirtualFile,
+) -> std::io::Result<u64> {
+    // TODO: use vectored write (writev) once supported by tokio-epoll-uring.
+    // There's chunks_vectored()
+    let mut buf = BytesMut::with_capacity(BUFFER_SIZE);
+    let mut bytes_amount: u64 = 0;
+    while let Some(chunk) = src.next().await {
+        let mut chunk = match chunk {
+            Ok(chunk) => chunk,
+            Err(e) => return Err(e),
+        };
+
+        if buf.len() + chunk.len() > BUFFER_SIZE {
+            {
+                // flush buf
+                let res;
+                let buf_pre_write = buf.len();
+                (buf, res) = dst.write_all(buf).await;
+                let nwritten = res?;
+                assert_eq!(nwritten, buf_pre_write);
+                buf.clear();
+                bytes_amount += u64::try_from(nwritten).unwrap();
+            }
+        }
+
+        // avoid memcpy for the middle of the chunk
+        if chunk.len() >= BUFFER_SIZE {
+            // do a big write
+            let res;
+            let buf_pre_write = chunk.len();
+            (chunk, res) = dst.write_all(chunk).await;
+            let nwritten = res?;
+            assert_eq!(nwritten, buf_pre_write);
+            bytes_amount += u64::try_from(nwritten).unwrap();
+            drop(chunk);
+            continue;
+        }
+
+        // in-memory copy the < BUFFER_SIZED tail of the chunk
+        assert!(chunk.len() < BUFFER_SIZE);
+        let mut chunk = &chunk[..];
+        while !chunk.is_empty() {
+            let need = BUFFER_SIZE - buf.len();
+            let have = chunk.len();
+            let n = std::cmp::min(need, have);
+            buf.extend_from_slice(&chunk[..n]);
+            chunk = &chunk[n..];
+            if buf.len() >= BUFFER_SIZE {
+                assert_eq!(buf.len(), BUFFER_SIZE);
+                {
+                    // flush buf
+                    let res;
+                    let buf_pre_write = buf.len();
+                    (buf, res) = dst.write_all(buf).await;
+                    let nwritten = res?;
+                    assert_eq!(nwritten, buf_pre_write);
+                    buf.clear();
+                    bytes_amount += u64::try_from(nwritten).unwrap();
+                }
+            }
+        }
+        assert!(chunk.is_empty(), "by now we should have drained the chunk");
+    }
+    {
+        // flush buf
+        let res;
+        let buf_pre_write = buf.len();
+        (buf, res) = dst.write_all(buf).await;
+        let nwritten = res?;
+        assert_eq!(nwritten, buf_pre_write);
+        buf.clear();
+        bytes_amount += u64::try_from(nwritten).unwrap();
+    }
     Ok(bytes_amount)
 }
