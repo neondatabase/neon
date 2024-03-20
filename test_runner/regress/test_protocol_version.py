2704f8cebee4ac000af4b2c4736f1498bb07725f
Konstantin Knizhnik <knizhnik@neon.tech>
2024-03-20 17:54:34 +0200

    Make ruff happy

--- test_runner/regress/test_protocol_version.py
+++ test_runner/regress/test_protocol_version.py
@@ -6,4 +6,4 @@ def test_protocol_version(neon_simple_env: NeonEnv):
     endpoint = env.endpoints.create_start("main", config_lines=["neon.protocol_version=1"])
     cur = endpoint.connect().cursor()
     cur.execute("show neon.protocol_version")
-    assert cur.fetchone() == ('1',)
+    assert cur.fetchone() == ("1",)
