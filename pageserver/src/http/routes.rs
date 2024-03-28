f1ef27f1e3b74ba6d7279107401d62f7a79c5d74
John Spray <john@neon.tech>
2024-03-28 13:56:27 +0000

    missing file

--- pageserver/src/http/routes.rs
+++ pageserver/src/http/routes.rs
@@ -994,14 +994,16 @@ async fn tenant_status(
     let state = get_state(&request);
 
     // In tests, sometimes we want to query the state of a tenant without auto-activating it if it's currently waiting.
-    let no_activate = parse_query_param(&request, "no_activate")?.unwrap_or(false);
+    let activate = true;
+    #[cfg(feature = "testing")]
+    let activate = parse_query_param(&request, "activate")?.unwrap_or(activate);
 
     let tenant_info = async {
         let tenant = state
             .tenant_manager
             .get_attached_tenant_shard(tenant_shard_id)?;
 
-        if !no_activate {
+        if activate {
             // This is advisory: we prefer to let the tenant activate on-demand when this function is
             // called, but it is still valid to return 200 and describe the current state of the tenant
             // if it doesn't make it into an active state.
