3f77f26aa29a0a250a494346fed2f294d690aa46
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2024-04-03 17:20:51 +0200

    Upload partial segments (#6530)
    Add support for backing up partial segments to remote storage. Disabled
    by default, can be enabled with `--partial-backup-enabled`.
    
    Safekeeper timeline has a background task which is subscribed to
    `commit_lsn` and `flush_lsn` updates. After the partial segment was
    updated (`flush_lsn` was changed), the segment will be uploaded to S3 in
    about 15 minutes.
    
    The filename format for partial segments is
    `Segment_Term_Flush_Commit_skNN.partial`, where:
    - `Segment` – the segment name, like `000000010000000000000001`
    - `Term` – current term
    - `Flush` – flush_lsn in hex format `{:016X}`, e.g. `00000000346BC568`
    - `Commit` – commit_lsn in the same hex format
    - `NN` – safekeeper_id, like `1`
    
    The full object name example:
    `000000010000000000000002_2_0000000002534868_0000000002534410_sk1.partial`
    
    Each safekeeper will keep info about remote partial segments in its
    control file. Code updates state in the control file before doing any S3
    operations. This way control file stores information about all
    potentially existing remote partial segments and can clean them up after
    uploading a newer version.
    
    
    Closes #6336

--- test_runner/regress/test_wal_acceptor_async.py
+++ test_runner/regress/test_wal_acceptor_async.py
@@ -10,6 +10,7 @@ import pytest
 import toml
 from fixtures.log_helper import getLogger
 from fixtures.neon_fixtures import Endpoint, NeonEnv, NeonEnvBuilder, Safekeeper
+from fixtures.remote_storage import RemoteStorageKind
 from fixtures.types import Lsn, TenantId, TimelineId
 
 log = getLogger("root.safekeeper_async")
@@ -199,7 +200,9 @@ async def run_restarts_under_load(
         # assert that at least one transaction has completed in every worker
         stats.check_progress()
 
-        victim.start()
+        # testing #6530, temporary here
+        # TODO: remove afer partial backup is enabled by default
+        victim.start(extra_opts=["--partial-backup-enabled", "--partial-backup-timeout=2s"])
 
     log.info("Iterations are finished, exiting coroutines...")
     stats.running = False
@@ -213,6 +216,7 @@ async def run_restarts_under_load(
 # Restart acceptors one by one, while executing and validating bank transactions
 def test_restarts_under_load(neon_env_builder: NeonEnvBuilder):
     neon_env_builder.num_safekeepers = 3
+    neon_env_builder.enable_safekeeper_remote_storage(RemoteStorageKind.LOCAL_FS)
     env = neon_env_builder.init_start()
 
     env.neon_cli.create_branch("test_safekeepers_restarts_under_load")
