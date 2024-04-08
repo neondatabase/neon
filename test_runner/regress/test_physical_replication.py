c85fd74d34a76dfe986f549436c21d92238b5fde
Konstantin Knizhnik <knizhnik@neon.tech>
2024-04-08 17:37:48 +0300

    Fix test_physical_replication test taken in acount autocommit behaviour of psycopg

--- test_runner/regress/test_physical_replication.py
+++ test_runner/regress/test_physical_replication.py
@@ -18,12 +18,15 @@ def test_physical_replication(neon_simple_env: NeonEnv):
                 )
         time.sleep(1)
         with env.endpoints.new_replica_start(origin=primary, endpoint_id="secondary") as secondary:
-            with primary.connect() as p_con:
-                with p_con.cursor() as p_cur:
-                    with secondary.connect() as s_con:
-                        with s_con.cursor() as s_cur:
-                            for pk in range(n_records):
-                                p_cur.execute("insert into t (pk) values (%s)", (pk,))
-                                s_cur.execute(
-                                    "select * from t where pk=%s", (random.randrange(1, n_records),)
-                                )
+            p_con = primary.connect()
+            s_con = secondary.connect()
+            count = 0
+            with p_con.cursor() as p_cur:
+                with s_con.cursor() as s_cur:
+                    for pk in range(n_records):
+                        p_cur.execute("insert into t (pk) values (%s)", (pk,))
+                        s_cur.execute(
+                            "select count(*) from t where pk=%s", (random.randrange(1, n_records),)
+                        )
+                        count += s_cur.fetchall()[0][0]
+            assert count > 0
