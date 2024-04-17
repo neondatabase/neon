406525033c4be996ae340e00ebcf65044dabbdf9
Joonas Koivunen <joonas@neon.tech>
2024-04-17 11:27:03 +0000

    test: update

--- test_runner/regress/test_timeline_detach_ancestor.py
+++ test_runner/regress/test_timeline_detach_ancestor.py
@@ -2,7 +2,6 @@ import enum
 from typing import List
 
 import pytest
-from fixtures.log_helper import log
 from fixtures.neon_fixtures import (
     NeonEnvBuilder,
     wait_for_last_flush_lsn,
@@ -49,10 +48,12 @@ class Branchpoint(str, enum.Enum):
 @pytest.mark.parametrize("branchpoint", Branchpoint.all())
 def test_ancestor_detach_branched_from(neon_env_builder: NeonEnvBuilder, branchpoint: Branchpoint):
     """
-    Creates a branch before, at or after the L0 lsn boundary.
+    Creates a branch relative to L0 lsn boundary according to Branchpoint. Later the timeline is detached.
     """
-    # TODO: parametrize
+    # TODO: parametrize; currently unimplemented over at pageserver
     restart_after = True
+    write_to_branch_first = True
+
     env = neon_env_builder.init_start(
         initial_tenant_conf={
             "gc_period": "0s",
@@ -73,7 +74,7 @@ def test_ancestor_detach_branched_from(neon_env_builder: NeonEnvBuilder, branchp
         client.timeline_checkpoint(env.initial_tenant, env.initial_timeline)
 
         ep.safe_psql("INSERT INTO foo SELECT i::bigint FROM generate_series(8192, 16383) g(i);")
-        last_record_lsn = wait_for_last_flush_lsn(env, ep, env.initial_tenant, env.initial_timeline)
+        wait_for_last_flush_lsn(env, ep, env.initial_tenant, env.initial_timeline)
 
     deltas = client.layer_map_info(env.initial_tenant, env.initial_timeline).delta_layers()
     # there is also the in-mem layer, but ignore it for now
@@ -87,19 +88,25 @@ def test_ancestor_detach_branched_from(neon_env_builder: NeonEnvBuilder, branchp
     if branchpoint == Branchpoint.EARLIER:
         branch_at = after_first_tx
         rows = 0
+        truncated_layers = 1
     elif branchpoint == Branchpoint.BEFORE_L0:
         branch_at = Lsn(last_lsn - 8)
         rows = 0
+        truncated_layers = 1
     elif branchpoint == Branchpoint.AT_L0:
         branch_at = Lsn(last_lsn)
         rows = 8192
+        truncated_layers = 0
     elif branchpoint == Branchpoint.AFTER_L0:
         branch_at = Lsn(last_lsn + 8)
         rows = 8192
+        # as there is no 8 byte walrecord, nothing should get copied from the straddling layer
+        truncated_layers = 0
     else:
         assert branchpoint == Branchpoint.LAST_RECORD_LSN
-        branch_at = last_record_lsn
+        branch_at = None
         rows = 16384
+        truncated_layers = 0
 
     name = "new main"
 
@@ -107,15 +114,28 @@ def test_ancestor_detach_branched_from(neon_env_builder: NeonEnvBuilder, branchp
         name, "main", env.initial_tenant, ancestor_start_lsn=branch_at
     )
 
-    with env.endpoints.create_start(name, tenant_id=env.initial_tenant) as ep:
-        assert ep.safe_psql("SELECT count(*) FROM foo;")[0][0] == rows
-        # make sure the ep is writable
-        # with BEFORE_L0, AFTER_L0 there will be a gap in Lsns caused by accurate end_lsn on straddling layers
-        ep.safe_psql("CREATE TABLE audit AS SELECT 1 as starts;")
-        wait_for_last_flush_lsn(env, ep, env.initial_tenant, timeline_id)
+    recorded = Lsn(client.timeline_detail(env.initial_tenant, timeline_id)["ancestor_lsn"])
+    if branch_at is None:
+        # fix it up if we need it later (currently unused)
+        branch_at = recorded
+    else:
+        assert branch_at == recorded, "the test should not use unaligned lsns"
 
-    # branch must have a flush for "PREV_LSN: none"
-    client.timeline_checkpoint(env.initial_tenant, timeline_id)
+    if write_to_branch_first:
+        with env.endpoints.create_start(name, tenant_id=env.initial_tenant) as ep:
+            assert ep.safe_psql("SELECT count(*) FROM foo;")[0][0] == rows
+            # make sure the ep is writable
+            # with BEFORE_L0, AFTER_L0 there will be a gap in Lsns caused by accurate end_lsn on straddling layers
+            ep.safe_psql("CREATE TABLE audit AS SELECT 1 as starts;")
+            wait_for_last_flush_lsn(env, ep, env.initial_tenant, timeline_id)
+
+        # branch must have a flush for "PREV_LSN: none"
+        client.timeline_checkpoint(env.initial_tenant, timeline_id)
+        branch_layers = set(
+            map(layer_name, client.layer_map_info(env.initial_tenant, timeline_id).historic_layers)
+        )
+    else:
+        branch_layers = set()
 
     # TODO: return value from this
     client.detach_ancestor(env.initial_tenant, timeline_id)
@@ -130,14 +150,21 @@ def test_ancestor_detach_branched_from(neon_env_builder: NeonEnvBuilder, branchp
     with env.endpoints.create_start(name, tenant_id=env.initial_tenant) as ep:
         assert ep.safe_psql("SELECT count(*) FROM foo;")[0][0] == rows
 
+    old_main = client.layer_map_info(env.initial_tenant, env.initial_timeline)
+    old_main = set(map(layer_name, old_main.historic_layers))
+
+    new_main = client.layer_map_info(env.initial_tenant, timeline_id)
+    new_main = set(map(layer_name, new_main.historic_layers))
+
+    new_main_copied_or_truncated = new_main - branch_layers
+    new_main_truncated = new_main_copied_or_truncated - old_main
+
+    assert len(new_main_truncated) == truncated_layers
+    # could additionally check that the symmetric difference has layers starting at the same lsn
+    # but if nothing was copied, then there is no nice rule.
+    # there could be a hole in LSNs between copied from the "old main" and the first branch layer.
+
     client.timeline_delete(env.initial_tenant, env.initial_timeline)
-
-    info = client.layer_map_info(env.initial_tenant, timeline_id)
-    info.historic_layers.sort(key=layer_name)
-
-    log.info("layers:")
-    for layer in info.historic_layers:
-        log.info(f"- {layer.layer_file_name}")
 
 
 # TODO:
@@ -145,4 +172,5 @@ def test_ancestor_detach_branched_from(neon_env_builder: NeonEnvBuilder, branchp
 # - reparenting is done
 # - after starting the operation, tenant is deleted
 # - after starting the operation, pageserver is shutdown
-# - branch near existing L1 boundary
+# - branch near existing L1 boundary, image layers?
+# - investigate: why are layers started at uneven lsn? not just after branching, but in general.
