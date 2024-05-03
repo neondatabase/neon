60a5694acf4da631b62e9ec12cc5831e301ffaa0
Arpad Müller <arpad@neon.tech>
2024-05-03 03:57:59 +0200

    ruff fixes

--- test_runner/regress/test_lsn_mapping.py
+++ test_runner/regress/test_lsn_mapping.py
@@ -1,8 +1,8 @@
 import re
 import time
 from datetime import datetime, timedelta, timezone
-import pytest
 
+import pytest
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import NeonEnvBuilder, wait_for_last_flush_lsn
 from fixtures.pageserver.http import PageserverApiException
