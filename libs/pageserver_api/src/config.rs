4a72fe09084fb569a43c6ed64eb7a21d30098e4a
Christian Schwarz <christian@neon.tech>
2024-05-03 15:17:45 +0000

    add requested backward-compatibility test

--- libs/pageserver_api/src/config.rs
+++ libs/pageserver_api/src/config.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 
 use const_format::formatcp;
 
+#[cfg(test)]
+mod tests;
+
 pub const DEFAULT_PG_LISTEN_PORT: u16 = 64000;
 pub const DEFAULT_PG_LISTEN_ADDR: &str = formatcp!("127.0.0.1:{DEFAULT_PG_LISTEN_PORT}");
 pub const DEFAULT_HTTP_LISTEN_PORT: u16 = 9898;
@@ -12,7 +15,7 @@ pub const DEFAULT_HTTP_LISTEN_ADDR: &str = formatcp!("127.0.0.1:{DEFAULT_HTTP_LI
 // itself, it is only used for registering the pageserver with the control
 // plane and/or storage controller.
 //
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(PartialEq, Eq, Debug, serde::Serialize, serde::Deserialize)]
 pub struct NodeMetadata {
     #[serde(rename = "host")]
     pub postgres_host: String,
