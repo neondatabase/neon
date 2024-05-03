1e7cd6ac9f3568ffe9db952cb89f8036330d27b5
Christian Schwarz <christian@neon.tech>
2024-05-03 19:15:38 +0200

    refactor: move `NodeMetadata` to `pageserver_api`; use it from `neon_local` (#7606)
    This is the first step towards representing all of Pageserver
    configuration as clean `serde::Serialize`able Rust structs in
    `pageserver_api`.
    
    The `neon_local` code will then use those structs instead of the crude
    `toml_edit` / string concatenation that it does today.
    
    refs https://github.com/neondatabase/neon/issues/7555
    
    ---------
    
    Co-authored-by: Alex Chi Z <iskyzh@gmail.com>

--- pageserver/src/config.rs
+++ pageserver/src/config.rs
@@ -9,7 +9,7 @@ use pageserver_api::shard::TenantShardId;
 use remote_storage::{RemotePath, RemoteStorageConfig};
 use serde;
 use serde::de::IntoDeserializer;
-use std::{collections::HashMap, env};
+use std::env;
 use storage_broker::Uri;
 use utils::crashsafe::path_with_suffix_extension;
 use utils::id::ConnectionId;
@@ -51,7 +51,7 @@ pub mod defaults {
     use crate::tenant::config::defaults::*;
     use const_format::formatcp;
 
-    pub use pageserver_api::{
+    pub use pageserver_api::config::{
         DEFAULT_HTTP_LISTEN_ADDR, DEFAULT_HTTP_LISTEN_PORT, DEFAULT_PG_LISTEN_ADDR,
         DEFAULT_PG_LISTEN_PORT,
     };
@@ -333,26 +333,6 @@ impl<T: Clone> BuilderValue<T> {
             },
         }
     }
-}
-
-// Certain metadata (e.g. externally-addressable name, AZ) is delivered
-// as a separate structure.  This information is not neeed by the pageserver
-// itself, it is only used for registering the pageserver with the control
-// plane and/or storage controller.
-//
-#[derive(serde::Deserialize)]
-pub(crate) struct NodeMetadata {
-    #[serde(rename = "host")]
-    pub(crate) postgres_host: String,
-    #[serde(rename = "port")]
-    pub(crate) postgres_port: u16,
-    pub(crate) http_host: String,
-    pub(crate) http_port: u16,
-
-    // Deployment tools may write fields to the metadata file beyond what we
-    // use in this type: this type intentionally only names fields that require.
-    #[serde(flatten)]
-    pub(crate) other: HashMap<String, serde_json::Value>,
 }
 
 // needed to simplify config construction
