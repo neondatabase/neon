df79d998ae79c916ca38eceb663bfbb8c0634300
Alex Chi Z <chi@neon.tech>
2024-05-09 14:54:56 -0400

    fix mypy lints
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- test_runner/regress/test_compatibility.py
+++ test_runner/regress/test_compatibility.py
@@ -250,11 +250,16 @@ def test_forward_compatibility(
 
         # not using env.pageserver.version because it was initialized before
         prev_pageserver_version_str = env.get_binary_version("pageserver")
-        print(prev_pageserver_version_str)
-        prev_pageserver_version = re.search(
+        prev_pageserver_version_match = re.search(
             "Neon page server git-env:(.*) failpoints: (.*), features: (.*)",
             prev_pageserver_version_str,
-        ).group(0)
+        )
+        if prev_pageserver_version_match is not None:
+            prev_pageserver_version = prev_pageserver_version_match.group(1)
+        else:
+            raise AssertionError(
+                "cannot find git hash in the version string: " + prev_pageserver_version_str
+            )
 
         check_neon_works(
             env,
