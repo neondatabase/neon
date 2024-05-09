13977eae249f82ec38cdaaf2573adfabac971003
Alex Chi Z <chi@neon.tech>
2024-05-09 10:30:17 -0400

    remove unused fields
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- pageserver/src/context.rs
+++ pageserver/src/context.rs
@@ -100,8 +100,6 @@ pub struct RequestContext {
     access_stats_behavior: AccessStatsBehavior,
     page_content_kind: PageContentKind,
     pub micros_spent_throttled: optional_counter::MicroSecondsCounterU32,
-    /// Total number of kilobytes of layer files processed in this request.
-    pub vectored_access_delta_file_size_kb: AtomicUsize,
 }
 
 /// The kind of access to the page cache.
@@ -158,7 +156,6 @@ impl RequestContextBuilder {
                 access_stats_behavior: AccessStatsBehavior::Update,
                 page_content_kind: PageContentKind::Unknown,
                 micros_spent_throttled: Default::default(),
-                vectored_access_delta_file_size_kb: AtomicUsize::new(0),
             },
         }
     }
@@ -173,7 +170,6 @@ impl RequestContextBuilder {
                 access_stats_behavior: original.access_stats_behavior,
                 page_content_kind: original.page_content_kind,
                 micros_spent_throttled: Default::default(),
-                vectored_access_delta_file_size_kb: AtomicUsize::new(0),
             },
         }
     }
