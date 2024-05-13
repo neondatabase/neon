476dd57a3a47f36185ae046c6d30c8b043036c68
Joonas Koivunen <joonas@neon.tech>
2024-05-13 10:59:32 +0000

    chore: the comma

--- test_runner/fixtures/pageserver/allowed_errors.py
+++ test_runner/fixtures/pageserver/allowed_errors.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
         "--input",
         type=argparse.FileType("r"),
         help="Pageserver logs file. Use '-' for stdin.",
-        required=True
+        required=True,
     )
 
     args = parser.parse_args()
