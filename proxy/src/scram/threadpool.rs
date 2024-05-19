f895ae10dbdfad729d63b5e32e9bafe285571988
Conrad Ludgate <conradludgate@gmail.com>
2024-05-19 23:12:46 +0100

    add fairness estimator

--- proxy/src/scram/threadpool.rs
+++ proxy/src/scram/threadpool.rs
@@ -4,7 +4,10 @@
 //! 1. Fairness per endpoint.
 //! 2. Yield support for high iteration counts.
 
-use std::sync::{Arc, OnceLock};
+use std::{
+    hash::Hash,
+    sync::{Arc, OnceLock},
+};
 
 use crossbeam_deque::{Injector, Stealer, Worker};
 use hmac::{
@@ -13,9 +16,11 @@ use hmac::{
 };
 use parking_lot::{Condvar, Mutex};
 use rand::Rng;
-use rand::{rngs::SmallRng, seq::SliceRandom, thread_rng, SeedableRng};
+use rand::{rngs::SmallRng, seq::SliceRandom, SeedableRng};
 use sha2::Sha256;
 use tokio::sync::oneshot;
+
+use crate::intern::EndpointIdInt;
 
 pub struct ThreadPool {
     queue: Injector<JobSpec>,
@@ -42,12 +47,17 @@ impl ThreadPool {
         }
     }
 
-    pub fn spawn_job(&self, pbkdf2: Pbkdf2) -> oneshot::Receiver<[u8; 32]> {
+    pub fn spawn_job(
+        &self,
+        endpoint: EndpointIdInt,
+        pbkdf2: Pbkdf2,
+    ) -> oneshot::Receiver<[u8; 32]> {
         let (tx, rx) = oneshot::channel();
 
         self.queue.push(JobSpec {
             response: tx,
             pbkdf2,
+            endpoint,
         });
 
         let mut lock = self.lock.lock();
@@ -74,6 +84,18 @@ impl ThreadPool {
             let pool = Arc::clone(self);
             std::thread::spawn(move || {
                 let mut rng = SmallRng::from_entropy();
+
+                // used to determine whether we should temporarily skip tasks
+                // for fairness
+                let mut sketch = CountMinSketch::new(32, 8);
+
+                /// interval when we should steal from the global queue
+                /// so that tail latencies are managed appropriately
+                const STEAL_INTERVAL: usize = 61;
+
+                /// How often to reset the sketch values
+                const SKETCH_RESET_INTERVAL: usize = 1021;
+
                 'wait: loop {
                     // wait for notification of work
                     {
@@ -115,18 +137,38 @@ impl ThreadPool {
 
                         // receiver is closed, cancel the task
                         if !job.response.is_closed() {
-                            match job.pbkdf2.turn() {
-                                std::task::Poll::Ready(result) => {
-                                    let _ = job.response.send(result);
+                            let rate = sketch.inc(&job.endpoint);
+
+                            const P: f64 = 32.0;
+                            // probability decreases as rate increases.
+                            // lower probability, higher chance of being skipped
+                            // rate = 0    => probability = 100%
+                            // rate = 10   => probability = 92.7%
+                            // rate = 50   => probability = 78.6%
+                            // rate = 500  => probability = 55.2%
+                            // rate = 1021 => probability = 49.8%
+                            let probability = P.ln() / (P + rate as f64).ln();
+                            if rng.gen_bool(probability) {
+                                match job.pbkdf2.turn() {
+                                    std::task::Poll::Ready(result) => {
+                                        let _ = job.response.send(result);
+                                    }
+                                    std::task::Poll::Pending => worker.push(job),
                                 }
-                                std::task::Poll::Pending => worker.push(job),
+                            } else {
+                                // skip for now
+                                worker.push(job)
                             }
                         }
 
                         // if we get stuck with a few long lived jobs in the queue
                         // it's better to try and steal from the queue too for fairness
-                        if i % 61 == 0 {
+                        if i % STEAL_INTERVAL == 0 {
                             let _ = pool.queue.steal_batch(&worker);
+                        }
+
+                        if i % SKETCH_RESET_INTERVAL == 0 {
+                            sketch.reset();
                         }
                     }
                 }
@@ -139,6 +181,53 @@ impl ThreadPool {
     }
 }
 
+/// estimator of hash jobs per second.
+/// <https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch>
+struct CountMinSketch {
+    // one for each width
+    hashers: Vec<ahash::RandomState>,
+    width: usize,
+    depth: usize,
+    // buckets, width*depth
+    buckets: Vec<u64>,
+}
+
+impl CountMinSketch {
+    fn new(width: usize, depth: usize) -> Self {
+        Self {
+            hashers: (0..width).map(|_| ahash::RandomState::new()).collect(),
+            width,
+            depth,
+            buckets: vec![0; width * depth],
+        }
+    }
+
+    fn inc<T: Hash>(&mut self, t: &T) -> u64 {
+        let mut min = 0;
+        for w in 0..self.width {
+            let hash = self.hashers[w].hash_one(t) as usize;
+            let bucket = &mut self.buckets[w * self.depth + hash % self.depth];
+            *bucket = bucket.saturating_add(1);
+            min = std::cmp::min(min, *bucket);
+        }
+        min
+    }
+
+    // fn get<T: Hash>(&mut self, t: &T) -> u64 {
+    //     let mut min = 0;
+    //     for w in 0..self.width {
+    //         let hash = self.hashers[w].hash_one(t) as usize;
+    //         min = std::cmp::min(min, self.buckets[w * self.depth + hash % self.depth]);
+    //     }
+    //     min
+    // }
+
+    fn reset(&mut self) {
+        self.buckets.clear();
+        self.buckets.resize(self.width * self.depth, 0);
+    }
+}
+
 impl Default for ThreadPool {
     fn default() -> Self {
         Self::new()
@@ -148,6 +237,7 @@ impl Default for ThreadPool {
 struct JobSpec {
     response: oneshot::Sender<[u8; 32]>,
     pbkdf2: Pbkdf2,
+    endpoint: EndpointIdInt,
 }
 
 pub struct Pbkdf2 {
@@ -209,6 +299,8 @@ impl Pbkdf2 {
 mod tests {
     use std::sync::Arc;
 
+    use crate::EndpointId;
+
     use super::*;
 
     #[tokio::test]
@@ -216,9 +308,12 @@ mod tests {
         let pool = Arc::new(ThreadPool::new());
         pool.spawn_workers(1);
 
+        let ep = EndpointId::from("foo");
+        let ep = EndpointIdInt::from(ep);
+
         let salt = [0x55; 32];
         let actual = pool
-            .spawn_job(Pbkdf2::start(b"password", &salt, 4096))
+            .spawn_job(ep, Pbkdf2::start(b"password", &salt, 4096))
             .await
             .unwrap();
 
