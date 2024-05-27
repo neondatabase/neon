068c158ca564b42c15a9b9a0b2cdfe4423809e1e
Konstantin Knizhnik <knizhnik@garret.ru>
2024-05-27 15:57:57 +0300

    Fix connect to PS on MacOS/X (#7885)
    ## Problem
    
    After [0e4f1826805d040a23c25c54f3993a942755dbc2] which introduce async
    connect
    Neon is not able to connect to page server.
    
    ## Summary of changes
    
    Perform sync commit at MacOS/X
    
    ## Checklist before requesting a review
    
    - [ ] I have performed a self-review of my code.
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] Do we need to implement analytics? if so did you add the relevant
    metrics to the dashboard?
    - [ ] If this PR requires public announcement, mark it with
    /release-notes label and add several sentences in this section.
    
    ## Checklist before merging
    
    - [ ] Do not forget to reformat commit message to not include the above
    checklist
    
    ---------
    
    Co-authored-by: Konstantin Knizhnik <knizhnik@neon.tech>

--- pgxn/neon/pagestore_smgr.c
+++ pgxn/neon/pagestore_smgr.c
@@ -584,9 +584,9 @@ prefetch_read(PrefetchRequest *slot)
 		slot->response != NULL ||
 		slot->my_ring_index != MyPState->ring_receive)
 		neon_shard_log(slot->shard_no, ERROR,
-					   "Incorrect prefetch read: status=%d response=%llx my=%llu receive=%llu",
-					   slot->status, (size_t) (void *) slot->response,
-					   slot->my_ring_index, MyPState->ring_receive);
+					   "Incorrect prefetch read: status=%d response=%p my=%lu receive=%lu",
+					   slot->status, slot->response,
+					   (long)slot->my_ring_index, (long)MyPState->ring_receive);
 
 	old = MemoryContextSwitchTo(MyPState->errctx);
 	response = (NeonResponse *) page_server->receive(slot->shard_no);
@@ -606,8 +606,8 @@ prefetch_read(PrefetchRequest *slot)
 	else
 	{
 		neon_shard_log(slot->shard_no, WARNING,
-					   "No response from reading prefetch entry %llu: %u/%u/%u.%u block %u. This can be caused by a concurrent disconnect",
-					   slot->my_ring_index,
+					   "No response from reading prefetch entry %lu: %u/%u/%u.%u block %u. This can be caused by a concurrent disconnect",
+					   (long)slot->my_ring_index,
 					   RelFileInfoFmt(BufTagGetNRelFileInfo(slot->buftag)),
 					   slot->buftag.forkNum, slot->buftag.blockNum);
 		return false;
