5a5775806f6651fdeaacb56af6a8de8447b2f350
Konstantin Knizhnik <knizhnik@neon.tech>
2024-06-03 21:07:50 +0300

    Restore check for poreserving pgdata_dir content

--- test_runner/fixtures/neon_fixtures.py
+++ test_runner/fixtures/neon_fixtures.py
@@ -4316,7 +4316,7 @@ def check_restored_datadir_content(
         restored_files = [f for f in restored_files if f not in ignored_files]
 
     # check that file sets are equal
-    # assert pgdata_files == restored_files
+    assert pgdata_files == restored_files
 
     # compare content of the files
     # filecmp returns (match, mismatch, error) lists
