75496dd7ddddd25dfe1fff07ac1e8a7291226ab0
Joonas Koivunen <joonas@neon.tech>
2024-06-07 16:43:59 +0200

    test: refactor and lower row count further
    3*checkpoint_distance is sized index is good enough.
    
    went a bit overboard with the histogram refactoring thinking I'd add
    compaction, but that'll always produces correct sized layers... unsure
    if we have a test for it though..

--- test_runner/regress/test_ingestion_layer_size.py
+++ test_runner/regress/test_ingestion_layer_size.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
 import pytest
+from typing import List, Union, Iterable
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import NeonEnvBuilder, wait_for_last_flush_lsn
 from fixtures.utils import human_bytes
+from fixtures.pageserver.http import HistoricLayerInfo, LayerMapInfo
 
 
 def test_ingesting_large_batches_of_images(neon_env_builder: NeonEnvBuilder, build_type: str):
@@ -13,10 +16,23 @@ def test_ingesting_large_batches_of_images(neon_env_builder: NeonEnvBuilder, bui
     if build_type == "debug":
         pytest.skip("debug run is unnecessarily slow")
 
-    checkpoint_distance = 64 * 1024**2
+    minimum_initdb_size = 20 * 1024**2
+    checkpoint_distance = 32 * 1024**2
+    minimum_good_layer_size = checkpoint_distance * 0.9
+    minimum_too_large_layer_size = 2 * checkpoint_distance
+
+    # index size: 99MiB
+    rows = 2_500_000
+
+    # bucket lower limits
+    buckets = [0, minimum_initdb_size, minimum_good_layer_size, minimum_too_large_layer_size]
+
+    assert minimum_initdb_size < minimum_good_layer_size, "keep checkpoint_distance higher than the initdb size (find it by experimenting)"
+
     env = neon_env_builder.init_start(
         initial_tenant_conf={
             "checkpoint_distance": f"{checkpoint_distance}",
+            # this test is interested in L0 sizes
             "compaction_period": "0s",
             "gc_period": "0s",
             "compaction_threshold": "255",
@@ -24,12 +40,19 @@ def test_ingesting_large_batches_of_images(neon_env_builder: NeonEnvBuilder, bui
         }
     )
 
-    # build a 391MB gin index which exhibits the same behaviour as the pgvector with the two phase build
+    # build a larger than 3*checkpoint_distance sized gin index.
+    # gin index building exhibits the same behaviour as the pgvector with the two phase build
     with env.endpoints.create_start("main") as ep, ep.cursor() as cur:
         cur.execute(
-            "create table int_array_test as select array_agg(g) as int_array from generate_series(1, 5000000) g group by g / 10;"
+            f"create table int_array_test as select array_agg(g) as int_array from generate_series(1, {rows}) g group by g / 10;"
         )
-        cur.execute("create index on int_array_test using gin (int_array);")
+        cur.execute("create index int_array_test_gin_index on int_array_test using gin (int_array);")
+        cur.execute("select pg_table_size('int_array_test_gin_index')")
+        size = cur.fetchone()
+        assert size is not None
+        assert isinstance(size[0], int)
+        log.info(f"gin index size: {human_bytes(size[0])}")
+        assert size[0] > checkpoint_distance * 3, f"gin index is not large enough: {human_bytes(size[0])}"
         wait_for_last_flush_lsn(env, ep, env.initial_tenant, env.initial_timeline)
 
     ps_http = env.pageserver.http_client()
@@ -37,33 +60,60 @@ def test_ingesting_large_batches_of_images(neon_env_builder: NeonEnvBuilder, bui
 
     infos = ps_http.layer_map_info(env.initial_tenant, env.initial_timeline)
     assert len(infos.in_memory_layers) == 0, "should had flushed open layers"
+    post_ingest = histogram_historic_layers(infos, buckets)
 
-    buckets = list(enumerate([0, 20 * 1024**2, checkpoint_distance * 0.9, 2 * checkpoint_distance]))
+    # describe first, assert later for easier debugging
+    log.info("non-cumulative layer size distribution after ingestion:")
+    print_layer_size_histogram(post_ingest)
 
+    assert (
+        post_ingest.counts[3] == 0
+    ), f"there should be no layers larger than 2*checkpoint_distance ({human_bytes(2*checkpoint_distance)})"
+    assert post_ingest.counts[1] == 1, "expect one smaller layer for initdb"
+    assert post_ingest.counts[0] <= 1, "expect at most one tiny layer from shutting down the endpoint"
+
+
+@dataclass
+class Histogram:
+    buckets: List[Union[int, float]]
+    counts: List[int]
+    sums: List[int]
+
+
+def histogram_historic_layers(infos: LayerMapInfo, minimum_sizes: List[Union[int, float]]) -> Histogram:
+
+    def log_layer(layer: HistoricLayerInfo) -> HistoricLayerInfo:
+        log.info(
+            f"{layer.layer_file_name} {human_bytes(layer.layer_file_size)} ({layer.layer_file_size} bytes)"
+        )
+        return layer
+
+    sizes = map(log_layer, infos.historic_layers)
+    sizes = (x.layer_file_size for x in sizes)
+    return histogram(sizes, minimum_sizes)
+
+
+def histogram(sizes: Iterable[int], minimum_sizes: List[Union[int, float]]) -> Histogram:
+    assert all(minimum_sizes[i] < minimum_sizes[i + 1] for i in range(len(minimum_sizes)))
+    buckets = list(enumerate(minimum_sizes))
     counts = [0 for _ in buckets]
     sums = [0 for _ in buckets]
 
-    for layer in infos.historic_layers:
+    for size in sizes:
         found = False
         for index, min_size in reversed(buckets):
-            if layer.layer_file_size >= min_size:
+            if size >= min_size:
                 counts[index] += 1
-                sums[index] += layer.layer_file_size
+                sums[index] += size
                 found = True
                 break
         assert found
 
-        log.info(
-            f"{layer.layer_file_name} {human_bytes(layer.layer_file_size)} ({layer.layer_file_size} bytes)"
-        )
+    return Histogram(minimum_sizes, counts, sums)
 
-    for index, min_size in buckets:
-        log.info(
-            f">= {human_bytes(min_size)}: {counts[index]} layers total {human_bytes(sums[index])}"
-        )
 
-    assert (
-        counts[3] == 0
-    ), f"there should be no layers larger than 2*checkpoint_distance ({human_bytes(2*checkpoint_distance)})"
-    assert counts[1] == 1, "expect one smaller layer for initdb"
-    assert counts[0] <= 1, "expect at most one tiny layer from shutting down the endpoint"
+def print_layer_size_histogram(h: Histogram):
+    for index, min_size in enumerate(h.buckets):
+        log.info(
+            f">= {human_bytes(min_size)}: {h.counts[index]} layers total {human_bytes(h.sums[index])}"
+        )
