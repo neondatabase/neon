e2197fabcb08de53a53d99ce1fa37da1c69820a7
BodoBolero <peterbendel@neon.tech>
2024-06-14 14:07:49 +0200

    separate job step for halfvec

--- test_runner/performance/test_perf_pgbench.py
+++ test_runner/performance/test_perf_pgbench.py
@@ -251,6 +251,15 @@ def test_pgbench_remote_select_only(remote_compare: PgCompare, scale: int, durat
 # Run this pgbench tests against an existing remote Postgres cluster with the necessary setup.
 @pytest.mark.parametrize("duration", get_durations_matrix())
 @pytest.mark.remote_cluster
-def test_pgbench_remote_pgvector(remote_compare: PgCompare, duration: int):
+def test_pgbench_remote_pgvector_hnsw(remote_compare: PgCompare, duration: int):
     run_test_pgbench(remote_compare, 1, duration, PgBenchLoadType.PGVECTOR_HNSW)
+
+
+# The following test runs on an existing database that has pgvector extension installed
+# and a table with 1 million embedding vectors loaded and indexed with halfvec.
+#
+# Run this pgbench tests against an existing remote Postgres cluster with the necessary setup.
+@pytest.mark.parametrize("duration", get_durations_matrix())
+@pytest.mark.remote_cluster
+def test_pgbench_remote_pgvector_halfvec(remote_compare: PgCompare, duration: int):
     run_test_pgbench(remote_compare, 1, duration, PgBenchLoadType.PGVECTOR_HALFVEC)
