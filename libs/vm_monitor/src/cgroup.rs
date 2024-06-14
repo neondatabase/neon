9053407b50232a82238e9da57b993768a77070d9
Matthias van de Meent <matthias@neon.tech>
2024-06-14 16:54:34 +0200

    Address comments:
    - Use a separate config variable for how many memory history logs to skip
        In passing, use time-based variables to handle this, rather than a
        number that may not handle the values well.
    - rename status_is_close_similar to status_is_close_or_similar
    - Add test covering status_is_close_or_similar that shows the expected behaviour.

--- libs/vm_monitor/src/cgroup.rs
+++ libs/vm_monitor/src/cgroup.rs
@@ -25,6 +25,8 @@ pub struct Config {
     ///
     /// For simplicity, this value must be greater than or equal to `memory_history_len`.
     memory_history_log_interval: usize,
+    /// The max number of iterations to skip before logging the next iteration
+    memory_history_log_noskip_interval: Duration,
 }
 
 impl Default for Config {
@@ -33,6 +35,7 @@ impl Default for Config {
             memory_poll_interval: Duration::from_millis(100),
             memory_history_len: 5, // use 500ms of history for decision-making
             memory_history_log_interval: 20, // but only log every ~2s (otherwise it's spammy)
+            memory_history_log_noskip_interval: Duration::from_secs(60), // but only if it's changed, or 60 seconds have passed
         }
     }
 }
@@ -53,7 +56,7 @@ pub struct CgroupWatcher {
 
 impl CgroupWatcher {
     /// Create a new `CgroupWatcher`.
-    #[tracing::instrument(skip_all, fields(%name))]
+    #[tracing::instrument(skip_all, fields(% name))]
     pub fn new(name: String) -> anyhow::Result<Self> {
         // TODO: clarify exactly why we need v2
         // Make sure cgroups v2 (aka unified) are supported
@@ -85,9 +88,10 @@ impl CgroupWatcher {
 
         // buffer for samples that will be logged. once full, it remains so.
         let history_log_len = self.config.memory_history_log_interval;
+        let max_skip = self.config.memory_history_log_noskip_interval;
         let mut history_log_buf = vec![MemoryStatus::zeroed(); history_log_len];
         let mut last_logged_memusage = MemoryStatus::zeroed();
-        let mut skipped = 0;
+        let mut can_skip_logs_until = Instant::now() - max_skip;
 
         for t in 0_u64.. {
             ticker.tick().await;
@@ -120,24 +124,21 @@ impl CgroupWatcher {
             //
             // We skip logging the data if data hasn't meaningfully changed in a while, unless
             // we've already ignored too many (=history_log_len) previous iterations.
-            if i == history_log_len - 1 {
-                if skipped > history_log_len
+            if i == history_log_len - 1
+                && (now > can_skip_logs_until
                     || !history_log_buf
                         .iter()
-                        .all(|usage| last_logged_memusage.status_is_close_similar(usage))
-                {
-                    info!(
-                        history = ?MemoryStatus::debug_slice(&history_log_buf),
-                        summary = ?summary,
-                        "Recent cgroup memory statistics history"
-                    );
+                        .all(|usage| last_logged_memusage.status_is_close_or_similar(usage)))
+            {
+                info!(
+                    history = ?MemoryStatus::debug_slice(&history_log_buf),
+                    summary = ?summary,
+                    "Recent cgroup memory statistics history"
+                );
 
-                    skipped = 0;
+                can_skip_logs_until = now + max_skip;
 
-                    last_logged_memusage = *history_log_buf.last().unwrap();
-                } else {
-                    skipped += 1;
-                }
+                last_logged_memusage = *history_log_buf.last().unwrap();
             }
 
             updates
@@ -253,7 +254,8 @@ impl MemoryStatus {
     /// Check if the other memory status is a close or similar result.
     /// Returns true if the larger value is not larger than the smaller value
     /// by 1/8 of the smaller value, and within 128MiB.
-    fn status_is_close_similar(&self, other: &MemoryStatus) -> bool {
+    /// See
+    fn status_is_close_or_similar(&self, other: &MemoryStatus) -> bool {
         let margin;
         let diff;
         if self.non_reclaimable >= other.non_reclaimable {
@@ -294,5 +296,66 @@ mod tests {
         assert_eq!(values(1, 4), [8, 9, 0, 1]);
         assert_eq!(values(2, 4), [9, 0, 1, 2]);
         assert_eq!(values(2, 10), [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]);
+    }
+
+    #[test]
+    fn check_similarity_behaviour() {
+        /// This all accesses private methods, so we can't actually run this
+        /// as doctests, because doctests run as an external crate.
+        let mut small = super::MemoryStatus {
+            non_reclaimable: 1024,
+        };
+        let mut large = super::MemoryStatus {
+            non_reclaimable: 1024 * 1024 * 1024 * 1024,
+        };
+
+        // objects are self-similar, no matter the size
+        assert_eq!(true, small.status_is_close_or_similar(&small));
+        assert_eq!(true, large.status_is_close_or_similar(&large));
+
+        // inequality is symmetric
+        assert_eq!(false, small.status_is_close_or_similar(&large));
+        assert_eq!(false, large.status_is_close_or_similar(&small));
+
+        small.non_reclaimable = 64;
+        large.non_reclaimable = (small.non_reclaimable / 8) * 9;
+
+        // objects are self-similar, no matter the size
+        assert_eq!(true, small.status_is_close_or_similar(&small));
+        assert_eq!(true, large.status_is_close_or_similar(&large));
+
+        // values are similar if the larger value is larger by less than
+        // 12.5%, i.e. 1/8 of the smaller value.
+        // In the example above, large is exactly 12.5% larger, so this doesn't
+        // match.
+        assert_eq!(false, small.status_is_close_or_similar(&large));
+        assert_eq!(false, large.status_is_close_or_similar(&small));
+
+        large.non_reclaimable -= 1;
+        assert_eq!(true, large.status_is_close_or_similar(&large));
+
+        assert_eq!(true, small.status_is_close_or_similar(&large));
+        assert_eq!(true, large.status_is_close_or_similar(&small));
+
+        // The 1/8 rule only applies up to 128MiB of difference
+        small.non_reclaimable = 1024 * 1024 * 1024 * 1024;
+        large.non_reclaimable = small.non_reclaimable / 8 * 9;
+        assert_eq!(true, small.status_is_close_or_similar(&small));
+        assert_eq!(true, large.status_is_close_or_similar(&large));
+
+        assert_eq!(false, small.status_is_close_or_similar(&large));
+        assert_eq!(false, large.status_is_close_or_similar(&small));
+        // the large value is put just above the threshold
+        large.non_reclaimable = small.non_reclaimable + 128 * 1024 * 1024;
+        assert_eq!(true, large.status_is_close_or_similar(&large));
+
+        assert_eq!(false, small.status_is_close_or_similar(&large));
+        assert_eq!(false, large.status_is_close_or_similar(&small));
+        // now below
+        large.non_reclaimable -= 1;
+        assert_eq!(true, large.status_is_close_or_similar(&large));
+
+        assert_eq!(true, small.status_is_close_or_similar(&large));
+        assert_eq!(true, large.status_is_close_or_similar(&small));
     }
 }
