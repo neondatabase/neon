a95562337043c4c18b92069842da305e9979e1da
Christian Schwarz <christian@neon.tech>
2024-07-02 16:29:09 +0200

    L0 flush: opt-in mechanism to bypass PageCache reads and writes (#8190)
    part of https://github.com/neondatabase/neon/issues/7418
    
    # Motivation
    
    (reproducing #7418)
    
    When we do an `InMemoryLayer::write_to_disk`, there is a tremendous
    amount of random read I/O, as deltas from the ephemeral file (written in
    LSN order) are written out to the delta layer in key order.
    
    In benchmarks (https://github.com/neondatabase/neon/pull/7409) we can
    see that this delta layer writing phase is substantially more expensive
    than the initial ingest of data, and that within the delta layer write a
    significant amount of the CPU time is spent traversing the page cache.
    
    # High-Level Changes
    
    Add a new mode for L0 flush that works as follows:
    
    * Read the full ephemeral file into memory -- layers are much smaller
    than total memory, so this is afforable
    * Do all the random reads directly from this in memory buffer instead of
    using blob IO/page cache/disk reads.
    * Add a semaphore to limit how many timelines may concurrently do this
    (limit peak memory).
    * Make the semaphore configurable via PS config.
    
    # Implementation Details
    
    The new `BlobReaderRef::Slice` is a temporary hack until we can ditch
    `blob_io` for `InMemoryLayer` => Plan for this is laid out in
    https://github.com/neondatabase/neon/issues/8183
    
    # Correctness
    
    The correctness of this change is quite obvious to me: we do what we did
    before (`blob_io`) but read from memory instead of going to disk.
    
    The highest bug potential is in doing owned-buffers IO. I refactored the
    API a bit in preliminary PR
    https://github.com/neondatabase/neon/pull/8186 to make it less
    error-prone, but still, careful review is requested.
    
    # Performance
    
    I manually measured single-client ingest performance from `pgbench -i
    ...`.
    
    Full report:
    https://neondatabase.notion.site/2024-06-28-benchmarking-l0-flush-performance-e98cff3807f94cb38f2054d8c818fe84?pvs=4
    
    tl;dr:
    
    * no speed improvements during ingest,  but
    * significantly lower pressure on PS PageCache (eviction rate drops to
    1/3)
      * (that's why I'm working on this)
    * noticable but modestly lower CPU time
    
    This is good enough for merging this PR because the changes require
    opt-in.
    
    We'll do more testing in staging & pre-prod.
    
    # Stability / Monitoring
    
    **memory consumption**: there's no _hard_ limit on max `InMemoryLayer`
    size (aka "checkpoint distance") , hence there's no hard limit on the
    memory allocation we do for flushing. In practice, we a) [log a
    warning](https://github.com/neondatabase/neon/blob/23827c6b0d400cbb9a972d4d05d49834816c40d1/pageserver/src/tenant/timeline.rs#L5741-L5743)
    when we flush oversized layers, so we'd know which tenant is to blame
    and b) if we were to put a hard limit in place, we would have to decide
    what to do if there is an InMemoryLayer that exceeds the limit.
    It seems like a better option to guarantee a max size for frozen layer,
    dependent on `checkpoint_distance`. Then limit concurrency based on
    that.
    
    **metrics**: we do have the
    [flush_time_histo](https://github.com/neondatabase/neon/blob/23827c6b0d400cbb9a972d4d05d49834816c40d1/pageserver/src/tenant/timeline.rs#L3725-L3726),
    but that includes the wait time for the semaphore. We could add a
    separate metric for the time spent after acquiring the semaphore, so one
    can infer the wait time. Seems unnecessary at this point, though.

--- pageserver/src/tenant/block_io.rs
+++ pageserver/src/tenant/block_io.rs
@@ -37,6 +37,7 @@ where
 pub enum BlockLease<'a> {
     PageReadGuard(PageReadGuard<'static>),
     EphemeralFileMutableTail(&'a [u8; PAGE_SZ]),
+    Slice(&'a [u8; PAGE_SZ]),
     #[cfg(test)]
     Arc(std::sync::Arc<[u8; PAGE_SZ]>),
     #[cfg(test)]
@@ -63,6 +64,7 @@ impl<'a> Deref for BlockLease<'a> {
         match self {
             BlockLease::PageReadGuard(v) => v.deref(),
             BlockLease::EphemeralFileMutableTail(v) => v,
+            BlockLease::Slice(v) => v,
             #[cfg(test)]
             BlockLease::Arc(v) => v.deref(),
             #[cfg(test)]
@@ -81,6 +83,7 @@ pub(crate) enum BlockReaderRef<'a> {
     FileBlockReader(&'a FileBlockReader<'a>),
     EphemeralFile(&'a EphemeralFile),
     Adapter(Adapter<&'a DeltaLayerInner>),
+    Slice(&'a [u8]),
     #[cfg(test)]
     TestDisk(&'a super::disk_btree::tests::TestDisk),
     #[cfg(test)]
@@ -99,11 +102,30 @@ impl<'a> BlockReaderRef<'a> {
             FileBlockReader(r) => r.read_blk(blknum, ctx).await,
             EphemeralFile(r) => r.read_blk(blknum, ctx).await,
             Adapter(r) => r.read_blk(blknum, ctx).await,
+            Slice(s) => Self::read_blk_slice(s, blknum),
             #[cfg(test)]
             TestDisk(r) => r.read_blk(blknum),
             #[cfg(test)]
             VirtualFile(r) => r.read_blk(blknum, ctx).await,
         }
+    }
+}
+
+impl<'a> BlockReaderRef<'a> {
+    fn read_blk_slice(slice: &[u8], blknum: u32) -> std::io::Result<BlockLease> {
+        let start = (blknum as usize).checked_mul(PAGE_SZ).unwrap();
+        let end = start.checked_add(PAGE_SZ).unwrap();
+        if end > slice.len() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::UnexpectedEof,
+                format!("slice too short, len={} end={}", slice.len(), end),
+            ));
+        }
+        let slice = &slice[start..end];
+        let page_sized: &[u8; PAGE_SZ] = slice
+            .try_into()
+            .expect("we add PAGE_SZ to start, so the slice must have PAGE_SZ");
+        Ok(BlockLease::Slice(page_sized))
     }
 }
 
