7e2a3d2728245d7d34e443b4a08ea041263f4f36
John Spray <john@neon.tech>
2024-07-04 15:05:41 +0100

    pageserver: downgrade stale generation messages to INFO (#8256)
    ## Problem
    
    When generations were new, these messages were an important way of
    noticing if something unexpected was going on. We found some real issues
    when investigating tests that unexpectedly tripped them.
    
    At time has gone on, this code is now pretty battle-tested, and as we do
    more live migrations etc, it's fairly normal to see the occasional
    message from a node with a stale generation.
    
    At this point the cognitive load on developers to selectively allow-list
    these logs outweighs the benefit of having them at warn severity.
    
    Closes: https://github.com/neondatabase/neon/issues/8080
    
    ## Summary of changes
    
    - Downgrade "Dropped remote consistent LSN updates" and "Dropping stale
    deletions" messages to INFO
    - Remove all the allow-list entries for these logs.

--- test_runner/performance/pageserver/pagebench/test_pageserver_max_throughput_getpage_at_latest_lsn.py
+++ test_runner/performance/pageserver/pagebench/test_pageserver_max_throughput_getpage_at_latest_lsn.py
@@ -164,10 +164,6 @@ def setup_tenant_template(env: NeonEnv, pg_bin: PgBin, scale: int):
     }
     template_tenant, template_timeline = env.neon_cli.create_tenant(set_default=True)
     env.pageserver.tenant_detach(template_tenant)
-    env.pageserver.allowed_errors.append(
-        # tenant detach causes this because the underlying attach-hook removes the tenant from storage controller entirely
-        ".*Dropped remote consistent LSN updates.*",
-    )
     env.pageserver.tenant_attach(template_tenant, config)
     ps_http = env.pageserver.http_client()
     with env.endpoints.create_start("main", tenant_id=template_tenant) as ep:
