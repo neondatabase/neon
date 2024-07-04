a36c679eb816424e152acaf976ca26d7eb04cc0a
Arpad Müller <arpad@neon.tech>
2024-07-04 03:22:52 +0200

    Don't issue calls for each timeline, but do a per-tenant listing
    Before:
    2024-07-04T01:16:22.446504Z  INFO Scanned 10 tenants, 88 objects. current <CENSORED>.
    2024-07-04T01:16:27.463923Z  INFO Scanned 20 tenants, 352 objects. current <CENSORED>.
    2024-07-04T01:16:31.973158Z  INFO Scanned 30 tenants, 499 objects. current <CENSORED>.
    2024-07-04T01:18:16.523722Z  INFO Scanned 40 tenants, 19919 objects. current <CENSORED>.
    2024-07-04T01:18:20.928643Z  INFO Scanned 50 tenants, 20018 objects. current <CENSORED>.
    
    After:
    2024-07-04T01:20:50.641208Z  INFO Scanned 10 shards, 97 objects. current <CENSORED>.
    2024-07-04T01:20:52.687879Z  INFO Scanned 20 shards, 368 objects. current <CENSORED>.
    2024-07-04T01:20:54.578079Z  INFO Scanned 30 shards, 522 objects. current <CENSORED>.
    2024-07-04T01:21:05.835102Z  INFO Scanned 40 shards, 19952 objects. current <CENSORED>.
    2024-07-04T01:21:08.150155Z  INFO Scanned 50 shards, 20060 objects. current <CENSORED>.
    
    So it goes from 118s down to 18s.

--- storage_scrubber/src/find_large_objects.rs
+++ storage_scrubber/src/find_large_objects.rs
@@ -2,9 +2,7 @@ use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    init_remote, list_objects_with_retries,
-    metadata_stream::{stream_tenant_timelines, stream_tenants},
-    BucketConfig, NodeKind,
+    init_remote, list_objects_with_retries, metadata_stream::stream_tenants, BucketConfig, NodeKind,
 };
 
 #[derive(Serialize, Deserialize)]
@@ -29,41 +27,41 @@ pub async fn find_large_objects(
     let mut object_ctr = 0u64;
     while let Some(tenant_shard_id) = tenants.next().await {
         let tenant_shard_id = tenant_shard_id?;
-        let mut timelines =
-            std::pin::pin!(stream_tenant_timelines(&s3_client, &target, tenant_shard_id).await?);
-        while let Some(timeline) = timelines.next().await {
-            let timeline = timeline?;
-            let target = target.timeline_root(&timeline);
-            let mut continuation_token = None;
-            loop {
-                let fetch_response =
-                    list_objects_with_retries(&s3_client, &target, continuation_token.clone())
-                        .await?;
-                for obj in fetch_response.contents().iter().filter(|o| {
-                    if let Some(obj_size) = o.size {
-                        min_size as i64 <= obj_size
-                    } else {
-                        false
-                    }
-                }) {
-                    objects.push(LargeObject {
-                        key: obj
-                            .key()
-                            .map(|k| k.to_owned())
-                            .unwrap_or_else(|| "<unknown key>".to_owned()),
-                        size: obj.size.unwrap() as u64,
-                    })
+        let mut tenant_root = target.tenant_root(&tenant_shard_id);
+        // We want the objects and not just common prefixes
+        tenant_root.delimiter.clear();
+        let mut continuation_token = None;
+        loop {
+            let fetch_response =
+                list_objects_with_retries(&s3_client, &tenant_root, continuation_token.clone())
+                    .await?;
+            for obj in fetch_response.contents().iter().filter(|o| {
+                if let Some(obj_size) = o.size {
+                    min_size as i64 <= obj_size
+                } else {
+                    false
                 }
-                object_ctr += fetch_response.contents().len() as u64;
-                match fetch_response.next_continuation_token {
-                    Some(new_token) => continuation_token = Some(new_token),
-                    None => break,
-                }
+            }) {
+                objects.push(LargeObject {
+                    key: obj
+                        .key()
+                        .map(|k| k.to_owned())
+                        .unwrap_or_else(|| "<unknown key>".to_owned()),
+                    size: obj.size.unwrap() as u64,
+                })
+            }
+            object_ctr += fetch_response.contents().len() as u64;
+            match fetch_response.next_continuation_token {
+                Some(new_token) => continuation_token = Some(new_token),
+                None => break,
             }
         }
+
         tenant_ctr += 1;
         if tenant_ctr % 10 == 0 {
-            tracing::info!("Scanned {tenant_ctr} tenants, {object_ctr} objects. current {tenant_shard_id}.");
+            tracing::info!(
+                "Scanned {tenant_ctr} shards, {object_ctr} objects. current {tenant_shard_id}."
+            );
         }
     }
     //let objects = Vec::new();
