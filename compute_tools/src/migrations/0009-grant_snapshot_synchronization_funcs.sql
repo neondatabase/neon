2334bd52034792aae25e398dc1656128d85c541a
Sasha Krassovsky <krassovskysasha@gmail.com>
2024-07-10 14:16:24 -0700

    Only perform grant on pg16

--- compute_tools/src/migrations/0009-grant_snapshot_synchronization_funcs.sql
+++ compute_tools/src/migrations/0009-grant_snapshot_synchronization_funcs.sql
@@ -1,2 +1,7 @@
-GRANT EXECUTE ON FUNCTION pg_export_snapshot TO neon_superuser;
-GRANT EXECUTE ON FUNCTION  pg_log_standby_snapshot TO neon_superuser;
+DO $$
+BEGIN
+    IF (SELECT setting::numeric >= 160000 FROM pg_settings WHERE name = 'server_version_num') THEN
+       EXECUTE 'GRANT EXECUTE ON FUNCTION pg_export_snapshot TO neon_superuser';
+       EXECUTE 'GRANT EXECUTE ON FUNCTION  pg_log_standby_snapshot TO neon_superuser';
+    END IF;
+END $$;
