21b3a191bfd1a1eb1a8df773431a92939033d063
Arpad Müller <arpad-m@users.noreply.github.com>
2024-07-19 21:01:59 +0200

    Add archival_config endpoint to pageserver (#8414)
    This adds an archival_config endpoint to the pageserver. Currently it
    has no effect, and always "works", but later the intent is that it will
    make a timeline archived/unarchived.
    
    - [x] add yml spec
    - [x] add endpoint handler
    
    Part of https://github.com/neondatabase/neon/issues/8088

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -21,6 +21,7 @@ use futures::FutureExt;
 use futures::StreamExt;
 use pageserver_api::models;
 use pageserver_api::models::AuxFilePolicy;
+use pageserver_api::models::TimelineArchivalState;
 use pageserver_api::models::TimelineState;
 use pageserver_api::models::TopTenantShardItem;
 use pageserver_api::models::WalRedoManagerStatus;
@@ -1226,6 +1227,14 @@ impl Tenant {
         }
 
         Ok(timeline_preloads)
+    }
+
+    pub async fn apply_timeline_archival_config(
+        &self,
+        _timeline_id: TimelineId,
+        _config: TimelineArchivalState,
+    ) -> anyhow::Result<()> {
+        Ok(())
     }
 
     pub(crate) fn tenant_shard_id(&self) -> TenantShardId {
