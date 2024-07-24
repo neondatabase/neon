b687cf47742cafc240738faa8f4ba78ef82ffb18
Alexey Masterov <alexeymasterov@neon.tech>
2024-07-24 10:29:38 +0200

    Some cleanup

--- test_runner/logical_repl/test_log_repl.py
+++ test_runner/logical_repl/test_log_repl.py
@@ -5,7 +5,7 @@ import hashlib
 import os
 import time
 from pathlib import Path
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import psycopg2
 import pytest
 import clickhouse_connect
@@ -34,12 +34,12 @@ def timeout_query_clickhouse(client, query: str, digest: str,
             m = hashlib.sha1()
             m.update(repr(tuple(res.result_rows)).encode())
             hash_res = m.hexdigest()
-            if hash_res == digest:
-                return res
         if res:
             log.debug(res.result_rows)
         if hash_res:
             log.debug('Hash: %s', hash_res)
+        if hash_res == digest:
+            return res
         time.sleep(interval)
     raise TimeoutError
 
@@ -63,31 +63,41 @@ def test_clickhouse(test_output_dir: Path, remote_pg: RemotePostgres):
     cmd = ["docker", "compose", "up", "-d", "--quiet-pull", "--no-color", "--wait"]
     subprocess_capture(test_output_dir, cmd, check=True, capture_stdout=True)
     try:
-        with psycopg2.connect(remote_pg.connstr()) as conn:
-            cur_control = conn.cursor()
-            with conn.cursor() as cur:
-                cur.execute("DROP TABLE IF EXISTS table1")
-                cur.execute("CREATE TABLE table1 (id integer primary key, column1 varchar(10));")
-                cur.execute("INSERT INTO table1 (id, column1) VALUES (1, 'abc'), (2, 'def');")
-            conn.commit()
-            client = clickhouse_connect.get_client(host='localhost')
-            client.command("SET allow_experimental_database_materialized_postgresql=1")
-            client.command("CREATE DATABASE db1_postgres ENGINE = "
-                           f"MaterializedPostgreSQL('{conn_options['host']}', "
-                           f"'{conn_options['dbname']}', "
-                           f"'{conn_options['user']}', '{conn_options['password']}') "
-                           "SETTINGS materialized_postgresql_tables_list = 'table1';")
-            res = timeout_query_clickhouse(client,
-                                           "select * from db1_postgres.table1 order by 1",
-                                           'ee600d8f7cd05bd0b169fa81f44300a9dd10085a')
-            log.debug(res.result_rows)
-            with conn.cursor() as cur2:
-                cur2.execute("INSERT INTO table1 (id, column1) VALUES (3, 'ghi'), (4, 'jkl');")
-            conn.commit()
-            res = timeout_query_clickhouse(client,
-                                           "select * from db1_postgres.table1 order by 1",
-                                           '9eba2daaf7e4d7d27ac849525f68b562ab53947d')
-            log.debug(res.result_rows)
+        conn = None
+        for _ in range(5):
+            try:
+                conn = psycopg2.connect(remote_pg.connstr())
+            except psycopg2.OperationalError as perr:
+                log.debug(perr)
+                time.sleep(1)
+            else:
+                break
+            raise TimeoutError
+        cur = conn.cursor()
+        cur.execute("DROP TABLE IF EXISTS table1")
+        cur.execute("CREATE TABLE table1 (id integer primary key, column1 varchar(10));")
+        cur.execute("INSERT INTO table1 (id, column1) VALUES (1, 'abc'), (2, 'def');")
+        conn.commit()
+        client = clickhouse_connect.get_client(host='localhost')
+        client.command("SET allow_experimental_database_materialized_postgresql=1")
+        client.command("CREATE DATABASE db1_postgres ENGINE = "
+                       f"MaterializedPostgreSQL('{conn_options['host']}', "
+                       f"'{conn_options['dbname']}', "
+                       f"'{conn_options['user']}', '{conn_options['password']}') "
+                       "SETTINGS materialized_postgresql_tables_list = 'table1';")
+        res = timeout_query_clickhouse(client,
+                                       "select * from db1_postgres.table1 order by 1",
+                                       'ee600d8f7cd05bd0b169fa81f44300a9dd10085a')
+        log.debug(res.result_rows)
+        cur.execute("INSERT INTO table1 (id, column1) VALUES (3, 'ghi'), (4, 'jkl');")
+        conn.commit()
+        res = timeout_query_clickhouse(client,
+                                       "select * from db1_postgres.table1 order by 1",
+                                       '9eba2daaf7e4d7d27ac849525f68b562ab53947d')
+        log.debug(res.result_rows)
+        log.debug('Sleeping before final checking if Neon is still alive')
+        time.sleep(3)
+        cur.execute('SELECT 1')
         cmd = ["docker", "compose", "down"]
         subprocess_capture(test_output_dir, cmd, check=True, capture_stdout=True)
     except Exception as e:
