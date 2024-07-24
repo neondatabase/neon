468c1873c77836adb35ffe91667c8107c7cd0e13
Alexey Masterov <alexeymasterov@neon.tech>
2024-07-24 11:14:44 +0200

    ruff format

--- test_runner/logical_repl/test_log_repl.py
+++ test_runner/logical_repl/test_log_repl.py
@@ -1,6 +1,7 @@
 """
 Test the logical replication in Neon with the different consumers
 """
+
 import hashlib
 import os
 import time
@@ -16,8 +17,13 @@ from fixtures.neon_fixtures import RemotePostgres
 from fixtures.utils import subprocess_capture
 
 
-def timeout_query_clickhouse(client, query: str, digest: str,
-                             timeout: timedelta = timedelta(seconds=30), interval: float = 0.5):
+def timeout_query_clickhouse(
+    client,
+    query: str,
+    digest: str,
+    timeout: timedelta = timedelta(seconds=30),
+    interval: float = 0.5,
+):
     """
     Repeatedly run the query on the client
     return answer if successful, raise an exception otherwise
@@ -28,7 +34,7 @@ def timeout_query_clickhouse(client, query: str, digest: str,
         try:
             res = client.query(query)
         except DatabaseError as dbe:
-            log.debug('Query: %s', query)
+            log.debug("Query: %s", query)
             log.debug(dbe)
         else:
             m = hashlib.sha1()
@@ -37,7 +43,7 @@ def timeout_query_clickhouse(client, query: str, digest: str,
         if res:
             log.debug(res.result_rows)
         if hash_res:
-            log.debug('Hash: %s', hash_res)
+            log.debug("Hash: %s", hash_res)
         if hash_res == digest:
             return res
         time.sleep(interval)
@@ -58,7 +64,7 @@ def test_clickhouse(test_output_dir: Path, remote_pg: RemotePostgres):
     """
     Test the logical replication having ClickHouse as a client
     """
-    os.chdir(os.path.join(Path(__file__).parent, 'clickhouse'))
+    os.chdir(os.path.join(Path(__file__).parent, "clickhouse"))
     conn_options = remote_pg.conn_options()
     cmd = ["docker", "compose", "up", "-d", "--quiet-pull", "--no-color", "--wait"]
     subprocess_capture(test_output_dir, cmd, check=True, capture_stdout=True)
@@ -78,26 +84,32 @@ def test_clickhouse(test_output_dir: Path, remote_pg: RemotePostgres):
         cur.execute("CREATE TABLE table1 (id integer primary key, column1 varchar(10));")
         cur.execute("INSERT INTO table1 (id, column1) VALUES (1, 'abc'), (2, 'def');")
         conn.commit()
-        client = clickhouse_connect.get_client(host='localhost')
+        client = clickhouse_connect.get_client(host="localhost")
         client.command("SET allow_experimental_database_materialized_postgresql=1")
-        client.command("CREATE DATABASE db1_postgres ENGINE = "
-                       f"MaterializedPostgreSQL('{conn_options['host']}', "
-                       f"'{conn_options['dbname']}', "
-                       f"'{conn_options['user']}', '{conn_options['password']}') "
-                       "SETTINGS materialized_postgresql_tables_list = 'table1';")
-        res = timeout_query_clickhouse(client,
-                                       "select * from db1_postgres.table1 order by 1",
-                                       'ee600d8f7cd05bd0b169fa81f44300a9dd10085a')
+        client.command(
+            "CREATE DATABASE db1_postgres ENGINE = "
+            f"MaterializedPostgreSQL('{conn_options['host']}', "
+            f"'{conn_options['dbname']}', "
+            f"'{conn_options['user']}', '{conn_options['password']}') "
+            "SETTINGS materialized_postgresql_tables_list = 'table1';"
+        )
+        res = timeout_query_clickhouse(
+            client,
+            "select * from db1_postgres.table1 order by 1",
+            "ee600d8f7cd05bd0b169fa81f44300a9dd10085a",
+        )
         log.debug(res.result_rows)
         cur.execute("INSERT INTO table1 (id, column1) VALUES (3, 'ghi'), (4, 'jkl');")
         conn.commit()
-        res = timeout_query_clickhouse(client,
-                                       "select * from db1_postgres.table1 order by 1",
-                                       '9eba2daaf7e4d7d27ac849525f68b562ab53947d')
+        res = timeout_query_clickhouse(
+            client,
+            "select * from db1_postgres.table1 order by 1",
+            "9eba2daaf7e4d7d27ac849525f68b562ab53947d",
+        )
         log.debug(res.result_rows)
-        log.debug('Sleeping before final checking if Neon is still alive')
+        log.debug("Sleeping before final checking if Neon is still alive")
         time.sleep(3)
-        cur.execute('SELECT 1')
+        cur.execute("SELECT 1")
         clickhouse_cleanup(test_output_dir)
     except Exception as e:
         clickhouse_cleanup(test_output_dir)
