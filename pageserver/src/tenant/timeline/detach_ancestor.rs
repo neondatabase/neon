14a0517c7f5b8fa4fd3b1ecc758b076e6b538d00
Joonas Koivunen <joonas@neon.tech>
2024-07-25 09:23:41 +0000

    also assert still ongoing

--- pageserver/src/tenant/timeline/detach_ancestor.rs
+++ pageserver/src/tenant/timeline/detach_ancestor.rs
@@ -1132,6 +1132,10 @@ pub(super) async fn detach_and_reparent(
         if let Some(ancestor) = existing {
             Ancestor::Detached(ancestor, ancestor_lsn)
         } else {
+            assert!(
+                still_ongoing,
+                "cannot complete if the operation is not still ongoing"
+            );
             let direct_children =
                 reparented_direct_children(detached, tenant).map_err(Error::from)?;
             return Ok(DetachingAndReparenting::AlreadyDone(direct_children));
