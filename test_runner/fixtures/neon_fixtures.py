6cad0455b07b9270dd0d9650c3e655fe6fa53f1e
Alexander Bayandin <alexander@neon.tech>
2024-07-27 20:01:10 +0100

    CI(test_runner): Upload all test artifacts if preserve_database_files is enabled (#7990)
    ## Problem
    
    There's a `NeonEnvBuilder#preserve_database_files` parameter that allows
    you to keep database files for debugging purposes (by default, files get
    cleaned up), but there's no way to get these files from a CI run.
    This PR adds handling of `NeonEnvBuilder#preserve_database_files` and
    adds the compressed test output directory to Allure reports (for tests
    with this parameter enabled).
    
    Ref https://github.com/neondatabase/neon/issues/6967
    
    ## Summary of changes
    - Compress and add the whole test output directory to Allure reports
    - Currently works only with `neon_env_builder` fixture
    - Remove `preserve_database_files = True` from sharding tests as
    unneeded
    
    ---------
    
    Co-authored-by: Christian Schwarz <christian@neon.tech>

--- test_runner/fixtures/neon_fixtures.py
+++ test_runner/fixtures/neon_fixtures.py
@@ -1394,7 +1394,7 @@ def _shared_simple_env(
         pg_distrib_dir=pg_distrib_dir,
         pg_version=pg_version,
         run_id=run_id,
-        preserve_database_files=pytestconfig.getoption("--preserve-database-files"),
+        preserve_database_files=cast(bool, pytestconfig.getoption("--preserve-database-files")),
         test_name=request.node.name,
         test_output_dir=test_output_dir,
         pageserver_virtual_file_io_engine=pageserver_virtual_file_io_engine,
@@ -1469,7 +1469,7 @@ def neon_env_builder(
         pg_version=pg_version,
         broker=default_broker,
         run_id=run_id,
-        preserve_database_files=pytestconfig.getoption("--preserve-database-files"),
+        preserve_database_files=cast(bool, pytestconfig.getoption("--preserve-database-files")),
         pageserver_virtual_file_io_engine=pageserver_virtual_file_io_engine,
         test_name=request.node.name,
         test_output_dir=test_output_dir,
@@ -1478,6 +1478,11 @@ def neon_env_builder(
         pageserver_default_tenant_config_compaction_algorithm=pageserver_default_tenant_config_compaction_algorithm,
     ) as builder:
         yield builder
+        # Propogate `preserve_database_files` to make it possible to use in other fixtures,
+        # like `test_output_dir` fixture for attaching all database files to Allure report.
+        request.node.user_properties.append(
+            ("preserve_database_files", builder.preserve_database_files)
+        )
 
 
 @dataclass
@@ -4478,7 +4483,16 @@ def test_output_dir(
 
     yield test_dir
 
-    allure_attach_from_dir(test_dir)
+    preserve_database_files = False
+    for k, v in request.node.user_properties:
+        # NB: the neon_env_builder fixture uses this fixture (test_output_dir).
+        # So, neon_env_builder's cleanup runs before here.
+        # The cleanup propagates NeonEnvBuilder.preserve_database_files into this user property.
+        if k == "preserve_database_files":
+            assert isinstance(v, bool)
+            preserve_database_files = v
+
+    allure_attach_from_dir(test_dir, preserve_database_files)
 
 
 class FileAndThreadLock:
