a4d3e0c747ebc82ce7b5a706883de5976d8e8c62
Arpad Müller <arpad-m@users.noreply.github.com>
2024-07-29 12:05:18 +0200

    Adopt list_streaming in tenant deletion (#8504)
    Uses the Stream based `list_streaming` function added by #8457 in tenant
    deletion, as suggested in https://github.com/neondatabase/neon/pull/7932#issuecomment-2150480180 .
    
    We don't have to worry about retries, as the function is wrapped inside
    an outer retry block. If there is a retryable error either during the
    listing or during deletion, we just do a fresh start.
    
    Also adds `+ Send` bounds as they are required by the
    `delete_tenant_remote` function.

--- libs/remote_storage/src/simulate_failures.rs
+++ libs/remote_storage/src/simulate_failures.rs
@@ -114,7 +114,7 @@ impl RemoteStorage for UnreliableWrapper {
         mode: ListingMode,
         max_keys: Option<NonZeroU32>,
         cancel: &CancellationToken,
-    ) -> impl Stream<Item = Result<Listing, DownloadError>> {
+    ) -> impl Stream<Item = Result<Listing, DownloadError>> + Send {
         async_stream::stream! {
             self.attempt(RemoteOp::ListPrefixes(prefix.cloned()))
                 .map_err(DownloadError::Other)?;
