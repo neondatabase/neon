31122adee3fa0dac6bec5bfd1682f04adf42e490
Christian Schwarz <christian@neon.tech>
2024-07-31 17:05:45 +0200

    refactor(page_service): Timeline gate guard holding + cancellation + shutdown (#8339)
    Since the introduction of sharding, the protocol handling loop in
    `handle_pagerequests` cannot know anymore which concrete
    `Tenant`/`Timeline` object any of the incoming `PagestreamFeMessage`
    resolves to.
    In fact, one message might resolve to one `Tenant`/`Timeline` while
    the next one may resolve to another one.
    
    To avoid going to tenant manager, we added the `shard_timelines` which
    acted as an ever-growing cache that held timeline gate guards open for
    the lifetime of the connection.
    The consequence of holding the gate guards open was that we had to be
    sensitive to every cached `Timeline::cancel` on each interaction with
    the network connection, so that Timeline shutdown would not have to wait
    for network connection interaction.
    
    We can do better than that, meaning more efficiency & better
    abstraction.
    I proposed a sketch for it in
    
    * https://github.com/neondatabase/neon/pull/8286
    
    and this PR implements an evolution of that sketch.
    
    The main idea is is that `mod page_service` shall be solely concerned
    with the following:
    1. receiving requests by speaking the protocol / pagestream subprotocol
    2. dispatching the request to a corresponding method on the correct
    shard/`Timeline` object
    3. sending response by speaking the protocol / pagestream subprotocol.
    
    The cancellation sensitivity responsibilities are clear cut:
    * while in `page_service` code, sensitivity to page_service cancellation
    is sufficient
    * while in `Timeline` code, sensitivity to `Timeline::cancel` is
    sufficient
    
    To enforce these responsibilities, we introduce the notion of a
    `timeline::handle::Handle` to a `Timeline` object that is checked out
    from a `timeline::handle::Cache` for **each request**.
    The `Handle` derefs to `Timeline` and is supposed to be used for a
    single async method invocation on `Timeline`.
    See the lengthy doc comment in `mod handle` for details of the design.

--- pageserver/src/tenant/timeline.rs
+++ pageserver/src/tenant/timeline.rs
@@ -3,6 +3,7 @@ pub(crate) mod compaction;
 pub mod delete;
 pub(crate) mod detach_ancestor;
 mod eviction_task;
+pub(crate) mod handle;
 mod init;
 pub mod layer_manager;
 pub(crate) mod logical_size;
@@ -17,6 +18,7 @@ use camino::Utf8Path;
 use chrono::{DateTime, Utc};
 use enumset::EnumSet;
 use fail::fail_point;
+use handle::ShardTimelineId;
 use once_cell::sync::Lazy;
 use pageserver_api::{
     key::{
@@ -424,6 +426,8 @@ pub struct Timeline {
     pub(crate) extra_test_dense_keyspace: ArcSwap<KeySpace>,
 
     pub(crate) l0_flush_global_state: L0FlushGlobalState,
+
+    pub(crate) handles: handle::PerTimelineState<crate::page_service::TenantManagerTypes>,
 }
 
 pub struct WalReceiverInfo {
@@ -1915,6 +1919,9 @@ impl Timeline {
         tracing::debug!("Cancelling CancellationToken");
         self.cancel.cancel();
 
+        // Ensure Prevent new page service requests from starting.
+        self.handles.shutdown();
+
         // Transition the remote_client into a state where it's only useful for timeline deletion.
         // (The deletion use case is why we can't just hook up remote_client to Self::cancel).)
         self.remote_client.stop();
@@ -2440,6 +2447,8 @@ impl Timeline {
                 extra_test_dense_keyspace: ArcSwap::new(Arc::new(KeySpace::default())),
 
                 l0_flush_global_state: resources.l0_flush_global_state,
+
+                handles: Default::default(),
             };
             result.repartition_threshold =
                 result.get_checkpoint_distance() / REPARTITION_FREQ_IN_CHECKPOINT_DISTANCE;
@@ -3707,6 +3716,17 @@ impl Timeline {
 
     pub(crate) fn get_shard_identity(&self) -> &ShardIdentity {
         &self.shard_identity
+    }
+
+    #[inline(always)]
+    pub(crate) fn shard_timeline_id(&self) -> ShardTimelineId {
+        ShardTimelineId {
+            shard_index: ShardIndex {
+                shard_number: self.shard_identity.number,
+                shard_count: self.shard_identity.count,
+            },
+            timeline_id: self.timeline_id,
+        }
     }
 
     ///
