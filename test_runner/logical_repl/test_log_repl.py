9e6b46b2e6d49dd3b5a0fd82b1dc4660f2a86004
Alexey Masterov <alexeymasterov@neon.tech>
2024-08-01 09:47:06 +0200

    Change formatting

--- test_runner/logical_repl/test_log_repl.py
+++ test_runner/logical_repl/test_log_repl.py
@@ -13,13 +13,15 @@ import requests
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import RemotePostgres
 from fixtures.utils import wait_until
-from kafka import KafkaConsumer  #type: ignore
+
+# type: ignore
+from kafka import KafkaConsumer
 
 
 def query_clickhouse(
-        client,
-        query: str,
-        digest: str,
+    client,
+    query: str,
+    digest: str,
 ) -> None:
     """
     Run the query on the client
