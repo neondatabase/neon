3d11f6e46ab814ca950626aa9d3ab669c6537e9c
Joonas Koivunen <joonas@neon.tech>
2024-08-05 13:19:44 +0000

    map globalshutdown to shuttingdown

--- pageserver/src/tenant/mgr.rs
+++ pageserver/src/tenant/mgr.rs
@@ -2045,7 +2045,8 @@ impl TenantManager {
                 None,
                 SpawnMode::Eager,
                 ctx,
-            )?;
+            )
+            .map_err(|_| Error::ShuttingDown)?;
 
             {
                 let mut g = tenant.ongoing_timeline_detach.lock().unwrap();
