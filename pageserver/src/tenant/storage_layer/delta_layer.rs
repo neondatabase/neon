5985adb5dcff082f501b55351e13c49d80ab3b26
Alex Chi Z <chi@neon.tech>
2024-08-06 14:20:21 +0800

    feat(pageserver): use split layer writer in gc-compaction
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- pageserver/src/tenant/storage_layer/delta_layer.rs
+++ pageserver/src/tenant/storage_layer/delta_layer.rs
@@ -694,12 +694,10 @@ impl DeltaLayerWriter {
             .await
     }
 
-    #[cfg(test)]
     pub(crate) fn num_keys(&self) -> usize {
         self.inner.as_ref().unwrap().num_keys
     }
 
-    #[cfg(test)]
     pub(crate) fn estimated_size(&self) -> u64 {
         let inner = self.inner.as_ref().unwrap();
         inner.blob_writer.size() + inner.tree.borrow_writer().size() + PAGE_SZ as u64
