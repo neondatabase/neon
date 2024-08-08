74f987d0843af5a15dcba5d85b5c93d0f43f5746
Konstantin Knizhnik <knizhnik@neon.tech>
2024-08-08 23:19:34 +0300

    Add tests for UPDATE,LOCK,MULTIINSERT

--- test_runner/regress/test_combocid.py
+++ test_runner/regress/test_combocid.py
@@ -1,7 +1,7 @@
 from fixtures.neon_fixtures import NeonEnvBuilder
 
 
-def test_combocid_delete(neon_env_builder: NeonEnvBuilder):
+def do_combocid_op(neon_env_builder: NeonEnvBuilder, op):
     env = neon_env_builder.init_start()
     endpoint = env.endpoints.create_start(
         "main",
@@ -29,10 +29,66 @@ def test_combocid_delete(neon_env_builder: NeonEnvBuilder):
     rows = cur.fetchall()
     assert len(rows) == 500
 
+    # Perform specified operation
+    cur.execute(op)
+
+    # Clear the cache, so that we exercise reconstructing the pages
+    # from WAL
+    cur.execute("SELECT clear_buffer_cache()")
+
+    # Check that the cursor opened earlier still works. If the
+    # combocids are not restored correctly, it won't.
+    cur.execute("fetch all from c1")
+    rows = cur.fetchall()
+    assert len(rows) == 500
+
+    cur.execute("rollback")
+
+
+def test_combocid_delete(neon_env_builder: NeonEnvBuilder):
+    do_combocid_op(neon_env_builder, "delete from t")
+
+
+def test_combocid_update(neon_env_builder: NeonEnvBuilder):
+    do_combocid_op(neon_env_builder, "update t set val=val+1")
+
+
+def test_combocid_lock(neon_env_builder: NeonEnvBuilder):
+    do_combocid_op(neon_env_builder, "select * from t for update")
+
+
+def test_combocid_multi_insert(neon_env_builder: NeonEnvBuilder):
+    env = neon_env_builder.init_start()
+    endpoint = env.endpoints.create_start(
+        "main",
+        config_lines=[
+            "shared_buffers='1MB'",
+        ],
+    )
+
+    conn = endpoint.connect()
+    cur = conn.cursor()
+    n_records = 1000
+
+    cur.execute("CREATE EXTENSION neon_test_utils")
+
+    cur.execute("create table t(id integer, val integer)")
+    cur.execute(f"insert into t select g, 0 from generate_series(1,{n_records}) g")
+    cur.execute("copy t to '/tmp/t.csv'")
+    cur.execute("truncate table t")
+
+    cur.execute("begin")
+    cur.execute("copy t from '/tmp/t.csv'")
+
+    # Open a cursor that scroll it halfway through
+    cur.execute("DECLARE c1 NO SCROLL CURSOR WITHOUT HOLD FOR SELECT * FROM t")
+    cur.execute("fetch 500 from c1")
+    rows = cur.fetchall()
+    assert len(rows) == 500
+
     # Delete all the rows. Because all of the rows were inserted earlier in the
     # same transaction, all the rows will get a combocid.
     cur.execute("delete from t")
-
     # Clear the cache, so that we exercise reconstructing the pages
     # from WAL
     cur.execute("SELECT clear_buffer_cache()")
