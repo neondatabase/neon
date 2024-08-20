d13fd375519da413572218331c6768d863e4c7d2
Conrad Ludgate <conradludgate@gmail.com>
2024-08-20 11:43:47 +0100

    fix warnings and parse jwks urls ahead of time

--- proxy/src/http/health_server.rs
+++ proxy/src/http/health_server.rs
@@ -7,7 +7,7 @@ use std::{
     net::TcpListener,
     sync::{Arc, Mutex},
 };
-use tracing::{info, info_span};
+use tracing::{info, info_span, warn};
 use utils::http::{
     endpoint::{self, request_span},
     error::ApiError,
@@ -15,7 +15,11 @@ use utils::http::{
     RouterBuilder, RouterService,
 };
 
-use crate::{auth::backend::local::JWKS_ROLE_MAP, console::messages::JwksRoleMapping, jemalloc};
+use crate::{
+    auth::backend::local::{JwksRoleSettings, JWKS_ROLE_MAP},
+    console::messages::JwksRoleMapping,
+    jemalloc,
+};
 
 async fn status_handler(_: Request<Body>) -> Result<Response<Body>, ApiError> {
     json_response(StatusCode::OK, "")
@@ -111,10 +115,79 @@ async fn update_jwks(req: Request<Body>) -> Result<Response<Body>, ApiError> {
     let bytes = to_bytes(req.into_body())
         .await
         .map_err(|e| ApiError::BadRequest(e.into()))?;
-    let data: JwksRoleMapping =
+    let mut data: JwksRoleMapping =
         serde_json::from_slice(&bytes).map_err(|e| ApiError::BadRequest(e.into()))?;
 
-    JWKS_ROLE_MAP.store(Some(Arc::new(data)));
+    let mut settings = None;
+
+    for mapping in data.roles.values_mut() {
+        for jwks in &mut mapping.jwks {
+            if !jwks.jwks_url.has_authority()
+                || jwks.jwks_url.scheme() != "http"
+                || jwks.jwks_url.scheme() != "https"
+            {
+                return json_response(StatusCode::BAD_REQUEST, "Invalid JWKS url. Must be HTTP");
+            }
+            if !jwks
+                .jwks_url
+                .host()
+                .is_some_and(|h| h != url::Host::Domain(""))
+            {
+                return json_response(
+                    StatusCode::BAD_REQUEST,
+                    "Invalid JWKS url. No domain listed",
+                );
+            }
+
+            // clear username, password and ports
+            jwks.jwks_url.set_port(None).expect(
+                "url can be a base and has a valid host and is not a file. should not error",
+            );
+            jwks.jwks_url.set_username("").expect(
+                "url can be a base and has a valid host and is not a file. should not error",
+            );
+            jwks.jwks_url.set_password(None).expect(
+                "url can be a base and has a valid host and is not a file. should not error",
+            );
+
+            // clear query params
+            jwks.jwks_url.set_fragment(None);
+            jwks.jwks_url.query_pairs_mut().clear().finish();
+
+            if jwks.jwks_url.scheme() != "https" {
+                // local testing is hard if we need to set up https support.
+                if cfg!(not(feature = "testing")) {
+                    jwks.jwks_url
+                        .set_scheme("https")
+                        .expect("should not error to set the scheme to https if it was http");
+                } else {
+                    warn!(scheme = jwks.jwks_url.scheme(), "JWKS url is not HTTPS");
+                }
+            }
+
+            let (pr, br) = settings.get_or_insert((jwks.project_id, jwks.branch_id));
+            if *pr != jwks.project_id {
+                return json_response(
+                    StatusCode::BAD_REQUEST,
+                    "inconsistent project IDs configured",
+                );
+            }
+            if *br != jwks.branch_id {
+                return json_response(
+                    StatusCode::BAD_REQUEST,
+                    "inconsistent branch IDs configured",
+                );
+            }
+        }
+    }
+
+    if let Some((project_id, branch_id)) = settings {
+        JWKS_ROLE_MAP.store(Some(Arc::new(JwksRoleSettings {
+            roles: data.roles,
+            project_id,
+            branch_id,
+        })));
+    }
 
     json_response(StatusCode::OK, ())
 }
