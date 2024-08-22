8d0c3faf11427fb618d930f797f1277b3be03fb6
Folke Behrens <folke@neon.tech>
2024-08-22 12:24:02 +0200

    Drop clippy::nursery

--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -5,11 +5,9 @@
     future_incompatible,
     // TODO: consider let_underscore
     nonstandard_style,
-    rust_2018_idioms,
-    rust_2021_compatibility,
     rust_2024_compatibility
 )]
-#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![warn(clippy::all, clippy::pedantic, clippy::cargo)]
 // List of denied lints from the clippy::restriction group.
 // https://rust-lang.github.io/rust-clippy/master/index.html#?groups=restriction
 #![warn(
@@ -45,14 +43,10 @@
     clippy::cast_possible_wrap,
     clippy::cast_precision_loss,
     clippy::cast_sign_loss,
-    clippy::cognitive_complexity,
     clippy::default_trait_access,
-    clippy::derive_partial_eq_without_eq,
     clippy::doc_markdown,
-    clippy::equatable_if_let,
     clippy::explicit_iter_loop,
     clippy::float_cmp,
-    clippy::future_not_send,
     clippy::if_not_else,
     clippy::ignored_unit_patterns,
     clippy::implicit_hasher,
@@ -65,7 +59,6 @@
     clippy::match_bool,
     clippy::match_same_arms,
     clippy::match_wild_err_arm,
-    clippy::missing_const_for_fn,
     clippy::missing_errors_doc,
     clippy::missing_panics_doc,
     clippy::module_name_repetitions,
@@ -75,30 +68,20 @@
     clippy::needless_pass_by_value,
     clippy::needless_raw_string_hashes,
     clippy::option_as_ref_cloned,
-    clippy::option_if_let_else,
-    clippy::or_fun_call,
-    clippy::redundant_clone,
     clippy::redundant_closure_for_method_calls,
     clippy::redundant_else,
-    clippy::redundant_pub_crate,
     clippy::return_self_not_must_use,
-    clippy::significant_drop_in_scrutinee,
-    clippy::significant_drop_tightening,
     clippy::similar_names,
     clippy::single_char_pattern,
     clippy::single_match_else,
-    clippy::string_lit_as_bytes,
     clippy::struct_excessive_bools,
     clippy::struct_field_names,
-    clippy::suboptimal_flops,
     clippy::too_many_lines,
-    clippy::tuple_array_conversions,
     clippy::uninlined_format_args,
     clippy::unnested_or_patterns,
     clippy::unreadable_literal,
     clippy::unused_async,
     clippy::unused_self,
-    clippy::use_self,
     clippy::used_underscore_binding,
     clippy::wildcard_imports
 )]
