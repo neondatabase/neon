b1fe356b92f44e4b1925c7ad1b70a11eed53bd34
Yuchen Liang <yuchen@neon.tech>
2024-08-26 17:09:33 -0400

    add ability to specify alignment in test
    Signed-off-by: Yuchen Liang <yuchen@neon.tech>

--- test_runner/fixtures/parametrize.py
+++ test_runner/fixtures/parametrize.py
@@ -35,6 +35,11 @@ def pageserver_virtual_file_io_engine() -> Optional[str]:
 
 
 @pytest.fixture(scope="function", autouse=True)
+def pageserver_io_buffer_alignment() -> Optional[str]:
+    return os.getenv("PAGESERVER_VIRTUAL_FILE_IO_ENGINE")
+
+
+@pytest.fixture(scope="function", autouse=True)
 def pageserver_aux_file_policy() -> Optional[AuxFileStore]:
     return None
 
