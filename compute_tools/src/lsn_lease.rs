2e93bb6de44d1f405a121b3ff636f9e898928ca0
Yuchen Liang <70461588+yliang412@users.noreply.github.com>
2024-08-28 10:23:16 -0400

    review: make background task failure explicit in warning log
    Co-authored-by: Alexey Kondratov <kondratov.aleksey@gmail.com>

--- compute_tools/src/lsn_lease.rs
+++ compute_tools/src/lsn_lease.rs
@@ -33,7 +33,7 @@ pub fn launch_lsn_lease_bg_task_for_static(compute: &Arc<ComputeNode>) {
         let _entered = span.entered();
         if let Err(e) = lsn_lease_bg_task(compute, tenant_id, timeline_id, lsn) {
             // TODO: might need stronger error feedback than logging an warning.
-            warn!("Request failed: {e}");
+            warn!("Exited with error: {e}");
         }
     });
 }
