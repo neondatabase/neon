a83c4621ade89a91f2b2b2613891698a8d2047f3
Alex Chi Z <chi@neon.tech>
2024-08-30 11:29:30 -0400

    resolve comments
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- storage_scrubber/src/checks.rs
+++ storage_scrubber/src/checks.rs
@@ -48,34 +48,51 @@ impl TimelineAnalysis {
     }
 }
 
+/// Checks whether a layer map is valid (i.e., is a valid result of the current compaction algorithm if nothing goes wrong).
+/// The function checks if we can split the LSN range of a delta layer only at the LSNs of the delta layers. For example,
+///
+/// ```plain
+/// |       |                 |       |
+/// |   1   |    |   2   |    |   3   |
+/// |       |    |       |    |       |
+/// ```
+///
+/// This is not a valid layer map because the LSN range of layer 1 intersects with the LSN range of layer 2. 1 and 2 should have
+/// the same LSN range.
+///
+/// The exception is that when layer 2 only contains a single key, it could be split over the LSN range. For example,
+///
+/// ```plain
+/// |       |    |   2   |    |       |
+/// |   1   |    |-------|    |   3   |
+/// |       |    |   4   |    |       |
+///
+/// If layer 2 and 4 contain the same single key, this is also a valid layer map.
 fn check_valid_layermap(metadata: &HashMap<LayerName, LayerFileMetadata>) -> Option<String> {
     let mut lsn_split_point = BTreeSet::new(); // TODO: use a better data structure (range tree / range set?)
     let mut all_delta_layers = Vec::new();
     for (name, _) in metadata.iter() {
         if let LayerName::Delta(layer) = name {
-            all_delta_layers.push(layer.clone());
+            if layer.key_range.start.next() != layer.key_range.end {
+                all_delta_layers.push(layer.clone());
+            }
         }
     }
     for layer in &all_delta_layers {
-        if layer.key_range.start.next() != layer.key_range.end {
-            let lsn_range = &layer.lsn_range;
-            lsn_split_point.insert(lsn_range.start);
-            lsn_split_point.insert(lsn_range.end);
-        }
+        let lsn_range = &layer.lsn_range;
+        lsn_split_point.insert(lsn_range.start);
+        lsn_split_point.insert(lsn_range.end);
     }
     for layer in &all_delta_layers {
-        let key_range = &layer.key_range;
-        if key_range.start.next() != key_range.end {
-            let lsn_range = layer.lsn_range.clone();
-            let intersects = lsn_split_point.range(lsn_range).collect_vec();
-            if intersects.len() > 1 {
-                let err = format!(
+        let lsn_range = layer.lsn_range.clone();
+        let intersects = lsn_split_point.range(lsn_range).collect_vec();
+        if intersects.len() > 1 {
+            let err = format!(
                         "layer violates the layer map LSN split assumption: layer {} intersects with LSN [{}]",
                         layer,
                         intersects.into_iter().map(|lsn| lsn.to_string()).join(", ")
                     );
-                return Some(err);
-            }
+            return Some(err);
         }
     }
     None
