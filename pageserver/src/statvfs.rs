5e41378b2765f20e591be906bd961e4444f6188e
Christian Schwarz <christian@neon.tech>
2024-09-04 14:41:39 +0200

    conditional compilation is hard

--- pageserver/src/statvfs.rs
+++ pageserver/src/statvfs.rs
@@ -91,7 +91,6 @@ pub mod mock {
                     block_size: *blocksize,
                 })
             }
-            #[cfg(feature = "testing")]
             Behavior::Failure { mocked_error } => Err((*mocked_error).into()),
         }
     }
