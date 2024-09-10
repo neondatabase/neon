dbde226f38362148052b93a94e6af2ef51e3201b
Alexey Masterov <alexeymasterov@neon.tech>
2024-09-10 12:21:09 +0200

    Add debug

--- test_runner/cloud_regress/test_cloud_regress.py
+++ test_runner/cloud_regress/test_cloud_regress.py
@@ -53,9 +53,11 @@ def setup(remote_pg: RemotePostgres):
             )
             conn.rollback()
             yield
+            log.info("Looking for extra roles...")
             cur.execute(
                 "SELECT rolname FROM pg_catalog.pg_roles WHERE oid > 16384 AND rolname <> 'neondb_owner'"
             )
+            log.info("Rows count: %s", cur.rowcount)
             for role in cur:
                 cur.execute(f"DROP ROLE {role[0]}")
             conn.commit()
