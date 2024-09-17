762576f3dce600b1e3e7ed897c70134f2038ec61
Heikki Linnakangas <heikki@neon.tech>
2024-09-17 19:56:22 +0300

    Add bucket_le as an explicit column to the views

--- pgxn/neon/neon_perf_counters.c
+++ pgxn/neon/neon_perf_counters.c
@@ -9,6 +9,8 @@
  */
 #include "postgres.h"
 
+#include <math.h>
+
 #include "funcapi.h"
 #include "miscadmin.h"
 #include "storage/proc.h"
@@ -79,6 +81,8 @@ inc_getpage_wait(uint64 latency_us)
 typedef struct
 {
 	char	   *name;
+	bool		is_bucket;
+	double		bucket_le;
 	double		value;
 } metric_t;
 
@@ -89,11 +93,14 @@ neon_perf_counters_to_metrics(neon_per_backend_counters *counters)
 	metric_t   *metrics = palloc((NUM_METRICS + 1) * sizeof(metric_t));
 	uint64		bucket_accum;
 	int			i = 0;
+	Datum		getpage_wait_str;
 
 	metrics[i].name = "getpage_wait_seconds_count";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->getpage_wait_us_count;
 	i++;
 	metrics[i].name = "getpage_wait_seconds_sum";
+	metrics[i].is_bucket = false;
 	metrics[i].value = ((double) counters->getpage_wait_us_sum) / 1000000.0;
 	i++;
 
@@ -102,47 +109,44 @@ neon_perf_counters_to_metrics(neon_per_backend_counters *counters)
 	{
 		uint64		threshold = getpage_wait_bucket_thresholds[bucketno];
 
-		if (threshold == UINT64_MAX)
-			metrics[i].name = "getpage_wait_seconds_bucket{le=\"+Inf\"}";
-		else if (threshold >= 1000000)
-		{
-			Assert(threshold % 1000000 == 0);
-			metrics[i].name = psprintf("getpage_wait_seconds_bucket{le=\"%u\"}", threshold / 1000000);
-		}
-		else if (threshold >= 1000)
-		{
-			Assert(threshold % 1000 == 0);
-			metrics[i].name = psprintf("getpage_wait_seconds_bucket{le=\"0.%03u\"}", threshold / 1000);
-		}
-		else
-			metrics[i].name = psprintf("getpage_wait_seconds_bucket{le=\"0.%06u\"}", threshold);
-
 		bucket_accum += counters->getpage_wait_us_bucket[bucketno];
+
+		metrics[i].name = "getpage_wait_seconds_bucket";
+		metrics[i].is_bucket = true;
+		metrics[i].bucket_le = (threshold == UINT64_MAX) ? INFINITY : ((double) threshold) / 1000000.0;
 		metrics[i].value = (double) bucket_accum;
 		i++;
 	}
 	metrics[i].name = "prefetch_requests_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->prefetch_requests_total;
 	i++;
 	metrics[i].name = "sync_requests_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->sync_requests_total;
 	i++;
 	metrics[i].name = "pageserver_requests_sent_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->pageserver_requests_sent_total;
 	i++;
 	metrics[i].name = "pageserver_requests_disconnects_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->pageserver_disconnects_total;
 	i++;
 	metrics[i].name = "pageserver_send_flushes_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->pageserver_send_flushes_total;
 	i++;
 	metrics[i].name = "prefetch_misses_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->prefetch_misses_total;
 	i++;
 	metrics[i].name = "prefetch_discards_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->prefetch_discards_total;
 	i++;
 	metrics[i].name = "file_cache_hits_total";
+	metrics[i].is_bucket = false;
 	metrics[i].value = (double) counters->file_cache_hits_total;
 	i++;
 
@@ -155,19 +159,39 @@ neon_perf_counters_to_metrics(neon_per_backend_counters *counters)
 	return metrics;
 }
 
+/*
+ * Write metric to three output Datums
+ */
+static void
+metric_to_datums(metric_t *m, Datum *values, bool *nulls)
+{
+	values[0] = CStringGetTextDatum(m->name);
+	nulls[0] = false;
+	if (m->is_bucket)
+	{
+		values[1] = Float8GetDatum(m->bucket_le);
+		nulls[1] = false;
+	}
+	else
+	{
+		values[1] = (Datum) 0;
+		nulls[1] = true;
+	}
+	values[2] = Float8GetDatum(m->value);
+	nulls[2] = false;
+}
+
 PG_FUNCTION_INFO_V1(neon_get_backend_perf_counters);
 Datum
 neon_get_backend_perf_counters(PG_FUNCTION_ARGS)
 {
 	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
-	Datum		values[4];
-	bool		nulls[4];
-	Datum		getpage_wait_str;
+	Datum		values[5];
+	bool		nulls[5];
 
 	/* We put all the tuples into a tuplestore in one go. */
 	InitMaterializedSRF(fcinfo, 0);
 
-	getpage_wait_str = CStringGetTextDatum("getpage_wait_us_bucket");
 	for (int procno = 0; procno < MaxBackends; procno++)
 	{
 		PGPROC	   *proc = GetPGProcByNumber(procno);
@@ -182,10 +206,7 @@ neon_get_backend_perf_counters(PG_FUNCTION_ARGS)
 
 		for (int i = 0; metrics[i].name != NULL; i++)
 		{
-			values[2] = CStringGetTextDatum(metrics[i].name);
-			nulls[2] = false;
-			values[3] = Float8GetDatum(metrics[i].value);
-			nulls[3] = false;
+			metric_to_datums(&metrics[i], &values[2], &nulls[2]);
 			tuplestore_putvalues(rsinfo->setResult, rsinfo->setDesc, values, nulls);
 		}
 
@@ -200,8 +221,8 @@ Datum
 neon_get_perf_counters(PG_FUNCTION_ARGS)
 {
 	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
-	Datum		values[2];
-	bool		nulls[2];
+	Datum		values[3];
+	bool		nulls[3];
 	Datum		getpage_wait_str;
 	neon_per_backend_counters totals = { 0 };
 	metric_t   *metrics;
@@ -210,7 +231,6 @@ neon_get_perf_counters(PG_FUNCTION_ARGS)
 	InitMaterializedSRF(fcinfo, 0);
 
 	/* Aggregate the counters across all backends */
-	getpage_wait_str = CStringGetTextDatum("getpage_wait_us_bucket");
 	for (int procno = 0; procno < MaxBackends; procno++)
 	{
 		neon_per_backend_counters *counters = &neon_per_backend_counters_shared[procno];
@@ -232,10 +252,7 @@ neon_get_perf_counters(PG_FUNCTION_ARGS)
 	metrics = neon_perf_counters_to_metrics(&totals);
 	for (int i = 0; metrics[i].name != NULL; i++)
 	{
-		values[0] = CStringGetTextDatum(metrics[i].name);
-		nulls[0] = false;
-		values[1] = Float8GetDatum(metrics[i].value);
-		nulls[1] = false;
+		metric_to_datums(&metrics[i], &values[0], &nulls[0]);
 		tuplestore_putvalues(rsinfo->setResult, rsinfo->setDesc, values, nulls);
 	}
 	pfree(metrics);
