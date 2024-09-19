9e47dbb24f25fa0be29ed2f43511a9e164eef047
Vlad Lazar <vlalazar.vlad@gmail.com>
2024-09-19 12:40:43 +0100

    fixup: tweak detached condition to handle onboarding via secondary only

--- storage_controller/src/reconciler.rs
+++ storage_controller/src/reconciler.rs
@@ -820,13 +820,10 @@ impl Reconciler {
             self.location_config(&node, conf, None, false).await?;
         }
 
-
-        // A None attached intent indicates that we are detaching from the primary location.
-        // Pass this information to the [`ComputeHook`] such that it can update its tenant-wide
-        // state.
-        if self.intent.attached.is_none() {
-            assert!(!self.detach.is_empty());
-
+        // The condition below identifies a detach. We must have no attached intent and
+        // must have been attached to something previously. Pass this information to
+        // the [`ComputeHook`] such that it can update its tenant-wide state.
+        if self.intent.attached.is_none() && !self.detach.is_empty() {
             // TODO: Consider notifying control plane about detaches. This would avoid situations
             // where the compute tries to start-up with a stale set of pageservers.
             self.compute_hook
