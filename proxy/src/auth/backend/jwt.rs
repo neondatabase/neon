e1f9b21091fb553f9b7e089d7a8b65f453bf6a6c
Conrad Ludgate <conradludgate@gmail.com>
2024-09-30 11:16:05 +0100

    add comment

--- proxy/src/auth/backend/jwt.rs
+++ proxy/src/auth/backend/jwt.rs
@@ -446,6 +446,21 @@ struct JwtPayload<'a> {
     session_id: Option<&'a str>,
 }
 
+/// `OneOrMany` supports parsing either a single item or an array of items.
+///
+/// Needed for <https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.3>
+///
+/// > The "aud" (audience) claim identifies the recipients that the JWT is
+/// > intended for.  Each principal intended to process the JWT MUST
+/// > identify itself with a value in the audience claim.  If the principal
+/// > processing the claim does not identify itself with a value in the
+/// > "aud" claim when this claim is present, then the JWT MUST be
+/// > rejected.  In the general case, the "aud" value is **an array of case-
+/// > sensitive strings**, each containing a StringOrURI value.  In the
+/// > special case when the JWT has one audience, the "aud" value MAY be a
+/// > **single case-sensitive string** containing a StringOrURI value.  The
+/// > interpretation of audience values is generally application specific.
+/// > Use of this claim is OPTIONAL.
 #[derive(Default, Debug)]
 struct OneOrMany(Vec<String>);
 
