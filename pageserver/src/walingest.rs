dc378cbb752b8015419046adc53a816d270db7ad
Konstantin Knizhnik <knizhnik@neon.tech>
2024-10-02 15:03:39 +0300

    Drop pg_stat data in case of abnormal Postgres termination

--- pageserver/src/walingest.rs
+++ pageserver/src/walingest.rs
@@ -1199,6 +1199,7 @@ impl WalIngest {
                 } else {
                     cp.oldestActiveXid = xlog_checkpoint.oldestActiveXid;
                 }
+                cp.redo = xlog_checkpoint.redo;
 
                 // Write a new checkpoint key-value pair on every checkpoint record, even
                 // if nothing really changed. Not strictly required, but it seems nice to
