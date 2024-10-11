e9557cfdd02fd4a5b11b224af5d5c1c7ac82143d
Alexey Masterov <alexeymasterov@neon.tech>
2024-10-11 14:00:14 +0200

    Change the combinations

--- test_runner/fixtures/utils.py
+++ test_runner/fixtures/utils.py
@@ -48,11 +48,10 @@ COMPONENT_BINARIES = {
 # fmt: off
 VERSIONS_COMBINATIONS = (
     {"storage_controller": "new", "storage_broker": "new", "compute": "new", "safekeeper": "new", "pageserver": "new"},
-    {"storage_controller": "old", "storage_broker": "old", "compute": "old", "safekeeper": "old", "pageserver": "new"},
-    {"storage_controller": "old", "storage_broker": "new", "compute": "old", "safekeeper": "new", "pageserver": "old"},
-    {"storage_controller": "new", "storage_broker": "old", "compute": "new", "safekeeper": "old", "pageserver": "old"},
-    {"storage_controller": "new", "storage_broker": "old", "compute": "old", "safekeeper": "new", "pageserver": "old"},
-    {"storage_controller": "old", "storage_broker": "new", "compute": "new", "safekeeper": "old", "pageserver": "old"},
+    {"storage_controller": "new", "storage_broker": "new", "compute": "old", "safekeeper": "old", "pageserver": "old"},
+    {"storage_controller": "new", "storage_broker": "new", "compute": "old", "safekeeper": "old", "pageserver": "new"},
+    {"storage_controller": "new", "storage_broker": "new", "compute": "old", "safekeeper": "new", "pageserver": "new"},
+    {"storage_controller": "old", "storage_broker": "old", "compute": "new", "safekeeper": "new", "pageserver": "new"},
 )
 # fmt: on
 
