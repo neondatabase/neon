3115e7f4818509aebcc1b43ec77d823af5ddfb14
Alexey Masterov <alexeymasterov@neon.tech>
2024-10-15 15:24:25 +0200

    Change the size

--- test_runner/fixtures/neon_fixtures.py
+++ test_runner/fixtures/neon_fixtures.py
@@ -3470,8 +3470,8 @@ class Endpoint(PgProtocol, LogUtils):
         config_lines = [
             "shared_buffers='512kB'",
             f"neon.file_cache_path='{self.lfc_path()}'",
-            "neon.max_file_cache_size='512kB'",
-            "neon.file_cache_size_limit='512kB'",
+            "neon.max_file_cache_size='32MB'",
+            "neon.file_cache_size_limit='32MB'",
         ] + config_lines
 
         self.config(config_lines)
