9d788c0d44d1c87a34926768485d1d9bd52d7870
Konstantin Knizhnik <knizhnik@neon.tech>
2024-10-24 22:15:35 +0300

    Report prewarm progress

--- pgxn/neon/neon--1.6--1.5.sql
+++ pgxn/neon/neon--1.6--1.5.sql
@@ -1,1 +1,3 @@
 DROP FUNCTION IF EXISTS save_local_cache_state();
+
+DROP FUNCTION IF EXISTS get_prewarm_info(out total_chunks integer, out curr_chunk integer, out prewarmed_pages integer, out skipped_pages integer);
