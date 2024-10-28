80466bdca24ff9de4e90299b0b9635fee8a9ab01
Conrad Ludgate <conradludgate@gmail.com>
2024-10-28 18:26:48 +0000

    remove postgres auth backend from proxy tests

--- test_runner/regress/test_proxy_allowed_ips.py
+++ test_runner/regress/test_proxy_allowed_ips.py
@@ -6,21 +6,50 @@ from fixtures.neon_fixtures import (
     NeonProxy,
     VanillaPostgres,
 )
+from pytest_httpserver import HTTPServer
 
 TABLE_NAME = "neon_control_plane.endpoints"
 
 
 # Proxy uses the same logic for psql and websockets.
 @pytest.mark.asyncio
-async def test_proxy_psql_allowed_ips(static_proxy: NeonProxy, vanilla_pg: VanillaPostgres):
+async def test_proxy_psql_allowed_ips(
+    static_proxy: NeonProxy,
+    vanilla_pg: VanillaPostgres,
+    httpserver: HTTPServer,
+):
+    [(rolpassword,)] = vanilla_pg.safe_psql(
+        "select rolpassword from pg_catalog.pg_authid where rolname = 'proxy'"
+    )
+
     # Shouldn't be able to connect to this project
-    vanilla_pg.safe_psql(
-        f"INSERT INTO {TABLE_NAME} (endpoint_id, allowed_ips) VALUES ('private-project', '8.8.8.8')"
+    httpserver.expect_request(
+        "/cplane/proxy_get_role_secret", query_string="project=private-project"
+    ).respond_with_json(
+        {
+            "role_secret": rolpassword,
+            "allowed_ips": ["8.8.8.8"],
+            "project_id": "foo-bar",
+        }
     )
+
     # Should be able to connect to this project
-    vanilla_pg.safe_psql(
-        f"INSERT INTO {TABLE_NAME} (endpoint_id, allowed_ips) VALUES ('generic-project', '::1,127.0.0.1')"
+    httpserver.expect_request(
+        "/cplane/proxy_get_role_secret", query_string="project=generic-project"
+    ).respond_with_json(
+        {
+            "role_secret": rolpassword,
+            "allowed_ips": ["::1", "127.0.0.1"],
+            "project_id": "foo-bar",
+        }
     )
+
+    # vanilla_pg.safe_psql(
+    #     f"INSERT INTO {TABLE_NAME} (endpoint_id, allowed_ips) VALUES ('private-project', '8.8.8.8')"
+    # )
+    # vanilla_pg.safe_psql(
+    #     f"INSERT INTO {TABLE_NAME} (endpoint_id, allowed_ips) VALUES ('generic-project', '::1,127.0.0.1')"
+    # )
 
     def check_cannot_connect(**kwargs):
         with pytest.raises(psycopg2.Error) as exprinfo:
@@ -51,12 +80,20 @@ async def test_proxy_psql_allowed_ips(static_proxy: NeonProxy, vanilla_pg: Vanil
 
 
 @pytest.mark.asyncio
-async def test_proxy_http_allowed_ips(static_proxy: NeonProxy, vanilla_pg: VanillaPostgres):
-    static_proxy.safe_psql("create user http_auth with password 'http' superuser")
+async def test_proxy_http_allowed_ips(
+    static_proxy: NeonProxy,
+    vanilla_pg: VanillaPostgres,
+    httpserver: HTTPServer,
+):
+    vanilla_pg.safe_psql("create user http_auth with password 'http' superuser")
 
-    # Shouldn't be able to connect to this project
-    vanilla_pg.safe_psql(
-        f"INSERT INTO {TABLE_NAME} (endpoint_id, allowed_ips) VALUES ('proxy', '8.8.8.8')"
+    # # Shouldn't be able to connect to this project
+    # vanilla_pg.safe_psql(
+    #     f"INSERT INTO {TABLE_NAME} (endpoint_id, allowed_ips) VALUES ('proxy', '8.8.8.8')"
+    # )
+
+    [(rolpassword,)] = vanilla_pg.safe_psql(
+        "select rolpassword from pg_catalog.pg_authid where rolname = 'proxy'"
     )
 
     def query(status: int, query: str, *args):
@@ -68,9 +105,32 @@ async def test_proxy_http_allowed_ips(static_proxy: NeonProxy, vanilla_pg: Vanil
             expected_code=status,
         )
 
-    query(400, "select 1;")  # ip address is not allowed
-    # Should be able to connect to this project
-    vanilla_pg.safe_psql(
-        f"UPDATE {TABLE_NAME} SET allowed_ips = '8.8.8.8,127.0.0.1' WHERE endpoint_id = 'proxy'"
+    # Shouldn't be able to connect to this project
+    httpserver.expect_oneshot_request(
+        "/cplane/proxy_get_role_secret", query_string="project=proxy"
+    ).respond_with_json(
+        {
+            "role_secret": rolpassword,
+            "allowed_ips": ["8.8.8.8"],
+            "project_id": "foo-bar",
+        }
     )
-    query(200, "select 1;")  # should work now
+
+    with httpserver.wait() as waiting:
+        query(400, "select 1;")  # ip address is not allowed
+    assert waiting.result
+
+    # Should be able to connect to this project
+    httpserver.expect_oneshot_request(
+        "/cplane/proxy_get_role_secret", query_string="project=proxy"
+    ).respond_with_json(
+        {
+            "role_secret": rolpassword,
+            "allowed_ips": ["8.8.8.8", "127.0.0.1"],
+            "project_id": "foo-bar",
+        }
+    )
+
+    with httpserver.wait() as waiting:
+        query(400, "select 1;")  # ip address is not allowed
+    assert waiting.result
