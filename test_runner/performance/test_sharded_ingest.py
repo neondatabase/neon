b177eac03ea7c97ade7dd80bbf3b57d060837e0b
Erik Grinaker <erik@neon.tech>
2024-11-01 15:41:45 +0100

    Ensure storage controller doesn't move shards

--- test_runner/performance/test_sharded_ingest.py
+++ test_runner/performance/test_sharded_ingest.py
@@ -8,6 +8,7 @@ from fixtures.common_types import Lsn, TenantShardId
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import (
     NeonEnvBuilder,
+    tenant_get_shards,
     wait_for_last_flush_lsn,
 )
 
@@ -30,13 +31,21 @@ def test_sharded_ingest(
     neon_env_builder.num_pageservers = shard_count
     env = neon_env_builder.init_start()
 
-    # Create a sharded tenant and timeline, and migrate it to the respective pageservers.
+    # Create a sharded tenant and timeline, and migrate it to the respective pageservers. Ensure
+    # the storage controller doesn't mess with shard placements.
+    #
+    # TODO: there should be a way to disable storage controller background reconciliations.
+    # Currently, disabling reconciliation also disables foreground operations.
     tenant_id, timeline_id = env.create_tenant(shard_count=shard_count)
 
     for shard_number in range(0, shard_count):
         tenant_shard_id = TenantShardId(tenant_id, shard_number, shard_count)
         pageserver_id = shard_number + 1
         env.storage_controller.tenant_shard_migrate(tenant_shard_id, pageserver_id)
+
+    shards = tenant_get_shards(env, tenant_id)
+    env.storage_controller.reconcile_until_idle()
+    assert tenant_get_shards(env, tenant_id) == shards, "shards moved"
 
     # Start the endpoint.
     endpoint = env.endpoints.create_start("main", tenant_id=tenant_id)
@@ -58,3 +67,5 @@ def test_sharded_ingest(
     end_lsn = Lsn(endpoint.safe_psql("select pg_current_wal_lsn()")[0][0])
     wal_written_mb = round((end_lsn - start_lsn) / (1024 * 1024))
     zenbenchmark.record("wal_written", wal_written_mb, "MB", MetricReport.TEST_PARAM)
+
+    assert tenant_get_shards(env, tenant_id) == shards, "shards moved"
