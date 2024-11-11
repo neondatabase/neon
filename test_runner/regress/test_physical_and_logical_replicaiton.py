e252a4208ba1736d7894fbe5ab6d08c0834feee6
Konstantin Knizhnik <knizhnik@neon.tech>
2024-11-11 21:46:21 +0200

    Bump postgres version

--- test_runner/regress/test_physical_and_logical_replicaiton.py
+++ test_runner/regress/test_physical_and_logical_replicaiton.py
@@ -5,20 +5,11 @@ import time
 from fixtures.neon_fixtures import NeonEnv, logical_replication_sync
 
 
-def test_physical_and_logical_replication(neon_simple_env: NeonEnv, vanilla_pg):
+def test_physical_and_logical_replication_slot_not_copied(neon_simple_env: NeonEnv, vanilla_pg):
     """Test read replica of a primary which has a logical replication publication"""
     env = neon_simple_env
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     n_records = 100000
-=======
-    n_records = 1000000
->>>>>>> 7fbb1d150 (Add test for combination of physical and logical replication)
-=======
-    n_records = 200000
->>>>>>> 8fefd8cd6 (Update test_physical_and_logical_replicaiton.py  to reproduce the problem)
-
     primary = env.endpoints.create_start(
         branch_name="main",
         endpoint_id="primary",
@@ -66,3 +57,51 @@ def test_physical_and_logical_replication(neon_simple_env: NeonEnv, vanilla_pg):
     # Check that LR slot is not copied to replica
     s_cur.execute("select count(*) from pg_replication_slots")
     assert s_cur.fetchall()[0][0] == 0
+
+
+def test_aux_not_logged_at_replica(neon_simple_env: NeonEnv, vanilla_pg):
+    """Test that AUX files are not saved at replica"""
+    env = neon_simple_env
+
+    n_records = 200000
+
+    primary = env.endpoints.create_start(
+        branch_name="main",
+        endpoint_id="primary",
+    )
+    p_con = primary.connect()
+    p_cur = p_con.cursor()
+    p_cur.execute("CREATE TABLE t(pk bigint primary key, payload text default repeat('?',200))")
+    p_cur.execute("create publication pub1 for table t")
+
+    # start subscriber
+    vanilla_pg.start()
+    vanilla_pg.safe_psql("CREATE TABLE t(pk bigint primary key, payload text)")
+    connstr = primary.connstr().replace("'", "''")
+    vanilla_pg.safe_psql(f"create subscription sub1 connection '{connstr}' publication pub1")
+
+    for pk in range(n_records):
+        p_cur.execute("insert into t (pk) values (%s)", (pk,))
+
+    # LR snapshot is stored each 15 seconds
+    time.sleep(16)
+
+    # start replica
+    secondary = env.endpoints.new_replica_start(
+        origin=primary,
+        endpoint_id="secondary",
+    )
+
+    s_con = secondary.connect()
+    s_cur = s_con.cursor()
+
+    logical_replication_sync(vanilla_pg, primary)
+
+    assert vanilla_pg.safe_psql("select count(*) from t")[0][0] == n_records
+    s_cur.execute("select count(*) from t")
+    assert s_cur.fetchall()[0][0] == n_records
+
+    primary.stop()
+    time.sleep(1)
+    secondary.stop()
+    assert not secondary.log_contains("cannot make new WAL entries during recovery")
