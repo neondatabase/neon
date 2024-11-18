261d065e6fd491046e47d819c39a1fb4993c8219
John Spray <john@neon.tech>
2024-11-18 08:59:05 +0000

    pageserver: respect no_sync in `VirtualFile` (#9772)
    ## Problem
    
    `no_sync` initially just skipped syncfs on startup (#9677). I'm also
    interested in flaky tests that time out during pageserver shutdown while
    flushing l0s, so to eliminate disk throughput as a source of issues
    there,
    
    ## Summary of changes
    
    - Drive-by change for test timeouts: add a couple more ::info logs
    during pageserver startup so it's obvious which part got stuck.
    - Add a SyncMode enum to configure VirtualFile and respect it in
    sync_all and sync_data functions
    - During pageserver startup, set SyncMode according to `no_sync`

--- pageserver/ctl/src/main.rs
+++ pageserver/ctl/src/main.rs
@@ -209,6 +209,7 @@ async fn print_layerfile(path: &Utf8Path) -> anyhow::Result<()> {
         10,
         virtual_file::api::IoEngineKind::StdFs,
         IoMode::preferred(),
+        virtual_file::SyncMode::Sync,
     );
     page_cache::init(100);
     let ctx = RequestContext::new(TaskKind::DebugTool, DownloadBehavior::Error);
