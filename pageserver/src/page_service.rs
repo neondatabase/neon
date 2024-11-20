c73e9e40e96170186776f33ed8aa45814254c98e
Christian Schwarz <christian@neon.tech>
2024-11-20 18:30:48 +0100

    try async-timer 1.0.0-beta15 (still signal-based timers)
    Results unchanged to 0.7.4
    
    https://www.notion.so/neondatabase/benchmarking-notes-143f189e004780c4a630cb5f426e39ba?pvs=4#144f189e004780e18416cc0faf2aca65

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -3,7 +3,7 @@
 
 use anyhow::{bail, Context};
 use async_compression::tokio::write::GzipEncoder;
-use async_timer::Oneshot;
+use async_timer::Timer;
 use bytes::Buf;
 use futures::FutureExt;
 use itertools::Itertools;
@@ -319,7 +319,7 @@ struct PageServerHandler {
     /// See [`PageServerConf::server_side_batch_timeout`]
     server_side_batch_timeout: Option<Duration>,
 
-    server_side_batch_timer: Pin<Box<async_timer::oneshot::Timer>>,
+    server_side_batch_timer: Pin<Box<async_timer::timer::Platform>>,
 }
 
 struct Carry {
@@ -589,9 +589,7 @@ impl PageServerHandler {
             timeline_handles: TimelineHandles::new(tenant_manager),
             cancel,
             server_side_batch_timeout,
-            server_side_batch_timer: Box::pin(async_timer::oneshot::Timer::new(
-                Duration::from_secs(999),
-            )), // reset each iteration
+            server_side_batch_timer: Box::pin(async_timer::new_timer(Duration::from_secs(999))), // reset each iteration
         }
     }
 
@@ -668,12 +666,7 @@ impl PageServerHandler {
                                         .msg
                                 ]));
                             } else {
-                                std::future::poll_fn(|ctx| {
-                                    self.server_side_batch_timer
-                                        .restart(batch_timeout, ctx.waker());
-                                    std::task::Poll::Ready(())
-                                })
-                                .await;
+                                self.server_side_batch_timer.restart(batch_timeout);
                                 batching_deadline_storage = Some(&mut self.server_side_batch_timer);
                                 Either::Right(
                                     batching_deadline_storage.as_mut().expect("we just set it"),
