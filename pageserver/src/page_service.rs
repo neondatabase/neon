1639b260027f2dc570c3501cb26e1bf215051407
Christian Schwarz <christian@neon.tech>
2024-11-20 14:36:46 +0100

    async-timer based approach
    With this, 10us batching timeout works, but it has some other wrinkles:
    
    - it uses the signal-based timer APIs instead of going through epoll (=> timerfd)
    = it needs to make a syscall for each batch, which costs around 1-2us, so, probably significant CPU time wasted on this.

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -3,6 +3,7 @@
 
 use anyhow::{bail, Context};
 use async_compression::tokio::write::GzipEncoder;
+use async_timer::Oneshot;
 use bytes::Buf;
 use futures::FutureExt;
 use itertools::Itertools;
@@ -22,6 +23,7 @@ use pq_proto::FeStartupPacket;
 use pq_proto::{BeMessage, FeMessage, RowDescriptor};
 use std::borrow::Cow;
 use std::io;
+use std::pin::Pin;
 use std::str;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -316,6 +318,8 @@ struct PageServerHandler {
 
     /// See [`PageServerConf::server_side_batch_timeout`]
     server_side_batch_timeout: Option<Duration>,
+
+    server_side_batch_timer: Pin<Box<async_timer::oneshot::Timer>>,
 }
 
 struct Carry {
@@ -585,6 +589,9 @@ impl PageServerHandler {
             timeline_handles: TimelineHandles::new(tenant_manager),
             cancel,
             server_side_batch_timeout,
+            server_side_batch_timer: Box::pin(async_timer::oneshot::Timer::new(
+                Duration::from_secs(999),
+            )), // reset each iteration
         }
     }
 
@@ -661,9 +668,13 @@ impl PageServerHandler {
                                         .msg
                                 ]));
                             } else {
-                                batching_deadline_storage = Some(Box::pin(async move {
-                                    tokio::time::sleep(batch_timeout).await;
-                                }));
+                                std::future::poll_fn(|ctx| {
+                                    self.server_side_batch_timer
+                                        .restart(batch_timeout, ctx.waker());
+                                    std::task::Poll::Ready(())
+                                })
+                                .await;
+                                batching_deadline_storage = Some(&mut self.server_side_batch_timer);
                                 Either::Right(
                                     batching_deadline_storage.as_mut().expect("we just set it"),
                                 )
