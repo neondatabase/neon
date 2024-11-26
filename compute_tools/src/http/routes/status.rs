817e1d938de2bcd340aa857951675c91368d5830
Tristan Partin <tristan@neon.tech>
2024-11-26 10:17:48 -0600

    Implement compute_ctl management API in Axum
    This is a refactor to create better abstractions related to our
    management server. It cleans up the code, and prepares everything for
    authorized communication to and from the control plane.
    
    Signed-off-by: Tristan Partin <tristan@neon.tech>

---
+++ compute_tools/src/http/routes/status.rs
@@ -0,0 +1,13 @@
+use std::{ops::Deref, sync::Arc};
+
+use axum::{extract::State, http::StatusCode, response::Response};
+use compute_api::responses::ComputeStatusResponse;
+
+use crate::{compute::ComputeNode, http::JsonResponse};
+
+pub(in crate::http) async fn get_status(State(compute): State<Arc<ComputeNode>>) -> Response {
+    let state = compute.state.lock().unwrap();
+    let body = ComputeStatusResponse::from(state.deref());
+
+    JsonResponse::success(StatusCode::OK, body)
+}
