ff914f323a3a3906537d4b81c216e5d7932f765c
Alexey Kondratov <kondratov.aleksey@gmail.com>
2024-11-28 15:46:35 +0100

    Add python test

--- test_runner/regress/test_compute_catalog.py
+++ test_runner/regress/test_compute_catalog.py
@@ -33,3 +33,63 @@ def test_compute_catalog(neon_simple_env: NeonEnv):
         assert (
             e.response.status_code == 404
         ), f"Expected 404 status code, but got {e.response.status_code}"
+
+
+def test_compute_create_databases(neon_simple_env: NeonEnv):
+    """
+    Test that compute_ctl can create and work with databases with special
+    characters (whitespaces, %, tabs) in the name.
+    """
+    env = neon_simple_env
+
+    # Create and start endpoint so that neon_local put all the generated
+    # stuff into the spec.json file.
+    endpoint = env.endpoints.create_start("main")
+
+    databases = [
+        {
+            "name": "neondb",
+            "owner": "cloud_admin",
+        },
+        {
+            "name": "db with spaces",
+            "owner": "cloud_admin",
+        },
+        {
+            "name": "db with%20spaces ",
+            "owner": "cloud_admin",
+        },
+        {
+            "name": "db with whitespaces	",
+            "owner": "cloud_admin",
+        },
+    ]
+
+    # Update the spec.json file to include new databases
+    # and reconfigure the endpoint to apply the changes.
+    endpoint.respec_deep(
+        **{
+            "skip_pg_catalog_updates": False,
+            "cluster": {
+                "databases": databases,
+            },
+        }
+    )
+    endpoint.reconfigure()
+
+    for db in databases:
+        # Check that database has a correct name in the system catalog
+        catalog_db = endpoint.safe_psql(
+            f"SELECT datname FROM pg_database WHERE datname = '{db['name']}'"
+        )
+        assert catalog_db is not None
+        assert len(catalog_db) == 1
+        assert catalog_db[0][0] == db["name"]
+
+        # Check that we can connect to this database without any issues
+        with endpoint.cursor(dbname=db["name"]) as cursor:
+            cursor.execute("SELECT * FROM current_database()")
+            curr_db = cursor.fetchone()
+            assert curr_db is not None
+            assert len(curr_db) == 1
+            assert curr_db[0] == db["name"]
