42fb3c4d30bf93ad0ad85bbd636a4262d205f673
Alexey Kondratov <kondratov.aleksey@gmail.com>
2024-11-28 22:38:30 +0100

    fix(compute_ctl): Allow usage of DB names with whitespaces (#9919)
    ## Problem
    
    We used `set_path()` to replace the database name in the connection
    string. It automatically does url-safe encoding if the path is not
    already encoded, but it does it as per the URL standard, which assumes
    that tabs can be safely removed from the path without changing the
    meaning of the URL. See, e.g.,
    https://url.spec.whatwg.org/#concept-basic-url-parser. It also breaks
    for DBs with properly %-encoded names, like with `%20`, as they are kept
    intact, but actually should be escaped.
    
    Yet, this is not true for Postgres, where it's completely valid to have
    trailing tabs in the database name.
    
    I think this is the PR that caused this regression
    https://github.com/neondatabase/neon/pull/9717, as it switched from
    `postgres::config::Config` back to `set_path()`.
    
    This was fixed a while ago already [1], btw, I just haven't added a test
    to catch this regression back then :(
    
    ## Summary of changes
    
    This commit changes the code back to use
    `postgres/tokio_postgres::Config` everywhere.
    
    While on it, also do some changes around, as I had to touch this code:
    1. Bump some logging from `debug` to `info` in the spec apply path. We
    do not use `debug` in prod, and it was tricky to understand what was
    going on with this bug in prod.
    2. Refactor configuration concurrency calculation code so it was
    reusable. Yet, still keep `1` in the case of reconfiguration. The
    database can be actively used at this moment, so we cannot guarantee
    that there will be enough spare connection slots, and the underlying
    code won't handle connection errors properly.
    3. Simplify the installed extensions code. It was spawning a blocking
    task inside async function, which doesn't make much sense. Instead, just
    have a main sync function and call it with `spawn_blocking` in the API
    code -- the only place we need it to be async.
    4. Add regression python test to cover this and related problems in the
    future. Also, add more extensive testing of schema dump and DBs and
    roles listing API.
    
    [1]:
    https://github.com/neondatabase/neon/commit/4d1e48f3b9a4b7064787513fd2c455f0001f6e18
    [2]:
    https://www.postgresql.org/message-id/flat/20151023003445.931.91267%40wrigleys.postgresql.org
    
    Resolves neondatabase/cloud#20869

--- compute_tools/src/installed_extensions.rs
+++ compute_tools/src/installed_extensions.rs
@@ -2,16 +2,15 @@ use compute_api::responses::{InstalledExtension, InstalledExtensions};
 use metrics::proto::MetricFamily;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use tracing::info;
-use url::Url;
 
 use anyhow::Result;
 use postgres::{Client, NoTls};
-use tokio::task;
 
 use metrics::core::Collector;
 use metrics::{register_uint_gauge_vec, UIntGaugeVec};
 use once_cell::sync::Lazy;
+
+use crate::pg_helpers::postgres_conf_for_db;
 
 /// We don't reuse get_existing_dbs() just for code clarity
 /// and to make database listing query here more explicit.
@@ -42,75 +41,51 @@ fn list_dbs(client: &mut Client) -> Result<Vec<String>> {
 ///
 /// Same extension can be installed in multiple databases with different versions,
 /// we only keep the highest and lowest version across all databases.
-pub async fn get_installed_extensions(connstr: Url) -> Result<InstalledExtensions> {
-    let mut connstr = connstr.clone();
+pub fn get_installed_extensions(connstr: &url::Url) -> Result<InstalledExtensions> {
+    let mut client = Client::connect(connstr.as_str(), NoTls)?;
+    let databases: Vec<String> = list_dbs(&mut client)?;
 
-    task::spawn_blocking(move || {
-        let mut client = Client::connect(connstr.as_str(), NoTls)?;
-        let databases: Vec<String> = list_dbs(&mut client)?;
+    let mut extensions_map: HashMap<String, InstalledExtension> = HashMap::new();
+    for db in databases.iter() {
+        let config = postgres_conf_for_db(connstr, db)?;
+        let mut db_client = config.connect(NoTls)?;
+        let extensions: Vec<(String, String)> = db_client
+            .query(
+                "SELECT extname, extversion FROM pg_catalog.pg_extension;",
+                &[],
+            )?
+            .iter()
+            .map(|row| (row.get("extname"), row.get("extversion")))
+            .collect();
 
-        let mut extensions_map: HashMap<String, InstalledExtension> = HashMap::new();
-        for db in databases.iter() {
-            connstr.set_path(db);
-            let mut db_client = Client::connect(connstr.as_str(), NoTls)?;
-            let extensions: Vec<(String, String)> = db_client
-                .query(
-                    "SELECT extname, extversion FROM pg_catalog.pg_extension;",
-                    &[],
-                )?
-                .iter()
-                .map(|row| (row.get("extname"), row.get("extversion")))
-                .collect();
+        for (extname, v) in extensions.iter() {
+            let version = v.to_string();
 
-            for (extname, v) in extensions.iter() {
-                let version = v.to_string();
+            // increment the number of databases where the version of extension is installed
+            INSTALLED_EXTENSIONS
+                .with_label_values(&[extname, &version])
+                .inc();
 
-                // increment the number of databases where the version of extension is installed
-                INSTALLED_EXTENSIONS
-                    .with_label_values(&[extname, &version])
-                    .inc();
-
-                extensions_map
-                    .entry(extname.to_string())
-                    .and_modify(|e| {
-                        e.versions.insert(version.clone());
-                        // count the number of databases where the extension is installed
-                        e.n_databases += 1;
-                    })
-                    .or_insert(InstalledExtension {
-                        extname: extname.to_string(),
-                        versions: HashSet::from([version.clone()]),
-                        n_databases: 1,
-                    });
-            }
+            extensions_map
+                .entry(extname.to_string())
+                .and_modify(|e| {
+                    e.versions.insert(version.clone());
+                    // count the number of databases where the extension is installed
+                    e.n_databases += 1;
+                })
+                .or_insert(InstalledExtension {
+                    extname: extname.to_string(),
+                    versions: HashSet::from([version.clone()]),
+                    n_databases: 1,
+                });
         }
+    }
 
-        let res = InstalledExtensions {
-            extensions: extensions_map.values().cloned().collect(),
-        };
+    let res = InstalledExtensions {
+        extensions: extensions_map.values().cloned().collect(),
+    };
 
-        Ok(res)
-    })
-    .await?
-}
-
-// Gather info about installed extensions
-pub fn get_installed_extensions_sync(connstr: Url) -> Result<()> {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .expect("failed to create runtime");
-    let result = rt
-        .block_on(crate::installed_extensions::get_installed_extensions(
-            connstr,
-        ))
-        .expect("failed to get installed extensions");
-
-    info!(
-        "[NEON_EXT_STAT] {}",
-        serde_json::to_string(&result).expect("failed to serialize extensions list")
-    );
-    Ok(())
+    Ok(res)
 }
 
 static INSTALLED_EXTENSIONS: Lazy<UIntGaugeVec> = Lazy::new(|| {
