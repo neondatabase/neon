1d642d6a57dd1cd1645a34aba5a2dd6e06a6c651
Conrad Ludgate <conrad@neon.tech>
2024-11-29 11:08:01 +0000

    chore(proxy): vendor a subset of rust-postgres (#9930)
    Our rust-postgres fork is getting messy. Mostly because proxy wants more
    control over the raw protocol than tokio-postgres provides. As such,
    it's diverging more and more. Storage and compute also make use of
    rust-postgres, but in more normal usage, thus they don't need our crazy
    changes.
    
    Idea: 
    * proxy maintains their subset
    * other teams use a minimal patch set against upstream rust-postgres
    
    Reviewing this code will be difficult. To implement it, I
    1. Copied tokio-postgres, postgres-protocol and postgres-types from
    https://github.com/neondatabase/rust-postgres/tree/00940fcdb57a8e99e805297b75839e7c4c7b1796
    2. Updated their package names with the `2` suffix to make them compile
    in the workspace.
    3. Updated proxy to use those packages
    4. Copied in the code from tokio-postgres-rustls 0.13 (with some patches
    applied https://github.com/jbg/tokio-postgres-rustls/pull/32
    https://github.com/jbg/tokio-postgres-rustls/pull/33)
    5. Removed as much dead code as I could find in the vendored libraries
    6. Updated the tokio-postgres-rustls code to use our existing channel
    binding implementation

---
+++ libs/proxy/postgres-protocol2/src/password/test.rs
@@ -0,0 +1,19 @@
+use crate::password;
+
+#[tokio::test]
+async fn test_encrypt_scram_sha_256() {
+    // Specify the salt to make the test deterministic. Any bytes will do.
+    let salt: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    assert_eq!(
+        password::scram_sha_256_salt(b"secret", salt).await,
+        "SCRAM-SHA-256$4096:AQIDBAUGBwgJCgsMDQ4PEA==$8rrDg00OqaiWXJ7p+sCgHEIaBSHY89ZJl3mfIsf32oY=:05L1f+yZbiN8O0AnO40Og85NNRhvzTS57naKRWCcsIA="
+    );
+}
+
+#[test]
+fn test_encrypt_md5() {
+    assert_eq!(
+        password::md5(b"secret", "foo"),
+        "md54ab2c5d00339c4b2a4e921d2dc4edec7"
+    );
+}
