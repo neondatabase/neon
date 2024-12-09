c2c11fd34ee08c9a4c41839f7576622988fc1f6c
Alexey Masterov <alexeymasterov@neon.tech>
2024-12-09 08:47:03 +0100

    Revert "Clean repeated testing."
    This reverts commit 6dc012721d29a5c7e0741df425c90d2f136e2422.

--- test_runner/regress/test_read_validation.py
+++ test_runner/regress/test_read_validation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from contextlib import closing
 
+import pytest
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import NeonEnv
 from fixtures.utils import USE_LFC, query_scalar
@@ -15,6 +16,8 @@ extensions = ["pageinspect", "neon_test_utils", "pg_buffercache"]
 #
 # Validation of reading different page versions
 #
+# TODO: remove the next line before merge
+@pytest.mark.repeat(100)
 def test_read_validation(neon_simple_env: NeonEnv):
     env = neon_simple_env
 
