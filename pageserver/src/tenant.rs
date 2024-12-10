6ad99826c1d175e12dea50120041bc0822830be6
Alex Chi Z. <4198311+skyzh@users.noreply.github.com>
2024-12-10 10:23:26 -0500

    fix(pageserver): refresh_gc_info should always increase cutoff (#9862)
    ## Problem
    
    close https://github.com/neondatabase/cloud/issues/19671
    
    ```
    Timeline -----------------------------
             ^ last GC happened LSN
                  ^ original retention period setting = 24hr
    > refresh-gc-info updates the gc_info
                  ^ planned cutoff (gc_info)
             ^ customer set retention to 48hr, and it's still within the last GC LSN
             ^1   ^2 we have two choices: (1) update the planned cutoff to
                     move backwards, or (2) keep the current one
    ```
    
    In this patch, we decided to keep the current cutoff instead of moving
    back the gc_info to avoid races. In the future, we could allow the
    planned gc cutoff to go back once cplane sends a retention_history
    tenant config update, but this requires a careful revisit of the code.
    
    ## Summary of changes
    
    Ensure that GC cutoffs never go back if retention settings get changed.
    
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -4506,7 +4506,12 @@ impl Tenant {
                 // - this timeline was created while we were finding cutoffs
                 // - lsn for timestamp search fails for this timeline repeatedly
                 if let Some(cutoffs) = gc_cutoffs.get(&timeline.timeline_id) {
-                    target.cutoffs = cutoffs.clone();
+                    let original_cutoffs = target.cutoffs.clone();
+                    // GC cutoffs should never go back
+                    target.cutoffs = GcCutoffs {
+                        space: Lsn(cutoffs.space.0.max(original_cutoffs.space.0)),
+                        time: Lsn(cutoffs.time.0.max(original_cutoffs.time.0)),
+                    }
                 }
             }
 
