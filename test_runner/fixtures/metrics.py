9ae980bf4f320c20dc48adce90e92e74fd2ea45c
Christian Schwarz <christian@neon.tech>
2024-12-11 14:37:08 +0100

    page_service: don't count time spent in Batcher towards smgr latency metrics (#10075)
    ## Problem
    
    With pipelining enabled, the time a request spends in the batcher stage
    counts towards the smgr op latency.
    
    If pipelining is disabled, that time is not accounted for.
    
    In practice, this results in a jump in smgr getpage latencies in various
    dashboards and degrades the internal SLO.
    
    ## Solution
    
    In a similar vein to #10042 and with a similar rationale, this PR stops
    counting the time spent in batcher stage towards smgr op latency.
    
    The smgr op latency metric is reduced to the actual execution time.
    
    Time spent in batcher stage is tracked in a separate histogram.
    I expect to remove that histogram after batching rollout is complete,
    but it will be helpful in the meantime to reason about the rollout.

--- test_runner/fixtures/metrics.py
+++ test_runner/fixtures/metrics.py
@@ -178,6 +178,7 @@ PAGESERVER_PER_TENANT_METRICS: tuple[str, ...] = (
     counter("pageserver_timeline_wal_records_received"),
     counter("pageserver_page_service_pagestream_flush_in_progress_micros"),
     *histogram("pageserver_page_service_batch_size"),
+    *histogram("pageserver_page_service_pagestream_batch_wait_time_seconds"),
     *PAGESERVER_PER_TENANT_REMOTE_TIMELINE_CLIENT_METRICS,
     # "pageserver_directory_entries_count", -- only used if above a certain threshold
     # "pageserver_broken_tenants_count" -- used only for broken
