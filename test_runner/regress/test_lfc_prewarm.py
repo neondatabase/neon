07027bde7d3f2c68b23272afc5f0207352822ffc
Konstantin Knizhnik <knizhnik@neon.tech>
2024-12-14 21:11:20 +0200

    Do not run test_lfc_prewarm test without LFC

--- test_runner/regress/test_lfc_prewarm.py
+++ test_runner/regress/test_lfc_prewarm.py
@@ -1,9 +1,11 @@
 import time
 
+import pytest
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import NeonEnv
 
 
+@pytest.mark.skipif(not USE_LFC, reason="LFC is disabled, skipping")
 def test_lfc_prewarm(neon_simple_env: NeonEnv):
     env = neon_simple_env
     n_records = 1000000
@@ -13,7 +15,6 @@ def test_lfc_prewarm(neon_simple_env: NeonEnv):
         config_lines=[
             "autovacuum = off",
             "shared_buffers=1MB",
-            "neon.file_cache_path='file.cache'",
             "neon.max_file_cache_size=1GB",
             "neon.file_cache_size_limit=1GB",
             "neon.file_cache_prewarm_limit=1000",
