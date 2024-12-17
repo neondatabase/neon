e1eb25bf2de59e652714f281f3b995074d4ff374
Konstantin Knizhnik <knizhnik@neon.tech>
2024-12-17 22:00:21 +0200

    Add test for too larger staistics file

--- test_runner/regress/test_pgstat.py
+++ test_runner/regress/test_pgstat.py
@@ -11,10 +11,10 @@ def test_pgstat(neon_simple_env: NeonEnv):
     if env.pg_version == PgVersion.V14:
         pytest.skip("PG14 doesn't support pgstat statistic persistence")
 
-    env.pageserver.allowed_errors.append(".*this timeline is using deprecated aux file policy V1.*")
-
     n = 10000
-    endpoint = env.endpoints.create_start("main")
+    endpoint = env.endpoints.create_start(
+        "main", config_lines=["neon_pgstat_file_size_limit=100kB"]
+    )
 
     con = endpoint.connect()
     cur = con.cursor()
@@ -59,4 +59,25 @@ def test_pgstat(neon_simple_env: NeonEnv):
     )
     rec = cur.fetchall()[0]
     # pgstat information should be discarded in case of abnormal termination
+    assert rec == (0, 0, 0, 0, 0, 0, 0, 0)
+
+    cur.execute("select sum(x) from t")
+
+    # create more relations to increase size of statistics
+    for i in range(1, 1000):
+        cur.execute(f"create table t{i}(pk integer primary key)")
+
+    cur.execute("select pg_stat_force_next_flush()")
+
+    endpoint.stop()
+    endpoint.start()
+
+    con = endpoint.connect()
+    cur = con.cursor()
+
+    cur.execute(
+        "select seq_scan,seq_tup_read,n_tup_ins,n_tup_upd,n_live_tup,n_dead_tup, vacuum_count,analyze_count from pg_stat_user_tables"
+    )
+    rec = cur.fetchall()[0]
+    # pgstat information is not restored because it's size exeeds 100k threshold
     assert rec == (0, 0, 0, 0, 0, 0, 0, 0)
