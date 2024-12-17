700395f3932b37337eb3f6b8804e3dcbc3dfdc4c
Alexey Masterov <alexeymasterov@neon.tech>
2024-12-17 16:35:54 +0100

    fix the logic

--- docker-compose/run-tests.sh
+++ docker-compose/run-tests.sh
@@ -7,8 +7,10 @@ LIST=$( (echo -e "${SKIP//","/"\n"}"; ls -d -- *-src) | sort | uniq -u)
 for d in ${LIST}; do
     [ -d "${d}" ] || continue
     psql -c "select 1" >/dev/null || break
-    if [ -f test.sh ]; then
+    if [ -f "${d}/test.sh" ]; then
+       cd "${d}" || exit 1
        ./test.sh || FAILED="${d} ${FAILED}"
+       cd ..
     else
        USE_PGXS=1 make -C "${d}" installcheck || FAILED="${d} ${FAILED}"
     fi
