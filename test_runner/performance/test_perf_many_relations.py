2b92c0efc8aab3f1f935538d9b43bb96253bb2c9
BodoBolero <peterbendel@neon.tech>
2024-12-17 11:21:07 +0100

    add test to workflow

--- test_runner/performance/test_perf_many_relations.py
+++ test_runner/performance/test_perf_many_relations.py
@@ -1,10 +1,10 @@
-
-
-from pathlib import Path
-import pytest
 import os
+from pathlib import Path
+
+import pytest
 from fixtures.compare_fixtures import PgCompare
 from fixtures.log_helper import log
+
 
 def get_num_relations(default: int = 1000) -> list[int]:
     # We parametrize each run with scale specifying the number of wanted child partitions.
@@ -30,9 +30,9 @@ def test_perf_many_relations(neon_with_baseline: PgCompare, num_relations: int):
 
     # prepare some base tables and the plpgsql procedures that we use to create the tables
     sql_file = Path(__file__).parent / "many_relations" / "create_many_relations.sql"
-    env.pg_bin.run_capture(["psql", env.pg.connstr(),"-f", str(sql_file)])
+    env.pg_bin.run_capture(["psql", env.pg.connstr(), "-f", str(sql_file)])
 
-    num_parent_tables = num_relations // 500 + 1  
+    num_parent_tables = num_relations // 500 + 1
     log.info(f"Creating {num_relations} relations in {num_parent_tables} parent tables")
 
     log.info(f"Creating {num_parent_tables} parent tables")
@@ -45,16 +45,19 @@ def test_perf_many_relations(neon_with_baseline: PgCompare, num_relations: int):
 
     # now run and measure the actual relation creation
     while num_relations_remaining > 0:
-        current_table +=1
+        current_table += 1
         parent_table_name = f"operations_scale_{num_relations}_{current_table}"
         if num_relations_remaining > 500:
             num_relations_to_create = 500
         else:
             num_relations_to_create = num_relations_remaining
         num_relations_remaining -= num_relations_to_create
-        log.info(f"Creating {num_relations_to_create} child tables in partitioned parent table '{parent_table_name}'")
+        log.info(
+            f"Creating {num_relations_to_create} child tables in partitioned parent table '{parent_table_name}'"
+        )
         sql = f"CALL create_operations_partitions( '{parent_table_name}', '2000-01-01', ('2000-01-01'::DATE + INTERVAL '1 day' * {num_relations_to_create})::DATE)"
         log.info(sql)
-        with env.zenbenchmark.record_duration(f"CREATE_TABLE/{current_table}/{num_relations_to_create}"):
+        with env.zenbenchmark.record_duration(
+            f"CREATE_TABLE/{current_table}/{num_relations_to_create}"
+        ):
             env.pg_bin.run_capture(["psql", env.pg.connstr(), "-c", sql])
-        
