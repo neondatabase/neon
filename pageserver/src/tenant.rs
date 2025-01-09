17654fe27faa3a9defa09fb8ee0d4f97e8c6f22f
Alex Chi Z <chi@neon.tech>
2025-01-09 13:57:53 -0500

    cleanup
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -5455,9 +5455,6 @@ pub(crate) mod harness {
                 lsn_lease_length_for_ts: Some(tenant_conf.lsn_lease_length_for_ts),
                 timeline_offloading: Some(tenant_conf.timeline_offloading),
                 wal_receiver_protocol_override: tenant_conf.wal_receiver_protocol_override,
-                gc_compaction_enabled: None,
-                gc_compaction_initial_threshold_mb: None,
-                gc_compaction_ratio_percent: None,
             }
         }
     }
