1e3a095f43c4283990dc960ad7584b5650e02571
Alexey Masterov <alexeymasterov@neon.tech>
2025-01-16 12:21:54 +0100

    Formatting

--- docker-compose/run-tests.sh
+++ docker-compose/run-tests.sh
@@ -8,5 +8,5 @@ LIST=$( (echo -e "${SKIP//","/"\n"}"; ls -d -- *-src) | sort | uniq -u)
 parallel -j3 '[ -d {} ] || exit 0; if ! psql -c "select 1" >/dev/null; then {echo {} >> $FAILED_FILE; exit 1; fi ; USE_PGXS=1 PGHOST=pcompute{%} make -C {} installcheck || echo {} >> $FAILED_FILE;' ::: ${LIST}
 FAILED=$(cat $FAILED_FILE)
 [ -z "${FAILED}" ] && exit 0
-echo $FAILED
+echo ${FAILED}
 exit 1
