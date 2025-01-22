3d326ffc498ba3b5548c634cb7ac1272c84c4c05
Alexey Masterov <alexeymasterov@neon.tech>
2025-01-22 15:09:17 +0100

    Fail the test if psql cannot connect to server

--- docker-compose/docker_compose_test.sh
+++ docker-compose/docker_compose_test.sh
@@ -22,7 +22,6 @@ PSQL_OPTION="-h localhost -U cloud_admin -p 55433 -d postgres"
 cleanup() {
     echo "show container information"
     docker ps
-    docker compose --profile test-extensions -f $COMPOSE_FILE logs
     echo "stop containers..."
     docker compose --profile test-extensions -f $COMPOSE_FILE down
 }
