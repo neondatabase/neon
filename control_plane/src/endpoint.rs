a74698424d63f613845e45d52cadf3e55095783b
Tristan Partin <tristan@neon.tech>
2025-01-23 11:12:27 -0600

    Require --compute-id when running compute_ctl
    The compute_id will be used when verifying claims related sent by the
    control plane.
    
    Signed-off-by: Tristan Partin <tristan@neon.tech>

--- control_plane/src/endpoint.rs
+++ control_plane/src/endpoint.rs
@@ -44,6 +44,8 @@ use std::process::Command;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
 
 use anyhow::{anyhow, bail, Context, Result};
 use compute_api::spec::Database;
@@ -664,6 +666,18 @@ impl Endpoint {
                     .join("postgres")
                     .to_str()
                     .unwrap(),
+            ])
+            // TODO: It would be nice if we generated compute IDs with the same
+            // algorithm as the real control plane.
+            .args([
+                "--compute-id",
+                &format!(
+                    "compute-{}",
+                    SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_secs()
+                ),
             ])
             .stdin(std::process::Stdio::null())
             .stderr(logfile.try_clone()?)
