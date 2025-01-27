439e35b5ad7c96d3a94008d9ba8837026e537d6a
Alexey Masterov <alexeymasterov@neon.tech>
2025-01-27 10:14:03 +0100

    Delete pg_hint_plan support, we will add it later

--- docker-compose/ext-src/pg_hint_plan-src/test-upgrade.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-cd "$(dirname ${0})"
-PG_REGRESS=$(dirname "$(pg_config --pgxs)")/../test/regress/pg_regress
-${PG_REGRESS} --inputdir=./ --bindir='/usr/local/pgsql/bin' --use-existing --encoding=UTF8 --dbname=contrib_regression init base_plan pg_hint_plan ut-init ut-S ut-J ut-L ut-G ut-R ut-fdw ut-W ut-T ut-fini hints_anywhere plpgsql
