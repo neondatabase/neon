f3b41e0a0e764969c48dc989f97cf47125e0732b
Alexey Masterov <alexeymasterov@neon.tech>
2025-01-30 15:15:55 +0100

    Add time checking

--- docker-compose/run-tests.sh
+++ docker-compose/run-tests.sh
@@ -5,7 +5,7 @@ cd /ext-src || exit 2
 export FAILED_FILE=/tmp/failed
 touch ${FAILED_FILE}
 LIST=$( (echo -e "${SKIP//","/"\n"}"; ls -d -- *-src) | sort | uniq -u)
-parallel -j3 '[ -d {} ] || exit 0; if ! psql -c "select 1" >/dev/null; then {echo {} >> $FAILED_FILE; exit 1; fi ; if [ -f /ext-src/{}/neon-test.sh ]; then echo Running from script; PGHOST=pcompute{%} /ext-src/{}/neon-test.sh || echo {} >> ${FAILED_FILE}; else echo Running using make; USE_PGXS=1 PGHOST=pcompute{%} make -C {} installcheck || echo {} >> $FAILED_FILE; fi;' ::: ${LIST}
+parallel -j3 '[ -d {} ] || exit 0; if ! psql -c "select 1" >/dev/null; then {echo {} >> $FAILED_FILE; exit 1; fi ; if [ -f /ext-src/{}/neon-test.sh ]; then echo Running from script; PGHOST=pcompute{%} time /ext-src/{}/neon-test.sh || echo {} >> ${FAILED_FILE}; else echo Running using make; time USE_PGXS=1 PGHOST=pcompute{%} make -C {} installcheck || echo {} >> $FAILED_FILE; fi;' ::: ${LIST}
 FAILED=$(cat $FAILED_FILE)
 [ -z "${FAILED}" ] && exit 0
 echo "${FAILED}"
