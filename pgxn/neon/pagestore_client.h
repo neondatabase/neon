ed72209a0678f48eaf7aef43df393b1e7653b23c
Konstantin Knizhnik <knizhnik@neon.tech>
2025-02-04 23:09:24 +0200

    Add test for prefetch in LFC

--- pgxn/neon/pagestore_client.h
+++ pgxn/neon/pagestore_client.h
@@ -233,6 +233,7 @@ extern char *neon_timeline;
 extern char *neon_tenant;
 extern int32 max_cluster_size;
 extern int  neon_protocol_version;
+extern bool lfc_store_prefetch_result;
 
 extern shardno_t get_shard_number(BufferTag* tag);
 
@@ -302,7 +303,7 @@ extern bool lfc_cache_contains(NRelFileInfo rinfo, ForkNumber forkNum,
 extern int lfc_cache_containsv(NRelFileInfo rinfo, ForkNumber forkNum,
 							   BlockNumber blkno, int nblocks, bits8 *bitmap);
 extern void lfc_init(void);
-extern void lfc_prefetch(NRelFileInfo rinfo, ForkNumber forknum, BlockNumber blkno,
+extern bool lfc_prefetch(NRelFileInfo rinfo, ForkNumber forknum, BlockNumber blkno,
 						 const void* buffer, XLogRecPtr lsn);
 
 
