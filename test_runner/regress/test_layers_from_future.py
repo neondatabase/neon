94a8d5d2b7ee65b91e41a2789b6d74f4e8ef04e4
John Spray <john@neon.tech>
2025-02-05 18:36:23 +0100

    tests: don't detach from controller in test_issue_5878
    Closes: https://github.com/neondatabase/neon/issues/10513

--- test_runner/regress/test_layers_from_future.py
+++ test_runner/regress/test_layers_from_future.py
@@ -172,7 +172,7 @@ def test_issue_5878(neon_env_builder: NeonEnvBuilder, attach_mode: str):
     # force removal of layers from the future
     tenant_conf = ps_http.tenant_config(tenant_id)
     generation_before_detach = get_generation_number()
-    env.pageserver.tenant_detach(tenant_id)
+    env.pageserver.http_client().tenant_detach(tenant_id)
     failpoint_deletion_queue = "deletion-queue-before-execute-pause"
 
     ps_http.configure_failpoints((failpoint_deletion_queue, "pause"))
