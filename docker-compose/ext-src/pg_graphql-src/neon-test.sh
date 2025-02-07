dacf19ab47de9e94ece3625c1f8496ab244ef154
Konstantin Knizhnik <knizhnik@neon.tech>
2025-02-07 10:07:53 +0200

    Set  neon.enable_event_triggers_for_superuser=on for runniung pg_graphql extension test

--- docker-compose/ext-src/pg_graphql-src/neon-test.sh
+++ docker-compose/ext-src/pg_graphql-src/neon-test.sh
@@ -9,6 +9,7 @@ TESTDIR="test"
 TESTS=$(ls "${TESTDIR}/sql" | sort )
 TESTS=${TESTS//\.sql/}
 psql -d contrib_regression -c "alter system set neon.enable_event_triggers_for_superuser=on"
+psql -d contrib_regression -c "select pg_reload_conf()"
 psql -v ON_ERROR_STOP=1 -f test/fixtures.sql -d contrib_regression
 ${REGRESS} --use-existing --dbname=contrib_regression --inputdir=${TESTDIR} ${TESTS}
 
