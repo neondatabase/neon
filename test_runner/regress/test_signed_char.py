7460299a7895231f02ca310e56c5b42abb1e248b
Suhas Thalanki <suhastvs@gmail.com>
2025-02-19 10:21:35 -0500

    added more details and cleaned up code

--- test_runner/regress/test_signed_char.py
+++ test_runner/regress/test_signed_char.py
@@ -1,48 +1,66 @@
 import os
 
+import pytest
 from fixtures.neon_fixtures import NeonEnv
 
-EXPECTED_OUT_PATH = os.path.join(
-    os.curdir, "test_runner", "regress", "data", "test_signed_char.out"
-)
+# In multi-character keys, the GIN index creates a CRC Hash of the first 3 bytes of the key.
+# The hash can have the first bit to be set or unset, needing to have a consistent representation
+# of char across architectures for consistent results. GIN stores these keys by their hashes
+# which determines the order in which the keys are obtained from the GIN index.
+# Using -fsigned-char enforces this order across platforms making this consistent.
+# The following query gets all the data present in the leaf page of a GIN index,
+# which is ordered by the CRC hash and is consistent across platforms.
 SIGNED_CHAR_EXTRACT = """
     WITH
+    -- Generates an intermediate table with block numbers of the index
   pagenumbers AS (
     SELECT num FROM generate_series(0, (pg_relation_size('test_payload_idx') / 8192) - 1) it(num)
   )
     SELECT num,
+    -- Gets the data of the page, skipping the first 8 bytes which is the LSN
     substr(page, 9, 8192-8),
+    -- Returns information about the GIN index opaque area
     (gin_page_opaque_info(page)).*
     FROM pagenumbers,
+    -- Gets a page from the respective blocks of the table
     LATERAL (SELECT get_raw_page('test_payload_idx', num)) AS p(page)
+    -- Filters to only return leaf pages from the GIN Index
     WHERE ARRAY['leaf'] = ((gin_page_opaque_info(page)).flags);
     """
 
+OUTPUT_DATA_DIR = "data"
+OUTPUT_DATA_FILE = "test_signed_char.out"
 
-def test_signed_char(neon_simple_env: NeonEnv):
+
+@pytest.fixture
+def output_path(request):
+    return os.path.join(os.path.dirname(request.module.__file__), OUTPUT_DATA_DIR, OUTPUT_DATA_FILE)
+
+
+def test_signed_char(neon_simple_env: NeonEnv, output_path):
     env = neon_simple_env
     endpoint = env.endpoints.create_start("main")
 
-    ses1 = endpoint.connect().cursor()
-    # Add the required extensions
-    ses1.execute("CREATE EXTENSION pg_trgm;")
-    ses1.execute("CREATE EXTENSION pageinspect;")
-    # Create a test table
-    ses1.execute("CREATE TABLE test (payload text);")
-    # Create a GIN based index
-    ses1.execute(
-        "CREATE INDEX test_payload_idx ON test USING gin (payload gin_trgm_ops) WITH (gin_pending_list_limit = 64);"
-    )
-    # insert a multibyte character to trigger order-dependent hashing
-    ses1.execute(
-        "INSERT INTO test SELECT '123456789BV' || CHR(127153) /* ace of spades, a multibyte character */ || i::text from generate_series(1, 40) as i(i);"
-    )
-    ses1.execute(SIGNED_CHAR_EXTRACT)
-    pages = ses1.fetchall()
+    with endpoint.connect().cursor() as ses1:
+        # Add the required extensions
+        ses1.execute("CREATE EXTENSION pg_trgm;")
+        ses1.execute("CREATE EXTENSION pageinspect;")
+        # Create a test table
+        ses1.execute("CREATE TABLE test (payload text);")
+        # Create a GIN based index
+        ses1.execute(
+            "CREATE INDEX test_payload_idx ON test USING gin (payload gin_trgm_ops) WITH (gin_pending_list_limit = 64);"
+        )
+        # insert a multibyte character to trigger order-dependent hashing
+        ses1.execute(
+            "INSERT INTO test SELECT '123456789BV' || CHR(127153) /* ace of spades, a multibyte character */ || i::text from generate_series(1, 40) as i(i);"
+        )
+        ses1.execute(SIGNED_CHAR_EXTRACT)
+        pages = ses1.fetchall()
     # Compare expected output
     page1 = pages[0]
     data = bytes(page1[1]).hex()
-    with open(EXPECTED_OUT_PATH, "rb") as f:
-        expected = f.read().decode("utf-8")
+    with open(output_path, encoding="utf-8") as f:
+        expected = f.read().rstrip()
 
     assert data == expected
