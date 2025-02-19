90917cece26a916ec93e338ff4cee78942a991ea
Vlad Lazar <vlad@neon.tech>
2025-02-19 11:37:48 +0100

    fixup: check against the last last record LSN

--- pageserver/src/tenant/timeline/walreceiver/walreceiver_connection.rs
+++ pageserver/src/tenant/timeline/walreceiver/walreceiver_connection.rs
@@ -360,10 +360,10 @@ pub(super) async fn handle_walreceiver_connection(
                             // This is valid, but check that we received records
                             // that we haven't seen before.
                             if let Some(first_rec) = batch.records.first() {
-                                if first_rec.next_record_lsn > expected_wal_start {
+                                if first_rec.next_record_lsn > last_rec_lsn {
                                     let msg = format!(
-                                        "Received record with next_record_lsn {} multiple times",
-                                        first_rec.next_record_lsn
+                                        "Received record with next_record_lsn multiple times ({} <= {})",
+                                        first_rec.next_record_lsn, expected_wal_start
                                     );
                                     critical!("{msg}");
                                     return Err(WalReceiverError::Other(anyhow!(msg)));
