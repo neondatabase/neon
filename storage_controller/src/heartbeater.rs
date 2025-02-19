733bffb2b54bada310c384b85bc8bb4af37f6044
Arpad Müller <arpad@neon.tech>
2025-02-19 23:12:54 +0100

    further fix

--- storage_controller/src/heartbeater.rs
+++ storage_controller/src/heartbeater.rs
@@ -140,6 +140,10 @@ where
                 request = self.receiver.recv() => {
                     match request {
                         Some(req) => {
+                            if req.reply.is_closed() {
+                                // Prevent a possibly infinite buildup of the receiver channel, if requests arrive faster than we can handle them
+                                continue;
+                            }
                             let res = self.heartbeat(req.servers).await;
                             // Ignore the return value in order to not panic if the heartbeat function's future was cancelled
                             _ = req.reply.send(res);
