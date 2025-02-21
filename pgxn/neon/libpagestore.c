f44cb5cb0f9cdfbddea32fbeb3724c847e5f14d9
Heikki Linnakangas <heikki@neon.tech>
2025-02-21 15:42:39 +0200

    Increase the timeout to 2 minutes, per discussion

--- pgxn/neon/libpagestore.c
+++ pgxn/neon/libpagestore.c
@@ -62,7 +62,7 @@ static int	max_reconnect_attempts = 60;
 static int	stripe_size;
 
 static int pageserver_response_log_timeout = 10000;
-static int pageserver_response_disconnect_timeout = 30000;
+static int pageserver_response_disconnect_timeout = 120000; /* 2 minutes */
 
 typedef struct
 {
@@ -1305,7 +1305,7 @@ pg_init_libpagestore(void)
 							"If the pageserver doesn't respond to a request within this timeout,"
 							"disconnect and reconnect.",
 							&pageserver_response_disconnect_timeout,
-							30000, 100, INT_MAX,
+							120000, 100, INT_MAX,
 							PGC_SUSET,
 							GUC_UNIT_MS,
 							NULL, NULL, NULL);
