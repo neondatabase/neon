877d6b7387c436f6b47c367c4561077e300a3ed9
Suhas Thalanki <suhastvs@gmail.com>
2025-02-28 13:07:56 -0500

    added lwlc_init in neon.h
    # Conflicts:
    #	pgxn/neon/neon.h

--- pgxn/neon/neon.h
+++ pgxn/neon/neon.h
@@ -50,6 +50,7 @@ extern uint32		WAIT_EVENT_NEON_WAL_DL;
 
 extern void pg_init_libpagestore(void);
 extern void pg_init_walproposer(void);
+extern void pg_init_lwlc(void);
 extern void pagestore_smgr_init(void);
 
 extern uint64 BackpressureThrottlingTime(void);
