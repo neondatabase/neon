cdb95d1a3523e0e1652c849d0a7f8b9b6203fb94
Vlad Lazar <vlad@neon.tech>
2025-03-07 18:06:10 +0100

    utils: optionally enable otel tracing in common logging utils
    This patch augments `utils::logging::init` with the ability to set up
    OTEL tracing infrastructure. The end goal is for the pageserver to use
    this in order to export perf traces.
    
    Note that an entirely different tracing subscriber is used. This is to
    avoid interference with the existing tracing set-up.
    
    For now, no service uses this functionality.

--- storage_controller/src/main.rs
+++ storage_controller/src/main.rs
@@ -240,6 +240,7 @@ fn main() -> anyhow::Result<()> {
     logging::init(
         LogFormat::Plain,
         logging::TracingErrorLayerEnablement::Disabled,
+        utils::logging::OtelEnablement::Disabled,
         logging::Output::Stdout,
     )?;
 
