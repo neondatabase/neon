9eddb68b8b55d7f1494b8f9dd603ec41bee13253
Tristan Partin <tristan@neon.tech>
2025-03-11 17:35:21 -0500

    Move maybe_add_request_id_header middleware into middleware module
    This matches the authorization middleware.
    
    Signed-off-by: Tristan Partin <tristan@neon.tech>

---
+++ compute_tools/src/http/middleware/request_id.rs
@@ -0,0 +1,16 @@
+use axum::{extract::Request, middleware::Next, response::Response};
+use uuid::Uuid;
+
+use crate::http::headers::X_REQUEST_ID;
+
+/// This middleware function allows compute_ctl to generate its own request ID
+/// if one isn't supplied. The control plane will always send one as a UUID. The
+/// neon Postgres extension on the other hand does not send one.
+pub async fn maybe_add_request_id_header(mut request: Request, next: Next) -> Response {
+    let headers = request.headers_mut();
+    if headers.get(X_REQUEST_ID).is_none() {
+        headers.append(X_REQUEST_ID, Uuid::new_v4().to_string().parse().unwrap());
+    }
+
+    next.run(request).await
+}
