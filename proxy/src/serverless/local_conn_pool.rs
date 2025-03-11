ec68d7661fd732664bd156b1bbfbadda57c516ce
Conrad Ludgate <conradludgate@gmail.com>
2025-03-11 09:36:12 +0000

    fix(proxy): delete prepared statements when discarding

--- proxy/src/serverless/local_conn_pool.rs
+++ proxy/src/serverless/local_conn_pool.rs
@@ -279,8 +279,7 @@ impl ClientInnerCommon<postgres_client::Client> {
             local_data.jti += 1;
             let token = resign_jwt(&local_data.key, payload, local_data.jti)?;
 
-            // discard all cannot run in a transaction. must be executed alone.
-            self.inner.batch_execute("discard all").await?;
+            self.inner.discard_all().await?;
 
             // initiates the auth session
             // this is safe from query injections as the jwt format free of any escape characters.
