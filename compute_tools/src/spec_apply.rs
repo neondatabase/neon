6af974548e7042ddefac5afa641a95879732be63
Anastasia Lubennikova <anastasia@neon.tech>
2025-03-19 00:13:36 +0000

    feat(compute_ctl): Add basic audit logging for computes. (#11170)
    if `audit_log_level` is set to Log, 
    preload pgaudit extension and log DDL with masked parameters into
    standard postgresql log

--- compute_tools/src/spec_apply.rs
+++ compute_tools/src/spec_apply.rs
@@ -286,7 +286,10 @@ impl ComputeNode {
                     phases.push(CreatePgauditlogtofileExtension);
                     phases.push(DisablePostgresDBPgAudit);
                 }
-                ComputeAudit::Log => { /* not implemented yet */ }
+                ComputeAudit::Log => {
+                    phases.push(CreatePgauditExtension);
+                    phases.push(DisablePostgresDBPgAudit);
+                }
                 ComputeAudit::Disabled => {}
             }
 
