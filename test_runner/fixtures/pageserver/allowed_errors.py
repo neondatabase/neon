ab8f3de4358eb5ee72c9b26fece40e67473f157d
Erik Grinaker <erik@neon.tech>
2025-03-20 14:05:41 +0100

    allowed_errors.py: ignore queue shutdown

--- test_runner/fixtures/pageserver/allowed_errors.py
+++ test_runner/fixtures/pageserver/allowed_errors.py
@@ -101,6 +101,7 @@ DEFAULT_PAGESERVER_ALLOWED_ERRORS = (
     ".*WARN.*path=/v1/utilization .*request was dropped before completing",
     # Can happen during shutdown
     ".*scheduling deletion on drop failed: queue is in state Stopped.*",
+    ".*scheduling deletion on drop failed: queue is shutting down.*",
     # L0 flush backpressure delays are expected under heavy ingest load. We want to exercise
     # this backpressure in tests.
     ".*delaying layer flush by \\S+ for compaction backpressure.*",
