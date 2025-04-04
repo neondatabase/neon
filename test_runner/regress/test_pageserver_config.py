fec3a5b18aefc2216841bd678baf95435cd10f04
Christian Schwarz <christian@neon.tech>
2025-04-04 17:36:40 +0200

    assert metric

--- test_runner/regress/test_pageserver_config.py
+++ test_runner/regress/test_pageserver_config.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 from fixtures.neon_fixtures import NeonEnv
 from fixtures.utils import run_only_on_default_postgres
@@ -7,7 +8,7 @@ from fixtures.utils import run_only_on_default_postgres
 @run_only_on_default_postgres(reason="does not use postgres")
 def test_unknown_config_items_handling(neon_simple_env: NeonEnv, what: str):
     """
-    Ensure we log unknown config fields.
+    Ensure we log unknown config fields and expose a metric for alerting.
     There are more unit tests in the Rust code for other TOML items.
     """
     env = neon_simple_env
@@ -17,25 +18,36 @@ def test_unknown_config_items_handling(neon_simple_env: NeonEnv, what: str):
             return None
         elif what == "top_level":
             config["unknown_top_level_config_item"] = 23
-            return r".*unknown_top_level_config_item.*"
+            return r"unknown_top_level_config_item"
         elif what == "nested":
             config["remote_storage"]["unknown_config_item"] = 23
-            return r".*remote_storage\.unknown_config_item.*"
+            return r"remote_storage.unknown_config_item"
         else:
             raise ValueError(f"Unknown what: {what}")
 
-    expect_warn_re = env.pageserver.edit_config_toml(edit_fn)
+    def get_metric() -> str:
+        metrics = env.pageserver.http_client().get_metrics()
+        samples = metrics.query_all("pageserver_config_ignored_items")
+        by_item = { sample.labels["item"]: sample.value for sample in samples }
+        assert by_item[""] == 0, "must always contain the empty item with value 0"
+        del by_item[""]
+        return by_item
 
-    if expect_warn_re is not None:
-        env.pageserver.allowed_errors.append(expect_warn_re)
+    expected_ignored_item = env.pageserver.edit_config_toml(edit_fn)
 
-    if expect_warn_re is not None:
-        assert not env.pageserver.log_contains(expect_warn_re)
+    if expected_ignored_item is not None:
+        expected_ignored_item_log_line_re = r".*ignoring unknown configuration item.*" + re.escape(expected_ignored_item)
+        env.pageserver.allowed_errors.append(expected_ignored_item_log_line_re)
+
+    if expected_ignored_item is not None:
+        assert not env.pageserver.log_contains(expected_ignored_item_log_line_re)
+        assert get_metric() == {}
 
     # in any way, unknown config items should not fail pageserver to start
     # TODO: extend this test with the config validator mode once we introduce it
     # https://github.com/neondatabase/cloud/issues/24349
     env.pageserver.restart()
 
-    if expect_warn_re is not None:
-        assert env.pageserver.log_contains(expect_warn_re)
+    if expected_ignored_item is not None:
+        assert env.pageserver.log_contains(expected_ignored_item_log_line_re)
+        assert get_metric() == {expected_ignored_item: 1}
