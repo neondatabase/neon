1d04b20e6f2f3b06d0b64906613a2c57fb184a2e
Heikki Linnakangas <heikki@neon.tech>
2025-04-05 18:41:39 +0300

    Misc cleanup of #includes and comments in the neon extension
    Remove useless and often wrong IDENTIFICATION comments. PostgreSQL
    sources have them, mostly for historical reasons, but there's no
    need for us to copy that style.
    
    Remove unnecessary #includes in header files, putting the #includes
    directly in the .c files that need them. The principle is that a
    header file should #include other header files if they need
    definitions from them, such that each header file can be compiled on
    its own, but not other #includes. (There are tools to enforce that,
    but this was just a manual clean up of violations that I happened to
    spot.)

--- pgxn/neon/file_cache.c
+++ pgxn/neon/file_cache.c
@@ -1,14 +1,10 @@
-/*
+/*-------------------------------------------------------------------------
  *
  * file_cache.c
  *
  *
  * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
- *
- *
- * IDENTIFICATION
- *	  pgxn/neon/file_cache.c
  *
  *-------------------------------------------------------------------------
  */
