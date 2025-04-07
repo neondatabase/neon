58f8ccd52a9403b75420fc841e4eca7513d80487
Christian Schwarz <christian@neon.tech>
2025-04-07 19:01:05 +0200

    revert previous two commits

--- test_runner/performance/pageserver/test_page_service_batching.py
+++ test_runner/performance/pageserver/test_page_service_batching.py
@@ -127,10 +127,19 @@ def test_throughput(
 
     env = neon_env_builder.init_start()
     ps_http = env.pageserver.http_client()
-    endpoint = env.endpoints.create_start("main")
+    endpoint = env.endpoints.create_start("main", config_lines=[
+        # minimal lfc & small shared buffers to force requests to pageserver
+        "neon.max_file_cache_size=1MB",
+        "shared_buffers=10MB",
+    ])
     conn = endpoint.connect()
     cur = conn.cursor()
 
+    cur.execute("SET max_parallel_workers_per_gather=0")  # disable parallel backends
+    cur.execute(f"SET effective_io_concurrency={effective_io_concurrency}")
+    cur.execute(
+        f"SET neon.readahead_buffer_size={readhead_buffer_size}"
+    )  # this is the current default value, but let's hard-code that
 
     cur.execute("CREATE EXTENSION IF NOT EXISTS neon;")
     cur.execute("CREATE EXTENSION IF NOT EXISTS neon_test_utils;")
@@ -195,34 +204,32 @@ def test_throughput(
                 ).value,
             )
 
-    def workload() -> Metrics:
-
-        conn = endpoint.connect(autocommit=False)
-        cur = conn.cursor()
-
-        cur.execute("SET max_parallel_workers_per_gather=0")  # disable parallel backends
-        cur.execute(f"SET effective_io_concurrency={effective_io_concurrency}")
-        cur.execute(
-            f"SET neon.readahead_buffer_size={readhead_buffer_size}"
-        )  # this is the current default value, but let's hard-code that
-
+    def workload(disruptor_started: threading.Event) -> Metrics:
+        disruptor_started.wait()
         start = time.time()
         iters = 0
         while time.time() - start < target_runtime or iters < 2:
-            log.info("Seqscan %d", iters)
             if iters == 1:
                 # round zero for warming up
                 before = get_metrics()
-            cur.execute(
-                "select clear_buffer_cache()"
-            )  # TODO: what about LFC? doesn't matter right now because LFC isn't enabled by default in tests
-            # cur.execute("select sum(data::bigint) from t")
-            # assert cur.fetchall()[0][0] == npages * (npages + 1) // 2
-            rewrite_fraction = 4
-            cur.execute("update t set data = data::bigint + %s where data::bigint %% %s = (%s %% %s)", (rewrite_fraction,rewrite_fraction,iters,rewrite_fraction))
+            cur.execute("select sum(data::bigint) from t")
+            assert cur.fetchall()[0][0] == npages * (npages + 1) // 2
             iters += 1
         after = get_metrics()
         return (after - before).normalize(iters - 1)
+
+    def disruptor(disruptor_started: threading.Event, stop_disruptor: threading.Event):
+        conn = endpoint.connect()
+        cur = conn.cursor()
+        iters = 0
+        while True:
+            cur.execute("SELECT pg_logical_emit_message(true, 'test', 'advancelsn')")
+            if stop_disruptor.is_set():
+                break
+            disruptor_started.set()
+            iters += 1
+            time.sleep(0.001)
+        return iters
 
     env.pageserver.patch_config_toml_nonrecursive(
         {"page_service_pipelining": dataclasses.asdict(pipelining_config)}
@@ -232,7 +239,17 @@ def test_throughput(
     env.pageserver.restart(extra_env_vars={"RUST_LOG": "info,pageserver::page_service=trace"})
 
     log.info("Starting workload")
-    metrics = workload()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        disruptor_started = threading.Event()
+        stop_disruptor = threading.Event()
+        disruptor_fut = executor.submit(disruptor, disruptor_started, stop_disruptor)
+        workload_fut = executor.submit(workload, disruptor_started)
+        metrics = workload_fut.result()
+        stop_disruptor.set()
+        ndisruptions = disruptor_fut.result()
+        log.info("Disruptor issued %d disrupting requests", ndisruptions)
+
 
     log.info("Results: %s", metrics)
 
