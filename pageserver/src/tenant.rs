9f9592f67e637aba38c5d16d0f254f06189c9907
Vlad Lazar <vlad@neon.tech>
2025-04-10 19:09:48 +0200

    fixup: borked during switch to constructor

--- pageserver/src/tenant.rs
+++ pageserver/src/tenant.rs
@@ -6786,7 +6786,7 @@ mod tests {
         for read in reads {
             info!("Doing vectored read on {:?}", read);
 
-            let query = VersionedKeySpaceQuery::uniform(keyspace, lsn);
+            let query = VersionedKeySpaceQuery::uniform(read.clone(), lsn);
 
             let vectored_res = tline
                 .get_vectored_impl(
