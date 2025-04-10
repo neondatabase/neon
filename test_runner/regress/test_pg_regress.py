c005cd9b98e544c8461c041b59c7875a4dc7570a
Konstantin Knizhnik <knizhnik@neon.tech>
2025-04-10 15:07:18 +0300

    Update comment

--- test_runner/regress/test_pg_regress.py
+++ test_runner/regress/test_pg_regress.py
@@ -239,7 +239,7 @@ def test_isolation(
             "neon.regress_test_mode = true",
             # Stack size should be increased for tests to pass with asan.
             "max_stack_depth = 4MB",
-            # Neon extensiosn starts 2 BGW so decreasing number of parallel workers which can affect parallel_deadlock test
+            # Neon extensiosn starts 2 BGW so decreasing number of parallel workers which can affect deadlock-parallel test if it hits max_worker_processes.
             "max_worker_processes = 16",
         ],
     )
