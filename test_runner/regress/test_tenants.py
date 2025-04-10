7a2a83d7c4dafced6278dabc4748c8ef1f76a45b
Alex Chi Z <chi@neon.tech>
2025-04-10 16:15:17 -0400

    fix(test): allow stale generation errors
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- test_runner/regress/test_tenants.py
+++ test_runner/regress/test_tenants.py
@@ -390,6 +390,7 @@ def test_create_churn_during_restart(neon_env_builder: NeonEnvBuilder):
     # Tenant creation requests which arrive out of order will generate complaints about
     # generation nubmers out of order.
     env.pageserver.allowed_errors.append(".*Generation .+ is less than existing .+")
+    env.pageserver.allowed_errors.append(".*due to stale generation.+")
 
     # Timeline::flush_and_shutdown cannot tell if it is hitting a failure because of
     # an incomplete attach, or some other problem.  In the field this should be rare,
