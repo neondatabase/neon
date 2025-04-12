6592d69a6700a2bd2e9f60c22af138ea0dafbdd0
BodoBolero <peterbendel@neon.tech>
2025-04-12 09:43:26 +0200

    increase wait_lsn_timeout to avoid WARN message in pageserver

--- test_runner/performance/test_ingest_insert_bulk.py
+++ test_runner/performance/test_ingest_insert_bulk.py
@@ -52,6 +52,8 @@ def test_ingest_insert_bulk(
         # would compete with Pageserver for bandwidth.
         # neon_env_builder.enable_safekeeper_remote_storage(s3_storage())
 
+    neon_env_builder.pageserver_config_override = "wait_lsn_timeout='600 s'"
+
     neon_env_builder.disable_scrub_on_exit()  # immediate shutdown may leave stray layers
     env = neon_env_builder.init_start()
 
