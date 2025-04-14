cae480d62fab477cac70c5229e6f47d863081c76
Alexey Masterov <alexeymasterov@neon.tech>
2025-04-14 17:04:27 +0200

    Fix some tests

--- docker-compose/ext-src/pg_ivm-src/regular-test.sh
+++ docker-compose/ext-src/pg_ivm-src/regular-test.sh
@@ -4,4 +4,6 @@ dropdb --if-exist contrib_regression
 createdb contrib_regression
 cd "$(dirname ${0})"
 PG_REGRESS=$(dirname "$(pg_config --pgxs)")/../test/regress/pg_regress
+patch -p1 <regular.patch
 ${PG_REGRESS} --use-existing --inputdir=./ --bindir='/usr/local/pgsql/bin' --dbname=contrib_regression pg_ivm create_immv refresh_immv
+patch -R -p1 <regular.patch
