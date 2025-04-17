a1d21189910538afe82f99af7789a6d24536c169
Vlad Lazar <vlad@neon.tech>
2025-04-17 13:59:49 +0200

    tests: add import completion across restart test

--- storage_controller/src/service.rs
+++ storage_controller/src/service.rs
@@ -3887,6 +3887,9 @@ impl Service {
     ) -> anyhow::Result<()> {
         tracing::info!("Finalizing timeline import");
 
+
+        pausable_failpoint!("timeline-import-pre-cplane-notification");
+
         let import_failed = import.completion_error().is_some();
 
         if !import_failed {
