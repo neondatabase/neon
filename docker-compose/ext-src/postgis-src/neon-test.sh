968c87ccac801a7673de46ef7f0d0a36b68dbd1d
Alexey Masterov <alexeymasterov@neon.tech>
2025-04-25 15:59:57 +0200

    fix the errors

--- docker-compose/ext-src/postgis-src/neon-test.sh
+++ docker-compose/ext-src/postgis-src/neon-test.sh
@@ -2,4 +2,7 @@
 set -ex
 cd "$(dirname "$0")"
 sed -i '/computed_columns/d' regress/core/tests.mk
+cp regress/runtest.mk regress/runtest.mk.bak
+sed -i '27,36d' regress/runtest.mk
+trap 'echo Cleaning up; mv regress/runtest.mk.bak regress/runtest.mk' EXIT INT TERM
 make installcheck-base
