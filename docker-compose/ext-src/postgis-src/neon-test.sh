a82dcbbcc856653be9fa8f0a87cbbb917efc489e
Alexey Masterov <alexeymasterov@neon.tech>
2025-04-28 10:01:01 +0200

    Fix the problem with the trap in the script

--- docker-compose/ext-src/postgis-src/neon-test.sh
+++ docker-compose/ext-src/postgis-src/neon-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ex
 cd "$(dirname "$0")"
-if [[ ${PG_REGRESS} = v17 ]]; then
+if [[ ${PG_VERSION} = v17 ]]; then
   sed -i '/computed_columns/d' regress/core/tests.mk
 fi
-cp regress/runtest.mk /tmp/runtest.mk.bak
+TMPDIR=$(mktemp -d /tmp/ext-test-XXXXXX)
+cp regress/runtest.mk "${TMPDIR}"
 sed -i '27,36d' regress/runtest.mk
-trap 'echo Cleaning up; mv /tmp/runtest.mk regress/runtest.mk' EXIT INT TERM
+trap 'echo Cleaning up; mv "${TMPDIR}/runtest.mk" regress/runtest.mk && rmdir "${TMPDIR}"' EXIT
 make installcheck-base
