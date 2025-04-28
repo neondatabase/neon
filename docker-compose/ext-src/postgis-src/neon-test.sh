35d58a92ab2c285b23bb1e6ddb0a2b76bbbdc590
Alexey Masterov <alexeymasterov@neon.tech>
2025-04-28 13:59:33 +0200

    Use patch instead of sed

--- docker-compose/ext-src/postgis-src/neon-test.sh
+++ docker-compose/ext-src/postgis-src/neon-test.sh
@@ -4,8 +4,6 @@ cd "$(dirname "$0")"
 if [[ ${PG_VERSION} = v17 ]]; then
   sed -i '/computed_columns/d' regress/core/tests.mk
 fi
-TMPDIR=$(mktemp -d /tmp/ext-test-XXXXXX)
-cp regress/runtest.mk "${TMPDIR}"
-sed -i '27,36d' regress/runtest.mk
-trap 'echo Cleaning up; mv "${TMPDIR}/runtest.mk" regress/runtest.mk && rmdir "${TMPDIR}"' EXIT
+patch -p1 <postgis-no-upgrade-test.patch
+trap 'echo Cleaning up; patch -R -p1 <postgis-no-upgrade-test.patch' EXIT
 make installcheck-base
