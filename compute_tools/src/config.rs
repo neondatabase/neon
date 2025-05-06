3d53f47a22b8cf29d8ec62c7562a93dc45068b77
Jakub Kołodziejczak <jakub@neon.tech>
2025-05-06 09:20:29 +0200

    don't wrap value in single quotes

--- compute_tools/src/config.rs
+++ compute_tools/src/config.rs
@@ -224,7 +224,7 @@ pub fn write_postgres_conf(
             writeln!(file, "pgaudit.log_rotation_age=5")?;
 
             // Enable audit logs for pg_session_jwt extension
-            writeln!(file, "pg_session_jwt.audit_log='on'")?;
+            writeln!(file, "pg_session_jwt.audit_log=on")?;
 
             // Add audit shared_preload_libraries, if they are not present.
             //
