5cd7f936f90978673a1f6a1dc64765e701035aa4
Conrad Ludgate <conrad@neon.tech>
2025-05-09 08:48:30 +0100

    fix(neon-rls): optimistically assume role grants are already assigned for replicas (#11811)
    ## Problem
    
    Read replicas cannot grant permissions for roles for Neon RLS. Usually
    the permission is already granted, so we can optimistically check. See
    INC-509
    
    ## Summary of changes
    
    Perform a permission lookup prior to actually executing any grants.

--- test_runner/fixtures/neon_fixtures.py
+++ test_runner/fixtures/neon_fixtures.py
@@ -4613,7 +4613,10 @@ class EndpointFactory:
         return self
 
     def new_replica(
-        self, origin: Endpoint, endpoint_id: str, config_lines: list[str] | None = None
+        self,
+        origin: Endpoint,
+        endpoint_id: str | None = None,
+        config_lines: list[str] | None = None,
     ):
         branch_name = origin.branch_name
         assert origin in self.endpoints
@@ -4629,7 +4632,10 @@ class EndpointFactory:
         )
 
     def new_replica_start(
-        self, origin: Endpoint, endpoint_id: str, config_lines: list[str] | None = None
+        self,
+        origin: Endpoint,
+        endpoint_id: str | None = None,
+        config_lines: list[str] | None = None,
     ):
         branch_name = origin.branch_name
         assert origin in self.endpoints
