68120cfa31b10eda7f807c74b6049f60d7400a45
a-masterov <72613290+a-masterov@users.noreply.github.com>
2025-05-14 15:19:53 +0200

    Fix Cloud Extensions Regression (#11907)
    ## Problem
    The regression test on extensions relied on the admin API to set the
    default endpoint settings, which is not stable and requires admin
    privileges. Specifically:
    - The workflow was using `default_endpoint_settings` to configure
    necessary PostgreSQL settings like `DateStyle`, `TimeZone`, and
    `neon.allow_unstable_extensions`
    - This approach was failing because the API endpoint for setting
    `default_endpoint_settings` was changed (referenced in a comment as
    issue #27108)
    - The admin API requires special privileges.
    ## Summary of changes
    We get rid of the admin API dependency and use ALTER DATABASE statements
    instead:
    **Removed the default_endpoint_settings mechanism:**
    - Removed the default_endpoint_settings input parameter from the
    neon-project-create action
    - Removed the API call that was attempting to set these settings at the
    project level
    - Completely removed the default_endpoint_settings configuration from
    the cloud-extensions workflow
    **Added database-level settings:**
    - Created a new `alter_db.sh` script that applies the same settings
    directly to each test database
    - Modified all extension test scripts to call this script after database
    creation

--- docker-compose/ext-src/plv8-src/regular-test.sh
+++ docker-compose/ext-src/plv8-src/regular-test.sh
@@ -3,6 +3,7 @@ set -ex
 cd "$(dirname ${0})"
 dropdb --if-exist contrib_regression
 createdb contrib_regression
+. ../alter_db.sh
 PG_REGRESS=$(dirname "$(pg_config --pgxs)")/../test/regress/pg_regress
 REGRESS="$(make -n installcheck | awk '{print substr($0,index($0,"init-extension"));}')"
 REGRESS="${REGRESS/startup_perms/}"
