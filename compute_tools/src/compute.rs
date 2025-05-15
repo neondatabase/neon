85886a1871de18e320ad287a6b01b71155c533ff
Lassi Pölönen <lassi.polonen@iki.fi>
2025-05-15 15:51:41 +0300

    Add metadata to pgaudit log logline
    Previously we were using project-id/endpoint-id as SYSLOGTAG, which has a
    limit of 32 characters, so the endpoint-id got truncated.
    
    The output is now in RFC5424 format, where the message is json encoded
    with additional metadata `endpoint_id` and `project_id`
    
    Also as pgaudit logs multiline messages, we now detect this by parsing
    the timestamp in the specific format, and consider non-matching lines to
    belong in the previous ones.
    
    Using structured-data would be an alternative, but leaning towards json
    due to be somewhat more generic.

--- compute_tools/src/compute.rs
+++ compute_tools/src/compute.rs
@@ -695,25 +695,18 @@ impl ComputeNode {
                 let log_directory_path = Path::new(&self.params.pgdata).join("log");
                 let log_directory_path = log_directory_path.to_string_lossy().to_string();
 
-                // Add project_id,endpoint_id tag to identify the logs.
+                // Add project_id,endpoint_id to identify the logs.
                 //
                 // These ids are passed from cplane,
-                // for backwards compatibility (old computes that don't have them),
-                // we set them to None.
-                // TODO: Clean up this code when all computes have them.
-                let tag: Option<String> = match (
-                    pspec.spec.project_id.as_deref(),
-                    pspec.spec.endpoint_id.as_deref(),
-                ) {
-                    (Some(project_id), Some(endpoint_id)) => {
-                        Some(format!("{project_id}/{endpoint_id}"))
-                    }
-                    (Some(project_id), None) => Some(format!("{project_id}/None")),
-                    (None, Some(endpoint_id)) => Some(format!("None,{endpoint_id}")),
-                    (None, None) => None,
-                };
+                let endpoint_id = pspec.spec.endpoint_id.as_deref().unwrap_or("");
+                let project_id = pspec.spec.project_id.as_deref().unwrap_or("");
 
-                configure_audit_rsyslog(log_directory_path.clone(), tag, &remote_endpoint)?;
+                configure_audit_rsyslog(
+                    log_directory_path.clone(),
+                    endpoint_id,
+                    project_id,
+                    &remote_endpoint,
+                )?;
 
                 // Launch a background task to clean up the audit logs
                 launch_pgaudit_gc(log_directory_path);
