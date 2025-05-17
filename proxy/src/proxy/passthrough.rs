68d561664bde6882d90bf6adc17793097b6e94d8
Conrad Ludgate <conradludgate@gmail.com>
2025-05-17 18:52:11 +0200

    proxy(passthrough): only instrument on debug

--- proxy/src/proxy/passthrough.rs
+++ proxy/src/proxy/passthrough.rs
@@ -13,7 +13,7 @@ use crate::stream::Stream;
 use crate::usage_metrics::{Ids, MetricCounterRecorder, USAGE_METRICS};
 
 /// Forward bytes in both directions (client <-> compute).
-#[tracing::instrument(skip_all)]
+#[tracing::instrument(level = "debug", skip_all)]
 pub(crate) async fn proxy_pass(
     client: impl AsyncRead + AsyncWrite + Unpin,
     compute: impl AsyncRead + AsyncWrite + Unpin,
