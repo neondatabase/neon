a21c1174edefdfb59fbdce9ae5696c446a3cfe0a
Erik Grinaker <erik@neon.tech>
2025-06-02 16:50:49 +0200

    pagebench: add gRPC support for `get-page-latest-lsn` (#12077)
    ## Problem
    
    We need gRPC support in Pagebench to benchmark the new gRPC Pageserver
    implementation.
    
    Touches #11728.
    
    ## Summary of changes
    
    Adds a `Client` trait to make the client transport swappable, and a gRPC
    client via a `--protocol grpc` parameter. This must also specify the
    connstring with the gRPC port:
    
    ```
    pagebench get-page-latest-lsn --protocol grpc --page-service-connstring grpc://localhost:51051
    ```
    
    The client is implemented using the raw Tonic-generated gRPC client, to
    minimize client overhead.

--- libs/pageserver_api/src/reltag.rs
+++ libs/pageserver_api/src/reltag.rs
@@ -24,7 +24,7 @@ use serde::{Deserialize, Serialize};
 // FIXME: should move 'forknum' as last field to keep this consistent with Postgres.
 // Then we could replace the custom Ord and PartialOrd implementations below with
 // deriving them. This will require changes in walredoproc.c.
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone, Copy, Serialize, Deserialize)]
 pub struct RelTag {
     pub forknum: u8,
     pub spcnode: Oid,
