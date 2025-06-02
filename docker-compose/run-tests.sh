be59d78cb15339a48659b049f57d0d331b523db7
Alexey Masterov <alexeymasterov@neon.tech>
2025-06-02 16:09:41 +0200

    change remote script

--- docker-compose/run-tests.sh
+++ docker-compose/run-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -x
 
 if [[ -v BENCHMARK_CONNSTR ]]; then
@@ -26,10 +26,15 @@ if [[ -v BENCHMARK_CONNSTR ]]; then
   fi
 fi
 REGULAR_USER=false
-while getopts r arg; do
-  case $arg in
+RUN_PARALLEL=false
+while getopts pr arg; do
+  case ${arg} in
   r)
     REGULAR_USER=true
+    shift $((OPTIND-1))
+    ;;
+  p)
+    RUN_PARALLEL=true
     shift $((OPTIND-1))
     ;;
   *) :
@@ -41,31 +46,36 @@ extdir=${1}
 
 cd "${extdir}" || exit 2
 FAILED=
+export FAILED_FILE=/tmp/failed
 LIST=$( (echo -e "${SKIP//","/"\n"}"; ls) | sort | uniq -u)
-for d in ${LIST}; do
-    [ -d "${d}" ] || continue
-    if ! psql -w -c "select 1" >/dev/null; then
-      FAILED="${d} ${FAILED}"
-      break
-    fi
-    if [[ ${REGULAR_USER} = true ]] && [ -f "${d}"/regular-test.sh ]; then
-       "${d}/regular-test.sh" || FAILED="${d} ${FAILED}"
-       continue
-    fi
+if [[ ${RUN_PARALLEL} = true ]]; then
+  parallel -j3 "[ -d {} ] || exit 0; if ! psql -c "select 1" >/dev/null; then {echo {} >> ${FAILED_FILE}; exit 1; fi ; if [ -f $extdir/{}/neon-test.sh ]; then echo Running from script; PGHOST=pcompute{%} time ${extdir}/{}/neon-test.sh || echo {} >> ${FAILED_FILE}; else echo Running using make; time USE_PGXS=1 PGHOST=pcompute{%} make -C {} installcheck || echo {} >> ${FAILED_FILE}; fi;" ::: ${LIST}
+else
+  for d in ${LIST}; do
+      [ -d "${d}" ] || continue
+      if ! psql -w -c "select 1" >/dev/null; then
+        FAILED="${d} ${FAILED}"
+        break
+      fi
+      if [[ ${REGULAR_USER} = true ]] && [ -f "${d}"/regular-test.sh ]; then
+        "${d}/regular-test.sh" || FAILED="${d} ${FAILED}"
+        continue
+      fi
 
-    if [ -f "${d}/neon-test.sh" ]; then
-       "${d}/neon-test.sh" || FAILED="${d} ${FAILED}"
-    else
-       USE_PGXS=1 make -C "${d}" installcheck || FAILED="${d} ${FAILED}"
-    fi
-done
-[ -z "${FAILED}" ] && exit 0
-for d in ${FAILED}; do
-  cat "$(find $d -name regression.diffs)"
-done
-for postgis_diff in /tmp/pgis_reg/*_diff; do
-  echo "${postgis_diff}:"
-  cat "${postgis_diff}"
-done
-echo "${FAILED}"
-exit 1
+      if [ -f "${d}/neon-test.sh" ]; then
+        "${d}/neon-test.sh" || FAILED="${d} ${FAILED}"
+      else
+        USE_PGXS=1 make -C "${d}" installcheck || FAILED="${d} ${FAILED}"
+      fi
+  done
+  [[ -z ${FAILED} ]] && exit 0
+  for d in ${FAILED}; do
+    cat "$(find $d -name regression.diffs)"
+  done
+  for postgis_diff in /tmp/pgis_reg/*_diff; do
+    echo "${postgis_diff}:"
+    cat "${postgis_diff}"
+  done
+  echo "${FAILED}"
+  exit 1
+fi
