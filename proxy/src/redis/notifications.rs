589bfdfd02c575b172a138ee5d174777da17e18a
Conrad Ludgate <conrad@neon.tech>
2025-06-02 09:38:35 +0100

    proxy: Changes to rate limits and GetEndpointAccessControl caches. (#12048)
    Precursor to https://github.com/neondatabase/cloud/issues/28333.
    
    We want per-endpoint configuration for rate limits, which will be
    distributed via the `GetEndpointAccessControl` API. This lays some of
    the ground work.
    
    1. Allow the endpoint rate limiter to accept a custom leaky bucket
    config on check.
    2. Remove the unused auth rate limiter, as I don't want to think about
    how it fits into this.
    3. Refactor the caching of `GetEndpointAccessControl`, as it adds
    friction for adding new cached data to the API.
    
    That third one was rather large. I couldn't find any way to split it up.
    The core idea is that there's now only 2 cache APIs.
    `get_endpoint_access_controls` and `get_role_access_controls`.
    
    I'm pretty sure the behaviour is unchanged, except I did a drive by
    change to fix #8989 because it felt harmless. The change in question is
    that when a password validation fails, we eagerly expire the role cache
    if the role was cached for 5 minutes. This is to allow for edge cases
    where a user tries to connect with a reset password, but the cache never
    expires the entry due to some redis related quirk (lag, or
    misconfiguration, or cplane error)

--- proxy/src/redis/notifications.rs
+++ proxy/src/redis/notifications.rs
@@ -233,29 +233,30 @@ impl<C: ProjectInfoCache + Send + Sync + 'static> MessageHandler<C> {
 
 fn invalidate_cache<C: ProjectInfoCache>(cache: Arc<C>, msg: Notification) {
     match msg {
-        Notification::AllowedIpsUpdate { allowed_ips_update } => {
-            cache.invalidate_allowed_ips_for_project(allowed_ips_update.project_id);
+        Notification::AllowedIpsUpdate {
+            allowed_ips_update: AllowedIpsUpdate { project_id },
         }
-        Notification::BlockPublicOrVpcAccessUpdated {
-            block_public_or_vpc_access_updated,
-        } => cache.invalidate_block_public_or_vpc_access_for_project(
-            block_public_or_vpc_access_updated.project_id,
-        ),
+        | Notification::BlockPublicOrVpcAccessUpdated {
+            block_public_or_vpc_access_updated: BlockPublicOrVpcAccessUpdated { project_id },
+        } => cache.invalidate_endpoint_access_for_project(project_id),
         Notification::AllowedVpcEndpointsUpdatedForOrg {
-            allowed_vpc_endpoints_updated_for_org,
-        } => cache.invalidate_allowed_vpc_endpoint_ids_for_org(
-            allowed_vpc_endpoints_updated_for_org.account_id,
-        ),
+            allowed_vpc_endpoints_updated_for_org: AllowedVpcEndpointsUpdatedForOrg { account_id },
+        } => cache.invalidate_endpoint_access_for_org(account_id),
         Notification::AllowedVpcEndpointsUpdatedForProjects {
-            allowed_vpc_endpoints_updated_for_projects,
-        } => cache.invalidate_allowed_vpc_endpoint_ids_for_projects(
-            allowed_vpc_endpoints_updated_for_projects.project_ids,
-        ),
-        Notification::PasswordUpdate { password_update } => cache
-            .invalidate_role_secret_for_project(
-                password_update.project_id,
-                password_update.role_name,
-            ),
+            allowed_vpc_endpoints_updated_for_projects:
+                AllowedVpcEndpointsUpdatedForProjects { project_ids },
+        } => {
+            for project in project_ids {
+                cache.invalidate_endpoint_access_for_project(project);
+            }
+        }
+        Notification::PasswordUpdate {
+            password_update:
+                PasswordUpdate {
+                    project_id,
+                    role_name,
+                },
+        } => cache.invalidate_role_secret_for_project(project_id, role_name),
         Notification::UnknownTopic => unreachable!(),
     }
 }
