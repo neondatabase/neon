ec2d60137eaaf95ad5e8463acc61624715a5f9f2
Konstantin Knizhnik <knizhnik@neon.tech>
2025-06-04 20:52:12 +0300

    Make ruff happpy

--- test_runner/regress/test_compute_metrics.py
+++ test_runner/regress/test_compute_metrics.py
@@ -469,8 +469,11 @@ def test_perf_counters(neon_simple_env: NeonEnv):
     cur.execute("set neon.monitor_query_exec_time = on")
     cur.execute("SELECT * FROM neon_perf_counters")
     cur.execute("SELECT * FROM neon_backend_perf_counters")
-    cur.execute("select value from neon_backend_perf_counters where metric='query_time_seconds_count' and pid=pg_backend_pid()")
+    cur.execute(
+        "select value from neon_backend_perf_counters where metric='query_time_seconds_count' and pid=pg_backend_pid()"
+    )
     assert cur.fetchall()[0][0] == 2
+
 
 def collect_metric(
     client: EndpointHttpClient,
