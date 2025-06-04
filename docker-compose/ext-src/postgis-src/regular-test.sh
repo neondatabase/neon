d554d329ddcfb447f1499473ff1e58a115a096bd
Alexey Masterov <alexeymasterov@neon.tech>
2025-06-04 23:21:51 +0200

    fix regular script

--- docker-compose/ext-src/postgis-src/regular-test.sh
+++ docker-compose/ext-src/postgis-src/regular-test.sh
@@ -13,5 +13,5 @@ psql -d contrib_regression -c "ALTER DATABASE contrib_regression SET TimeZone='U
 patch -p1 <"postgis-common-${PG_VERSION}.patch"
 patch -p1 <"postgis-regular-${PG_VERSION}.patch"
 psql -d contrib_regression -f raster_outdb_template.sql
-trap 'patch -R -p1 <postgis-no-upgrade-test.patch && patch -R -p1 <"postgis-regular-${PG_VERSION}.patch"' EXIT
+trap 'patch -R -p1 <postgis-regular-${PG_VERSION}.patch && patch -R -p1 <"postgis-common-${PG_VERSION}.patch"' EXIT
 POSTGIS_REGRESS_DB=contrib_regression RUNTESTFLAGS=--nocreate make installcheck-base
