86718052a3d347798a8441ac8d778b54f9ce25fd
Alexey Masterov <alexeymasterov@neon.tech>
2025-06-09 13:34:54 +0200

    Cleanup

--- docker-compose/docker_compose_test.sh
+++ docker-compose/docker_compose_test.sh
@@ -43,9 +43,6 @@ for pg_version in ${TEST_VERSION_ONLY-14 15 16 17}; do
     cleanup
     PG_TEST_VERSION=$((pg_version < 16 ? 16 : pg_version))
     PG_VERSION=${pg_version} PG_TEST_VERSION=${PG_TEST_VERSION} docker compose up --quiet-pull --build -d
-    # XXX remove before merge
-    docker compose cp ext-src neon-test-extensions:/
-
     echo "wait until the compute is ready. timeout after 60s. "
     cnt=0
     while sleep 3; do
