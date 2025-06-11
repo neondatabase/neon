09c48c3d6dd07d230657c1aca86a6a9130b0ce15
Dimitri Fontaine <dim@tapoueh.org>
2025-06-11 14:45:42 +0200

    Review and improve regression testing for Event Triggers support.

--- test_runner/sql_regress/sql/neon-event-triggers.sql
+++ test_runner/sql_regress/sql/neon-event-triggers.sql
@@ -21,14 +21,13 @@ execute procedure admin_proc();
 
 set role neon_user;
 
--- Non-priveleged neon user should not be able to create event trigers
+-- Non-privileged neon user should not be able to create event trigers
 create event trigger on_ddl2 on ddl_command_end
 execute procedure admin_proc();
 
 set role neon_admin;
 
--- neon_super user should be able to create event trigers
-
+-- neon_superuser should be able to create event trigers
 create or replace function neon_proc()
     returns event_trigger
     language plpgsql as
@@ -40,7 +39,6 @@ $$;
 
 create event trigger on_ddl2 on ddl_command_end
 execute procedure neon_proc();
-
 
 \c neondb neon_admin
 
@@ -55,14 +53,16 @@ $$;
 
 create or replace function neondb_secdef_proc()
     returns event_trigger
-    language plpgsql as
+    language plpgsql
+    SECURITY DEFINER
+as
 $$
 begin
     raise notice 'neondb secdef event trigger is executed for %', current_user;
 end;
-$$ security definer;
+$$;
 
--- Neon superuser should be able to create event triggers
+-- neon_admin (neon_superuser member) should be able to create event triggers
 create event trigger on_ddl3 on ddl_command_end
 execute procedure neondb_proc();
 
@@ -72,25 +72,20 @@ execute procedure neondb_secdef_proc();
 -- Check that event trigger is fired for neon_admin
 create table t1(x integer);
 
-\c regression cloud_admin
--- Check that event triggers are not fired for superuser
+-- Check that event trigger can be skipped
+set neon.event_triggers to false;
 create table t2(x integer);
 
--- Now enable event triggers execution for superuser
-set neon.enable_event_triggers_for_superuser=on;
+\c regression cloud_admin
 
--- Check that even trigger is fired in this case
+-- Check that event triggers are not fired for superuser
 create table t3(x integer);
 
 \c neondb cloud_admin
--- Check that event triggers are not fired for superuser
+
+-- Check that neondb_proc() is not fired for superuser
+-- Check that neondb_secdef_proc() is fired for superuser (running as neon_admin)
 create table t4(x integer);
-
--- Now enable event triggers execution for superuser
-set neon.enable_event_triggers_for_superuser=on;
-
--- Check that even trigger is fired in this case
-create table t5(x integer);
 
 \c neondb neon_admin
 
