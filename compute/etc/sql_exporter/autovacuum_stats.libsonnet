5dd2360dd09c339fd53a5ef42b1d018fedde00f7
Suhas Thalanki <suhastvs@gmail.com>
2025-06-11 10:50:00 -0700

    updated to have only autovacuum count

--- compute/etc/sql_exporter/autovacuum_stats.libsonnet
+++ compute/etc/sql_exporter/autovacuum_stats.libsonnet
@@ -3,15 +3,11 @@
   type: 'gauge',
   help: 'Autovacuum statistics for all tables across databases',
   key_labels: [
-    'datname',
-    'schemaname',
-    'relname',
+    'dbname',
   ],
   value_label: 'metric',
   values: [
     'autovacuum_count',
-    'autoanalyze_count',
-    'last_autovacuum',
   ],
   query: importstr 'sql_exporter/autovacuum_stats.sql',
 }
--- compute/etc/sql_exporter/autovacuum_stats.sql
+++ compute/etc/sql_exporter/autovacuum_stats.sql
@@ -2,24 +2,18 @@ WITH dbs AS (
   SELECT datname 
   FROM pg_database 
   WHERE datconnlimit != -2
-    AND datname NOT IN ('postgres', 'template0', 'template1')
+    AND datname NOT IN ('postgres')
     AND NOT datistemplate
 )
 SELECT 
-  d.datname,
-  t.schemaname,
-  t.relname,
-  t.last_autovacuum,
-  t.autovacuum_count,
-  t.autoanalyze_count
+  d.datname as dbname,
+  t.autovacuum_count as autovacuum_count
 FROM dbs d,
 LATERAL (
   SELECT * FROM dblink(
-    'dbname=' || d.datname || ' user=' || current_user,
-    'SELECT schemaname, relname, last_autovacuum, autovacuum_count, autoanalyze_count 
+    'dbname=' || d.datname || ' user=' || current_user || ' connect_timeout=5',
+    'SELECT sum(autovacuum_count) as autovacuum_count
      FROM pg_stat_all_tables 
-     WHERE schemaname NOT IN (''pg_catalog'', ''information_schema'')
-     ORDER BY last_autovacuum DESC limit 10'
-  ) AS t(schemaname name, relname name, last_autovacuum timestamptz, 
-         autovacuum_count bigint, autoanalyze_count bigint)
+     WHERE schemaname NOT IN (''pg_catalog'', ''information_schema'')'
+  ) AS t(autovacuum_count bigint)
 ) t;
