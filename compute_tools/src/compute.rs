e7b1f63f688049d8dd0f3feaa81e25168cde198b
Conrad Ludgate <conradludgate@gmail.com>
2025-06-16 22:37:12 +0100

    add logs for TLS

--- compute_tools/src/compute.rs
+++ compute_tools/src/compute.rs
@@ -2187,6 +2187,7 @@ impl ComputeNode {
                         match state.status {
                             // let's update the state to config pending
                             ComputeStatus::ConfigurationPending | ComputeStatus::Running => {
+                                info!("reconfiguring compute due to TLS certificate renewal");
                                 state.set_status(
                                     ComputeStatus::ConfigurationPending,
                                     &self.state_changed,
@@ -2214,8 +2215,10 @@ impl ComputeNode {
 
                     // wait for a new certificate update
                     if handle.block_on(cert_watch.changed()).is_err() {
+                        error!("certificate renewal monitoring task closed unexpectedly");
                         break;
                     }
+                    info!("TLS certificates renewed");
                 }
             });
         }
