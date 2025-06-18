d9b1201e5b4a3ead430e0d6ddc0bafe7d45dd8bf
Alexander Lakhin <alexander.lakhin@neon.tech>
2025-06-18 12:59:03 +0300

    Fix typos in update_orc_pipeline_step_results.sql

--- test_runner/performance/large_synthetic_oltp/update_orc_pipeline_step_results.sql
+++ test_runner/performance/large_synthetic_oltp/update_orc_pipeline_step_results.sql
@@ -1,5 +1,5 @@
--- update approximately 3000 rows or 1 MB in theocr.ocr_pipeline_step_results      table
--- takes about 11 secondss
+-- update approximately 3000 rows or 1 MB in the ocr_pipeline_step_results table
+-- takes about 11 seconds
 UPDATE     ocr.ocr_pipeline_step_results 
 SET created_at = now()
 WHERE ctid in (
