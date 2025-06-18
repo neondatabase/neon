5319a9cbc0554c94dbf00990f8b75c4964152df7
Alexander Lakhin <alexander.lakhin@neon.tech>
2025-06-18 13:14:58 +0300

    Fix table name in comments inside update_values.sql

--- test_runner/performance/large_synthetic_oltp/update_values.sql
+++ test_runner/performance/large_synthetic_oltp/update_values.sql
@@ -1,4 +1,4 @@
--- update approximately 2500 rows or 1 MB in the vertices table
+-- update approximately 2500 rows or 1 MB in the values table
 -- takes about 3 minutes
 UPDATE workflows.values
 SET bool_value = NOT bool_value
