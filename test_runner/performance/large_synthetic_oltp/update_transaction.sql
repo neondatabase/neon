d9a59ec2df1bb92890dec23e2a13c1b835fd60af
Alexander Lakhin <alexander.lakhin@neon.tech>
2025-06-18 13:13:41 +0300

    Fix table name in comments inside update_transaction.sql

--- test_runner/performance/large_synthetic_oltp/update_transaction.sql
+++ test_runner/performance/large_synthetic_oltp/update_transaction.sql
@@ -1,4 +1,4 @@
--- update approximately 2000 rows or 301 MB in the relabeled_transactions table
+-- update approximately 2000 rows or 301 MB in the transaction table
 -- takes about 90 seconds
 UPDATE transaction.transaction
 SET is_last = NOT is_last
