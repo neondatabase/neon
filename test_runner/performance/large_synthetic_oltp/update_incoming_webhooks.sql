9851f038486a19aea23c37050368b125fbcdd31e
Alexander Lakhin <alexander.lakhin@neon.tech>
2025-06-18 13:11:52 +0300

    Fix table name in comments inside update_incoming_webhooks.sql

--- test_runner/performance/large_synthetic_oltp/update_incoming_webhooks.sql
+++ test_runner/performance/large_synthetic_oltp/update_incoming_webhooks.sql
@@ -1,4 +1,4 @@
--- update approximately 2000 rows or 1 MB in the relabeled_transactions table
+-- update approximately 2000 rows or 1 MB in the incoming_webhooks table
 -- takes about 5 seconds
 UPDATE webhook.incoming_webhooks
 SET is_body_encrypted = NOT is_body_encrypted
