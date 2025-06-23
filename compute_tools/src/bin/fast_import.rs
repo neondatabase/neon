0ef8e4ff47d1e3b18ace14d3af2b0116e9461991
Matthias van de Meent <matthias@neon.tech>
2025-06-23 17:09:28 +0200

    Replace PostgresMajorVersion with PgMajorVersion
    It had the same function, but wasn't shared across the project.

--- compute_tools/src/bin/fast_import.rs
+++ compute_tools/src/bin/fast_import.rs
@@ -29,9 +29,8 @@ use anyhow::{Context, bail};
 use aws_config::BehaviorVersion;
 use camino::{Utf8Path, Utf8PathBuf};
 use clap::{Parser, Subcommand};
-use compute_tools::extension_server::{PostgresMajorVersion, get_pg_version};
+use compute_tools::extension_server::get_pg_version;
 use nix::unistd::Pid;
-use postgres_versioninfo::PgMajorVersion;
 use std::ops::Not;
 use tracing::{Instrument, error, info, info_span, warn};
 use utils::fs_ext::is_directory_empty;
@@ -180,12 +179,8 @@ impl PostgresProcess {
             .await
             .context("create pgdata directory")?;
 
-        let pg_version = match get_pg_version(self.pgbin.as_ref()) {
-            PostgresMajorVersion::V14 => PgMajorVersion::PG14,
-            PostgresMajorVersion::V15 => PgMajorVersion::PG15,
-            PostgresMajorVersion::V16 => PgMajorVersion::PG16,
-            PostgresMajorVersion::V17 => PgMajorVersion::PG17,
-        };
+        let pg_version = get_pg_version(self.pgbin.as_ref());
+
         postgres_initdb::do_run_initdb(postgres_initdb::RunInitdbArgs {
             superuser: initdb_user,
             locale: DEFAULT_LOCALE, // XXX: this shouldn't be hard-coded,
