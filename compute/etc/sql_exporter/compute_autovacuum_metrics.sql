6b9b027c5e694966c9f654ac56e6b942e92b155d
Suhas Thalanki <suhastvs@gmail.com>
2025-06-26 11:45:46 -0400

    updating to limit 10 like pg_stats_userdb

--- compute/etc/sql_exporter/compute_autovacuum_metrics.sql
+++ compute/etc/sql_exporter/compute_autovacuum_metrics.sql
@@ -2,4 +2,4 @@ SELECT datname database_name,
   mxid_age(datminmxid) min_mxid_age,
   age(datfrozenxid) frozen_xid_age
 FROM pg_database
-ORDER BY min_mxid_age DESC, frozen_xid_age DESC LIMIT 5;
+ORDER BY min_mxid_age DESC, frozen_xid_age DESC LIMIT 10;
