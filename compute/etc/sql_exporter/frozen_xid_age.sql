8bad05ae63756946a8033a9c647f8f20f341ffff
Suhas Thalanki <suhastvs@gmail.com>
2025-06-26 10:59:25 -0400

    updated styling, added compute_ prefix

--- compute/etc/sql_exporter/frozen_xid_age.sql
+++
@@ -1,4 +0,0 @@
-SELECT datname database_name,
-         age(datfrozenxid) frozen_xid_age
-FROM     pg_database
-ORDER BY frozen_xid_age desc limit 5;
--- compute/etc/sql_exporter/min_mxid_age.sql
+++
@@ -1,4 +0,0 @@
-SELECT datname database_name,
-         mxid_age(datminmxid) min_mxid_age
-FROM     pg_database
-ORDER BY min_mxid_age desc limit 5;
