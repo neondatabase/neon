1500dbf4463d0809fcd98fdc9d24946d20c0016d
Mikhail Kot <to@myrrc.dev>
2025-07-04 19:02:31 +0100

    better test

--- test_runner/regress/test_replica_promotes.py
+++ test_runner/regress/test_replica_promotes.py
@@ -1,16 +1,17 @@
 """
-File with secondary->primary promotion testing.
-
-This far, only contains a test that we don't break and that the data is persisted.
+Secondary -> primary promotion testing
 """
+
+from enum import StrEnum
 from typing import cast
+
 import psycopg2
-from fixtures.common_types import Lsn
-from enum import Enum
 import pytest
+from fixtures.common_types import Lsn
 from fixtures.log_helper import log
 from fixtures.neon_fixtures import Endpoint, NeonEnv, wait_replica_caughtup
 from fixtures.utils import USE_LFC
+from psycopg2.extensions import cursor as Cursor
 from pytest import raises
 
 
@@ -23,12 +24,24 @@ def stop_and_check_lsn(ep: Endpoint, expected_lsn: Lsn | None):
         assert lsn == expected_lsn, f"{expected_lsn=} != {lsn=}"
 
 
-class PromoteMethod(Enum):
-    COMPUTE_CTL = False
-    POSTGRES = True
+def get_lsn_triple(cur: Cursor) -> tuple[str, str, str]:
+    cur.execute(
+        """
+        SELECT pg_current_wal_insert_lsn(),
+               pg_current_wal_lsn(),
+               pg_current_wal_flush_lsn()
+        """
+    )
+    return cast("tuple[str, str, str]", cur.fetchone())
 
 
-QUERY_OPTIONS = PromoteMethod.POSTGRES, PromoteMethod.COMPUTE_CTL
+class PromoteMethod(StrEnum):
+    COMPUTE_CTL = "compute-ctl"
+    POSTGRES = "postgres"
+
+
+METHOD_OPTIONS = [e for e in PromoteMethod]
+METHOD_IDS = [e.value for e in PromoteMethod]
 
 # test two primaries
 # test promote failed
@@ -37,8 +50,8 @@ QUERY_OPTIONS = PromoteMethod.POSTGRES, PromoteMethod.COMPUTE_CTL
 
 
 @pytest.mark.skipif(not USE_LFC, reason="LFC is disabled, skipping")
-@pytest.mark.parametrize("query", QUERY_OPTIONS, ids=["postgres", "compute-ctl"])
-def test_replica_promotes(neon_simple_env: NeonEnv, query: PromoteMethod):
+@pytest.mark.parametrize("method", METHOD_OPTIONS, ids=METHOD_IDS)
+def test_replica_promotes(neon_simple_env: NeonEnv, method: PromoteMethod):
     """
     Test that a replica safely promotes, and can commit data updates which
     show up when the primary boots up after the promoted secondary endpoint
@@ -53,27 +66,19 @@ def test_replica_promotes(neon_simple_env: NeonEnv, query: PromoteMethod):
 
     with primary.connect() as primary_conn:
         primary_cur = primary_conn.cursor()
-        if query is PromoteMethod.COMPUTE_CTL:
-            primary_cur.execute("create extension neon")
-
+        primary_cur.execute("create extension neon")
         primary_cur.execute(
             "create table t(pk bigint GENERATED ALWAYS AS IDENTITY, payload integer)"
         )
         primary_cur.execute("INSERT INTO t(payload) SELECT generate_series(1, 100)")
-        primary_cur.execute(
-            """
-            SELECT pg_current_wal_insert_lsn(),
-                   pg_current_wal_lsn(),
-                   pg_current_wal_flush_lsn()
-            """
-        )
-        lsn_triple = cast("tuple[str, str, str]", primary_cur.fetchone())
+
+        lsn_triple = get_lsn_triple(primary_cur)
         log.info(f"Primary: Current LSN after workload is {lsn_triple}")
         expected_primary_lsn: Lsn = Lsn(lsn_triple[2])
         primary_cur.execute("show neon.safekeepers")
         safekeepers = primary_cur.fetchall()[0][0]
 
-    if query is PromoteMethod.COMPUTE_CTL:
+    if method is PromoteMethod.COMPUTE_CTL:
         primary.http_client().offload_lfc()
     else:
         wait_replica_caughtup(primary, secondary)
@@ -91,38 +96,27 @@ def test_replica_promotes(neon_simple_env: NeonEnv, query: PromoteMethod):
         secondary_cur.execute("select count(*) from t")
         assert secondary_cur.fetchone() == (100,)
 
-    primary_endpoint_id: str = primary.endpoint_id
+    primary_endpoint_id = primary.endpoint_id
     stop_and_check_lsn(primary, expected_primary_lsn)
 
     # Reconnect to the secondary to make sure we get a read-write connection
     promo_conn = secondary.connect()
     promo_cur = promo_conn.cursor()
     http_client = secondary.http_client()
-    if query is PromoteMethod.COMPUTE_CTL:
-        # TODO Do we need to prewarm if LFC is full?
-        # PG:2025-06-10 15:54:36.203 GMT [1079329] LOG:  LFC: skip prewarm because LFC is already filled
+    if method is PromoteMethod.COMPUTE_CTL:
         http_client.prewarm_lfc(primary_endpoint_id)
-
-        # control plane knows safekeepers, here we simulate it by querying primary
-        lsn = primary.terminate_flush_lsn
-        assert lsn
+        # control plane knows safekeepers, simulate it by querying primary
+        assert (lsn := primary.terminate_flush_lsn)
         safekeepers_lsn = {"safekeepers": safekeepers, "wal_flush_lsn": lsn}
-        promote = http_client.promote(safekeepers_lsn)
-        assert promote["status"] == "completed"
+        assert http_client.promote(safekeepers_lsn)["status"] == "completed"
     else:
         promo_cur.execute(f"alter system set neon.safekeepers='{safekeepers}'")
         promo_cur.execute("select pg_reload_conf()")
         promo_cur.execute("SELECT * FROM pg_promote()")
         assert promo_cur.fetchone() == (True,)
 
-    promo_cur.execute(
-        """
-        SELECT pg_current_wal_insert_lsn(),
-               pg_current_wal_lsn(),
-               pg_current_wal_flush_lsn()
-        """
-    )
-    log.info(f"Secondary: LSN after promotion is {promo_cur.fetchone()}")
+    lsn_triple = get_lsn_triple(promo_cur)
+    log.info(f"Secondary: LSN after promotion is {lsn_triple}")
 
     # Reconnect to the secondary to make sure we get a read-write connection
     with secondary.connect() as conn, conn.cursor() as new_primary_cur:
@@ -140,14 +134,8 @@ def test_replica_promotes(neon_simple_env: NeonEnv, query: PromoteMethod):
 
         new_primary_cur.execute("select count(*) from t")
         assert new_primary_cur.fetchone() == (200,)
-        new_primary_cur.execute(
-            """
-            SELECT pg_current_wal_insert_lsn(),
-                   pg_current_wal_lsn(),
-                   pg_current_wal_flush_lsn()
-            """
-        )
-        lsn_triple = cast("tuple[str, str, str]", new_primary_cur.fetchone())
+
+        lsn_triple = get_lsn_triple(new_primary_cur)
         log.info(f"Secondary: LSN after workload is {lsn_triple}")
         expected_promoted_lsn = Lsn(lsn_triple[2])
 
@@ -155,23 +143,13 @@ def test_replica_promotes(neon_simple_env: NeonEnv, query: PromoteMethod):
         new_primary_cur.execute("select payload from t")
         assert new_primary_cur.fetchall() == [(it,) for it in range(1, 201)]
 
-    secondary.stop()
     stop_and_check_lsn(secondary, expected_promoted_lsn)
 
-    # secondaries don't sync safekeepers on finish so LSN will be None
-    stop_and_check_lsn(secondary, None)
     primary = env.endpoints.create_start(branch_name="main", endpoint_id="primary2")
 
     with primary.connect() as new_primary:
         new_primary_cur = new_primary.cursor()
-        new_primary_cur.execute(
-            """
-            SELECT pg_current_wal_insert_lsn(),
-                   pg_current_wal_lsn(),
-                   pg_current_wal_flush_lsn()
-            """
-        )
-        lsn_triple = cast("tuple[str, str, str]", new_primary_cur.fetchone())
+        lsn_triple = get_lsn_triple(new_primary_cur)
         expected_primary_lsn = Lsn(lsn_triple[2])
         log.info(f"New primary: Boot LSN is {lsn_triple}")
 
