49b1cd7435a9af377d7a771bafae81298f0f3854
Alexey Masterov <alexey.masterov@databricks.com>
2025-07-04 10:05:58 +0200

    An attempt to make it compatible with yq 4.16.2

--- docker-compose/docker_compose_test.sh
+++ docker-compose/docker_compose_test.sh
@@ -24,14 +24,9 @@ for i in $(seq 1 "${PARALLEL_COMPUTES}"); do
 done
 cp docker-compose.yml docker-compose.yml.bak
 trap 'mv docker-compose.yml.bak docker-compose.yml' EXIT
-# For debug only, please remove before merge
-cat /etc/debian_version
-yq --version
 if [[ ${PARALLEL_COMPUTES} -gt 1 ]]; then
   for i in $(seq 2 "${PARALLEL_COMPUTES}"); do
-    yq -i "
-      .services.compute${i} = ( .services.compute1 | (del .build) | (del .ports)) |
-      .services.compute${i}.depends_on = [\"compute1\"]" docker-compose.yml
+    yq -i ".services.compute${i} = ( .services.compute1 | (del .build) | (del .ports)) | .services.compute${i}.depends_on = [\"compute1\"]" docker-compose.yml
   done
 fi
 cd "$(dirname "${0}")"
