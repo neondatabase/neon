5cdfaeb68243e088f1652647e6894e3fb8654240
Mikhail Kot <to@myrrc.dev>
2025-07-10 19:26:07 +0100

    initial

--- compute_tools/src/http/routes/promote.rs
+++ compute_tools/src/http/routes/promote.rs
@@ -4,9 +4,9 @@ use http::StatusCode;
 
 pub(in crate::http) async fn promote(
     compute: axum::extract::State<std::sync::Arc<crate::compute::ComputeNode>>,
-    Form(safekeepers_lsn): Form<compute_api::responses::SafekeepersLsn>,
+    Form(cfg): Form<compute_api::responses::PromoteConfig>,
 ) -> axum::response::Response {
-    let state = compute.promote(safekeepers_lsn).await;
+    let state = compute.promote(cfg).await;
     if let compute_api::responses::PromoteState::Failed { error } = state {
         return JsonResponse::error(StatusCode::INTERNAL_SERVER_ERROR, error);
     }
