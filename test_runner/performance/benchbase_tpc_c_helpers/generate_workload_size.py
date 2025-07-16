ccc92f3a430e04be481516e71aa9119e1d6967ff
BodoBolero <peterbendel@neon.tech>
2025-07-16 17:58:31 +0200

    first version of real test for one scale factor

---
+++ test_runner/performance/benchbase_tpc_c_helpers/generate_workload_size.py
@@ -0,0 +1,241 @@
+import os
+import math
+import sys
+from pathlib import Path
+import argparse
+
+CONFIGS_DIR = Path("../configs")
+SCRIPTS_DIR = Path("../scripts")
+
+# Constants
+WARMUP_TIME_SECONDS = 1200  # 20 minutes
+BENCHMARK_TIME_SECONDS = 3600  # 1 hour  
+RAMP_STEP_TIME_SECONDS = 300  # 5 minutes
+BASE_TERMINALS = 130
+TERMINALS_PER_WAREHOUSE = 0.2
+OPTIMAL_RATE_FACTOR = 0.7  # 70% of max rate
+BATCH_SIZE = 1000
+LOADER_THREADS = 4
+TRANSACTION_WEIGHTS = "45,43,4,4,4"  # NewOrder, Payment, OrderStatus, Delivery, StockLevel
+# Ramp-up rate multipliers
+RAMP_RATE_FACTORS = [0.4, 0.6, 0.7, 0.9, 1.0, 1.1, 2.0]
+
+# Templates for XML configs
+WARMUP_XML = '''<?xml version="1.0"?>
+<parameters>
+    <type>POSTGRES</type>
+    <driver>org.postgresql.Driver</driver>
+    <url>jdbc:postgresql://{hostname}/neondb?sslmode=require&amp;ApplicationName=tpcc&amp;reWriteBatchedInserts=true</url>
+    <username>neondb_owner</username>
+    <password>{password}</password>
+    <reconnectOnConnectionFailure>true</reconnectOnConnectionFailure>
+    <isolation>TRANSACTION_READ_COMMITTED</isolation>
+    <batchsize>{batch_size}</batchsize>
+    <scalefactor>{warehouses}</scalefactor>
+    <loaderThreads>0</loaderThreads>
+    <terminals>{terminals}</terminals>
+    <works>
+        <work>
+            <time>{warmup_time}</time>
+            <weights>{transaction_weights}</weights>
+            <rate>unlimited</rate>
+            <arrival>POISSON</arrival>
+            <distribution>ZIPFIAN</distribution>
+        </work>
+    </works>
+    <transactiontypes>
+        <transactiontype><name>NewOrder</name></transactiontype>
+        <transactiontype><name>Payment</name></transactiontype>
+        <transactiontype><name>OrderStatus</name></transactiontype>
+        <transactiontype><name>Delivery</name></transactiontype>
+        <transactiontype><name>StockLevel</name></transactiontype>
+    </transactiontypes>
+</parameters>
+'''
+
+MAX_RATE_XML = '''<?xml version="1.0"?>
+<parameters>
+    <type>POSTGRES</type>
+    <driver>org.postgresql.Driver</driver>
+    <url>jdbc:postgresql://{hostname}/neondb?sslmode=require&amp;ApplicationName=tpcc&amp;reWriteBatchedInserts=true</url>
+    <username>neondb_owner</username>
+    <password>{password}</password>
+    <reconnectOnConnectionFailure>true</reconnectOnConnectionFailure>
+    <isolation>TRANSACTION_READ_COMMITTED</isolation>
+    <batchsize>{batch_size}</batchsize>
+    <scalefactor>{warehouses}</scalefactor>
+    <loaderThreads>0</loaderThreads>
+    <terminals>{terminals}</terminals>
+    <works>
+        <work>
+            <time>{benchmark_time}</time>
+            <weights>{transaction_weights}</weights>
+            <rate>unlimited</rate>
+            <arrival>POISSON</arrival>
+            <distribution>ZIPFIAN</distribution>
+        </work>
+    </works>
+    <transactiontypes>
+        <transactiontype><name>NewOrder</name></transactiontype>
+        <transactiontype><name>Payment</name></transactiontype>
+        <transactiontype><name>OrderStatus</name></transactiontype>
+        <transactiontype><name>Delivery</name></transactiontype>
+        <transactiontype><name>StockLevel</name></transactiontype>
+    </transactiontypes>
+</parameters>
+'''
+
+OPT_RATE_XML = '''<?xml version="1.0"?>
+<parameters>
+    <type>POSTGRES</type>
+    <driver>org.postgresql.Driver</driver>
+    <url>jdbc:postgresql://{hostname}/neondb?sslmode=require&amp;ApplicationName=tpcc&amp;reWriteBatchedInserts=true</url>
+    <username>neondb_owner</username>
+    <password>{password}</password>
+    <reconnectOnConnectionFailure>true</reconnectOnConnectionFailure>
+    <isolation>TRANSACTION_READ_COMMITTED</isolation>
+    <batchsize>{batch_size}</batchsize>
+    <scalefactor>{warehouses}</scalefactor>
+    <loaderThreads>0</loaderThreads>
+    <terminals>{terminals}</terminals>
+    <works>
+        <work>
+            <time>{benchmark_time}</time>
+            <rate>{opt_rate}</rate>
+            <weights>{transaction_weights}</weights>
+            <arrival>POISSON</arrival>
+            <distribution>ZIPFIAN</distribution>
+        </work>
+    </works>
+    <transactiontypes>
+        <transactiontype><name>NewOrder</name></transactiontype>
+        <transactiontype><name>Payment</name></transactiontype>
+        <transactiontype><name>OrderStatus</name></transactiontype>
+        <transactiontype><name>Delivery</name></transactiontype>
+        <transactiontype><name>StockLevel</name></transactiontype>
+    </transactiontypes>
+</parameters>
+'''
+
+RAMP_UP_XML = '''<?xml version="1.0"?>
+<parameters>
+    <type>POSTGRES</type>
+    <driver>org.postgresql.Driver</driver>
+    <url>jdbc:postgresql://{hostname}/neondb?sslmode=require&amp;ApplicationName=tpcc&amp;reWriteBatchedInserts=true</url>
+    <username>neondb_owner</username>
+    <password>{password}</password>
+    <reconnectOnConnectionFailure>true</reconnectOnConnectionFailure>
+    <isolation>TRANSACTION_READ_COMMITTED</isolation>
+    <batchsize>{batch_size}</batchsize>
+    <scalefactor>{warehouses}</scalefactor>
+    <loaderThreads>0</loaderThreads>
+    <terminals>{terminals}</terminals>
+    <works>
+{works}
+    </works>
+    <transactiontypes>
+        <transactiontype><name>NewOrder</name></transactiontype>
+        <transactiontype><name>Payment</name></transactiontype>
+        <transactiontype><name>OrderStatus</name></transactiontype>
+        <transactiontype><name>Delivery</name></transactiontype>
+        <transactiontype><name>StockLevel</name></transactiontype>
+    </transactiontypes>
+</parameters>
+'''
+
+WORK_TEMPLATE = f'''        <work>\n            <time>{RAMP_STEP_TIME_SECONDS}</time>\n            <rate>{{rate}}</rate>\n            <weights>{TRANSACTION_WEIGHTS}</weights>\n            <arrival>POISSON</arrival>\n            <distribution>ZIPFIAN</distribution>\n        </work>\n'''
+
+# Templates for shell scripts
+EXECUTE_SCRIPT = '''docker run --network=host --rm \
+  -v $(pwd)/configs:/configs \
+  -v $(pwd)/results:/results \
+    ghcr.io/neondatabase-labs/benchbase-postgres:latest-arm64\
+  -b tpcc \
+  -c /configs/execute_{warehouses}_warehouses_warmup.xml \
+  -d /results \
+  --create=false --load=false --execute=true
+docker run --network=host --rm \
+  -v $(pwd)/configs:/configs \
+  -v $(pwd)/results:/results \
+    ghcr.io/neondatabase-labs/benchbase-postgres:latest-arm64\
+  -b tpcc \
+  -c /configs/execute_{warehouses}_warehouses_{suffix}.xml \
+  -d /results \
+  --create=false --load=false --execute=true\n'''
+
+LOAD_XML = '''<?xml version="1.0"?>
+<parameters>
+    <type>POSTGRES</type>
+    <driver>org.postgresql.Driver</driver>
+    <url>jdbc:postgresql://{hostname}/neondb?sslmode=require&amp;ApplicationName=tpcc&amp;reWriteBatchedInserts=true</url>
+    <username>neondb_owner</username>
+    <password>{password}</password>
+    <reconnectOnConnectionFailure>true</reconnectOnConnectionFailure>
+    <isolation>TRANSACTION_READ_COMMITTED</isolation>
+    <batchsize>{batch_size}</batchsize>
+    <scalefactor>{warehouses}</scalefactor>
+    <loaderThreads>{loader_threads}</loaderThreads>
+</parameters>
+'''
+
+LOAD_SCRIPT = '''docker run --network=host --rm \
+  -v $(pwd)/configs:/configs \
+  -v $(pwd)/results:/results \
+    ghcr.io/neondatabase-labs/benchbase-postgres:latest-arm64\
+  -b tpcc \
+  -c /configs/load_{warehouses}_warehouses.xml \
+  -d /results \
+  --create=true --load=true --execute=false\n'''
+
+def write_file(path, content):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        with open(path, 'w') as f:
+            f.write(content)
+    except IOError as e:
+        print(f"Error writing {path}: {e}")
+        sys.exit(1)
+    # If it's a shell script, set executable permission
+    if str(path).endswith('.sh'):
+        os.chmod(path, 0o755)
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate BenchBase workload configs and scripts.")
+    parser.add_argument('--warehouses', type=int, required=True, help='Number of warehouses')
+    parser.add_argument('--max-rate', type=int, required=True, help='Max rate (TPS)')
+    parser.add_argument('--hostname', type=str, required=True, help='Database hostname')
+    parser.add_argument('--password', type=str, required=True, help='Database password')
+    args = parser.parse_args()
+
+    warehouses = args.warehouses
+    max_rate = args.max_rate
+    hostname = args.hostname
+    password = args.password
+    opt_rate = math.ceil(max_rate * OPTIMAL_RATE_FACTOR)
+    # Calculate terminals as next rounded integer of 40% of warehouses
+    terminals = math.ceil(BASE_TERMINALS + warehouses * TERMINALS_PER_WAREHOUSE)
+    ramp_rates = [math.ceil(max_rate * factor) for factor in RAMP_RATE_FACTORS]
+
+    # Write configs
+    write_file(CONFIGS_DIR / f"execute_{warehouses}_warehouses_warmup.xml", WARMUP_XML.format(warehouses=warehouses, hostname=hostname, password=password, terminals=terminals, batch_size=BATCH_SIZE, warmup_time=WARMUP_TIME_SECONDS, transaction_weights=TRANSACTION_WEIGHTS))
+    write_file(CONFIGS_DIR / f"execute_{warehouses}_warehouses_max_rate.xml", MAX_RATE_XML.format(warehouses=warehouses, hostname=hostname, password=password, terminals=terminals, batch_size=BATCH_SIZE, benchmark_time=BENCHMARK_TIME_SECONDS, transaction_weights=TRANSACTION_WEIGHTS))
+    write_file(CONFIGS_DIR / f"execute_{warehouses}_warehouses_opt_rate.xml", OPT_RATE_XML.format(warehouses=warehouses, opt_rate=opt_rate, hostname=hostname, password=password, terminals=terminals, batch_size=BATCH_SIZE, benchmark_time=BENCHMARK_TIME_SECONDS, transaction_weights=TRANSACTION_WEIGHTS))
+
+    ramp_works = "".join([WORK_TEMPLATE.format(rate=rate) for rate in ramp_rates])
+    write_file(CONFIGS_DIR / f"execute_{warehouses}_warehouses_ramp_up.xml", RAMP_UP_XML.format(warehouses=warehouses, works=ramp_works, hostname=hostname, password=password, terminals=terminals, batch_size=BATCH_SIZE))
+
+    # Loader config
+    write_file(CONFIGS_DIR / f"load_{warehouses}_warehouses.xml", LOAD_XML.format(warehouses=warehouses, hostname=hostname, password=password, batch_size=BATCH_SIZE, loader_threads=LOADER_THREADS))
+
+    # Write scripts
+    for suffix in ["max_rate", "opt_rate", "ramp_up"]:
+        script = EXECUTE_SCRIPT.format(warehouses=warehouses, suffix=suffix)
+        write_file(SCRIPTS_DIR / f"execute_{warehouses}_warehouses_{suffix}.sh", script)
+
+    # Loader script
+    write_file(SCRIPTS_DIR / f"load_{warehouses}_warehouses.sh", LOAD_SCRIPT.format(warehouses=warehouses))
+
+    print(f"Generated configs and scripts for {warehouses} warehouses and max rate {max_rate}.")
+
+if __name__ == "__main__":
+    main() 
