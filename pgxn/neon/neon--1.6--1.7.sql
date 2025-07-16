fb139345c4691c4fbdf60f77416511cdd8b5010c
Konstantin Knizhnik <knizhnik@garret.ru>
2025-07-16 18:25:59 +0300

    Update pgxn/neon/neon--1.6--1.7.sql
    Co-authored-by: Matthias van de Meent <matthias@neon.tech>

--- pgxn/neon/neon--1.6--1.7.sql
+++ pgxn/neon/neon--1.6--1.7.sql
@@ -1,3 +1,3 @@
-create function neon_communicator_min_inflight_request_lsn() returns pg_lsn
+create function neon_communicator_min_inflight_request_lsn() returns pg_catalog.pg_lsn
 AS 'MODULE_PATHNAME', 'neon_communicator_min_inflight_request_lsn'
 LANGUAGE C;
