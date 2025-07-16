c939110d0a2a7e3a6e14d9463bbbfc544d030de5
Alex Chi Z <chi@neon.tech>
2025-07-16 17:19:40 -0400

    test(storcon): add test cases for 404 passthrough
    Signed-off-by: Alex Chi Z <chi@neon.tech>

--- pageserver/src/tenant/mgr.rs
+++ pageserver/src/tenant/mgr.rs
@@ -834,6 +834,10 @@ impl TenantManager {
         mut spawn_mode: SpawnMode,
         ctx: &RequestContext,
     ) -> Result<Option<Arc<TenantShard>>, UpsertLocationError> {
+        fail::fail_point!("upsert-location", |_| {
+            return Ok(None);
+        });
+
         debug_assert_current_span_has_tenant_id();
         info!("configuring tenant location to state {new_location_config:?}");
 
