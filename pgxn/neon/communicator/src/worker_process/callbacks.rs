620d3669e0209beebf213b27d4899480f7d6b382
Heikki Linnakangas <heikki@neon.tech>
2025-07-18 12:38:47 +0300

    more minor comment cleanup

--- pgxn/neon/communicator/src/worker_process/callbacks.rs
+++ pgxn/neon/communicator/src/worker_process/callbacks.rs
@@ -1,8 +1,9 @@
-//! C callbacks to PostgreSQL facilities that the neon extension needs to provide. These are
-//! implemented in `neon/pgxn/communicator_process.c`.  The function signatures better match!
+//! C callbacks to PostgreSQL facilities that the neon extension needs to provide. These
+//! are implemented in `neon/pgxn/communicator_process.c`. The function signatures better
+//! match!
 //!
-//! These are called from the communicator threads! Careful what you do, most Postgres functions are
-//! not safe to call in that context.
+//! These are called from the communicator threads! Careful what you do, most Postgres
+//! functions are not safe to call in that context.
 
 #[cfg(not(test))]
 unsafe extern "C" {
@@ -10,10 +11,10 @@ unsafe extern "C" {
     pub fn callback_get_lfc_metrics_unsafe() -> LfcMetrics;
 }
 
-// Compile unit tests with dummy versions of the functions. Unit tests
-// cannot call back into the C code. (As of this writing, no unit tests
-// even exists in the communicator package, but the code coverage build
-// still builds these and tries to link with the external C code.)
+// Compile unit tests with dummy versions of the functions. Unit tests cannot call back
+// into the C code. (As of this writing, no unit tests even exists in the communicator
+// package, but the code coverage build still builds these and tries to link with the
+// external C code.)
 #[cfg(test)]
 unsafe fn callback_set_my_latch_unsafe() {
     panic!("not usable in unit tests");
@@ -39,8 +40,8 @@ pub struct LfcMetrics {
 
     // working set size looking back 1..60 minutes.
     //
-    // Index 0 is size of working set accessed within last 1 minute,
-    // index 59 is size of working set accessed within last 60 minutes.
+    // Index 0 is the size of the working set accessed within last 1 minute,
+    // index 59 is the size of the working set accessed within last 60 minutes.
     pub lfc_approximate_working_set_size_windows: [i64; 60],
 }
 
