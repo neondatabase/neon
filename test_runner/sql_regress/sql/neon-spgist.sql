878fe725112d10e35530bad064d56cda9badb0cf
Kosntantin Knizhnik <konstantin.knizhnik@databricks.com>
2025-07-22 09:02:20 +0300

    Add description to sql_regress/sql/neon-spgist.sql

--- test_runner/sql_regress/sql/neon-spgist.sql
+++ test_runner/sql_regress/sql/neon-spgist.sql
@@ -1,3 +1,4 @@
+-- Test unlogged build of SPGIST index (no "Page evicted with zero LSN" error)
 create table spgist_point_tbl(id int4, p point);
 create index spgist_point_idx on spgist_point_tbl using spgist(p) with (fillfactor = 25);
 insert into spgist_point_tbl (id, p)        select g,      point(g*10, g*10) from generate_series(1, 10000) g;
