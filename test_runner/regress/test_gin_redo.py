e0db31456bacd79f01d736de38ea27c7b077acc2
Heikki Linnakangas <heikki@neon.tech>
2025-07-29 16:45:38 +0300

    remove leftover debugging change
    I made this change a long time ago while debugging a test failure, but
    I never meant to commit it.

--- test_runner/regress/test_gin_redo.py
+++ test_runner/regress/test_gin_redo.py
@@ -16,7 +16,6 @@ def test_gin_redo(neon_simple_env: NeonEnv):
     secondary = env.endpoints.new_replica_start(origin=primary, endpoint_id="secondary")
     con = primary.connect()
     cur = con.cursor()
-    cur.execute("select pg_switch_wal()")
     cur.execute("create table gin_test_tbl(id integer, i int4[])")
     cur.execute("create index gin_test_idx on gin_test_tbl using gin (i)")
     cur.execute("insert into gin_test_tbl select g,array[3, 1, g] from generate_series(1, 10000) g")
